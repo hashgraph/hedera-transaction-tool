5cf2b512fa317cfb379c4e3f8f004f2cb8038e46
Martin Dobrev <martindobrev87@gmail.com>
2023-11-20 09:35:59 +0200

    Vite config updated

--- vite.config.js
+++ vite.config.js
@@ -1,23 +1,21 @@
-const Path = require('path');
-const vuePlugin = require('@vitejs/plugin-vue')
+import Path from 'path';
+import vuePlugin from '@vitejs/plugin-vue';
 
-const { defineConfig } = require('vite');
+import { defineConfig } from 'vite';
 
 /**
  * https://vitejs.dev/config
  */
-const config = defineConfig({
-    root: Path.join(__dirname, 'src', 'renderer'),
-    publicDir: 'public',
-    server: {
-        port: 8080,
-    },
-    open: false,
-    build: {
-        outDir: Path.join(__dirname, 'build', 'renderer'),
-        emptyOutDir: true,
-    },
-    plugins: [vuePlugin()],
+export default defineConfig({
+  root: Path.join(__dirname, 'src', 'renderer'),
+  publicDir: 'public',
+  server: {
+    port: 8080,
+  },
+  open: false,
+  build: {
+    outDir: Path.join(__dirname, 'build', 'renderer'),
+    emptyOutDir: true,
+  },
+  plugins: [vuePlugin()],
 });
-
-module.exports = config;
