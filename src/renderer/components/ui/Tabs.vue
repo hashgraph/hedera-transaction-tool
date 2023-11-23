2c7c6f47173a963624cd1cb8d82e0e7646bc0727
Svetoslav Borislavov <svetoslav.borislavov@limechain.tech>
2023-11-23 13:14:19 +0200

    Tabs component created

---
+++ src/renderer/components/ui/Tabs.vue
@@ -0,0 +1,43 @@
+<script setup lang="ts">
+import { defineProps } from 'vue';
+
+export interface TabItem {
+  title: string;
+  notifications?: number;
+  content?: any;
+}
+
+defineProps<{ items: TabItem[]; activeIndex: number }>();
+defineEmits(['update:active-index']);
+</script>
+
+<template>
+  <div class="tabs">
+    <ul class="nav nav-tabs">
+      <li
+        class="nav-item p-0 text-semi-bold text-primary-emphasis"
+        v-for="(item, i) in items"
+        :key="item.title"
+      >
+        <button
+          class="link-menu nav-link text-small gap-3"
+          :class="{
+            active: i === activeIndex,
+            'border-main-gradient': i === activeIndex,
+          }"
+          @click="$emit('update:active-index', i)"
+        >
+          {{ item.title }}
+          <span
+            v-if="item.notifications"
+            class="notification d-inline-block rounded-circle bg-primary text-white"
+            >{{ item.notifications.toFixed(0) }}</span
+          >
+        </button>
+      </li>
+    </ul>
+    <div class="mt-6">
+      <slot :name="items[activeIndex].title"></slot>
+    </div>
+  </div>
+</template>
--- src/renderer/pages/Transactions.vue
+++ src/renderer/pages/Transactions.vue
@@ -1,5 +1,21 @@
 <script setup lang="ts">
-import { reactive } from 'vue';
+import { reactive, ref } from 'vue';
+
+import Tabs, { TabItem } from '../components/ui/Tabs.vue';
+
+const activeTabIndex = ref(1);
+
+const tabItems: TabItem[] = [
+  { title: 'Ready for Review' },
+  { title: 'Ready to Sign', notifications: 3 },
+  { title: 'In Progress' },
+  { title: 'Ready for Submission' },
+  { title: 'Completed' },
+  { title: 'Drafts' },
+];
+
+const [readyToReview, readyToSign, inProgress, readyForSubmission, completed, drafts] =
+  tabItems.map(t => t.title);
 
 const transactions = reactive([
   {
@@ -19,36 +35,50 @@ const transactions = reactive([
   },
 ]);
 </script>
+
 <template>
   <div class="p-10">
     <h1 class="text-huge text-bold">Transactions</h1>
-
     <div class="mt-7">
-      <div
-        v-for="(item, index) in transactions"
-        :key="index"
-        class="rounded bg-dark-blue-800 p-4"
-        :class="{ 'mt-4': index !== 0 }"
-      >
-        <div class="d-flex justify-content-between align-items-start">
+      <Tabs :items="tabItems" v-model:active-index="activeTabIndex">
+        <template #[readyToReview]> First Tab </template>
+        <template #[readyToSign]>
           <div>
-            <p class="text-main">{{ item.title }}</p>
-            <p class="text-micro mt-3">{{ item.account }}</p>
-          </div>
-          <div>
-            <div class="d-flex justify-content-end align-items-center">
-              <button class="btn btn-outline-primary me-3">Details</button>
-              <button class="btn btn-primary">Sign</button>
+            <div
+              v-for="(item, index) in transactions"
+              :key="index"
+              class="rounded bg-dark-blue-800 p-4"
+              :class="{ 'mt-4': index !== 0 }"
+            >
+              <div class="d-flex justify-content-between align-items-start">
+                <div>
+                  <p class="text-main">{{ item.title }}</p>
+                  <p class="text-micro mt-3">{{ item.account }}</p>
+                </div>
+                <div>
+                  <div class="d-flex justify-content-end align-items-center">
+                    <button class="btn btn-outline-primary me-3">Details</button>
+                    <button class="btn btn-primary">Sign</button>
+                  </div>
+                </div>
+              </div>
+
+              <div class="mt-4">
+                <span class="text-micro me-4">Approvers</span>
+                <span class="badge text-bg-primary me-2">alice@acme.com</span>
+                <span class="badge text-bg-primary">joe@acme.com</span>
+              </div>
             </div>
           </div>
-        </div>
+        </template>
+        <template #[inProgress]> Third Tab </template>
 
-        <div class="mt-4">
-          <span class="text-micro me-4">Approvers</span>
-          <span class="badge text-bg-primary me-2">alice@acme.com</span>
-          <span class="badge text-bg-primary">joe@acme.com</span>
-        </div>
-      </div>
+        <template #[readyForSubmission]> Fourth Tab </template>
+
+        <template #[completed]> Fifth Tab </template>
+
+        <template #[drafts]> Sixth Tab </template>
+      </Tabs>
     </div>
   </div>
 </template>
