fdfb812b712ef293192d1679b12213eb1df14ed1
Martin Dobrev <martindobrev87@gmail.com>
2023-11-29 16:15:38 +0200

    Fetch account id from public key

--- src/renderer/pages/Settings.vue
+++ src/renderer/pages/Settings.vue
@@ -1,4 +1,5 @@
 <script setup lang="ts">
+import axios from 'axios';
 import { onMounted, ref, watch } from 'vue';
 
 import { Organization } from '../../main/modules/store';
@@ -57,7 +58,7 @@ const handleRemoveOrganization = async (serverUrl: string) => {
 };
 
 /* Keys */
-const keyPairs = ref<{ privateKey: string; publicKey: string }[]>([]);
+const keyPairs = ref<{ privateKey: string; publicKey: string; accountId?: string }[]>([]);
 
 onMounted(async () => {
   keyPairs.value = await getStoredKeyPairs();
@@ -68,7 +69,20 @@ const handleGenerateKeyPair = async () => {
   try {
     const newKeyPair = await generateKeyPair('', index.value++);
     if (newKeyPair) {
-      keyPairs.value.push(newKeyPair);
+      const mainnetLink = (await getMirrorNodeConfig()).mainnetLink;
+      let accountId;
+
+      try {
+        const {
+          data: { accounts },
+        } = await axios.get(`${mainnetLink}/accounts/?account.publickey=${newKeyPair.publicKey}`);
+        if (accounts.length > 0) {
+          accountId = accounts[0].account;
+        }
+      } catch (error) {
+        console.log('error', error);
+      }
+      keyPairs.value = [...keyPairs.value, { ...newKeyPair, accountId }];
     }
   } catch (error) {
     console.log(error);
@@ -197,12 +211,16 @@ const handleClearConfig = () => {
               class="mt-5 p-4 bg-dark-blue-800"
             >
               <div class="form-group">
-                <label>DER Encoded Private key</label>
+                <label class="form-label">Encoded Private key</label>
                 <input type="text" readonly class="form-control py-3" :value="keyPair.privateKey" />
               </div>
               <div class="form-group mt-3">
-                <label>DER Encoded Public key</label>
+                <label class="form-label">Encoded Public key</label>
                 <input type="text" readonly class="form-control py-3" :value="keyPair.publicKey" />
+              </div>
+              <div v-show="keyPair.accountId" class="form-group mt-3">
+                <label class="form-label">Account ID</label>
+                <input type="text" readonly class="form-control py-3" :value="keyPair.accountId" />
               </div>
             </div>
           </div>
--- src/renderer/pages/Styleguide.vue
+++ src/renderer/pages/Styleguide.vue
@@ -1,8 +1,8 @@
 <script setup lang="ts">
-import { useToast } from 'vue-toast-notification';
+// import { useToast } from 'vue-toast-notification';
 import 'vue-toast-notification/dist/theme-sugar.css';
 
-const $toast = useToast();
+// const $toast = useToast();
 
 // $toast.open({
 //   message: 'This is a success toast',
