976b1736c35e14a7f576ece44f25330933e32069
Svetoslav Borislavov <svetoslav.borislavov@limechain.tech>
2023-11-30 18:39:38 +0200

    Key pairs FE service updated

--- src/renderer/services/keyPairService.ts
+++ src/renderer/services/keyPairService.ts
@@ -1,31 +1,50 @@
-import { PrivateKey } from '@hashgraph/sdk';
+import { Mnemonic, PrivateKey } from '@hashgraph/sdk';
+
+import axios from 'axios';
 
 const getStoredPrivateKeys = () => window.electronAPI.privateKey.getStored();
 
-const generatePrivateKey = (passphrase: string, index: number) =>
-  window.electronAPI.privateKey.generate(passphrase, index);
+export const generatePrivateKey = async (words: string[], passphrase: string, keyIndex: number) => {
+  if (words.length !== 24) {
+    throw Error('Invalod Recovery Phrase');
+  }
+
+  const mnemonic = await Mnemonic.fromWords(words);
+
+  const privateKey = await mnemonic.toStandardEd25519PrivateKey(passphrase, keyIndex);
+
+  return privateKey;
+};
+
+export const storePrivateKey = (privateKey: string, index: number) =>
+  window.electronAPI.privateKey.store(privateKey, index);
 
 export const getStoredKeyPairs = async () => {
   try {
     const privateKeys = await getStoredPrivateKeys();
 
     return privateKeys
-      .map(pk => PrivateKey.fromStringED25519(pk))
       .map(pk => ({
-        privateKey: pk.toStringRaw(),
-        publicKey: pk.publicKey.toStringRaw(),
+        privateKey: PrivateKey.fromStringED25519(pk.privateKey),
+        index: pk.index,
+      }))
+      .map(pk => ({
+        privateKey: pk.privateKey.toStringRaw(),
+        index: pk.index,
+        publicKey: pk.privateKey.publicKey.toStringRaw(),
       }));
   } catch (error) {
     return [];
   }
 };
 
-export const generateKeyPair = async (passphrase: string, index: number) => {
-  const privateKey = await generatePrivateKey(passphrase, index);
-  const parsedPrivateKey = PrivateKey.fromStringED25519(privateKey);
-
-  return {
-    privateKey: privateKey,
-    publicKey: parsedPrivateKey.publicKey.toStringRaw(),
-  };
+export const getAccountId = async (mirrorNodeURL: string, publicKey: string) => {
+  try {
+    const { data } = await axios.get(`${mirrorNodeURL}/accounts/?account.publickey=${publicKey}`);
+    if (data.accounts.length > 0) {
+      return data.accounts[0].account;
+    }
+  } catch (error) {
+    console.log(error);
+  }
 };
