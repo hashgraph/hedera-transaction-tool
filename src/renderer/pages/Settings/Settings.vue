5f6c49dcf30824b36ead48d6f1f8133060ed7488
Svetoslav Borislavov <svetoslav.borislavov@limechain.tech>
2023-12-01 15:24:25 +0200

    Settings tab updated

--- src/renderer/pages/Settings/Settings.vue
+++ src/renderer/pages/Settings/Settings.vue
@@ -1,25 +1,31 @@
 <script setup lang="ts">
-import { ref } from 'vue';
+import { computed, ref, watch } from 'vue';
 
 import useMirrorNodeLinksStore from '../../stores/storeMirrorNodeLinks';
 
 import AppTabs, { TabItem } from '../../components/ui/AppTabs.vue';
 import AppButton from '../../components/ui/AppButton.vue';
-import GeneralTab from './components/GeneralTab.vue';
-import WorkGroupsTab from './components/WorkGroupsTab.vue';
-import KeysTab from './components/KeysTab.vue';
 import useOrganizationsStore from '../../stores/storeOrganizations';
+import { useRouter, RouterView } from 'vue-router';
 
-/* Props */
-const props = defineProps<{ tab: string }>();
+/* Route */
+const router = useRouter();
 
 /* Tabs */
 const tabItems: TabItem[] = [{ title: 'General' }, { title: 'Work Groups' }, { title: 'Keys' }];
-const propTabIndex = tabItems.findIndex(
-  t => t.title.toLocaleLowerCase().replaceAll(' ', '-') === props.tab,
+const tabTitles = tabItems.map(t => t.title.toLocaleLowerCase().replaceAll(' ', '-'));
+
+const propTabIndex = tabTitles.findIndex(
+  t =>
+    router.currentRoute.value.path
+      .split('/')
+      .filter(p => p)
+      .reverse()[0]
+      .toLocaleLowerCase() === t,
 );
+
 const activeTabIndex = ref(propTabIndex >= 0 ? propTabIndex : 0);
-const [general, workGroups, keys] = tabItems.map(t => t.title);
+const activeTabTitle = computed(() => tabItems[activeTabIndex.value].title);
 
 /* Stores */
 const mirrorNodeLinks = useMirrorNodeLinksStore();
@@ -31,6 +37,11 @@ const handleClearConfig = async () => {
   mirrorNodeLinks.refetch();
   organizationsStore.refetch();
 };
+
+/* Watchers */
+watch(activeTabIndex, newIndex => {
+  router.push(tabTitles[newIndex]);
+});
 </script>
 <template>
   <div class="p-10">
@@ -38,14 +49,8 @@ const handleClearConfig = async () => {
     <AppButton color="secondary" @click="handleClearConfig">Clear Config</AppButton>
     <div class="mt-7">
       <AppTabs :items="tabItems" v-model:active-index="activeTabIndex">
-        <template #[general]>
-          <GeneralTab />
-        </template>
-        <template #[workGroups]>
-          <WorkGroupsTab />
-        </template>
-        <template #[keys]>
-          <KeysTab />
+        <template #[activeTabTitle]>
+          <RouterView />
         </template>
       </AppTabs>
     </div>
--- src/renderer/router/index.ts
+++ src/renderer/router/index.ts
@@ -13,21 +13,46 @@ import FileService from '../pages/FileService';
 import ContactList from '../pages/ContactList';
 import RecoveryPhrase from '../pages/RecoveryPhrase';
 import Login from '../pages/Login';
+import GeneralTab from '../pages/Settings/components/GeneralTab.vue';
+import WorkGroupsTab from '../pages/Settings/components/WorkGroupsTab.vue';
+import KeysTab from '../pages/Settings/components/KeysTab.vue';
 
 const routes: RouteRecordRaw[] = [
-  { path: '/', component: Welocome },
-  { path: '/style-guide', component: StyleGuide },
-  { path: '/transactions', component: Transactions },
-  { path: '/accounts', component: Accounts },
-  { path: '/tokens', component: Tokens },
-  { path: '/smart-contracts', component: SmartContracts },
-  { path: '/consensus-service', component: ConsensusService },
-  { path: '/file-service', component: FileService },
-  { path: '/contact-list', component: ContactList },
-  { path: '/help', component: Help },
-  { path: '/settings/:tab', component: Settings, props: true },
-  { path: '/recovery-phrase', component: RecoveryPhrase },
-  { path: '/login', component: Login },
+  {
+    path: '/',
+    name: 'welcome',
+    component: Welocome,
+  },
+  { path: '/style-guide', name: 'styleGuide', component: StyleGuide },
+  { path: '/transactions', name: 'transactions', component: Transactions },
+  { path: '/accounts', name: 'accounts', component: Accounts },
+  { path: '/tokens', name: 'tokens', component: Tokens },
+  { path: '/smart-contracts', name: 'smartContracts', component: SmartContracts },
+  { path: '/consensus-service', name: 'consensusService', component: ConsensusService },
+  { path: '/file-service', name: 'fileService', component: FileService },
+  { path: '/contact-list', name: 'contactList', component: ContactList },
+  { path: '/help', name: 'help', component: Help },
+  { path: '/recovery-phrase', name: 'recoveryPhrase', component: RecoveryPhrase },
+  { path: '/login', name: 'login', component: Login },
+  {
+    path: '/settings',
+    name: 'settings',
+    component: Settings,
+    children: [
+      {
+        path: 'general',
+        component: GeneralTab,
+      },
+      {
+        path: 'work-groups',
+        component: WorkGroupsTab,
+      },
+      {
+        path: 'keys',
+        component: KeysTab,
+      },
+    ],
+  },
 ];
 
 const router = createRouter({
