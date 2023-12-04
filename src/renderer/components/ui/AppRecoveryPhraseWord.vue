814788919cd201450cb788bea5157cc447a7cfaf
Svetoslav Borislavov <svetoslav.borislavov@limechain.tech>
2023-12-04 16:05:12 +0200

    AppRecoveryPhraseWord created

---
+++ src/renderer/components/ui/AppRecoveryPhraseWord.vue
@@ -0,0 +1,22 @@
+<script setup lang="ts">
+import { computed, ref } from 'vue';
+
+defineProps<{ word: string }>();
+
+const isVisible = ref(false);
+
+const inputType = computed(() => (isVisible.value ? 'text' : 'password'));
+
+const handleVisibiltyChange = () => {
+  isVisible.value = !isVisible.value;
+};
+</script>
+<template>
+  <div class="recovery-phrase-word-readonly position-relative">
+    <input class="form-control rounded-4 border" :type="inputType" readonly :value="word" />
+    <Transition name="fade" mode="out-in">
+      <i v-if="!isVisible" class="bi bi-eye" @click="handleVisibiltyChange"></i>
+      <i v-else class="bi bi-eye-slash" @click="handleVisibiltyChange"></i>
+    </Transition>
+  </div>
+</template>
