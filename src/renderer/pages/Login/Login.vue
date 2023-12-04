712c3e75ee4840cf35d497f47b03fbe5249198a0
Svetoslav Borislavov <svetoslav.borislavov@limechain.tech>
2023-12-04 12:25:16 +0200

    Login DOM updated

--- src/renderer/pages/Login/Login.vue
+++ src/renderer/pages/Login/Login.vue
@@ -42,37 +42,38 @@ const handleOnFormSubmit = (event: Event) => {
 </script>
 
 <template>
-  <div class="p-10 d-flex justify-content-center align-items-center">
-    <div>
-      <h1 class="text-huge text-bold text-center">Login</h1>
-      <p class="text-main mt-3 text-center">Please Enter Temporary email and password</p>
-      <form @submit="handleOnFormSubmit" class="form-login">
-        <div class="mt-4">
-          <input
-            v-model="inputEmail"
-            type="text"
-            class="form-control rounded-4"
-            :class="{ 'is-invalid': inputEmailInvalid }"
-            placeholder="Email"
-          />
-          <div v-if="inputEmailInvalid" class="invalid-feedback">Invalid e-mail.</div>
-        </div>
-        <div class="mt-4">
-          <input
-            v-model="inputPassword"
-            type="password"
-            class="form-control rounded-4"
-            :class="{ 'is-invalid': inputPasswordInvalid }"
-            placeholder="Password"
-          />
-          <div v-if="inputPasswordInvalid" class="invalid-feedback">Invalid password.</div>
-        </div>
-        <div class="d-grid mt-4">
-          <AppButton color="primary" size="large" type="submit" class="mt-2 rounded-4"
-            >Login</AppButton
-          >
-        </div>
-      </form>
-    </div>
+  <div class="p-10 d-flex flex-column justify-content-center align-items-center">
+    <h1 class="text-huge text-bold text-center">Login</h1>
+    <p class="text-main mt-5 text-center">Please Enter Temporary email and password</p>
+    <form
+      @submit="handleOnFormSubmit"
+      class="form-login mt-5 w-100 d-flex flex-column justify-content-center align-items-center gap-4"
+    >
+      <div class="col-12 col-md-8 col-lg-6">
+        <input
+          v-model="inputEmail"
+          type="text"
+          class="form-control rounded-4"
+          :class="{ 'is-invalid': inputEmailInvalid }"
+          placeholder="Email"
+        />
+        <div v-if="inputEmailInvalid" class="invalid-feedback">Invalid e-mail.</div>
+      </div>
+      <div class="col-12 col-md-8 col-lg-6">
+        <input
+          v-model="inputPassword"
+          type="password"
+          class="form-control rounded-4"
+          :class="{ 'is-invalid': inputPasswordInvalid }"
+          placeholder="Password"
+        />
+        <div v-if="inputPasswordInvalid" class="invalid-feedback">Invalid password.</div>
+      </div>
+      <div class="col-12 col-md-8 col-lg-6">
+        <AppButton color="primary" size="large" type="submit" class="mt-2 w-100 rounded-4"
+          >Login</AppButton
+        >
+      </div>
+    </form>
   </div>
 </template>
