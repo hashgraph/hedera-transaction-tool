ad4869753ac8703322979dac1be8d10576ece874
Svetoslav Borislavov <svetoslav.borislavov@limechain.tech>
2023-12-08 17:39:35 +0200

    Read File Query functionality added

--- src/renderer/pages/CreateTransaction/txTypeComponentMapping.ts
+++ src/renderer/pages/CreateTransaction/txTypeComponentMapping.ts
@@ -1,7 +1,9 @@
 import CreateFile from './components/CreateFile.vue';
+import ReadFileVue from './components/ReadFile.vue';
 
 const txTypeComponentMapping = {
   createFile: CreateFile,
+  readFile: ReadFileVue,
 };
 
 export default txTypeComponentMapping;
