44690bd223fc76a33ecff1aa977f02bddf8b0aa5
Svetoslav Borislavov <svetoslav.borislavov@limechain.tech>
2023-12-08 18:11:52 +0200

    File Update Transaction POC created

--- src/renderer/pages/CreateTransaction/txTypeComponentMapping.ts
+++ src/renderer/pages/CreateTransaction/txTypeComponentMapping.ts
@@ -1,9 +1,11 @@
 import CreateFile from './components/CreateFile.vue';
 import ReadFileVue from './components/ReadFile.vue';
+import UpdateFileVue from './components/UpdateFile.vue';
 
 const txTypeComponentMapping = {
   createFile: CreateFile,
   readFile: ReadFileVue,
+  updateFile: UpdateFileVue,
 };
 
 export default txTypeComponentMapping;
