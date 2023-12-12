c80a9af1a350e9886bbd6165e2f41359c28a0db8
Svetoslav Borislavov <svetoslav.borislavov@limechain.tech>
2023-12-12 10:55:01 +0200

    User Login card created

---
+++ src/renderer/pages/Welcome/components/UserLogin.vue
@@ -0,0 +1,102 @@
+<script setup lang="ts">
+import { ref } from 'vue';
+import { useRouter } from 'vue-router';
+
+import { jwtDecode } from 'jwt-decode';
+
+import { IUserData } from '../../../../main/shared/interfaces/IUserData';
+
+import useUserStateStore from '../../../stores/storeUserState';
+import useOrganizationsStore from '../../../stores/storeOrganizations';
+
+import AppButton from '../../../components/ui/AppButton.vue';
+
+const router = useRouter();
+const userStateStore = useUserStateStore();
+const organizationsStore = useOrganizationsStore();
+
+const inputEmail = ref('');
+const inputPassword = ref('');
+const inputEmailInvalid = ref(false);
+const inputPasswordInvalid = ref(false);
+
+const handleRegister = () => {};
+
+const handleOnFormSubmit = (event: Event) => {
+  event.preventDefault();
+
+  const emailValid = inputEmail.value.trim() === '';
+  const passwordValid = inputPassword.value.trim() === '';
+
+  inputEmailInvalid.value = emailValid;
+  inputPasswordInvalid.value = passwordValid;
+
+  if (!inputEmailInvalid.value && !inputPasswordInvalid.value) {
+    //SEND LOGIN REQUEST
+    const loginRes = {
+      successful: true,
+      accessToken:
+        'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJlbWFpbCI6InBpcEBnbWFpbC5jb20iLCJ1c2VybmFtZSI6IkhlZGVyYVVzZXIiLCJ1c2VySWQiOiIxMjM0NTY3ODkifQ.iPZBw37mI7iBgOOPzDQYilx_y4h-DLE2h8EqEg6ZgbU',
+      isInitial: false,
+    };
+
+    try {
+      const decodedUserData: IUserData = jwtDecode(loginRes.accessToken);
+      userStateStore.logUser(loginRes.accessToken, decodedUserData);
+    } catch (error) {
+      console.log(error);
+    }
+
+    if (loginRes.isInitial) {
+      router.push({ name: 'newPassword' });
+    } else {
+      //REDIRECT TO DEFAULT LOGGED ROUTE?
+      router.push({ name: 'settingsGeneral' });
+    }
+  }
+};
+</script>
+<template>
+  <div>
+    <div
+      class="container-welcome-card container-modal-card p-5 border border-dark-subtle rounded-4"
+    >
+      <i class="bi bi-person mt-5 extra-large-icon d-block text-dark"></i>
+      <h4 class="mt-4 text-main text-bold text-center">Login as Personal User</h4>
+      <p class="text-secondary text-small lh-base text-center">
+        Lorem ipsum dolor sit amet, consectetur adipiscing elit.
+      </p>
+      <form
+        @submit="handleOnFormSubmit"
+        class="form-login mt-5 w-100 d-flex flex-column justify-content-center align-items-center"
+      >
+        <input
+          v-model="inputEmail"
+          type="text"
+          class="form-control rounded-4"
+          :class="{ 'is-invalid': inputEmailInvalid }"
+          placeholder="Email"
+        />
+        <div v-if="inputEmailInvalid" class="invalid-feedback">Invalid e-mail.</div>
+        <input
+          v-model="inputPassword"
+          type="password"
+          class="mt-4 form-control rounded-4"
+          :class="{ 'is-invalid': inputPasswordInvalid }"
+          placeholder="Password"
+        />
+        <div v-if="inputPasswordInvalid" class="invalid-feedback">Invalid password.</div>
+        <RouterLink
+          :to="{ name: 'forgotPassword' }"
+          class="align-self-start d-inline-block ms-3 mt-3 text-muted text-small cursor-pointer text-decoration-none"
+          >Forgot password</RouterLink
+        >
+        <AppButton color="primary" size="large" type="submit" class="mt-4 w-100 rounded-4"
+          >Login</AppButton
+        >
+        <p class="mt-4">or</p>
+        <p class="mt-4 text-bold cursor-pointer" @click="handleRegister">Register</p>
+      </form>
+    </div>
+  </div>
+</template>
