8bd847a70e551d4710ad972978e390ba9322753a
Svetoslav Borislavov <svetoslav.borislavov@limechain.tech>
2023-12-13 13:01:19 +0200

    AppRecoveryPhraseWord updated

--- src/renderer/components/ui/AppRecoveryPhraseWord.vue
+++ src/renderer/components/ui/AppRecoveryPhraseWord.vue
@@ -3,11 +3,13 @@ import { computed, ref } from 'vue';
 
 const props = defineProps<{
   word: string;
+  handleWordChange?: (newWord: string) => void;
   readonly?: boolean;
   withToggler?: boolean;
   index?: number;
   visibleInitially?: boolean;
 }>();
+const emit = defineEmits(['update:word']);
 
 const isVisible = ref(props.visibleInitially);
 
@@ -15,6 +17,13 @@ const inputType = computed(() => (isVisible.value ? 'text' : 'password'));
 
 const handleVisibiltyChange = () => {
   isVisible.value = !isVisible.value;
+};
+
+const handldeWordInput = (e: Event) => {
+  const value = (e.target as HTMLInputElement).value;
+  props.handleWordChange && props.handleWordChange(value);
+
+  emit('update:word', value);
 };
 </script>
 <template>
@@ -31,6 +40,7 @@ const handleVisibiltyChange = () => {
       :type="inputType"
       :readonly="readonly"
       :value="word"
+      @input="handldeWordInput"
     />
     <Transition name="fade" mode="out-in" v-if="withToggler">
       <i v-if="!isVisible" class="bi bi-eye" @click="handleVisibiltyChange"></i>
