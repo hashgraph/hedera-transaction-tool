f8052f798e106942ad0584348810d488472a10be
Svetoslav Borislavov <svetoslav.borislavov@limechain.tech>
2023-12-14 14:50:06 +0200

    ProtobuffKey decoding added

--- src/renderer/services/electronUtilsService.ts
+++ src/renderer/services/electronUtilsService.ts
@@ -1,1 +1,17 @@
+import { Key, KeyList } from '@hashgraph/sdk';
+
 export const openExternal = (url: string) => window.electronAPI.utils.openExternal(url);
+
+export const decodeProtobuffKey = async (protobuffKey: string) => {
+  const key = await window.electronAPI.utils.decodeProtobuffKey(protobuffKey);
+
+  if (key.thresholdKey) {
+    return KeyList.__fromProtobufThresoldKey(key.thresholdKey);
+  }
+  if (key.keyList) {
+    return KeyList.__fromProtobufKeyList(key.keyList);
+  }
+  if (key.ed25519 || key.ECDSASecp256k1) {
+    return Key._fromProtobufKey(key);
+  }
+};
