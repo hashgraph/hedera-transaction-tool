eba2e76aa8448b24c06bec8dfeb448c93a0e77d7
Svetoslav Borislavov <svetoslav.borislavov@limechain.tech>
2023-12-14 10:08:25 +0200

    AppSwitch created

---
+++ src/renderer/components/ui/AppSwitch.vue
@@ -0,0 +1,28 @@
+<script setup lang="ts">
+defineProps<{
+  checked: boolean;
+  name: string;
+  size?: 'sm' | 'md' | 'lg' | 'xl';
+  label?: string;
+  disabled?: boolean;
+}>();
+defineEmits(['update:checked']);
+</script>
+<template>
+  <div
+    class="form-check form-switch d-flex align-items-center gap-3"
+    :class="[size && `form-switch-${size}`]"
+  >
+    <input
+      class="form-check-input"
+      type="checkbox"
+      role="switch"
+      :checked="checked"
+      @input="$emit('update:checked', !checked)"
+      :name="name"
+    />
+    <label v-if="label" class="form-check-label text-small" :for="name">
+      {{ label }}
+    </label>
+  </div>
+</template>
