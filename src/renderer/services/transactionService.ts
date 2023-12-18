283d10d42a2e761ccb3523f4f45d640fa288bc19
Svetoslav Borislavov <svetoslav.borislavov@limechain.tech>
2023-12-18 11:38:33 +0200

    Get signatures function created

--- src/renderer/services/transactionService.ts
+++ src/renderer/services/transactionService.ts
@@ -1,4 +1,13 @@
-import { AccountId, Timestamp, TransactionId } from '@hashgraph/sdk';
+import {
+  AccountId,
+  PrivateKey,
+  PublicKey,
+  Timestamp,
+  Transaction,
+  TransactionId,
+} from '@hashgraph/sdk';
+import { IKeyPairWithAccountId } from '../../main/shared/interfaces/IKeyPair';
+import { decryptPrivateKey } from './keyPairService';
 
 export const createTransactionId = (
   accountId: AccountId | string,
@@ -17,4 +26,28 @@ export const createTransactionId = (
   }
 
   return TransactionId.withValidStart(accountId, validStart);
+};
+
+export const getTransactionSignatures = async (
+  keyPairs: IKeyPairWithAccountId[],
+  transaction: Transaction,
+  addToTransaction: boolean,
+  userId: string,
+  password: string,
+) => {
+  const signatures: { publicKey: PublicKey; signature: Uint8Array }[] = [];
+
+  await Promise.all(
+    keyPairs.map(async keyPair => {
+      const privateKeyString = await decryptPrivateKey(userId, password, keyPair.publicKey);
+
+      const privateKey = PrivateKey.fromStringED25519(privateKeyString);
+      const signature = privateKey.signTransaction(transaction);
+      signatures.push({ publicKey: PublicKey.fromString(keyPair.publicKey), signature });
+    }),
+  );
+
+  addToTransaction && signatures.forEach(s => transaction.addSignature(s.publicKey, s.signature));
+
+  return signatures;
 };
