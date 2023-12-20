bcad9234cad31abe05eb185e2f9f47482109706e
Svetoslav Borislavov <svetoslav.borislavov@limechain.tech>
2023-12-20 09:14:04 +0200

    Setup Organizations updated

--- src/renderer/pages/SetupOrganization/SetupOrganization.vue
+++ src/renderer/pages/SetupOrganization/SetupOrganization.vue
@@ -1,14 +1,16 @@
 <script setup lang="ts">
 import { ref } from 'vue';
-import { useRouter } from 'vue-router';
 
-import AppButton from '../../components/ui/AppButton.vue';
 import useOrganizationsStore from '../../stores/storeOrganizations';
 import useUserStateStore from '../../stores/storeUserState';
 
-const router = useRouter();
+import AppButton from '../../components/ui/AppButton.vue';
+import AppModal from '../../components/ui/AppModal.vue';
+
 const organizationsStore = useOrganizationsStore();
 const userStateStore = useUserStateStore();
+
+const isSuccessModalShown = ref(false);
 
 const organizationName = ref('');
 const serverUrl = ref('');
@@ -27,12 +29,12 @@ const handleContinue = async (e: Event) => {
     });
 
     userStateStore.setUserRole('organization');
+
+    isSuccessModalShown.value = true;
   } catch (error) {
     console.log(error);
     // Open toast
   }
-
-  router.push({ name: 'welcome' });
 };
 </script>
 <template>
@@ -40,6 +42,7 @@ const handleContinue = async (e: Event) => {
     class="p-10 d-flex flex-column justify-content-center align-items-center flex-1 overflow-hidden"
   >
     <h1 class="text-huge text-bold text-center">Setup Organization</h1>
+    <p class="mt-5">Please Enter Organisation details</p>
     <form
       @submit="handleContinue"
       class="mt-5 w-100 d-flex flex-column justify-content-center align-items-center gap-4"
@@ -77,5 +80,32 @@ const handleContinue = async (e: Event) => {
         >
       </div>
     </form>
+    <AppModal v-model:show="isSuccessModalShown" class="common-modal">
+      <div class="p-5">
+        <i
+          class="bi bi-x-lg d-inline-block cursor-pointer"
+          style="line-height: 16px"
+          @click="isSuccessModalShown = false"
+        ></i>
+        <div class="mt-5 text-center">
+          <i
+            class="bi bi-check-circle-fill extra-large-icon cursor-pointer"
+            style="line-height: 16px"
+          ></i>
+        </div>
+
+        <h3 class="mt-5 text-main text-center text-bold">Organizations Added</h3>
+        <p class="text-center text-small">
+          Lorem ipsum dolor sit amet, consectetur adipiscing elit.
+        </p>
+        <AppButton
+          color="primary"
+          size="large"
+          class="mt-5 w-100 rounded-4"
+          @click="$router.push({ name: 'welcome' })"
+          >Done</AppButton
+        >
+      </div>
+    </AppModal>
   </div>
 </template>
