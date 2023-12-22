f6862b21fbfb9819493c383394de4e801e4d3833
Svetoslav Borislavov <svetoslav.borislavov@limechain.tech>
2023-12-22 13:38:31 +0200

    useAccountId now supports abort controllers

--- src/renderer/composables/useAccountId.ts
+++ src/renderer/composables/useAccountId.ts
@@ -13,10 +13,15 @@ import { flattenKeyList } from '../services/keyPairService';
 export default function useAccountId() {
   const networkStore = useNetworkStore();
 
+  /* State */
   const accountId = ref<string>('');
   const accountInfo = ref<MirrorNodeAccountInfo | null>(null);
   const allowances = ref<MirrorNodeAllowance[]>([]);
 
+  const accountInfoController = ref<AbortController | null>(null);
+  const allowancesController = ref<AbortController | null>(null);
+
+  /* Computed */
   const isValid = computed(() => Boolean(accountInfo.value));
   const key = computed(() => accountInfo.value?.key);
   const keysFlattened = computed(() =>
@@ -25,22 +30,49 @@ export default function useAccountId() {
       : [],
   );
 
+  /* Watchers */
   watch(accountId, async newAccountId => {
+    cancelPreviousRequests();
+
     if (!newAccountId) return resetData();
 
     try {
       AccountId.fromString(newAccountId);
 
-      accountInfo.value = await getAccountInfo(newAccountId, networkStore.mirrorNodeBaseURL);
-      allowances.value = await getAccountAllowances(newAccountId, networkStore.mirrorNodeBaseURL);
+      accountInfoController.value = new AbortController();
+      const accountInfoRes = await getAccountInfo(
+        newAccountId,
+        networkStore.mirrorNodeBaseURL,
+        accountInfoController.value,
+      );
+
+      allowancesController.value = new AbortController();
+      const accountAllowancesRes = await getAccountAllowances(
+        newAccountId,
+        networkStore.mirrorNodeBaseURL,
+        allowancesController.value,
+      );
+
+      accountInfo.value = accountInfoRes.data;
+      allowances.value = accountAllowancesRes.data;
     } catch (e) {
       resetData();
     }
   });
 
+  /* Functions */
   function resetData() {
     accountInfo.value = null;
     allowances.value = [];
   }
+
+  function cancelPreviousRequests() {
+    accountInfoController.value?.abort();
+    allowancesController.value?.abort();
+
+    accountInfoController.value = null;
+    allowancesController.value = null;
+  }
+
   return { accountId, accountInfo, key, keysFlattened, allowances, isValid };
 }
