c76ad8ff38fb0eb07c3d3b586fff71cbadfa8139
Svetoslav Borislavov <svetoslav.borislavov@limechain.tech>
2024-01-09 09:35:28 +0200

    Account create/link links added
    Signed-off-by: Svetoslav Borislavov <svetoslav.borislavov@limechain.tech>

--- src/renderer/pages/Accounts/Accouts.vue
+++ src/renderer/pages/Accounts/Accouts.vue
@@ -1,10 +1,34 @@
+<script setup lang="ts">
+import AppButton from '../../components/ui/AppButton.vue';
+</script>
 <template>
   <div class="p-10">
-    <h1 class="text-huge text-bold">Accounts</h1>
-    <div>
-      <RouterLink class="link-menu mt-2" to="/recovery-phrase"
-        ><i class="bi bi-capsule"></i><span>Go to Recovery Phrase</span></RouterLink
-      >
+    <div class="h-100 d-flex row">
+      <div class="col-4 col-xl-3 border-end pe-4">
+        <div class="dropdown">
+          <AppButton
+            color="primary"
+            class="w-100 d-flex align-items-center justify-content-center"
+            data-bs-toggle="dropdown"
+            ><i class="bi bi-plus-lg text-subheader"></i> Add Account</AppButton
+          >
+          <ul class="dropdown-menu text-small">
+            <li class="dropdown-item">
+              <RouterLink to="create-transaction/createAccount" class="dropdown-item"
+                ><i class="bi bi-plus-lg"></i>
+                Create New
+              </RouterLink>
+            </li>
+            <li class="dropdown-item">
+              <RouterLink to="accounts/link-existing" class="dropdown-item"
+                ><i class="bi bi-plus-lg"></i>
+                Link Existing Account
+              </RouterLink>
+            </li>
+          </ul>
+        </div>
+      </div>
+      <div class="col-8 col-xl-9"></div>
     </div>
   </div>
 </template>
