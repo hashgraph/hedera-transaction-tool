5811653e8ef693973624c405e6e9f0a67f770264
Svetoslav Borislavov <svetoslav.borislavov@limechain.tech>
2024-01-10 15:43:10 +0200

    User validation added
    Signed-off-by: Svetoslav Borislavov <svetoslav.borislavov@limechain.tech>

--- src/renderer/pages/Welcome/components/UserLogin.vue
+++ src/renderer/pages/Welcome/components/UserLogin.vue
@@ -1,6 +1,7 @@
 <script setup lang="ts">
-import { ref } from 'vue';
+import { onMounted, reactive, ref, watch } from 'vue';
 import { jwtDecode } from 'jwt-decode';
+import Tooltip from 'bootstrap/js/dist/tooltip';
 
 import { IUserData } from '../../../../main/shared/interfaces';
 
@@ -10,8 +11,8 @@ import { useRouter } from 'vue-router';
 import { useToast } from 'vue-toast-notification';
 
 import { getStoredKeysSecretHashes } from '../../../services/keyPairService';
+import { isEmail } from '../../../utils/validator';
 
-import AppSwitch from '../../../components/ui/AppSwitch.vue';
 import AppButton from '../../../components/ui/AppButton.vue';
 
 /* Stores */
@@ -24,25 +25,33 @@ const router = useRouter();
 /* State */
 const inputEmail = ref('');
 const inputPassword = ref('');
+const isInitialLogin = ref(false); // Temporary
 
 const inputEmailInvalid = ref(false);
 const inputPasswordInvalid = ref(false);
-
-const isInitialLogin = ref(false); // Temporary
+const passwordRequirements = reactive({
+  length: false,
+  lowercase: false,
+  uppercase: false,
+  number: false,
+  special: false,
+});
+const tooltipContent = ref('');
 
 /* Handlers */
 const handleOnFormSubmit = async (event: Event) => {
   event.preventDefault();
 
-  const emailValid = inputEmail.value.trim() === '';
-  const passwordValid = inputPassword.value.trim() === '';
+  inputEmailInvalid.value = inputEmail.value.trim() === '' || !isEmail(inputEmail.value);
+  inputPasswordInvalid.value =
+    inputPassword.value.trim() === '' || !isPasswordStrong(inputPassword.value);
 
-  inputEmailInvalid.value = emailValid;
-  inputPasswordInvalid.value = passwordValid;
+  if (inputPasswordInvalid.value) {
+    toast.error('Password too weak', { position: 'top-right' });
+    return;
+  }
 
   if (!inputEmailInvalid.value && !inputPasswordInvalid.value) {
-    //SEND LOGIN REQUEST
-
     try {
       const loginRes = {
         successful: true,
@@ -76,6 +85,88 @@ const handleOnFormSubmit = async (event: Event) => {
     }
   }
 };
+const handleResetData = () => {};
+
+/* Hooks */
+onMounted(() => {
+  const tooltipTriggerList = document.querySelectorAll('[data-bs-toggle="tooltip"]');
+  Array.from(tooltipTriggerList).map(tooltipTriggerEl => new Tooltip(tooltipTriggerEl));
+
+  isPasswordStrong(inputPassword.value);
+  setTooltipContent();
+});
+
+/* Misc */
+function isPasswordStrong(password: string) {
+  const validationRegex = [
+    { regex: /.{10,}/ }, // min 10 letters,
+    { regex: /[0-9]/ }, // numbers from 0 - 9
+    { regex: /[a-z]/ }, // letters from a - z (lowercase)
+    { regex: /[A-Z]/ }, // letters from A-Z (uppercase),
+    { regex: /[^A-Za-z0-9]/ }, // special characters
+  ];
+
+  passwordRequirements.length = validationRegex[0].regex.test(password);
+  passwordRequirements.number = validationRegex[1].regex.test(password);
+  passwordRequirements.lowercase = validationRegex[2].regex.test(password);
+  passwordRequirements.uppercase = validationRegex[3].regex.test(password);
+  passwordRequirements.special = validationRegex[4].regex.test(password);
+
+  const isStrong = validationRegex.reduce((isStrong, item) => {
+    const isValid = item.regex.test(password);
+    return isStrong && isValid;
+  }, true);
+
+  return isStrong;
+}
+
+function setTooltipContent() {
+  tooltipContent.value = `
+          <div class='d-flex flex-column align-items-start px-3'>
+            <div class='${
+              passwordRequirements.lowercase ? 'text-success' : 'text-danger'
+            }'><i class='bi bi-${
+              passwordRequirements.lowercase ? 'check' : 'x'
+            }'></i>Lower case character</div>
+            <div class='${
+              passwordRequirements.uppercase ? 'text-success' : 'text-danger'
+            }'><i class='bi bi-${
+              passwordRequirements.uppercase ? 'check' : 'x'
+            }'></i>Upper case character</div>
+            <div class='${
+              passwordRequirements.number ? 'text-success' : 'text-danger'
+            }'><i class='bi bi-${passwordRequirements.number ? 'check' : 'x'}'></i>Have number</div>
+            <div class='${
+              passwordRequirements.special ? 'text-success' : 'text-danger'
+            }'><i class='bi bi-${
+              passwordRequirements.special ? 'check' : 'x'
+            }'></i>Special character</div>
+            <div class='${
+              passwordRequirements.length ? 'text-success' : 'text-danger'
+            }'><i class='bi bi-${
+              passwordRequirements.length ? 'check' : 'x'
+            }'></i>Be at least 10 characters</div>
+          </div>
+        `;
+
+  const tooltipList = document.querySelectorAll('[data-bs-toggle="tooltip"]');
+  Array.from(tooltipList).forEach(tooltipEl => {
+    const tooltip = Tooltip.getInstance(tooltipEl);
+    tooltip?.setContent({ '.tooltip-inner': tooltipContent.value });
+  });
+}
+/* Watchers */
+watch(inputPassword, pass => {
+  if (isPasswordStrong(pass) || pass.length === 0) {
+    inputPasswordInvalid.value = false;
+  }
+  setTooltipContent();
+});
+watch(inputEmail, pass => {
+  if (isEmail(pass) || pass.length === 0) {
+    inputEmailInvalid.value = false;
+  }
+});
 </script>
 <template>
   <div class="container-welcome-card container-modal-card p-5 border border-dark-subtle rounded-4">
@@ -108,22 +199,23 @@ const handleOnFormSubmit = async (event: Event) => {
         class="mt-4 form-control rounded-4"
         :class="{ 'is-invalid': inputPasswordInvalid }"
         placeholder="Enter password"
+        data-bs-toggle="tooltip"
+        data-bs-animation="false"
+        data-bs-placement="right"
+        data-bs-custom-class="wide-xl-tooltip text-start"
+        data-bs-html="true"
+        data-bs-title="_"
       />
       <div v-if="inputPasswordInvalid" class="invalid-feedback">Invalid password.</div>
       <div class="mt-3">
-        <RouterLink :to="{ name: 'forgotPassword' }" class="text-small link-primary"
-          >Forgot password</RouterLink
-        >
+        <span @click="handleResetData" class="text-small link-primary">Reset data</span>
       </div>
-      <!-- Temporary -->
-      <div class="w-100 mt-4 d-flex justify-content-start">
-        <AppSwitch
-          v-model:checked="isInitialLogin"
-          name="is-initial-login"
-          label="Initial Login"
-        ></AppSwitch>
-      </div>
-      <AppButton color="primary" size="large" type="submit" class="mt-5 w-100 rounded-4"
+      <AppButton
+        color="primary"
+        size="large"
+        type="submit"
+        class="rounded-4 mt-5 w-100"
+        :disabled="inputEmail.length === 0 || inputPassword.length === 0"
         >Login</AppButton
       >
 
@@ -137,3 +229,4 @@ const handleOnFormSubmit = async (event: Event) => {
     </form>
   </div>
 </template>
+../../../utils/validator
