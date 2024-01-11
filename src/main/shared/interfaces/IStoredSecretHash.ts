9135b6f35f34ca1c9a98cc948d32d5a11ad72b0e
Svetoslav Borislavov <svetoslav.borislavov@limechain.tech>
2024-01-11 09:57:50 +0200

    Stored Secret Hash JSON Schema created
    Signed-off-by: Svetoslav Borislavov <svetoslav.borislavov@limechain.tech>

--- src/main/shared/interfaces/IStoredSecretHash.ts
+++ src/main/shared/interfaces/IStoredSecretHash.ts
@@ -1,7 +1,23 @@
-import { IKeyPair } from './';
+import { IKeyPair, keyPairJSONSchema } from './';
 
 export interface IStoredSecretHash {
   name?: string;
   secretHash: string;
   keyPairs: IKeyPair[];
 }
+
+export const storedSecretHashJSONSchema = {
+  type: 'object',
+  properties: {
+    name: {
+      type: 'string',
+    },
+    secretHash: {
+      type: 'string',
+    },
+    keyPairs: {
+      type: 'array',
+      items: keyPairJSONSchema,
+    },
+  },
+};
