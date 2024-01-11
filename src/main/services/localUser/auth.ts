16562543257a301cd9840239c511c29044b4ccd7
Svetoslav Borislavov <svetoslav.borislavov@limechain.tech>
2024-01-11 09:10:01 +0200

    Local user auth service partly created
    Signed-off-by: Svetoslav Borislavov <svetoslav.borislavov@limechain.tech>

---
+++ src/main/services/localUser/auth.ts
@@ -0,0 +1,81 @@
+import fs from 'fs/promises';
+
+import Store, { Schema } from 'electron-store';
+
+import { hash } from '../../utils/crypto';
+
+export type SchemaProperties = {
+  email: string;
+  passwordHash: string;
+  organizationCredentials: { serverUrl: string; email: string; encryptedPassword: string }[];
+};
+
+/* persisting accounts data in a JSON file */
+export default function getLocalUserAuthStore(email: string) {
+  const schema: Schema<SchemaProperties> = {
+    email: { type: 'string' },
+    passwordHash: { type: 'string' },
+    organizationCredentials: {
+      type: 'array',
+      items: {
+        type: 'object',
+        properties: {
+          serverUrl: {
+            type: 'string',
+          },
+          email: {
+            type: 'string',
+          },
+          encryptedPassword: {
+            type: 'string',
+          },
+        },
+      },
+      default: [],
+    },
+  };
+
+  const store = new Store({
+    schema,
+    cwd: `${email}`,
+    name: `${email}-auth`,
+    clearInvalidConfig: true,
+  });
+
+  return store;
+}
+
+export const register = (email: string, password: string) => {
+  const store = getLocalUserAuthStore(email);
+
+  store.set('email', email);
+  const passwordHash = hash(password).toString('hex');
+  store.set('passwordHash', passwordHash);
+};
+
+export const login = async (email: string, password: string, autoRegister?: boolean) => {
+  const store = getLocalUserAuthStore(email);
+
+  const isRegistered = Boolean(store.get('email')) && Boolean(store.get('passwordHash'));
+  console.log(autoRegister);
+
+  if (!isRegistered) {
+    if (autoRegister) {
+      register(email, password);
+    } else {
+      await fs.unlink(store.path);
+      throw new Error('User does not exists');
+    }
+  }
+
+  const isInitial = false;
+
+  if (hash(password).toString('hex') !== store.get('passwordHash')) {
+    throw new Error('Incorrect password');
+  }
+
+  return {
+    email,
+    isInitial,
+  };
+};
