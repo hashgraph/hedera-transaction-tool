36b3143971f59653d625b97f1852b2d50bf82c37
Martin Dobrev <martindobrev87@gmail.com>
2024-01-23 15:20:19 +0200

    Small changes
    Signed-off-by: Martin Dobrev <martindobrev87@gmail.com>

--- src/renderer/pages/AccountSetup/components/KeyPairs.vue
+++ src/renderer/pages/AccountSetup/components/KeyPairs.vue
@@ -178,11 +178,16 @@ defineExpose({
     </div> -->
     <div class="form-group mt-5">
       <label class="form-label">Nickname <span class="fw-normal">- Optional</span></label>
-      <input v-model="nickname" type="text" class="form-control" placeholder="Enter Nickname" />
+      <input
+        v-model="nickname"
+        type="text"
+        class="form-control is-fill"
+        placeholder="Enter Nickname"
+      />
     </div>
     <div class="form-group w-25 mt-5">
       <label class="form-label">Key Type</label>
-      <input type="text" class="form-control" value="ED25519" readonly />
+      <input type="text" class="form-control is-fill" value="ED25519" readonly />
     </div>
     <!-- <AppSwitch
         v-model:checked="advancedMode"
@@ -230,27 +235,15 @@ defineExpose({
 
     <div class="form-group mt-5">
       <label class="form-label">ED25519 Private Key</label>
-      <p class="text-break">{{ privateKey }}</p>
+      <p class="text-break text-secondary">{{ privateKey }}</p>
     </div>
     <div class="form-group mt-4">
       <label class="form-label">ED25519 Public Key</label>
-      <p class="text-break">{{ publicKey }}</p>
+      <p class="text-break text-secondary">{{ publicKey }}</p>
     </div>
     <hr class="my-6" />
     <div class="alert alert-secondary d-flex align-items-start mb-0" role="alert">
-      <p>
-        <svg
-          xmlns="http://www.w3.org/2000/svg"
-          class="bi bi-exclamation-triangle-fill flex-shrink-0 me-3"
-          width="16"
-          height="20"
-          role="img"
-        >
-          <path
-            d="M8.982 1.566a1.13 1.13 0 0 0-1.96 0L.165 13.233c-.457.778.091 1.767.98 1.767h13.713c.889 0 1.438-.99.98-1.767L8.982 1.566zM8 5c.535 0 .954.462.9.995l-.35 3.507a.552.552 0 0 1-1.1 0L7.1 5.995A.905.905 0 0 1 8 5zm.002 6a1 1 0 1 1 0 2 1 1 0 0 1 0-2z"
-          />
-        </svg>
-      </p>
+      <i class="bi bi-exclamation-triangle text-warning me-3"></i>
 
       <div>
         <p class="fw-semibold">Sharing Key Pair</p>
