d3213996f7cbd120b777ef22452ea5d87a90215e
Svetoslav Borislavov <svetoslav.borislavov@limechain.tech>
2024-01-24 16:24:43 +0200

    ITransaction updated
    Signed-off-by: Svetoslav Borislavov <svetoslav.borislavov@limechain.tech>

--- src/main/shared/interfaces/ITransaction.ts
+++ src/main/shared/interfaces/ITransaction.ts
@@ -1,5 +1,3 @@
-import { JSONSchema } from 'json-schema-typed';
-
 export interface ITransaction {
   id: string;
   name: string;
@@ -19,44 +17,3 @@ export interface ITransaction {
   updated_at: number;
   group_id?: string;
 }
-
-export const transactionJSONSchema: JSONSchema = {
-  properties: {
-    id: { type: 'string' },
-    name: { type: 'string' },
-    type: { type: 'string' },
-    description: { type: 'string' },
-    transaction_id: { type: 'string' },
-    transaction_hash: { type: 'string' },
-    body: { type: 'string' },
-    status: { type: 'string' },
-    status_code: { type: 'string' },
-    user_id: { type: 'string' },
-    key_id: { type: 'string' },
-    signature: { type: 'string' },
-    valid_start: { type: 'string' },
-    executed_at: { type: 'string' },
-    created_at: { type: 'string' },
-    updated_at: { type: 'string' },
-    group_id: { type: 'string' },
-  },
-  required: [
-    'id',
-    'name',
-    'type',
-    'description',
-    'transaction_id',
-    'transaction_hash',
-    'body',
-    'status',
-    'status_code',
-    'user_id',
-    'key_id',
-    'signature',
-    'valid_start',
-    'executed_at',
-    'created_at',
-    'updated_at',
-  ],
-  additionalProperties: false,
-};
