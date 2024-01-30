ae9bf5d5157d12bdf7ff7a4689a5cfc3908143fe
Svetoslav Borislavov <svetoslav.borislavov@limechain.tech>
2024-01-30 18:09:45 +0200

    CreateAccount updated
    Signed-off-by: Svetoslav Borislavov <svetoslav.borislavov@limechain.tech>

--- src/renderer/pages/CreateTransaction/components/Account/CreateAccount.vue
+++ src/renderer/pages/CreateTransaction/components/Account/CreateAccount.vue
@@ -1,5 +1,5 @@
 <script setup lang="ts">
-import { computed, reactive, ref } from 'vue';
+import { computed, reactive, ref, watch } from 'vue';
 import {
   AccountId,
   AccountCreateTransaction,
@@ -18,10 +18,13 @@ import useNetworkStore from '../../../../stores/storeNetwork';
 import { add } from '../../../../services/accountsService';
 import { createTransactionId } from '../../../../services/transactionService';
 
+import { getDateTimeLocalInputValue } from '../../../../utils';
+
 import TransactionProcessor from '../../../../components/TransactionProcessor.vue';
 import AppButton from '../../../../components/ui/AppButton.vue';
 import AppSwitch from '../../../../components/ui/AppSwitch.vue';
 import AppInput from '../../../../components/ui/AppInput.vue';
+import TransactionIdControls from '@renderer/components/TransactionIdControls.vue';
 
 /* Stores */
 const user = useUserStore();
@@ -35,7 +38,7 @@ const payerData = useAccountId();
 const transactionProcessor = ref<typeof TransactionProcessor | null>(null);
 
 const transaction = ref<AccountCreateTransaction | null>(null);
-const validStart = ref('');
+const validStart = ref(getDateTimeLocalInputValue(new Date()));
 const maxTransactionfee = ref(2);
 
 const accountData = reactive({
@@ -45,7 +48,7 @@ const accountData = reactive({
   initialBalance: 0,
   stakedAccountId: '',
   stakedNodeId: '',
-  declineStakingReward: false,
+  acceptStakingRewards: true,
   memo: '',
 });
 const ownerKeyText = ref('');
@@ -78,7 +81,7 @@ const handleCreate = async e => {
       .setNodeAccountIds([new AccountId(3)])
       .setKey(keyList.value)
       .setReceiverSignatureRequired(accountData.receiverSignatureRequired)
-      .setDeclineStakingReward(accountData.declineStakingReward)
+      .setDeclineStakingReward(!accountData.acceptStakingRewards)
       .setInitialBalance(Number(accountData.initialBalance))
       .setMaxAutomaticTokenAssociations(Number(accountData.maxAutomaticTokenAssociations))
       .setAccountMemo(accountData.memo);
@@ -102,6 +105,20 @@ const handleExecuted = async ({ receipt }: { receipt: TransactionReceipt }) => {
   await add(user.data.id, accountId);
   toast.success(`Account ${accountId} linked`, { position: 'bottom-right' });
 };
+
+/* Watchers */
+watch(
+  () => accountData.stakedAccountId,
+  id => {
+    try {
+      if (id !== '0') {
+        accountData.stakedAccountId = AccountId.fromString(id).toString();
+      }
+    } catch (error) {
+      /* empty */
+    }
+  },
+);
 </script>
 <template>
   <form @submit="handleCreate">
@@ -118,64 +135,86 @@ const handleExecuted = async ({ receipt }: { receipt: TransactionReceipt }) => {
       </div>
     </div>
 
-    <div class="mt-4 d-flex flex-wrap gap-5">
-      <div class="form-group col-4">
-        <label class="form-label">Set Payer ID (Required)</label>
-        <label v-if="payerData.isValid.value" class="d-block form-label text-secondary"
-          >Balance: {{ payerData.accountInfo.value?.balance || 0 }}</label
-        >
-        <AppInput
-          :model-value="payerData.accountIdFormatted.value"
-          @update:model-value="v => (payerData.accountId.value = v)"
-          :filled="true"
-          placeholder="Enter Payer ID"
-        />
-      </div>
-      <div class="form-group">
-        <label class="form-label">Set Valid Start Time (Required)</label>
-        <AppInput v-model="validStart" type="datetime-local" step="1" :filled="true" />
-      </div>
-      <div class="form-group">
-        <label class="form-label">Set Max Transaction Fee (Optional)</label>
-        <AppInput v-model="maxTransactionfee" type="number" min="0" :filled="true" />
-      </div>
-    </div>
-    <div class="mt-4 form-group w-75">
-      <label class="form-label">Set Key/s (Required)</label>
-      <div class="d-flex gap-3">
-        <AppInput
-          v-model="ownerKeyText"
-          :filled="true"
-          placeholder="Enter owner public key"
-          style="max-width: 555px"
-          @keypress="e => e.code === 'Enter' && handleAdd()"
-        />
-        <AppButton type="button" color="secondary" @click="handleAdd">Add</AppButton>
-      </div>
-    </div>
-    <div class="mt-4 w-75">
-      <template v-for="key in ownerKeys" :key="key">
-        <div class="d-flex align-items-center gap-3">
-          <AppInput readonly :filled="true" :model-value="key" style="max-width: 555px" />
-          <i
-            class="bi bi-x-lg d-inline-block cursor-pointer"
-            style="line-height: 16px"
-            @click="ownerKeys = ownerKeys.filter(k => k !== key)"
-          ></i>
+    <TransactionIdControls
+      v-model:payer-id="payerData.accountId.value"
+      v-model:valid-start="validStart"
+      v-model:max-transaction-fee="maxTransactionfee"
+      class="mt-4"
+    />
+
+    <hr class="separator my-6" />
+
+    <div class="row">
+      <div class="form-group col-8 col-xl-6">
+        <label class="form-label">Keys</label>
+        <div class="d-flex gap-3">
+          <AppInput
+            v-model="ownerKeyText"
+            :filled="true"
+            placeholder="Enter owner public key"
+            @keypress="e => e.code === 'Enter' && handleAdd()"
+          />
+          <AppButton type="button" color="secondary" @click="handleAdd">Add</AppButton>
         </div>
-      </template>
+        <template v-for="key in ownerKeys" :key="key">
+          <div class="d-flex align-items-center gap-3 mt-4">
+            <AppInput readonly :filled="true" :model-value="key" />
+            <i
+              class="bi bi-x-lg cursor-pointer"
+              @click="ownerKeys = ownerKeys.filter(k => k !== key)"
+            ></i>
+          </div>
+        </template>
+      </div>
     </div>
-    <div class="mt-4 form-group w-50">
-      <label class="form-label">Set Initial Balance in HBar (Optional)</label>
-      <AppInput
-        v-model="accountData.initialBalance"
-        :filled="true"
-        type="number"
-        min="0"
-        placeholder="Enter Hbar amount"
+
+    <hr class="separator my-6" />
+
+    <div class="form-group">
+      <AppSwitch
+        v-model:checked="accountData.acceptStakingRewards"
+        size="md"
+        name="accept-staking-awards"
+        label="Accept Staking Awards"
       />
     </div>
-    <div class="mt-4 form-group w-50">
+    <div class="row mt-6">
+      <div class="form-group col-8 col-md-5 col-xl-4 col-xxl-3">
+        <label class="form-label">Set Staked Node Id (Optional)</label>
+        <AppInput
+          v-model="accountData.stakedNodeId"
+          :disabled="accountData.stakedAccountId.length > 0"
+          :filled="true"
+          type="number"
+          min="0"
+          placeholder="Enter Node Id"
+        />
+      </div>
+      <div class="form-group col-8 col-md-5 col-xl-4 col-xxl-3">
+        <label class="form-label">Set Staked Account Id (Optional)</label>
+        <AppInput
+          v-model="accountData.stakedAccountId"
+          :disabled="Number(accountData.stakedNodeId) > 0"
+          :filled="true"
+          placeholder="Enter Account Id"
+        />
+      </div>
+    </div>
+    <div class="row mt-6">
+      <div class="form-group col-8 col-md-5 col-xl-4 col-xxl-3">
+        <label class="form-label">Account Memo</label>
+        <AppInput
+          v-model="accountData.memo"
+          :filled="true"
+          maxlength="100"
+          placeholder="Enter Account Memo"
+        />
+      </div>
+    </div>
+
+    <hr class="separator my-6" />
+
+    <div class="form-group mt-6">
       <AppSwitch
         v-model:checked="accountData.receiverSignatureRequired"
         size="md"
@@ -183,53 +222,29 @@ const handleExecuted = async ({ receipt }: { receipt: TransactionReceipt }) => {
         label="Receiver Signature Required"
       />
     </div>
-    <div class="mt-4 form-group w-50">
-      <label class="form-label">Set Max Automatic Token Associations (Optional)</label>
-      <AppInput
-        v-model="accountData.maxAutomaticTokenAssociations"
-        :min="0"
-        :max="5000"
-        :filled="true"
-        type="number"
-        placeholder="Enter Max Token Auto Associations"
-      />
-    </div>
-    <div class="mt-4 form-group w-50">
-      <label class="form-label">Set Staked Account Id (Optional)</label>
-      <AppInput
-        v-model="accountData.stakedAccountId"
-        :disabled="Number(accountData.stakedNodeId) > 0"
-        :filled="true"
-        placeholder="Enter Account Id"
-      />
-    </div>
-    <div class="mt-4 form-group w-50">
-      <label class="form-label">Set Staked Node Id (Optional)</label>
-      <AppInput
-        v-model="accountData.stakedNodeId"
-        :disabled="accountData.stakedAccountId.length > 0"
-        :filled="true"
-        type="number"
-        min="0"
-        placeholder="Enter Node Id"
-      />
-    </div>
-    <div class="mt-4 form-group w-50">
-      <AppSwitch
-        v-model:checked="accountData.declineStakingReward"
-        size="md"
-        name="decline-signature"
-        label="Decline Staking Reward"
-      />
-    </div>
-    <div class="mt-4 form-group w-50">
-      <label class="form-label">Set Account Memo (Optional)</label>
-      <AppInput
-        v-model="accountData.memo"
-        :filled="true"
-        maxlength="100"
-        placeholder="Enter Account Memo"
-      />
+
+    <div class="row mt-6">
+      <div class="form-group col-8 col-md-5 col-xl-4 col-xxl-3">
+        <label class="form-label">Initial Balance in HBar</label>
+        <AppInput
+          v-model="accountData.initialBalance"
+          :filled="true"
+          type="number"
+          min="0"
+          placeholder="Enter Hbar amount"
+        />
+      </div>
+      <div class="form-group col-8 col-md-5 col-xl-4 col-xxl-3">
+        <label class="form-label">Set Max Automatic Token Associations</label>
+        <AppInput
+          v-model="accountData.maxAutomaticTokenAssociations"
+          :min="0"
+          :max="5000"
+          :filled="true"
+          type="number"
+          placeholder="Enter Max Token Auto Associations"
+        />
+      </div>
     </div>
   </form>
 
