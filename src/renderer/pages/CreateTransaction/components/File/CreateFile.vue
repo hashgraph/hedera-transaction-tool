bea91b7f5c31bcbe2a1e03f762eeca48a37967ee
Martin Dobrev <martindobrev87@gmail.com>
2024-01-30 11:42:53 +0200

    File transactions updated
    Signed-off-by: Martin Dobrev <martindobrev87@gmail.com>

--- src/renderer/pages/CreateTransaction/components/File/CreateFile.vue
+++ src/renderer/pages/CreateTransaction/components/File/CreateFile.vue
@@ -86,64 +86,71 @@ const handleCreate = async e => {
 };
 </script>
 <template>
-  <div class="p-4 border rounded-4">
-    <div class="d-flex justify-content-between">
-      <div class="d-flex align-items-start">
-        <i class="bi bi-arrow-up me-2"></i>
-        <span class="text-title text-bold">Create File Transaction</span>
+  <form class="mt-4" @submit="handleCreate">
+    <div class="d-flex justify-content-between align-items-center">
+      <h2 class="text-title text-bold">Create File Transaction</h2>
+
+      <div class="d-flex justify-content-end align-items-center">
+        <AppButton
+          color="primary"
+          type="submit"
+          size="large"
+          :disabled="keyList._keys.length === 0 || !payerData.isValid.value"
+          >Sign & Submit</AppButton
+        >
       </div>
     </div>
-    <form class="mt-4" @submit="handleCreate">
-      <div class="mt-4 d-flex flex-wrap gap-5">
-        <div class="form-group col-4">
-          <label class="form-label">Set Payer ID (Required)</label>
-          <label v-if="payerData.isValid.value" class="d-block form-label text-secondary"
-            >Balance: {{ payerData.accountInfo.value?.balance || 0 }}</label
-          >
-          <AppInput
-            :model-value="payerData.accountIdFormatted.value"
-            @update:model-value="v => (payerData.accountId.value = v)"
-            :filled="true"
-            placeholder="Enter Payer ID"
-          />
-        </div>
-        <div class="form-group">
-          <label class="form-label">Set Valid Start Time (Required)</label>
-          <AppInput v-model="validStart" type="datetime-local" step="1" :filled="true" />
-        </div>
-        <div class="form-group">
-          <label class="form-label">Set Max Transaction Fee (Optional)</label>
-          <AppInput v-model="maxTransactionFee" type="number" min="0" :filled="true" />
-        </div>
+
+    <div class="mt-4 d-flex flex-wrap gap-5">
+      <div class="form-group col-4">
+        <label class="form-label">Set Payer ID (Required)</label>
+        <label v-if="payerData.isValid.value" class="d-block form-label text-secondary"
+          >Balance: {{ payerData.accountInfo.value?.balance || 0 }}</label
+        >
+        <AppInput
+          :model-value="payerData.accountIdFormatted.value"
+          @update:model-value="v => (payerData.accountId.value = v)"
+          :filled="true"
+          placeholder="Enter Payer ID"
+        />
       </div>
-      <div class="mt-4 form-group w-75">
-        <label class="form-label">Set Keys (Required)</label>
-        <div class="d-flex gap-3">
-          <AppInput
-            v-model="ownerKeyText"
-            :filled="true"
-            placeholder="Enter owner public key"
-            style="max-width: 555px"
-            @keypress="e => e.code === 'Enter' && handleAdd()"
-          />
-          <AppButton color="secondary" type="button" class="rounded-4" @click="handleAdd"
-            >Add</AppButton
-          >
+      <div class="form-group">
+        <label class="form-label">Set Valid Start Time (Required)</label>
+        <AppInput v-model="validStart" type="datetime-local" step="1" :filled="true" />
+      </div>
+      <div class="form-group">
+        <label class="form-label">Set Max Transaction Fee (Optional)</label>
+        <AppInput v-model="maxTransactionFee" type="number" min="0" :filled="true" />
+      </div>
+    </div>
+    <div class="mt-4 form-group w-75">
+      <label class="form-label">Set Keys (Required)</label>
+      <div class="d-flex gap-3">
+        <AppInput
+          v-model="ownerKeyText"
+          :filled="true"
+          placeholder="Enter owner public key"
+          style="max-width: 555px"
+          @keypress="e => e.code === 'Enter' && handleAdd()"
+        />
+        <AppButton color="secondary" type="button" class="rounded-4" @click="handleAdd"
+          >Add</AppButton
+        >
+      </div>
+    </div>
+    <div class="mt-4 w-75">
+      <template v-for="key in ownerKeys" :key="key">
+        <div class="d-flex align-items-center gap-3">
+          <AppInput readonly :filled="true" :value="key" style="max-width: 555px" />
+          <i
+            class="bi bi-x-lg d-inline-block cursor-pointer"
+            style="line-height: 16px"
+            @click="ownerKeys = ownerKeys.filter(k => k !== key)"
+          ></i>
         </div>
-      </div>
-      <div class="mt-4 w-75">
-        <template v-for="key in ownerKeys" :key="key">
-          <div class="d-flex align-items-center gap-3">
-            <AppInput readonly :filled="true" :value="key" style="max-width: 555px" />
-            <i
-              class="bi bi-x-lg d-inline-block cursor-pointer"
-              style="line-height: 16px"
-              @click="ownerKeys = ownerKeys.filter(k => k !== key)"
-            ></i>
-          </div>
-        </template>
-      </div>
-      <!-- <div class="mt-4 form-group w-50">
+      </template>
+    </div>
+    <!-- <div class="mt-4 form-group w-50">
         <label class="form-label">Set File Memo (Optional)</label>
         <AppInput
           v-model="memo"
@@ -153,7 +160,7 @@ const handleCreate = async e => {
           placeholder="Enter memo"
         />
       </div> -->
-      <!-- <div class="mt-4 form-group w-25">
+    <!-- <div class="mt-4 form-group w-25">
         <label class="form-label">Set Expiration Time (Optional)</label>
         <AppInput
           v-model="expirationTimestamp"
@@ -162,7 +169,7 @@ const handleCreate = async e => {
           placeholder="Enter timestamp"
         />
       </div> -->
-      <!-- <div class="mt-4 form-group w-25">
+    <!-- <div class="mt-4 form-group w-25">
         <label for="fileUpload" class="form-label">
           <span for="fileUpload" class="btn btn-primary">Upload File (.json, .txt)</span>
         </label>
@@ -175,49 +182,39 @@ const handleCreate = async e => {
           @change="handleFileImport"
         />
       </div> -->
-      <div class="mt-4 form-group w-75">
-        <label class="form-label">Set File Contents</label>
-        <textarea v-model="content" class="form-control is-fill" rows="10"></textarea>
-      </div>
-      <div class="mt-4">
-        <!-- <AppButton size="small" color="secondary" class="me-3 px-4 rounded-4">Save Draft</AppButton> -->
-        <AppButton
-          color="primary"
-          type="submit"
-          size="large"
-          :disabled="keyList._keys.length === 0 || !payerData.isValid.value"
-          >Create</AppButton
-        >
-      </div>
-    </form>
-    <TransactionProcessor
-      ref="transactionProcessor"
-      :transaction-bytes="transaction?.toBytes() || null"
-      :on-close-success-modal-click="
-        () => {
-          payerData.accountId.value = '';
-          validStart = '';
-          maxTransactionFee = 2;
-          ownerKeys = [];
-          memo = '';
-          expirationTimestamp = undefined;
+    <div class="mt-4 form-group w-75">
+      <label class="form-label">Set File Contents</label>
+      <textarea v-model="content" class="form-control is-fill" rows="10"></textarea>
+    </div>
+  </form>
 
-          transaction = null;
-        }
-      "
-    >
-      <template #successHeading>File created successfully</template>
-      <template #successContent>
-        <p
-          v-if="transactionProcessor?.transactionResult"
-          class="text-small d-flex justify-content-between align-items mt-2"
-        >
-          <span class="text-bold text-secondary">File ID:</span>
-          <span>{{
-            new AccountId(transactionProcessor.transactionResult.receipt.fileId).toString() || ''
-          }}</span>
-        </p>
-      </template>
-    </TransactionProcessor>
-  </div>
+  <TransactionProcessor
+    ref="transactionProcessor"
+    :transaction-bytes="transaction?.toBytes() || null"
+    :on-close-success-modal-click="
+      () => {
+        payerData.accountId.value = '';
+        validStart = '';
+        maxTransactionFee = 2;
+        ownerKeys = [];
+        memo = '';
+        expirationTimestamp = undefined;
+
+        transaction = null;
+      }
+    "
+  >
+    <template #successHeading>File created successfully</template>
+    <template #successContent>
+      <p
+        v-if="transactionProcessor?.transactionResult"
+        class="text-small d-flex justify-content-between align-items mt-2"
+      >
+        <span class="text-bold text-secondary">File ID:</span>
+        <span>{{
+          new AccountId(transactionProcessor.transactionResult.receipt.fileId).toString() || ''
+        }}</span>
+      </p>
+    </template>
+  </TransactionProcessor>
 </template>
