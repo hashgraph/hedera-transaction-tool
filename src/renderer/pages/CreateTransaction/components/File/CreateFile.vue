96169fac30eb66479dbaf7469daba918682bab9c
Svetoslav Borislavov <svetoslav.borislavov@limechain.tech>
2024-01-30 12:36:22 +0200

    Minor layout changes
    Signed-off-by: Svetoslav Borislavov <svetoslav.borislavov@limechain.tech>

--- src/renderer/pages/CreateTransaction/components/File/CreateFile.vue
+++ src/renderer/pages/CreateTransaction/components/File/CreateFile.vue
@@ -86,7 +86,7 @@ const handleCreate = async e => {
 };
 </script>
 <template>
-  <form class="mt-4" @submit="handleCreate">
+  <form @submit="handleCreate">
     <div class="d-flex justify-content-between align-items-center">
       <h2 class="text-title text-bold">Create File Transaction</h2>
 
--- src/renderer/pages/CreateTransaction/components/File/UpdateFile.vue
+++ src/renderer/pages/CreateTransaction/components/File/UpdateFile.vue
@@ -148,7 +148,7 @@ const handleCreate = async e => {
 watch(fileMeta, () => (content.value = ''));
 </script>
 <template>
-  <form class="mt-4" @submit="handleCreate">
+  <form @submit="handleCreate">
     <div class="d-flex justify-content-between align-items-center">
       <h2 class="text-title text-bold">Update File Transaction</h2>
 
