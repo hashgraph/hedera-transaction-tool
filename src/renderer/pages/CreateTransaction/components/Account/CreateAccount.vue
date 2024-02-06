371d5ee748db139485ccf5809eef329cfc31efa9
Svetoslav Borislavov <svetoslav.borislavov@limechain.tech>
2024-02-06 13:42:40 +0200

    Only unique keys restriction added
    Signed-off-by: Svetoslav Borislavov <svetoslav.borislavov@limechain.tech>

--- src/renderer/pages/CreateTransaction/components/Account/CreateAccount.vue
+++ src/renderer/pages/CreateTransaction/components/Account/CreateAccount.vue
@@ -19,13 +19,14 @@ import { add } from '../../../../services/accountsService';
 import { createTransactionId } from '../../../../services/transactionService';
 
 import { getDateTimeLocalInputValue } from '../../../../utils';
+import { isPublicKey } from '../../../../utils/validator';
 
 import TransactionProcessor from '../../../../components/Transaction/TransactionProcessor.vue';
 import AppButton from '../../../../components/ui/AppButton.vue';
 import AppSwitch from '../../../../components/ui/AppSwitch.vue';
 import AppInput from '../../../../components/ui/AppInput.vue';
 import TransactionIdControls from '../../../../components/Transaction/TransactionIdControls.vue';
-import TransactionHeaderControls from '@renderer/components/Transaction/TransactionHeaderControls.vue';
+import TransactionHeaderControls from '../../../../components/Transaction/TransactionHeaderControls.vue';
 
 /* Stores */
 const user = useUserStore();
@@ -61,13 +62,7 @@ const keyList = computed(() => new KeyList(ownerKeys.value.map(key => PublicKey.
 /* Handlers */
 const handleAdd = () => {
   ownerKeys.value.push(ownerKeyText.value);
-  ownerKeys.value = ownerKeys.value.filter(key => {
-    try {
-      return PublicKey.fromString(key);
-    } catch (error) {
-      return false;
-    }
-  });
+  ownerKeys.value = [...new Set(ownerKeys.value.filter(isPublicKey))];
   ownerKeyText.value = '';
 };
 
