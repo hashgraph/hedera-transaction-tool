c0d534edaaf5a2d9bb1f42d7f62b65f55fdddac2
Svetoslav Borislavov <svetoslav.borislavov@limechain.tech>
2024-02-21 09:51:06 +0200

    Account Ids Select component created
    Signed-off-by: Svetoslav Borislavov <svetoslav.borislavov@limechain.tech>

---
+++ src/renderer/components/AccountIdsSelect.vue
@@ -0,0 +1,41 @@
+<script setup lang="ts">
+import { computed, onMounted } from 'vue';
+
+import useKeyPairsStore from '@renderer/stores/storeKeyPairs';
+
+/* Props */
+const props = defineProps<{
+  accountId: string;
+  selectDefault?: boolean;
+}>();
+
+/* Emits */
+const emit = defineEmits(['update:accountId']);
+
+/* Stores */
+const keyPairs = useKeyPairsStore();
+
+/* Computed */
+const accoundIds = computed(() => keyPairs.accoundIds.map(a => a.accountIds).flat());
+
+/* Handlers */
+const handleAccountIdChange = (e: Event) => {
+  const selectEl = e.target as HTMLSelectElement;
+  emit('update:accountId', selectEl.value);
+};
+
+/* Hooks */
+onMounted(() => {
+  if (props.accountId.length === 0 && props.selectDefault) {
+    emit('update:accountId', accoundIds.value[0]);
+  }
+});
+</script>
+
+<template>
+  <select class="form-select" :value="accountId" @change="handleAccountIdChange">
+    <template v-for="accountId in accoundIds" :key="accountId">
+      <option :value="accountId">{{ accountId }}</option>
+    </template>
+  </select>
+</template>
