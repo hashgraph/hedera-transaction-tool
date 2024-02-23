8e214c65e3fee21769daff9ffc695dd48e577e8f
Svetoslav Borislavov <svetoslav.borislavov@limechain.tech>
2024-02-23 08:55:28 +0200

    Added inactive Save Draft button in TransactionHeaderControls
    Signed-off-by: Svetoslav Borislavov <svetoslav.borislavov@limechain.tech>

--- src/renderer/components/Transaction/TransactionHeaderControls.vue
+++ src/renderer/components/Transaction/TransactionHeaderControls.vue
@@ -1,32 +1,49 @@
 <script setup lang="ts">
+import { computed } from 'vue';
+
+import { getTransactionType } from '@renderer/utils/transactions';
+
 import AppButton from '@renderer/components/ui/AppButton.vue';
+import { addDraft } from '@renderer/services/transactionDraftsService';
 
 /* Props */
-withDefaults(
+const props = withDefaults(
   defineProps<{
     handleSign?: () => void;
+    handleSaveDraft?: () => void;
     createRequirements?: any;
     headingText?: string;
     buttonText?: string;
     buttonType?: string;
+    transactionBytes?: Uint8Array;
   }>(),
   {
     buttonText: 'Sign & Submit',
     buttonType: 'submit',
   },
 );
+
+const type = computed(() => props.transactionBytes && getTransactionType(props.transactionBytes));
+
+/* Handlers */
+const saveDraft = () => {
+  if (!props.transactionBytes || !type.value) return;
+
+  addDraft(props.transactionBytes);
+};
 </script>
 <template>
-  <div class="d-flex justify-content-between align-items-center">
-    <div class="d-flex align-items-center">
-      <AppButton type="button" color="secondary" class="btn-icon-only me-4" @click="$router.back()">
-        <i class="bi bi-arrow-left"></i>
-      </AppButton>
-
-      <h2 class="text-title text-bold">{{ headingText }}</h2>
-    </div>
-
+  <div class="d-flex justify-content-between align-items-center flex-wrap gap-3">
+    <h2 class="text-title text-bold">{{ headingText }}</h2>
     <div class="d-flex justify-content-end align-items-center">
+      <AppButton
+        color="secondary"
+        :outline="true"
+        type="button"
+        @click="() => (handleSaveDraft ? handleSaveDraft() : saveDraft())"
+        class="me-3"
+        ><i class="bi bi-save"></i> Save Draft</AppButton
+      >
       <AppButton
         color="primary"
         :type="buttonType"
