f85a0ce574ddad071fd9a2198d22c5e57dcdbad4
Svetoslav Borislavov <svetoslav.borislavov@limechain.tech>
2024-02-26 07:54:01 +0200

    Save to temp util added
    Signed-off-by: Svetoslav Borislavov <svetoslav.borislavov@limechain.tech>

--- src/main/utils/index.ts
+++ src/main/utils/index.ts
@@ -1,3 +1,20 @@
+import path from 'node:path';
+import fs from 'fs/promises';
+
 export const getNumberArrayFromString = (str: string) => {
   return str.split(',').map(n => Number(n));
+};
+
+export const saveToTemp = async (filePath: string, content: Buffer) => {
+  try {
+    const directory = path.dirname(filePath);
+    await fs.mkdir(directory, { recursive: true });
+    await fs.writeFile(filePath, content);
+
+    return true;
+  } catch (error) {
+    console.log(error);
+
+    return false;
+  }
 };
