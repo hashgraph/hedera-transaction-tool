759a7c0f0a092123399903b1fac6f1ec0f61d07c
Svetoslav Borislavov <svetoslav.borislavov@limechain.tech>
2024-02-27 20:06:41 +0200

    Vite config updated
    Signed-off-by: Svetoslav Borislavov <svetoslav.borislavov@limechain.tech>

---
+++ vite.config.ts
@@ -0,0 +1,66 @@
+import { resolve } from 'node:path';
+import fs from 'node:fs';
+
+import { defineConfig } from 'vite';
+
+import vue from '@vitejs/plugin-vue';
+import electron from 'vite-plugin-electron/simple';
+import eslint from 'vite-plugin-eslint';
+
+import pkg from './package.json';
+
+// https://vitejs.dev/config/
+export default defineConfig(({ command }) => {
+  fs.rmSync('dist-electron', { recursive: true, force: true });
+
+  const isServe = command === 'serve';
+  const isBuild = command === 'build';
+  const sourcemap = isServe || !!process.env.VSCODE_DEBUG;
+
+  return {
+    resolve: {
+      alias: {
+        '@renderer': resolve('src/renderer'),
+        '@main': resolve('src/main'),
+      },
+    },
+    plugins: [
+      vue(),
+      electron({
+        main: {
+          // Shortcut of `build.lib.entry`
+          entry: 'src/main/index.ts',
+          vite: {
+            build: {
+              sourcemap,
+              minify: isBuild,
+              outDir: 'dist-electron/main',
+              rollupOptions: {
+                external: Object.keys('dependencies' in pkg ? pkg.dependencies : {}),
+              },
+            },
+          },
+        },
+        preload: {
+          input: 'src/preload/index.ts',
+          vite: {
+            build: {
+              sourcemap: sourcemap ? 'inline' : undefined,
+              minify: isBuild,
+              outDir: 'dist-electron/preload',
+              rollupOptions: {
+                external: Object.keys('dependencies' in pkg ? pkg.dependencies : {}),
+              },
+            },
+          },
+        },
+        renderer: {},
+      }),
+      eslint(),
+    ],
+    server: {
+      port: 8082,
+    },
+    clearScreen: false,
+  };
+});
