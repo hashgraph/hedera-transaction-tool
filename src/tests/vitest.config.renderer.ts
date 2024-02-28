62d7827791d53d351a93406f16b93471b5abfe86
Svetoslav Borislavov <svetoslav.borislavov@limechain.tech>
2024-02-28 10:31:50 +0200

    Vitest renderer config updated
    Signed-off-by: Svetoslav Borislavov <svetoslav.borislavov@limechain.tech>

--- src/tests/vitest.config.renderer.ts
+++ src/tests/vitest.config.renderer.ts
@@ -1,6 +1,8 @@
 import { resolve } from 'node:path';
 import { defineConfig } from 'vitest/config';
 
+import vue from '@vitejs/plugin-vue';
+
 export default defineConfig({
   resolve: {
     alias: {
@@ -11,5 +13,7 @@ export default defineConfig({
   test: {
     environment: 'happy-dom',
     globals: true,
+    include: ['src/tests/renderer/**/*.{test,spec}.{ts,js}'],
   },
+  plugins: [vue()],
 });
