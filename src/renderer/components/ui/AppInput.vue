addab95829de869f3febc2710dd2ac9c525cdccc
Svetoslav Borislavov <svetoslav.borislavov@limechain.tech>
2024-02-28 12:08:51 +0200

    AppInput ref added
    Signed-off-by: Svetoslav Borislavov <svetoslav.borislavov@limechain.tech>

--- src/renderer/components/ui/AppInput.vue
+++ src/renderer/components/ui/AppInput.vue
@@ -1,12 +1,16 @@
 <script setup lang="ts">
-import { computed } from 'vue';
+import { computed, ref } from 'vue';
 
+/* Props */
 const props = defineProps<{
   modelValue?: string | number;
   filled?: boolean;
   size?: 'small' | 'large' | undefined;
 }>();
 defineEmits(['update:modelValue']);
+
+/* State */
+const inputRef = ref<HTMLInputElement | null>();
 
 /* Computed */
 const sizeClass = computed(() => {
@@ -25,9 +29,15 @@ const sizeClass = computed(() => {
   return className;
 });
 const fillClass = computed(() => (props.filled ? 'is-fill' : ''));
+
+/* Exposes */
+defineExpose({
+  inputRef,
+});
 </script>
 <template>
   <input
+    ref="inputRef"
     :value="modelValue"
     @input="$emit('update:modelValue', ($event.target! as HTMLInputElement).value)"
     :class="[sizeClass, fillClass]"
