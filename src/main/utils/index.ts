4dcc5047c1ff1878c98499da9b3e012178effb47
Svetoslav Borislavov <svetoslav.borislavov@limechain.tech>
2024-02-28 08:36:30 +0200

    JSONtoUInt8Array util added
    Signed-off-by: Svetoslav Borislavov <svetoslav.borislavov@limechain.tech>

--- src/main/utils/index.ts
+++ src/main/utils/index.ts
@@ -5,6 +5,15 @@ export const getNumberArrayFromString = (str: string) => {
   return str.split(',').map(n => Number(n));
 };
 
+export const JSONtoUInt8Array = json => {
+  const str = JSON.stringify(json, null, 0);
+  const ret = new Uint8Array(str.length);
+  for (let i = 0; i < str.length; i++) {
+    ret[i] = str.charCodeAt(i);
+  }
+  return ret;
+};
+
 export const saveContentToPath = async (filePath: string, content: Buffer) => {
   try {
     const directory = path.dirname(filePath);
