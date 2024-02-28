94474feb3391589366e1d707584004a9b20f5e9b
Svetoslav Borislavov <svetoslav.borislavov@limechain.tech>
2024-02-28 09:06:26 +0200

    Vitest workspace updated
    Signed-off-by: Svetoslav Borislavov <svetoslav.borislavov@limechain.tech>

--- vitest.workspace.ts
+++ vitest.workspace.ts
@@ -2,37 +2,19 @@ import { defineWorkspace } from 'vitest/config';
 
 export default defineWorkspace([
   {
-    extends: './vite.config.js',
-    test: {
-      name: 'renderer',
-      environment: 'happy-dom',
-      globals: true,
-      include: ['./src/tests/main/**/*.{test,spec}.{ts,js}'],
-    },
-  },
-  {
-    extends: './vite.config.js',
+    extends: './vite.config.ts',
     test: {
       name: 'main',
       environment: 'node',
-      globals: true,
-      include: ['./src/tests/main/**/*.{test,spec}.{ts,js}'],
+      include: ['src/tests/main/**/*.{test,spec}.{ts,js}'],
     },
   },
   {
-    extends: './vite.config.js',
+    extends: './vite.config.ts',
     test: {
-      name: 'e2e',
-      globals: true,
-      /**
-       * For e2e tests, have vitest search only in the project root 'tests' folder.
-       */
-      include: ['./tests/**/*.{test,spec}.{js,mjs,cjs,ts,mts,cts,jsx,tsx}'],
-      /**
-       * The default timeout of 5000ms is sometimes not enough for playwright.
-       */
-      testTimeout: 30_000,
-      hookTimeout: 30_000,
+      name: 'renderer',
+      environment: 'happy-dom',
+      include: ['src/tests/renderer/**/*.{test,spec}.{ts,js}'],
     },
   },
 ]);
