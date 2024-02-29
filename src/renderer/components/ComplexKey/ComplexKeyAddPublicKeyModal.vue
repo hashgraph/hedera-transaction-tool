0c2d012883ab879e716c4962c16ee8bc04f51139
Svetoslav Borislavov <svetoslav.borislavov@limechain.tech>
2024-02-29 15:39:42 +0200

    Modal for adding public keys created
    Signed-off-by: Svetoslav Borislavov <svetoslav.borislavov@limechain.tech>

---
+++ src/renderer/components/ComplexKey/ComplexKeyAddPublicKeyModal.vue
@@ -0,0 +1,87 @@
+<script setup lang="ts">
+import { ref } from 'vue';
+
+import { PublicKey } from '@hashgraph/sdk';
+
+import useKeyPairsStore from '@renderer/stores/storeKeyPairs';
+
+import { isPublicKey } from '@renderer/utils/validator';
+
+import AppButton from '@renderer/components/ui/AppButton.vue';
+import AppModal from '@renderer/components/ui/AppModal.vue';
+import AppInput from '@renderer/components/ui/AppInput.vue';
+import AppPublicKeyInput from '../ui/AppPublicKeyInput.vue';
+
+/* Props */
+const props = defineProps<{
+  show: boolean;
+  onPublicKeyAdd: (publicKey: PublicKey) => void;
+}>();
+
+/* Emits */
+const emit = defineEmits(['update:show']);
+
+/* Stores */
+const keyPairs = useKeyPairsStore();
+
+/* State */
+const publicKey = ref('');
+
+/* Handlers */
+const handleShowUpdate = show => emit('update:show', show);
+
+const handleInsert = () => {
+  if (!isPublicKey(publicKey.value)) {
+    throw new Error('Invalid public key');
+  }
+
+  props.onPublicKeyAdd(PublicKey.fromString(publicKey.value));
+  handleShowUpdate(false);
+};
+</script>
+<template>
+  <AppModal :show="show" @update:show="handleShowUpdate" class="medium-modal">
+    <div class="p-4">
+      <div>
+        <i class="bi bi-x-lg cursor-pointer" @click="$emit('update:show', false)"></i>
+      </div>
+      <h1 class="text-title text-center">Add Public Key</h1>
+      <div class="mt-5">
+        <AppInput
+          v-model:model-value="publicKey"
+          filled
+          type="text"
+          placeholder="Enter Public Key"
+        />
+      </div>
+      <hr class="separator my-5" />
+      <div>
+        <h3 class="text-small">Recent</h3>
+        <div class="mt-4 overflow-auto" :style="{ height: '150px', paddingRight: '10px' }">
+          <template v-for="pk in keyPairs.publicKeys" :key="pk">
+            <div class="mt-3" @click="publicKey = pk">
+              <AppPublicKeyInput :model-value="pk" filled class="cursor-pointer" readonly />
+            </div>
+          </template>
+        </div>
+      </div>
+      <hr class="separator my-5" />
+      <div class="row justify-content-between">
+        <div class="col-4 d-grid">
+          <AppButton color="secondary" type="button" @click="handleShowUpdate(false)"
+            >Cancel</AppButton
+          >
+        </div>
+        <div class="col-4 d-grid">
+          <AppButton
+            color="primary"
+            type="button"
+            @click="handleInsert"
+            :disabled="!isPublicKey(publicKey)"
+            >Insert</AppButton
+          >
+        </div>
+      </div>
+    </div>
+  </AppModal>
+</template>
