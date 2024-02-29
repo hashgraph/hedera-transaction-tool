ee0dcd4aa3e6a07b887bd588b30b5a5b1854f795
Svetoslav Borislavov <svetoslav.borislavov@limechain.tech>
2024-02-29 16:55:38 +0200

    Complex Key error modal added
    Signed-off-by: Svetoslav Borislavov <svetoslav.borislavov@limechain.tech>

--- src/renderer/components/ComplexKey/ComplexKeyModal.vue
+++ src/renderer/components/ComplexKey/ComplexKeyModal.vue
@@ -5,6 +5,7 @@ import { Key, KeyList } from '@hashgraph/sdk';
 
 import AppButton from '@renderer/components/ui/AppButton.vue';
 import AppModal from '@renderer/components/ui/AppModal.vue';
+import AppCustomIcon from '@renderer/components/ui/AppCustomIcon.vue';
 import ComplexKey from '@renderer/components/ComplexKey/ComplexKey.vue';
 
 /* Props */
@@ -18,6 +19,7 @@ const emit = defineEmits(['update:show', 'update:modelKey']);
 
 /* State */
 const currentKey = ref<Key | null>(null);
+const errorModalShow = ref(false);
 
 /* Handlers */
 const handleShowUpdate = show => emit('update:show', show);
@@ -31,7 +33,8 @@ const handleSave = e => {
     currentKey.value === null ||
     (currentKey.value instanceof KeyList && currentKey.value.toArray().length === 0)
   ) {
-    throw new Error('Key List Cannot Be Empty');
+    errorModalShow.value = true;
+    return;
   }
 
   emit('update:modelKey', currentKey.value);
@@ -59,5 +62,32 @@ const modalContentContainerStyle = { padding: '0 10%', height: '80%' };
         </div>
       </form>
     </div>
+    <AppModal
+      class="common-modal"
+      v-model:show="errorModalShow"
+      :close-on-click-outside="false"
+      :close-on-escape="false"
+    >
+      <div class="p-5">
+        <div>
+          <i class="bi bi-x-lg cursor-pointer" @click="errorModalShow = false"></i>
+        </div>
+        <div class="text-center">
+          <AppCustomIcon :name="'error'" style="height: 160px" />
+        </div>
+        <h3 class="text-center text-title text-bold mt-4">Error</h3>
+        <p class="text-center text-small text-secondary mt-3">
+          You cannot save this structure with empty list
+        </p>
+        <hr class="separator my-5" />
+        <div class="row justify-content-center mt-4">
+          <div class="col-6 d-grid">
+            <AppButton type="button" color="secondary" @click="errorModalShow = false"
+              >Close</AppButton
+            >
+          </div>
+        </div>
+      </div>
+    </AppModal>
   </AppModal>
 </template>
