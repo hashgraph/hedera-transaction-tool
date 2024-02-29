fd9b2c44ddb457ef0a9b92b3137b272dc7003f80
Svetoslav Borislavov <svetoslav.borislavov@limechain.tech>
2024-02-29 16:36:41 +0200

    Public key add modal updated
    Signed-off-by: Svetoslav Borislavov <svetoslav.borislavov@limechain.tech>

--- src/renderer/components/ComplexKey/ComplexKeyAddPublicKeyModal.vue
+++ src/renderer/components/ComplexKey/ComplexKeyAddPublicKeyModal.vue
@@ -10,7 +10,7 @@ import { isPublicKey } from '@renderer/utils/validator';
 import AppButton from '@renderer/components/ui/AppButton.vue';
 import AppModal from '@renderer/components/ui/AppModal.vue';
 import AppInput from '@renderer/components/ui/AppInput.vue';
-import AppPublicKeyInput from '../ui/AppPublicKeyInput.vue';
+import AppPublicKeyInput from '@renderer/components/ui/AppPublicKeyInput.vue';
 
 /* Props */
 const props = defineProps<{
@@ -30,7 +30,9 @@ const publicKey = ref('');
 /* Handlers */
 const handleShowUpdate = show => emit('update:show', show);
 
-const handleInsert = () => {
+const handleInsert = (e: Event) => {
+  e.preventDefault();
+
   if (!isPublicKey(publicKey.value)) {
     throw new Error('Invalid public key');
   }
@@ -42,46 +44,44 @@ const handleInsert = () => {
 <template>
   <AppModal :show="show" @update:show="handleShowUpdate" class="medium-modal">
     <div class="p-4">
-      <div>
-        <i class="bi bi-x-lg cursor-pointer" @click="$emit('update:show', false)"></i>
-      </div>
-      <h1 class="text-title text-center">Add Public Key</h1>
-      <div class="mt-5">
-        <AppInput
-          v-model:model-value="publicKey"
-          filled
-          type="text"
-          placeholder="Enter Public Key"
-        />
-      </div>
-      <hr class="separator my-5" />
-      <div>
-        <h3 class="text-small">Recent</h3>
-        <div class="mt-4 overflow-auto" :style="{ height: '150px', paddingRight: '10px' }">
-          <template v-for="pk in keyPairs.publicKeys" :key="pk">
-            <div class="mt-3" @click="publicKey = pk">
-              <AppPublicKeyInput :model-value="pk" filled class="cursor-pointer" readonly />
-            </div>
-          </template>
+      <form @submit="handleInsert">
+        <div>
+          <i class="bi bi-x-lg cursor-pointer" @click="$emit('update:show', false)"></i>
         </div>
-      </div>
-      <hr class="separator my-5" />
-      <div class="row justify-content-between">
-        <div class="col-4 d-grid">
-          <AppButton color="secondary" type="button" @click="handleShowUpdate(false)"
-            >Cancel</AppButton
-          >
+        <h1 class="text-title text-center">Add Public Key</h1>
+        <div class="mt-5">
+          <AppInput
+            v-model:model-value="publicKey"
+            filled
+            type="text"
+            placeholder="Enter Public Key"
+          />
         </div>
-        <div class="col-4 d-grid">
-          <AppButton
-            color="primary"
-            type="button"
-            @click="handleInsert"
-            :disabled="!isPublicKey(publicKey)"
-            >Insert</AppButton
-          >
+        <hr class="separator my-5" />
+        <div>
+          <h3 class="text-small">Recent</h3>
+          <div class="mt-4 overflow-auto" :style="{ height: '150px', paddingRight: '10px' }">
+            <template v-for="pk in keyPairs.publicKeys" :key="pk">
+              <div class="mt-3" @click="publicKey = pk">
+                <AppPublicKeyInput :model-value="pk" filled class="cursor-pointer" readonly />
+              </div>
+            </template>
+          </div>
         </div>
-      </div>
+        <hr class="separator my-5" />
+        <div class="row justify-content-between">
+          <div class="col-4 d-grid">
+            <AppButton color="secondary" type="button" @click="handleShowUpdate(false)"
+              >Cancel</AppButton
+            >
+          </div>
+          <div class="col-4 d-grid">
+            <AppButton color="primary" type="submit" :disabled="!isPublicKey(publicKey)"
+              >Insert</AppButton
+            >
+          </div>
+        </div>
+      </form>
     </div>
   </AppModal>
 </template>
