28ec4ae866e2f989250bf7b6d0c80c346567cc95
Svetoslav Borislavov <svetoslav.borislavov@limechain.tech>
2024-03-11 09:03:46 +0200

    AppListItem added
    Signed-off-by: Svetoslav Borislavov <svetoslav.borislavov@limechain.tech>

---
+++ src/renderer/components/ui/AppListItem.vue
@@ -0,0 +1,15 @@
+<script setup lang="ts">
+defineProps<{
+  selected?: boolean;
+  value: string;
+}>();
+</script>
+<template>
+  <div
+    class="list-item text-small cursor-pointer overflow-hidden"
+    :selected="selected ? selected : undefined"
+    :value="value"
+  >
+    <slot></slot>
+  </div>
+</template>
