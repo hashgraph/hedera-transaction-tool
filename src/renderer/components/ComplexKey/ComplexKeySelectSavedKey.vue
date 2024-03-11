e51e8a058e16b0b0acc2c62f7dfe670d64900a87
Svetoslav Borislavov <svetoslav.borislavov@limechain.tech>
2024-03-11 10:32:23 +0200

    Minor margin update
    Signed-off-by: Svetoslav Borislavov <svetoslav.borislavov@limechain.tech>

--- src/renderer/components/ComplexKey/ComplexKeySelectSavedKey.vue
+++ src/renderer/components/ComplexKey/ComplexKeySelectSavedKey.vue
@@ -109,6 +109,7 @@ onBeforeMount(async () => {
                 :value="kl.id"
                 @click="handleSelectKeyList(kl.id)"
                 :selected="kl.id === complexKeyId"
+                class="mt-3"
               >
                 <div
                   class="d-flex justify-content-between align-items-center flex-nowrap text-nowrap"
