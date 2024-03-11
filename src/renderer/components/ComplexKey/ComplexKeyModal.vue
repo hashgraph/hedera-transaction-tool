b1017c4b70903e9418d1d98467498cb91a40ec66
Svetoslav Borislavov <svetoslav.borislavov@limechain.tech>
2024-03-11 07:51:02 +0200

    Save key list UI added
    Signed-off-by: Svetoslav Borislavov <svetoslav.borislavov@limechain.tech>

--- src/renderer/components/ComplexKey/ComplexKeyModal.vue
+++ src/renderer/components/ComplexKey/ComplexKeyModal.vue
@@ -1,5 +1,5 @@
 <script setup lang="ts">
-import { ref, watch } from 'vue';
+import { computed, ref, watch } from 'vue';
 
 import { Key, KeyList } from '@hashgraph/sdk';
 
@@ -7,6 +7,7 @@ import { isKeyListValid } from '@renderer/utils/sdk';
 
 import AppButton from '@renderer/components/ui/AppButton.vue';
 import AppModal from '@renderer/components/ui/AppModal.vue';
+import AppInput from '@renderer/components/ui/AppInput.vue';
 import AppCustomIcon from '@renderer/components/ui/AppCustomIcon.vue';
 import ComplexKey from '@renderer/components/ComplexKey/ComplexKey.vue';
 import KeyStructure from '@renderer/components/KeyStructure.vue';
@@ -23,26 +24,44 @@ const emit = defineEmits(['update:show', 'update:modelKey']);
 /* State */
 const currentKey = ref<Key | null>(props.modelKey);
 const errorModalShow = ref(false);
+const saveKeyListModalShown = ref(false);
 const summaryMode = ref(false);
+const keyListNickname = ref('');
+
+/* Computed */
+const currentKeyInvalid = computed(
+  () =>
+    currentKey.value === null ||
+    (currentKey.value instanceof KeyList && !isKeyListValid(currentKey.value)),
+);
 
 /* Handlers */
 const handleShowUpdate = show => emit('update:show', show);
 
 const handleComplexKeyUpdate = (key: KeyList) => (currentKey.value = key);
 
-const handleSave = e => {
+const handleSaveButtonClick = e => {
   e.preventDefault();
 
-  if (
-    currentKey.value === null ||
-    (currentKey.value instanceof KeyList && !isKeyListValid(currentKey.value))
-  ) {
+  if (currentKeyInvalid.value) {
+    errorModalShow.value = true;
+    return;
+  }
+
+  saveKeyListModalShown.value = true;
+};
+
+const handleSaveKeyList = async e => {
+  e.preventDefault();
+
+  if (currentKeyInvalid.value) {
     errorModalShow.value = true;
     return;
   }
 
   emit('update:modelKey', currentKey.value);
   handleShowUpdate(false);
+  saveKeyListModalShown.value = false;
 };
 
 const handleClose = () => {
@@ -63,7 +82,7 @@ const modalContentContainerStyle = { padding: '0 10%', height: '80%' };
 <template>
   <AppModal :show="show" @update:show="handleShowUpdate" class="full-screen-modal">
     <div class="p-5 h-100">
-      <form @submit="handleSave" class="h-100">
+      <form @submit="handleSaveButtonClick" class="h-100">
         <div>
           <i class="bi bi-x-lg cursor-pointer" @click="handleClose"></i>
         </div>
@@ -115,6 +134,40 @@ const modalContentContainerStyle = { padding: '0 10%', height: '80%' };
             >
           </div>
         </div>
+      </div>
+    </AppModal>
+    <AppModal
+      v-model:show="saveKeyListModalShown"
+      class="common-modal"
+      :close-on-click-outside="false"
+      :close-on-escape="false"
+    >
+      <div class="p-5">
+        <div>
+          <i class="bi bi-x-lg cursor-pointer" @click="saveKeyListModalShown = false"></i>
+        </div>
+        <form class="mt-3" @submit="handleSaveKeyList">
+          <h3 class="text-center text-title text-bold">Enter your password</h3>
+          <div class="form-group mt-5 mb-4">
+            <label class="form-label">Nickname</label>
+            <AppInput
+              v-model:model-value="keyListNickname"
+              :filled="true"
+              placeholder="Enter Name of Key List"
+            />
+          </div>
+          <hr class="separator" />
+          <div class="row mt-4">
+            <div class="col-6 d-grid">
+              <AppButton color="secondary" type="button" @click="saveKeyListModalShown = false"
+                >Cancel</AppButton
+              >
+            </div>
+            <div class="col-6 d-grid">
+              <AppButton color="primary" :disabled="keyListNickname.length === 0">Save</AppButton>
+            </div>
+          </div>
+        </form>
       </div>
     </AppModal>
   </AppModal>
