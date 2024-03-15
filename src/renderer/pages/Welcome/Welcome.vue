ea366ea1d1d04c53ff8b69c3586f4caff4d2bd26
Svetoslav Borislavov <svetoslav.borislavov@limechain.tech>
2024-03-15 15:41:22 +0200

    Minor change
    Signed-off-by: Svetoslav Borislavov <svetoslav.borislavov@limechain.tech>

--- src/renderer/pages/Welcome/Welcome.vue
+++ src/renderer/pages/Welcome/Welcome.vue
@@ -3,8 +3,6 @@ import UserLogin from './components/UserLogin.vue';
 </script>
 <template>
   <div class="p-10 flex-column flex-centered flex-1 overflow-hidden">
-    <div class="fill-remaining flex-column flex-centered">
-      <UserLogin />
-    </div>
+    <UserLogin />
   </div>
 </template>
