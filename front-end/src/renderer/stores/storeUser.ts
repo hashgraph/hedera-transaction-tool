ea24be05918bffa0dd1e663d01c1768a18063b58
Svetoslav Borislavov <svetoslav.borislavov@limechain.tech>
2024-03-29 15:12:31 +0200

    User store refactor started
    Signed-off-by: Svetoslav Borislavov <svetoslav.borislavov@limechain.tech>

--- front-end/src/renderer/stores/storeUser.ts
+++ front-end/src/renderer/stores/storeUser.ts
@@ -1,132 +1,45 @@
-import { reactive, watch } from 'vue';
+import { computed, reactive, ref } from 'vue';
 import { defineStore } from 'pinia';
 
-import { KeyPair, Organization } from '@prisma/client';
-import { IUserKey } from '@main/shared/interfaces';
+import { KeyPair } from '@prisma/client';
 
 import {
-  getConnectedOrganizations,
-  getOrganizationsToSignIn,
-  shouldSignInOrganization,
-} from '@renderer/services/organizationCredentials';
-import { ping } from '@renderer/services/organization';
-
-export interface UserStore {
-  isLoggedIn: boolean | null;
-  id: string;
-  email: string;
-  password: string;
-  secretHashes: string[];
-  activeOrganization: Organization | null;
-  organizationServerActive: boolean | null;
-  connectedOrganizations: Organization[];
-  organizationsToSignIn: { credential_id?: string; email?: string; organization: Organization }[];
-  isSigningInOrganization: boolean;
-  organizationState: {
-    passwordTemporary: boolean;
-    organizationKeys: IUserKey[];
-    secretHashes: string[];
-  } | null;
-  organizationId: number | null;
-}
-
-export const localServerUrl = '';
+  PersonalUser,
+  PublicKeyAccounts,
+  RecoveryPhrase,
+  ConnectedOrganization,
+} from '@renderer/types';
 
 const useUserStore = defineStore('user', () => {
   /* State */
-  const data = reactive<UserStore>({
-    isLoggedIn: null,
-    id: '',
-    email: '',
-    password: '',
-    secretHashes: [],
-    activeOrganization: null,
-    organizationServerActive: null,
-    connectedOrganizations: [],
-    organizationsToSignIn: [],
-    isSigningInOrganization: false,
-    organizationState: null,
-    organizationId: null,
+  /** Keys */
+  const keyPairs = ref<KeyPair[]>([]);
+  const publicKeyToAccounts = ref<PublicKeyAccounts[]>([]);
+  const recoveryPhrase = ref<RecoveryPhrase | null>(null);
+
+  /** Personal */
+  const personal = reactive<PersonalUser>({
+    isLoggedIn: false,
   });
 
-  /* Actions */
-  function setActiveOrganization(organization: Organization | null) {
-    data.activeOrganization = organization;
-  }
+  /** Organization */
+  const selectedOrganization = ref<ConnectedOrganization | null>(null);
+  const organizations = ref<ConnectedOrganization[]>([]);
 
-  function login(id: string, email: string, secretHashes: string[]) {
-    data.isLoggedIn = true;
-    data.email = email;
-    data.id = id && id.length > 0 ? id : '';
-    data.secretHashes = secretHashes;
-  }
-
-  function logout() {
-    data.isLoggedIn = false;
-    data.email = '';
-    data.id = '';
-    data.secretHashes = [];
-  }
-
-  async function fetchConnectedOrganizations() {
-    if (data.isLoggedIn && data.id.length > 0) {
-      data.connectedOrganizations = await getConnectedOrganizations(data.id);
-      data.organizationsToSignIn = await getOrganizationsToSignIn(data.id);
-    }
-  }
-
-  /* Getter */
-  const shouldSetupAccount = (localKeyPairs: KeyPair[]) => {
-    return (
-      (data.activeOrganization &&
-        data.organizationState &&
-        (data.organizationState.passwordTemporary ||
-          data.organizationState.secretHashes.length === 0 ||
-          !data.organizationState.organizationKeys
-            .filter(k => k.mnemonicHash)
-            .every(key =>
-              localKeyPairs
-                .filter(kp => kp.secret_hash)
-                .some(kp => kp.public_key === key.publicKey),
-            ))) ||
-      data.secretHashes.length === 0
-    );
-  };
-
-  /* Watchers */
-  watch(
-    () => data.id,
-    async () => {
-      await fetchConnectedOrganizations();
-    },
-  );
-
-  watch(
-    () => data.activeOrganization,
-    async activeOrganization => {
-      if (activeOrganization) {
-        const flag = await shouldSignInOrganization(data.id, activeOrganization.id);
-        if (
-          flag &&
-          !data.organizationsToSignIn.find(o => o.organization.id === activeOrganization.id)
-        ) {
-          data.organizationsToSignIn.push({ organization: activeOrganization });
-        }
-
-        data.organizationServerActive = await ping(activeOrganization.serverUrl);
-      } else {
-        data.organizationServerActive = null;
-      }
-    },
-  );
+  /* Computed */
+  /** Keys */
+  const secretHashes = computed(() => keyPairs.value.map(kp => kp.secret_hash).filter(Boolean));
+  const publicKeys = computed(() => keyPairs.value.map(kp => kp.public_key));
 
   return {
-    data,
-    shouldSetupAccount,
-    setActiveOrganization,
-    login,
-    logout,
-    fetchConnectedOrganizations,
+    keyPairs,
+    publicKeyToAccounts,
+    recoveryPhrase,
+    personal,
+    selectedOrganization,
+    organizations,
+    secretHashes,
+    publicKeys,
   };
 });
 
