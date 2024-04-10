bfac8364a1fc1e3b74ef8413c48333f2f9995318
Svetoslav Borislavov <svetoslav.borislavov@limechain.tech>
2024-04-10 14:51:42 +0300

    User service cleaned
    Signed-off-by: Svetoslav Borislavov <svetoslav.borislavov@limechain.tech>

--- back-end/apps/api/src/users/users.service.ts
+++ back-end/apps/api/src/users/users.service.ts
@@ -1,110 +1,21 @@
 import {
-  Inject,
   Injectable,
   InternalServerErrorException,
   UnauthorizedException,
   UnprocessableEntityException,
 } from '@nestjs/common';
-import { ClientProxy } from '@nestjs/microservices';
-import { ConfigService } from '@nestjs/config';
-import { JwtService } from '@nestjs/jwt';
 import { InjectRepository } from '@nestjs/typeorm';
 
 import { Repository } from 'typeorm';
 import { FindOptionsWhere } from 'typeorm/find-options/FindOptionsWhere';
 
-import { Response } from 'express';
-import { totp } from 'otplib';
-
 import * as bcrypt from 'bcryptjs';
-
-import { NOTIFICATIONS_SERVICE } from '@app/common';
 
 import { User, UserStatus } from '@entities';
 
-import { OtpDto } from './dtos';
-
-import { OtpPayload } from '../interfaces/otp-payload.interface';
-
-totp.options = {
-  digits: 8,
-  step: 60,
-  window: 20,
-};
-
 @Injectable()
 export class UsersService {
-  constructor(
-    @InjectRepository(User) private repo: Repository<User>,
-    private readonly configService: ConfigService,
-    private readonly jwtService: JwtService,
-    @Inject(NOTIFICATIONS_SERVICE) private readonly notificationsService: ClientProxy,
-  ) {}
-
-  async createOtp(user: User, response: Response): Promise<void> {
-    const email = user.email;
-    const secret = this.getOtpSecret(email);
-    // If the user exists, create an TOTP for the secret + email
-    // const token = totp.generate(secret.concat(email));
-    // I'm not sure if I need to add email.
-    const token = totp.generate(secret);
-    // Send the totp to the email
-    this.notificationsService.emit('notify_email', {
-      email,
-      subject: 'Password Reset',
-      text: `Hello, your OTP is ${token}`,
-    });
-    // Now that it has been verified, create a jwt that is not 'verified' and
-    // set it on the response.
-    const otpPayload: OtpPayload = { email, verified: false };
-    this.setOtpCookie(response, otpPayload);
-  }
-
-  async verifyOtp(user: User, { token }: OtpDto, response: Response): Promise<boolean> {
-    const email = user.email;
-    const secret = this.getOtpSecret(email);
-    try {
-      if (!totp.check(token, secret)) {
-        throw new UnauthorizedException('Incorrect token');
-      }
-      // Now that it has been verified, create a new jwt that is 'valid' and
-      // set it on the response.
-      const otpPayload: OtpPayload = { email, verified: true };
-      this.setOtpCookie(response, otpPayload);
-
-      // Change the user status to 'NEW'. For a new user, this is no change.
-      // For a current user, this locks them from the organization data until a new password is set.
-      await this.updateUser(user, { status: UserStatus.NEW });
-
-      return true;
-    } catch (err) {
-      // Possible errors
-      // - options validation
-      // - "Invalid input - it is not base32 encoded string" (if 32 is used)
-      console.error(err);
-    }
-  }
-
-  // Get the otp secret and add the email, making it unique per user.
-  // The other approach would be to create a unique key per user and store
-  // it in the database.
-  private getOtpSecret(email: string): string {
-    return this.configService.get<string>('OTP_SECRET').concat(email);
-  }
-
-  private setOtpCookie(response: Response, otpPayload: OtpPayload) {
-    // Get the expiration to set on the cookie
-    const expires = new Date();
-    expires.setSeconds(expires.getSeconds() + totp.options.step * (totp.options.window as number));
-
-    const accessToken: string = this.jwtService.sign(otpPayload);
-    response.cookie('otp', accessToken, {
-      httpOnly: true,
-      expires,
-      sameSite: this.configService.get('NODE_ENV') === 'production' ? 'none' : 'lax',
-      secure: this.configService.get('NODE_ENV') === 'production',
-    });
-  }
+  constructor(@InjectRepository(User) private repo: Repository<User>) {}
 
   /* Creates a user with a given email and password. */
   async createUser(email: string, password: string): Promise<User> {
