fdec4c52ebc9dd371cc2bc47eeb6ae5c8364623d
Svetoslav Borislavov <svetoslav.borislavov@limechain.tech>
2024-04-11 17:54:12 +0300

    Backend endpoint requests added in auth service
    Signed-off-by: Svetoslav Borislavov <svetoslav.borislavov@limechain.tech>

--- front-end/src/renderer/services/organization/auth.ts
+++ front-end/src/renderer/services/organization/auth.ts
@@ -35,3 +35,61 @@ export const login = async (
     throw new Error('Failed Sign in Organization');
   }
 };
+
+/* Changes the password */
+export const changePassword = async (
+  organizationServerUrl: string,
+  oldPassword: string,
+  newPassword: string,
+): Promise<void> => {
+  try {
+    const response = await axios.patch(
+      `${organizationServerUrl}/auth/change-password`,
+      {
+        oldPassword,
+        newPassword,
+      },
+      {
+        withCredentials: true,
+      },
+    );
+    return response.data;
+  } catch (error) {
+    let message = 'Failed change user password';
+
+    if (error instanceof AxiosError) {
+      throwIfNoResponse(error);
+
+      const errorMessage = error.response?.data?.message;
+      if ([400, 401].includes(error.response?.status || 0) && message.length > 0) {
+        message = errorMessage;
+      }
+    }
+    throw new Error(message);
+  }
+};
+
+/* Sends a reset password request */
+export const resetPassword = async (
+  organizationServerUrl: string,
+  email: string,
+): Promise<void> => {
+  try {
+    const response = await axios.post(`${organizationServerUrl}/auth/reset-password`, {
+      email,
+    });
+    return response.data;
+  } catch (error) {
+    let message = 'Failed request passoword reset';
+
+    if (error instanceof AxiosError) {
+      throwIfNoResponse(error);
+
+      const errorMessage = error.response?.data?.message;
+      if ([400, 401].includes(error.response?.status || 0) && message.length > 0) {
+        message = errorMessage;
+      }
+    }
+    throw new Error(message);
+  }
+};
