87c453459a35a995bfc638ef2a0ab92c735ff8dc
Svetoslav Borislavov <svetoslav.borislavov@limechain.tech>
2024-04-16 10:53:37 +0300

    Create transaction DTO updated
    Signed-off-by: Svetoslav Borislavov <svetoslav.borislavov@limechain.tech>

--- back-end/apps/api/src/transactions/dto/create-transaction.dto.ts
+++ back-end/apps/api/src/transactions/dto/create-transaction.dto.ts
@@ -1,6 +1,6 @@
-import { IsDate, IsNumber, IsOptional, IsString } from 'class-validator';
+import { IsDate, IsNotEmpty, IsNumber, IsOptional, IsString } from 'class-validator';
 import { Type } from 'class-transformer';
-import { IsBuffer } from '../../validator/is-buffer.validator';
+import { TransforToUint8Array } from '@app/common/transformers/transformUint8array';
 
 //TODO approvers and observers can be added to this dto, validatenested,
 // also adding cascade to the transaction relations to enable single saves
@@ -11,13 +11,15 @@ export class CreateTransactionDto {
   @IsString()
   description: string;
 
-  @IsBuffer()
+  @IsNotEmpty()
+  @TransforToUint8Array()
   body: Buffer;
 
   @IsNumber()
   creatorKeyId: number;
 
-  @IsBuffer()
+  @IsNotEmpty()
+  @TransforToUint8Array()
   signature: Buffer;
 
   @Type(() => Date)
