d067698ee5750e9264550298c8f1f8891b071741
Svetoslav Borislavov <svetoslav.borislavov@limechain.tech>
2024-04-16 10:53:56 +0300

    Temporary cache cleared on interval
    Signed-off-by: Svetoslav Borislavov <svetoslav.borislavov@limechain.tech>

--- back-end/libs/common/src/mirrorNode/mirror-node.service.ts
+++ back-end/libs/common/src/mirrorNode/mirror-node.service.ts
@@ -22,14 +22,13 @@ export class MirrorNodeService {
     private readonly httpService: HttpService,
   ) {
     this.mirrorNodeBaseURL = MirrorNodeBaseURL.fromName(this.configService.get('HEDERA_NETWORK'));
+
+    setInterval(this.clearCache, this.cacheExpirationMs);
   }
 
   /* Get the account inforamtion for accountId */
   async getAccountInfo(accountId: string): Promise<AccountInfo> {
-    if (
-      this.accountInfoCache[accountId] &&
-      this.accountInfoCache[accountId].lastUpdated + this.cacheExpirationMs > new Date().getTime()
-    ) {
+    if (this.accountInfoCache[accountId] && !this.isCacheExpired(accountId)) {
       return this.accountInfoCache[accountId].accountInfo;
     }
     const { data } = await this.httpService.axiosRef.get<AccountInfo>(
@@ -42,5 +41,21 @@ export class MirrorNodeService {
     };
 
     return data;
+  }
+
+  /* Clear the cache */
+  clearCache() {
+    this.accountInfoCache = {};
+    for (const accountId in this.accountInfoCache) {
+      this.isCacheExpired(accountId) && delete this.accountInfoCache[accountId];
+    }
+  }
+
+  /* Getter for whether the cache for an account is expired */
+  private isCacheExpired(accountId: string): boolean {
+    return (
+      !this.accountInfoCache[accountId] ||
+      this.accountInfoCache[accountId].lastUpdated + this.cacheExpirationMs < new Date().getTime()
+    );
   }
 }
