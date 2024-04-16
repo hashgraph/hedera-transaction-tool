f1391c99b6890e213cd403fc03be63f79f387f95
Svetoslav Borislavov <svetoslav.borislavov@limechain.tech>
2024-04-16 09:52:33 +0300

    Minor
    Signed-off-by: Svetoslav Borislavov <svetoslav.borislavov@limechain.tech>

--- back-end/apps/api/src/auth/strategies/index.ts
+++ back-end/apps/api/src/auth/strategies/index.ts
@@ -1,5 +1,4 @@
 export * from './local.strategy';
 export * from './jwt.strategy';
-export * from './otp-local.strategy';
 export * from './otp-jwt.strategy';
 export * from './otp-verified.strategy';
