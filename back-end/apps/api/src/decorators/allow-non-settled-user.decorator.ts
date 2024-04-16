d127a16aca5790a7c8be7bbab4674366a2d7b284
Svetoslav Borislavov <svetoslav.borislavov@limechain.tech>
2024-04-16 11:03:58 +0300

    Unused items removed
    Signed-off-by: Svetoslav Borislavov <svetoslav.borislavov@limechain.tech>

--- back-end/apps/api/src/decorators/allow-non-settled-user.decorator.ts
+++
@@ -1,4 +0,0 @@
-import { SetMetadata } from '@nestjs/common';
-
-export const ALLOW_NOT_SETTLED_USER = 'allow_not_settled_user';
-export const AllowNotSettledUser = () => SetMetadata(ALLOW_NOT_SETTLED_USER, true);
