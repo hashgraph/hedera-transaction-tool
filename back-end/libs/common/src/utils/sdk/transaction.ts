2afac97de3d32cd0f282114c1db69311e3391be5
Svetoslav Borislavov <svetoslav.borislavov@limechain.tech>
2024-04-16 10:53:37 +0300

    Get TransactionType Enum Value util created
    Signed-off-by: Svetoslav Borislavov <svetoslav.borislavov@limechain.tech>

--- back-end/libs/common/src/utils/sdk/transaction.ts
+++ back-end/libs/common/src/utils/sdk/transaction.ts
@@ -1,8 +1,47 @@
 import { Transaction } from '@hashgraph/sdk';
 
+import { TransactionType } from '@app/common/database/entities';
+
 export const isExpired = (transaction: Transaction) => {
   const validStart = transaction.transactionId.validStart.toDate();
   const duration = transaction.transactionValidDuration;
 
   return new Date().getTime() >= validStart.getTime() + duration;
+};
+
+export const getTransactionTypeEnumValue = (transaction: Transaction): TransactionType => {
+  const sdkType = transaction.constructor.name
+    .slice(transaction.constructor.name.startsWith('_') ? 1 : 0)
+    .split(/(?=[A-Z])/)
+    .join(' ')
+    .replace('Transaction', '')
+    .trim()
+    .toLocaleUpperCase();
+
+  switch (sdkType) {
+    case TransactionType.ACCOUNT_CREATE:
+      return TransactionType.ACCOUNT_CREATE;
+    case TransactionType.ACCOUNT_UPDATE:
+      return TransactionType.ACCOUNT_UPDATE;
+    case TransactionType.ACCOUNT_DELETE:
+      return TransactionType.ACCOUNT_DELETE;
+    case TransactionType.FILE_CREATE:
+      return TransactionType.FILE_CREATE;
+    case TransactionType.FILE_APPEND:
+      return TransactionType.FILE_APPEND;
+    case TransactionType.FILE_UPDATE:
+      return TransactionType.FILE_UPDATE;
+    case TransactionType.FILE_DELETE:
+      return TransactionType.FILE_DELETE;
+    case TransactionType.FREEZE:
+      return TransactionType.FREEZE;
+    case TransactionType.SYSTEM_DELETE:
+      return TransactionType.SYSTEM_DELETE;
+    case TransactionType.SYSTEM_UNDELETE:
+      return TransactionType.SYSTEM_UNDELETE;
+    case TransactionType.TRANSFER:
+      return TransactionType.TRANSFER;
+    default:
+      throw new Error(`Unsupported transaction type: ${sdkType}`);
+  }
 };
