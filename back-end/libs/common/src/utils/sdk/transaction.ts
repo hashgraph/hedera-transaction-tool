2b4fcd73a8c4b7851dfc7cb5d61ed9960c602391
Svetoslav Borislavov <svetoslav.borislavov@limechain.tech>
2024-04-17 15:20:34 +0300

    Transaction status from error util added
    Signed-off-by: Svetoslav Borislavov <svetoslav.borislavov@limechain.tech>

--- back-end/libs/common/src/utils/sdk/transaction.ts
+++ back-end/libs/common/src/utils/sdk/transaction.ts
@@ -173,3 +173,11 @@ export const isAlreadySigned = (transaction: Transaction, publicKey: string | Pu
 
   return transaction._signerPublicKeys.has(publicKey);
 };
+
+export const getStatusCodeFromMessage = (message: string) => {
+  if (message.includes('TRANSACTION_EXPIRED')) {
+    return 4;
+  } else {
+    return 21;
+  }
+};
