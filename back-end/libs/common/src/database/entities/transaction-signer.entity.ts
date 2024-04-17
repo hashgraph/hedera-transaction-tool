73af96eaf5d81963f339179f9b89464e4baacf72
Svetoslav Borislavov <svetoslav.borislavov@limechain.tech>
2024-04-17 10:51:34 +0300

    Transaction entity updated
    Signed-off-by: Svetoslav Borislavov <svetoslav.borislavov@limechain.tech>

--- back-end/libs/common/src/database/entities/transaction-signer.entity.ts
+++ back-end/libs/common/src/database/entities/transaction-signer.entity.ts
@@ -1,4 +1,11 @@
-import { CreateDateColumn, Entity, ManyToOne, PrimaryGeneratedColumn } from 'typeorm';
+import {
+  Column,
+  CreateDateColumn,
+  Entity,
+  JoinColumn,
+  ManyToOne,
+  PrimaryGeneratedColumn,
+} from 'typeorm';
 import { Transaction } from './transaction.entity';
 import { UserKey } from './user-key.entity';
 import { User } from './user.entity';
@@ -8,14 +15,26 @@ export class TransactionSigner {
   @PrimaryGeneratedColumn()
   id: number;
 
-  @ManyToOne(() => Transaction, transaction => transaction.signers, { eager: true })
+  @ManyToOne(() => Transaction, transaction => transaction.signers)
+  @JoinColumn({ name: 'transactionId' })
   transaction: Transaction;
 
-  @ManyToOne(() => UserKey, userKey => userKey.signedTransactions, { eager: true })
+  @Column()
+  transactionId: number;
+
+  @ManyToOne(() => UserKey, userKey => userKey.signedTransactions)
+  @JoinColumn({ name: 'userKeyId' })
   userKey: UserKey;
 
+  @Column()
+  userKeyId: number;
+
   @ManyToOne(() => User, user => user.signerForTransactions)
+  @JoinColumn({ name: 'userId' })
   user: User;
+
+  @Column()
+  userId: number;
 
   @CreateDateColumn()
   createdAt: Date;
