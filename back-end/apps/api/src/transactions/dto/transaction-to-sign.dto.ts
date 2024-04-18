228d732581af0c812412f33703c947faf5b64583
Svetoslav Borislavov <svetoslav.borislavov@limechain.tech>
2024-04-18 16:15:16 +0300

    Transactions to sign count endpoint added (#329)
    Signed-off-by: Svetoslav Borislavov <svetoslav.borislavov@limechain.tech>

--- back-end/apps/api/src/transactions/dto/transaction-to-sign.dto.ts
+++ back-end/apps/api/src/transactions/dto/transaction-to-sign.dto.ts
@@ -1,8 +1,10 @@
 import { Expose, Type } from 'class-transformer';
 import { TransactionDto } from './transaction.dto';
+import { ApiProperty } from '@nestjs/swagger';
 
 export class TransactionToSignDto {
   @Expose()
+  @ApiProperty({ type: TransactionDto })
   @Type(() => TransactionDto)
   transaction: TransactionDto;
 
