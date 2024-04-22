304cfadc1d4452867e4550316e7f2231a6f484bf
Svetoslav Borislavov <svetoslav.borislavov@limechain.tech>
2024-04-22 12:56:58 +0300

    Transaction upload signature endpoint request function added
    Signed-off-by: Svetoslav Borislavov <svetoslav.borislavov@limechain.tech>

--- front-end/src/renderer/services/organization/transaction.ts
+++ front-end/src/renderer/services/organization/transaction.ts
@@ -3,7 +3,7 @@ import { throwIfNoResponse } from '.';
 
 const controller = 'transactions';
 
-/* Login the user */
+/* Submits a transaction to the back end */
 export const submitTransaction = async (
   serverUrl: string,
   name: string,
@@ -30,6 +30,40 @@ export const submitTransaction = async (
     console.log(data);
 
     return { id: data.id, body: data.body };
+  } catch (error: any) {
+    let message = 'Failed submit transaction';
+
+    if (error instanceof AxiosError) {
+      throwIfNoResponse(error);
+
+      const errorMessage = error.response?.data?.message;
+      if ([400, 401].includes(error.response?.status || 0) && message.length > 0) {
+        message = errorMessage;
+      }
+    }
+    throw new Error(message);
+  }
+};
+
+/* Uploads signatures to the back end */
+export const uploadSignature = async (
+  serverUrl: string,
+  transactionId: number,
+  publicKeyId: number,
+  signatures: { [key: string]: string },
+): Promise<void> => {
+  try {
+    const { data } = await axios.post(
+      `${serverUrl}/${controller}/${transactionId}/signers`,
+      {
+        publicKeyId,
+        signatures,
+      },
+      {
+        withCredentials: true,
+      },
+    );
+    console.log(data);
   } catch (error: any) {
     let message = 'Failed submit transaction';
 
