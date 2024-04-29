b408e9cee310dff1512f14ab828e08bf76e4f997
Svetoslav Borislavov <svetoslav.borislavov@limechain.tech>
2024-04-29 15:57:16 +0300

    Chain: Transaction status implementation started
    Signed-off-by: Svetoslav Borislavov <svetoslav.borislavov@limechain.tech>

--- back-end/apps/chain/src/transaction-status/transaction-status.service.ts
+++ back-end/apps/chain/src/transaction-status/transaction-status.service.ts
@@ -1,26 +1,49 @@
 import { Injectable } from '@nestjs/common';
 import { InjectRepository } from '@nestjs/typeorm';
+import { Cron, CronExpression, SchedulerRegistry } from '@nestjs/schedule';
 
-import { Repository } from 'typeorm';
+import { In, MoreThan, Repository } from 'typeorm';
 import {
   FileAppendTransaction,
   FileUpdateTransaction,
   Transaction as SDKTransaction,
 } from '@hashgraph/sdk';
 
+import { MirrorNodeService, ableToSign, computeSignatureKey } from '@app/common';
+
 import { Transaction, TransactionStatus } from '@entities';
 
 import { UpdateTransactionStatusDto } from './dto';
-import { MirrorNodeService, ableToSign, computeSignatureKey } from '@app/common';
+import { ExecuteService } from '../execute';
 
 @Injectable()
 export class TransactionStatusService {
   constructor(
     @InjectRepository(Transaction) private transactionRepo: Repository<Transaction>,
+    private schedulerRegistry: SchedulerRegistry,
+    private readonly executeService: ExecuteService,
     private readonly mirrorNodeService: MirrorNodeService,
   ) {}
 
-  /* Checks if the signers are enough to sign the transaction */
+  /* Updates the transaction statuses on app start */
+  @Cron(new Date(Date.now() + 1 * 1000), {
+    name: 'initial_transactions_status_update',
+  })
+  async andleInitialTransactionStatusUpdate() {
+    console.log('Initial transaction status update');
+    await this.updateTransactions();
+  }
+
+  /* Updates the transaction statuses every X time */
+  @Cron(CronExpression.EVERY_30_MINUTES, {
+    name: 'transactions_status_update',
+  })
+  async handleCron() {
+    console.log('Transactions status update');
+    await this.updateTransactions();
+  }
+
+  /* Checks if the signers are enough to sign the transaction and update its status */
   async updateTransactionStatus({ id }: UpdateTransactionStatusDto) {
     const transaction = await this.transactionRepo.findOne({ where: { id } });
 
@@ -41,9 +64,57 @@ export class TransactionStatusService {
     const sigantureKey = await computeSignatureKey(sdkTransaction, this.mirrorNodeService);
 
     /* Checks if the transaction has valid siganture */
-    if (!ableToSign([...sdkTransaction._signerPublicKeys], sigantureKey)) return;
+    const isAbleToSign = !ableToSign([...sdkTransaction._signerPublicKeys], sigantureKey);
 
-    transaction.status = TransactionStatus.WAITING_FOR_EXECUTION;
-    await this.transactionRepo.save(transaction);
+    await this.transactionRepo.update(
+      {
+        id,
+      },
+      {
+        status: isAbleToSign
+          ? TransactionStatus.WAITING_FOR_EXECUTION
+          : TransactionStatus.WAITING_FOR_SIGNATURES,
+      },
+    );
+  }
+
+  /* Checks if the signers are enough to sign the transactions and update their status */
+  async updateTransactions() {
+    const transactions = await this.transactionRepo.find({
+      where: {
+        status:
+          In[(TransactionStatus.WAITING_FOR_SIGNATURES, TransactionStatus.WAITING_FOR_EXECUTION)],
+        validStart: MoreThan(new Date(new Date().getTime() - 180 * 1_000)),
+      },
+    });
+
+    for (const transaction of transactions) {
+      /* Gets the SDK transaction from the transaction body */
+      const sdkTransaction = SDKTransaction.fromBytes(transaction.body);
+
+      /* Throws an error if the transaction is a file update/append transaction */
+      if (
+        sdkTransaction instanceof FileUpdateTransaction ||
+        sdkTransaction instanceof FileAppendTransaction
+      )
+        continue;
+
+      /* Gets the signature key */
+      const sigantureKey = await computeSignatureKey(sdkTransaction, this.mirrorNodeService);
+
+      /* Checks if the transaction has valid siganture */
+      const isAbleToSign = ableToSign([...sdkTransaction._signerPublicKeys], sigantureKey);
+
+      await this.transactionRepo.update(
+        {
+          id: transaction.id,
+        },
+        {
+          status: isAbleToSign
+            ? TransactionStatus.WAITING_FOR_EXECUTION
+            : TransactionStatus.WAITING_FOR_SIGNATURES,
+        },
+      );
+    }
   }
 }
