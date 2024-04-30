7050b91c6bce35a7b4fbc05f923f6c1c1d0a443c
Svetoslav Borislavov <svetoslav.borislavov@limechain.tech>
2024-04-30 14:26:28 +0300

    Contacts interface updated
    Signed-off-by: Svetoslav Borislavov <svetoslav.borislavov@limechain.tech>

--- front-end/src/main/shared/interfaces/Contacts.ts
+++ front-end/src/main/shared/interfaces/Contacts.ts
@@ -1,22 +1,8 @@
+import { IUser, IUserKey } from './organization';
+
 export interface Contact {
-  id: string;
-  user_id: string;
-  key_name: string;
-  email: string;
-  organization: string | null;
-  organization_user_id: string | null;
-  associated_accounts: AssociatedAccount[];
-  public_keys: ContactPublicKey[];
+  user: IUser;
+  userKeys: IUserKey[];
+  nickname: string;
 }
-
-export interface AssociatedAccount {
-  id: string;
-  contact_id: string;
-  account_id: string;
-}
-
-export interface ContactPublicKey {
-  id: string;
-  contact_id: string;
-  public_key: string;
-}
+[];
