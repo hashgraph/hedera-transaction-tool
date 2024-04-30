4f9031eed2608527e67278802025b077fa9a821b
Svetoslav Borislavov <svetoslav.borislavov@limechain.tech>
2024-04-30 14:26:28 +0300

    Contacts store updated
    Signed-off-by: Svetoslav Borislavov <svetoslav.borislavov@limechain.tech>

--- front-end/src/renderer/stores/storeContacts.ts
+++ front-end/src/renderer/stores/storeContacts.ts
@@ -1,147 +1,55 @@
 import { ref } from 'vue';
 import { defineStore } from 'pinia';
 
-import { AssociatedAccount, Contact, ContactPublicKey } from '@main/shared/interfaces';
+import { IUser, IUserKey } from '@main/shared/interfaces/organization';
 
 import useUserStore from './storeUser';
 
-import {
-  getOrganizationContacts,
-  getPersonalContacts,
-  addContact,
-  removeContact,
-  updateContact,
-} from '@renderer/services/contactsService';
-import {
-  getBackendContactPublicKeys,
-  getBackendOrganizationContacts,
-} from '@renderer/services/organization/contacts';
-import {
-  getContactPublicKeys,
-  addContactPublicKey,
-} from '@renderer/services/contactPublicKeysService';
-import { getAssociatedAccounts } from '@renderer/services/associatedAccountsService';
+import { getUserKeys, getUsers } from '@renderer/services/organization';
 
 import { isLoggedInOrganization, isUserLoggedIn } from '@renderer/utils/userStoreHelpers';
+import { getOrganizationContacts } from '@renderer/services/contactsService';
 
 const useContactsStore = defineStore('contacts', () => {
   const user = useUserStore();
 
   /* State */
-  const contacts = ref<Contact[]>(new Array<Contact>());
+  const contacts = ref<
+    {
+      user: IUser;
+      userKeys: IUserKey[];
+      nickname: string;
+    }[]
+  >([]);
 
   /* Actions */
   async function fetch() {
-    await new Promise(r => setTimeout(r, 2000));
+    if (!isUserLoggedIn(user.personal)) throw new Error('User is not logged in');
 
-    if (isUserLoggedIn(user.personal)) {
-      if (isLoggedInOrganization(user.selectedOrganization)) {
-        const backendContacts = await getBackendOrganizationContacts(
-          user.selectedOrganization.serverUrl,
-        );
-        const localContacts = await getOrganizationContacts(
-          user.personal.id,
-          user.selectedOrganization.nickname,
-        );
-        for (const contact of backendContacts) {
-          let localContact = localContacts.find(lc => lc.organization_user_id == contact.id);
-          if (contact?.id == user.selectedOrganization.userId) {
-            continue;
-          } else if (!localContact) {
-            localContact = await add(
-              user.personal.id,
-              contact.email,
-              contact.email,
-              user.selectedOrganization.nickname,
-              contact.id.toString(),
-              [],
-              [],
-            );
-          }
-          const backendPublicKeys = await getBackendContactPublicKeys(
-            user.selectedOrganization.serverUrl,
-            contact.id,
-          );
-          const localPublicKeys = await getContactPublicKeys(localContact.id);
-          for (const publicKey of backendPublicKeys) {
-            if (!localPublicKeys.find(lpk => lpk.public_key == publicKey.publicKey)) {
-              await addContactPublicKey(publicKey.publicKey, localContact.id);
-            }
-          }
-          const orgContacts = await getOrganizationContacts(
-            user.personal.id,
-            user.selectedOrganization.nickname,
-          );
-          contacts.value = new Array<Contact>();
-          for (const contact of orgContacts) {
-            const publicKeys = await getContactPublicKeys(contact.id);
-            contacts.value.push({
-              ...contact,
-              associated_accounts: [],
-              public_keys: publicKeys ? publicKeys : new Array<ContactPublicKey>(),
-            });
-          }
-        }
-      } else {
-        contacts.value = [];
-        const apiContacts = await getPersonalContacts(user.personal.id);
-        let associatedAccounts = new Array<AssociatedAccount>();
-        let publicKeys = new Array<ContactPublicKey>();
-        for (const contact of apiContacts) {
-          associatedAccounts = await getAssociatedAccounts(contact.id);
-          publicKeys = await getContactPublicKeys(contact.id);
-          contacts.value.push({
-            ...contact,
-            associated_accounts: associatedAccounts,
-            public_keys: publicKeys ? publicKeys : new Array<ContactPublicKey>(),
-          });
-        }
+    if (isLoggedInOrganization(user.selectedOrganization)) {
+      contacts.value = [];
+
+      const users = await getUsers(user.selectedOrganization.serverUrl);
+
+      const orgContacts = await getOrganizationContacts(
+        user.personal.id,
+        user.selectedOrganization.id,
+        user.selectedOrganization.userId,
+      );
+
+      for (const orgUser of users) {
+        const userKeys = await getUserKeys(user.selectedOrganization.serverUrl, orgUser.id);
+        contacts.value.push({
+          user: orgUser,
+          userKeys: userKeys,
+          nickname: orgContacts.find(c => c.organization_user_id === orgUser.id)?.nickname || '',
+        });
       }
     }
   }
 
-  async function add(
-    userId: string,
-    keyName: string,
-    email: string,
-    organization: string | null,
-    organizationUserId: string | null,
-    associatedAccounts: string[],
-    publicKeys: string[],
-  ) {
-    return await addContact(
-      {
-        id: '',
-        user_id: userId,
-        key_name: keyName,
-        email,
-        public_keys: [],
-        organization,
-        associated_accounts: [],
-        organization_user_id: organizationUserId,
-      },
-      associatedAccounts.map(acc => {
-        return { id: '', contact_id: '', account_id: acc };
-      }),
-      publicKeys.map(pk => {
-        return { id: '', contact_id: '', public_key: pk };
-      }),
-    );
-  }
-
-  async function remove(userId: string, id: string) {
-    await removeContact(userId, id);
-  }
-
-  async function edit(id: string, userId: string, keyName: string) {
-    await updateContact(id, userId, { key_name: keyName });
-  }
-
   return {
     contacts,
-    add,
-    remove,
-    edit,
     fetch,
   };
 });
