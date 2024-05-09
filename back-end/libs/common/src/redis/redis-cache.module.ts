281b618f6bf047b8ff9456041ff4f39a9c137b04
Svetoslav Borislavov <svetoslav.borislavov@limechain.tech>
2024-05-09 13:36:42 +0300

    Redis cache module created
    Signed-off-by: Svetoslav Borislavov <svetoslav.borislavov@limechain.tech>

---
+++ back-end/libs/common/src/redis/redis-cache.module.ts
@@ -0,0 +1,19 @@
+import { Module } from '@nestjs/common';
+import { CacheModule } from '@nestjs/cache-manager';
+
+import * as redisStore from 'cache-manager-redis-store';
+
+/* Not using registerAsync because it is not providing CACHE_MANAGER */
+@Module({
+  imports: [
+    CacheModule.register({
+      isGlobal: true,
+      store: redisStore,
+      host: process.env.REDIS_HOST,
+      port: process.env.REDIS_PORT,
+      username: process.env.REDIS_USERNAME,
+      password: process.env.REDIS_PASSWORD,
+    }),
+  ],
+})
+export class RedisCacheModule {}
