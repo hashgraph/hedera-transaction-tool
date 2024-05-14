31ce5b0426a34585207e30956c903e432ad1a4ba
John Bair <john.bair@swirldslabs.com>
2024-05-14 16:54:00 -0700

    Added param to allow for standard remove (specifically used by transaction group deletes).
    Signed-off-by: John Bair <john.bair@swirldslabs.com>

--- back-end/apps/api/src/transactions/transactions.service.ts
+++ back-end/apps/api/src/transactions/transactions.service.ts
@@ -349,7 +349,7 @@ export class TransactionsService {
   }
 
   /* Remove the transaction for the given transaction id. */
-  async removeTransaction(id: number, user: UserDto): Promise<boolean> {
+  async removeTransaction(user: UserDto, id: number, softRemove: boolean = true): Promise<boolean> {
     const transaction = await this.getTransactionById(id);
 
     if (!transaction) {
@@ -360,7 +360,11 @@ export class TransactionsService {
       throw new BadRequestException('Only the creator of the transaction is able to delete it');
     }
 
-    await this.repo.softRemove(transaction);
+    if (softRemove) {
+      await this.repo.softRemove(transaction);
+    } else {
+      await this.repo.remove(transaction);
+    }
 
     return true;
   }
