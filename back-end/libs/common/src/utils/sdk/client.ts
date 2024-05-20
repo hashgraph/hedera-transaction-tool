076dc9c2f00ecfd76694e2b2304a91ac22b3f08e
Svetoslav Borislavov <svetoslav.borislavov@limechain.tech>
2024-05-20 14:06:18 +0300

    Get client from name util added
    Signed-off-by: Svetoslav Borislavov <svetoslav.borislavov@limechain.tech>

--- back-end/libs/common/src/utils/sdk/client.ts
+++ back-end/libs/common/src/utils/sdk/client.ts
@@ -1,5 +1,10 @@
 import { ConfigService } from '@nestjs/config';
+
 import { Client } from '@hashgraph/sdk';
+
+import { Network } from '.';
 
 export const getClientFromConfig = (config: ConfigService) =>
   Client.forName(config.get('HEDERA_NETWORK'));
+
+export const getClientFromName = (network: Network) => Client.forName(network);
