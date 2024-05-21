90698d7b21bc6ace6f7f60d26d4e2e66e354ba01
Martin Dobrev <martindobrev87@gmail.com>
2024-05-21 11:00:46 +0300

    Active state added on Settings tabs
    Signed-off-by: Martin Dobrev <martindobrev87@gmail.com>

--- front-end/src/renderer/pages/Settings/components/KeysTab.vue
+++ front-end/src/renderer/pages/Settings/components/KeysTab.vue
@@ -177,12 +177,13 @@ watch(isDeleteModalShown, newVal => {
 <template>
   <div class="flex-column-100">
     <div class="mb-3">
-      <template v-for="tab in Object.values(Tabs)" :key="tab">
+      <template v-for="(tab, i) in Object.values(Tabs)" :key="tab">
         <AppButton
           :data-testid="`tab-${tab}`"
           type="button"
           color="borderless"
           class="min-w-unset"
+          :class="[tab === currentTab ? 'active' : '', i !== 0 ? 'ms-3' : '']"
           @click="handleTabChange(tab)"
         >
           {{ tab }}
