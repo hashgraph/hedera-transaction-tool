cdf84ead40e6f4dad2b44b3093f934d4ab05b1bc
Svetoslav Borislavov <svetoslav.borislavov@limechain.tech>
2024-05-30 19:52:53 +0300

    useDisposableWs created
    Signed-off-by: Svetoslav Borislavov <svetoslav.borislavov@limechain.tech>

---
+++ front-end/src/renderer/composables/useDisposableWs.ts
@@ -0,0 +1,23 @@
+import { onBeforeUnmount, onMounted, ref } from 'vue';
+
+import useWebsocketStore from '@renderer/stores/storeWebsocketConnection';
+
+export default function useDisposableWs() {
+  /* State */
+  const ws = useWebsocketStore();
+  const disposers = ref<(() => void)[]>([]);
+
+  /* Methods */
+  function on(event: string, callback: (data: any) => void) {
+    disposers.value.push(ws.on(event, callback));
+  }
+
+  /* Hooks */
+  onMounted(async () => {});
+
+  onBeforeUnmount(() => {
+    disposers.value.forEach(dispose => dispose());
+  });
+
+  return { on };
+}
