cd0204cbdb84dff749070fb67141a56b90a6d3d0
Svetoslav Borislavov <svetoslav.borislavov@limechain.tech>
2024-05-30 18:15:51 +0300

    GET_PORT message pattern constant added
    Signed-off-by: Svetoslav Borislavov <svetoslav.borislavov@limechain.tech>

--- back-end/apps/notifications/src/websocket/websocket.controller.ts
+++ back-end/apps/notifications/src/websocket/websocket.controller.ts
@@ -1,17 +1,26 @@
-import { EventPattern, Payload } from '@nestjs/microservices';
+import { EventPattern, MessagePattern, Payload } from '@nestjs/microservices';
+import { ConfigService } from '@nestjs/config';
 import { Controller } from '@nestjs/common';
 
-import { NOTIFY_CLIENT, NotifyClientDto } from '@app/common';
+import { GET_PORT, NOTIFY_CLIENT, NotifyClientDto } from '@app/common';
 
 import { WebsocketGateway } from './websocket.gateway';
 
 @Controller('websocket')
 export class WebsocketController {
-  constructor(private readonly websocketGateway: WebsocketGateway) {}
+  constructor(
+    private readonly websocketGateway: WebsocketGateway,
+    private readonly configService: ConfigService,
+  ) {}
 
   //TODO Need to make sure that Rabbitmq is sending this out to all of them, not just one
   @EventPattern(NOTIFY_CLIENT)
   async notifyClient(@Payload() payload: NotifyClientDto) {
     return this.websocketGateway.notifyClient(payload);
+  }
+
+  @MessagePattern(GET_PORT)
+  async getPort() {
+    return this.configService.get<number>('HTTP_PORT');
   }
 }
--- back-end/libs/common/src/constants/eventPatterns.ts
+++ back-end/libs/common/src/constants/eventPatterns.ts
@@ -1,1 +1,2 @@
 export const NOTIFY_CLIENT = 'notify_client';
+export const GET_PORT = 'get_port';
