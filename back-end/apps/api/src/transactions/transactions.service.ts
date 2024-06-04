c1dd012b297825373e4cea5fc0f82daa2246f320
Svetoslav Borislavov <svetoslav.borislavov@limechain.tech>
2024-06-04 10:54:21 +0300

    Get transactions to sign minor update
    Signed-off-by: Svetoslav Borislavov <svetoslav.borislavov@limechain.tech>

--- back-end/apps/api/src/transactions/transactions.service.ts
+++ back-end/apps/api/src/transactions/transactions.service.ts
@@ -15,7 +15,7 @@ import {
   Transaction as SDKTransaction,
 } from '@hashgraph/sdk';
 
-import { Repository, MoreThan, EntityManager, FindManyOptions, Brackets } from 'typeorm';
+import { Repository, EntityManager, FindManyOptions, Brackets } from 'typeorm';
 
 import { Transaction, TransactionSigner, TransactionStatus, User, UserKey } from '@entities';
 
@@ -156,13 +156,17 @@ export class TransactionsService {
     user: User,
     { page, limit, size, offset }: Pagination,
     sort?: Sorting[],
+    filter?: Filtering[],
   ): Promise<
     PaginatedResourceDto<{
       transaction: Transaction;
       keysToSign: number[];
     }>
   > {
-    let result: {
+    const where = getWhere<Transaction>(filter);
+    const order = getOrder(sort);
+
+    const result: {
       transaction: Transaction;
       keysToSign: number[];
     }[] = [];
@@ -180,9 +184,8 @@ export class TransactionsService {
     }
 
     const transactions = await this.repo.find({
-      where: {
-        validStart: MoreThan(new Date(new Date().getTime() - 180 * 1_000)),
-      },
+      where,
+      order,
     });
 
     for (const transaction of transactions) {
@@ -190,18 +193,6 @@ export class TransactionsService {
       const keysToSign = await this.userKeysToSign(transaction, user);
 
       if (keysToSign.length > 0) result.push({ transaction, keysToSign });
-    }
-
-    if (sort && sort.length) {
-      result = result.sort((a, b) => {
-        for (const { property, direction } of sort) {
-          if (a.transaction[property] < b.transaction[property])
-            return direction === 'asc' ? -1 : 1;
-          if (a.transaction[property] > b.transaction[property])
-            return direction === 'asc' ? 1 : -1;
-        }
-        return 0;
-      });
     }
 
     return {
