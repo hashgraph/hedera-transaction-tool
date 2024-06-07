a4e9d99dcfb122763f8758dd73dcd2d5e2fe370f
Svetoslav Borislavov <svetoslav.borislavov@limechain.tech>
2024-06-07 16:06:15 +0300

    Fix transaction details transaction id visualization (#492)
    Signed-off-by: Svetoslav Borislavov <svetoslav.borislavov@limechain.tech>

--- front-end/src/renderer/pages/TransactionDetails/compontents/AccountApproveAllowanceDetails.vue
+++ front-end/src/renderer/pages/TransactionDetails/compontents/AccountApproveAllowanceDetails.vue
@@ -20,7 +20,7 @@ onBeforeMount(() => {
 const approvalHeadingClass = 'text-subheader text-dark-blue mt-5';
 const detailItemLabelClass = 'text-micro text-semi-bold text-dark-blue';
 const detailItemValueClass = 'text-small overflow-hidden mt-1';
-const commonColClass = 'col-6 col-md-5 col-lg-4 col-xl-3 py-3';
+const commonColClass = 'col-6 col-lg-5 col-xl-4 col-xxl-3 overflow-hidden py-3';
 </script>
 <template>
   <div v-if="transaction instanceof AccountAllowanceApproveTransaction && true" class="mt-5">
