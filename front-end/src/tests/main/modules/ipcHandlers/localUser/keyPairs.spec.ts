8a530c137ad6cb270bae25e68aa54020b9723291
Svetoslav Borislavov <svetoslav.borislavov@limechain.tech>
2024-06-13 09:40:16 +0300

    Key pairs module tests added
    Signed-off-by: Svetoslav Borislavov <svetoslav.borislavov@limechain.tech>

---
+++ front-end/src/tests/main/modules/ipcHandlers/localUser/keyPairs.spec.ts
@@ -0,0 +1,165 @@
+import { ipcMain } from 'electron';
+import { Prisma } from '@prisma/client';
+
+import registerKeyPairsHandlers from '@main/modules/ipcHandlers/localUser/keyPairs';
+import {
+  storeKeyPair,
+  deleteSecretHashes,
+  changeDecryptionPassword,
+  decryptPrivateKey,
+  deleteEncryptedPrivateKeys,
+  getKeyPairs,
+  getSecretHashes,
+  deleteKeyPair,
+} from '@main/services/localUser';
+import { MockedObject } from 'vitest';
+import { mockDeep } from 'vitest-mock-extended';
+
+vi.mock('electron', () => ({
+  ipcMain: { handle: vi.fn() },
+}));
+
+vi.mock('@main/services/localUser', () => ({
+  storeKeyPair: vi.fn(),
+  deleteSecretHashes: vi.fn(),
+  changeDecryptionPassword: vi.fn(),
+  decryptPrivateKey: vi.fn(),
+  deleteEncryptedPrivateKeys: vi.fn(),
+  getKeyPairs: vi.fn(),
+  getSecretHashes: vi.fn(),
+  deleteKeyPair: vi.fn(),
+}));
+
+describe('IPC handlers Key Pairs', () => {
+  beforeEach(() => {
+    vi.clearAllMocks();
+    registerKeyPairsHandlers();
+  });
+
+  const ipcMainMO = ipcMain as unknown as MockedObject<Electron.IpcMain>;
+  const event: Electron.IpcMainEvent = mockDeep<Electron.IpcMainEvent>();
+
+  test('Should register handlers for each event', () => {
+    const events = [
+      'store',
+      'changeDecryptionPassword',
+      'decryptPrivateKey',
+      'getAll',
+      'getSecretHashes',
+      'deleteEncryptedPrivateKeys',
+      'clear',
+    ];
+
+    expect(
+      events.every(util =>
+        ipcMainMO.handle.mock.calls.some(([channel]) => channel === `keyPairs:${util}`),
+      ),
+    ).toBe(true);
+  });
+
+  test('Should set up store key pair handler', async () => {
+    const storeHandler = ipcMainMO.handle.mock.calls.find(([e]) => e === 'keyPairs:store');
+    expect(storeHandler).toBeDefined();
+
+    const keyPair: Prisma.KeyPairUncheckedCreateInput = {
+      public_key: 'publicKey',
+      private_key: 'privateKey',
+      user_id: 'userId',
+      type: 'type',
+      index: 1,
+    };
+    const password = 'password';
+    const encrypted = true;
+
+    storeHandler && (await storeHandler[1](event, keyPair, password, encrypted));
+    expect(storeKeyPair).toHaveBeenCalledWith(keyPair, password, encrypted);
+  });
+
+  test('Should set up changeDecryptionPassword handler', async () => {
+    const handler = ipcMainMO.handle.mock.calls.find(
+      ([e]) => e === 'keyPairs:changeDecryptionPassword',
+    );
+    expect(handler).toBeDefined();
+
+    const userId = 'userId';
+    const oldPassword = 'oldPassword';
+    const newPassword = 'newPassword';
+
+    handler && (await handler[1](event, userId, oldPassword, newPassword));
+    expect(changeDecryptionPassword).toHaveBeenCalledWith(userId, oldPassword, newPassword);
+  });
+
+  test('Should set up decryptPrivateKey handler', async () => {
+    const handler = ipcMainMO.handle.mock.calls.find(([e]) => e === 'keyPairs:decryptPrivateKey');
+    expect(handler).toBeDefined();
+
+    const userId = 'userId';
+    const password = 'password';
+    const publicKey = 'publicKey';
+
+    handler && (await handler[1](event, userId, password, publicKey));
+    expect(decryptPrivateKey).toHaveBeenCalledWith(userId, password, publicKey);
+  });
+
+  test('Should set up getAll handler', async () => {
+    const handler = ipcMainMO.handle.mock.calls.find(([e]) => e === 'keyPairs:getAll');
+    expect(handler).toBeDefined();
+
+    const userId = 'userId';
+    const organizationId = 'organizationId';
+
+    handler && (await handler[1](event, userId, organizationId));
+    expect(getKeyPairs).toHaveBeenCalledWith(userId, organizationId);
+  });
+
+  test('Should set up getSecretHashes handler', async () => {
+    const handler = ipcMainMO.handle.mock.calls.find(([e]) => e === 'keyPairs:getSecretHashes');
+    expect(handler).toBeDefined();
+
+    const userId = 'userId';
+    const organizationId = 'organizationId';
+
+    handler && (await handler[1](event, userId, organizationId));
+    expect(getSecretHashes).toHaveBeenCalledWith(userId, organizationId);
+  });
+
+  test('Should set up deleteEncryptedPrivateKeys handler', async () => {
+    const handler = ipcMainMO.handle.mock.calls.find(
+      ([e]) => e === 'keyPairs:deleteEncryptedPrivateKeys',
+    );
+    expect(handler).toBeDefined();
+
+    const userId = 'userId';
+    const organizationId = 'organizationId';
+
+    handler && (await handler[1](event, userId, organizationId));
+    expect(deleteEncryptedPrivateKeys).toHaveBeenCalledWith(userId, organizationId);
+  });
+
+  test('Should set up deleteKeyPair handler', async () => {
+    const handler = ipcMainMO.handle.mock.calls.find(([e]) => e === 'keyPairs:deleteKeyPair');
+    expect(handler).toBeDefined();
+
+    const keyPairId = 'keyPairId';
+
+    handler && (await handler[1](event, keyPairId));
+    expect(deleteKeyPair).toHaveBeenCalledWith(keyPairId);
+  });
+
+  test('Should set up clear handler', async () => {
+    const handler = ipcMainMO.handle.mock.calls.find(([e]) => e === 'keyPairs:clear');
+    expect(handler).toBeDefined();
+
+    const userId = 'userId';
+    const organizationId = 'organizationId';
+
+    if (!handler) return;
+
+    await handler[1](event, userId, organizationId);
+    expect(deleteSecretHashes).toHaveBeenCalledWith(userId, organizationId);
+
+    vi.mocked(deleteSecretHashes).mockRejectedValueOnce(new Error('Error'));
+    const result = await handler[1](event, userId, organizationId);
+    expect(result).toBe(false);
+  });
+});
