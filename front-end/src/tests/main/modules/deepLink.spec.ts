058569472d5ab0fcbf22351b2447585dc83dcf06
Svetoslav Borislavov <svetoslav.borislavov@limechain.tech>
2024-06-13 09:40:16 +0300

    Deep link module tests added
    Signed-off-by: Svetoslav Borislavov <svetoslav.borislavov@limechain.tech>

---
+++ front-end/src/tests/main/modules/deepLink.spec.ts
@@ -0,0 +1,48 @@
+import { mockDeep } from 'vitest-mock-extended';
+
+import deepLink, { PROTOCOL_NAME } from '@main/modules/deepLink';
+
+import { BrowserWindow } from 'electron';
+
+vi.mock('electron', () => ({
+  BrowserWindow: vi.fn(),
+}));
+
+describe('deepLink', () => {
+  let window: BrowserWindow;
+  let event: { preventDefault: () => void; readonly defaultPrevented: boolean };
+
+  beforeEach(() => {
+    window = mockDeep<BrowserWindow>({
+      webContents: {
+        send: vi.fn(),
+      },
+      show: vi.fn(),
+    });
+    event = {
+      preventDefault: vi.fn(),
+      defaultPrevented: false,
+    };
+  });
+
+  test('Should prevent default event and send otp token if url contains token', () => {
+    const token = 'testToken';
+    const url = `${PROTOCOL_NAME}://token=${token}`;
+
+    deepLink(window, event, url);
+
+    expect(event.preventDefault).toHaveBeenCalled();
+    expect(window.webContents.send).toHaveBeenCalledWith('deepLink:otp', token);
+    expect(window.show).toHaveBeenCalled();
+  });
+
+  test('Should prevent default event but not send otp token if url does not contain token', () => {
+    const url = `${PROTOCOL_NAME}://`;
+
+    deepLink(window, event, url);
+
+    expect(event.preventDefault).toHaveBeenCalled();
+    expect(window.webContents.send).not.toHaveBeenCalled();
+    expect(window.show).not.toHaveBeenCalled();
+  });
+});
