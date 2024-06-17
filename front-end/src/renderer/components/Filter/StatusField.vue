292429909ba536acbf21433897d70c728413a97c
Svetoslav Borislavov <svetoslav.borislavov@limechain.tech>
2024-06-17 14:28:50 +0300

    feat: status field realized with multiple select field
    Signed-off-by: Svetoslav Borislavov <svetoslav.borislavov@limechain.tech>

--- front-end/src/renderer/components/Filter/StatusField.vue
+++ front-end/src/renderer/components/Filter/StatusField.vue
@@ -3,8 +3,7 @@ import { computed } from 'vue';
 
 import { ITransaction, TransactionStatus } from '@main/shared/interfaces';
 
-import AppButton from '@renderer/components/ui/AppButton.vue';
-import AppCheckBox from '@renderer/components/ui/AppCheckBox.vue';
+import MultipleSelectFilterField from './MultipleSelectFilterField.vue';
 
 /* Props */
 const props = defineProps<{
@@ -13,6 +12,7 @@ const props = defineProps<{
     rule: string;
     value: string | string[];
   } | null;
+  inline?: boolean;
   history?: boolean;
 }>();
 
@@ -24,48 +24,6 @@ const selectedStatuses = computed(() => {
   if (!props.filter) return [];
   return Array.isArray(props.filter.value) ? props.filter.value : [props.filter.value];
 });
-
-/* Handlers */
-const handleSelectStatus = (status: TransactionStatus) => {
-  if (selectedStatuses.value.includes(status)) {
-    removeStatus(status);
-  } else {
-    addStatus(status);
-  }
-};
-
-/* Functions */
-function addStatus(status: TransactionStatus) {
-  if (selectedStatuses.value.length === 0 || !props.filter) {
-    emit('update:filter', {
-      property: 'status',
-      rule: 'in',
-      value: [status],
-    });
-  } else {
-    emit('update:filter', {
-      ...props.filter,
-      value: [...props.filter.value, status],
-    });
-  }
-}
-
-function removeStatus(status: TransactionStatus) {
-  if (!selectedStatuses.value.includes(status) || !props.filter) return;
-
-  let newFilter;
-
-  if (selectedStatuses.value.length === 1) {
-    emit('update:filter', null);
-  } else {
-    newFilter = {
-      ...props.filter,
-      value: selectedStatuses.value.filter(f => f !== status),
-    };
-  }
-
-  emit('update:filter', newFilter);
-}
 
 /* Misc */
 const allowedStatuses = [
@@ -85,45 +43,21 @@ const statuses = Object.values(TransactionStatus)
   }));
 </script>
 <template>
-  <div class="flex-centered justify-content-between gap-4">
-    <p>Status</p>
-    <div class="dropdown">
-      <AppButton
-        class="text-dark-emphasis border border-1 w-100 px-3 py-1"
-        type="button"
-        data-bs-toggle="dropdown"
-        data-bs-auto-close="outside"
-      >
-        <span class="overflow-hidden">
+  <MultipleSelectFilterField
+    :filter="filter"
+    @update:filter="emit('update:filter', $event)"
+    :property="'status'"
+    :label="'Status'"
+    :items="statuses"
+    :inline="inline"
+  >
+    <template #dropdown-toggler-content>
+      <span class="flex-centered">
+        <template v-if="inline"> <i class="bi bi-filter text-subheader me-1"></i>Status </template>
+        <template v-else>
           {{ selectedStatuses.length > 0 ? 'Select Statuses' : 'All' }}
-        </span>
-      </AppButton>
-      <ul class="dropdown-menu text-small mt-2">
-        <template v-for="status in statuses" :key="status">
-          <div class="d-flex align-items-center mt-2">
-            <div
-              class="visible-on-hover activate-on-sibling-hover"
-              :selected="selectedStatuses.includes(status.value) ? true : undefined"
-            >
-              <AppCheckBox
-                :checked="selectedStatuses.includes(status.value)"
-                @update:checked="handleSelectStatus(status.value)"
-                name="select-transaction-status"
-                class="cursor-pointer"
-              />
-            </div>
-            <div
-              class="dropdown-item container-multiple-select activate-on-sibling-hover overflow-hidden w-100 p-3"
-              :class="{
-                'is-selected': selectedStatuses.includes(status.value),
-              }"
-              @click="handleSelectStatus(status.value)"
-            >
-              <p class="text-small text-semi-bold overflow-hidden">{{ status.label }}</p>
-            </div>
-          </div>
         </template>
-      </ul>
-    </div>
-  </div>
+      </span>
+    </template>
+  </MultipleSelectFilterField>
 </template>
