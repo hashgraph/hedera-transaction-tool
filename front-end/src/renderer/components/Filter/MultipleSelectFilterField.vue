180db8309df4ca70eff92ed10423dd8c37ffcc99
Svetoslav Borislavov <svetoslav.borislavov@limechain.tech>
2024-06-17 14:28:50 +0300

    feat: remove filter button added
    Signed-off-by: Svetoslav Borislavov <svetoslav.borislavov@limechain.tech>

--- front-end/src/renderer/components/Filter/MultipleSelectFilterField.vue
+++ front-end/src/renderer/components/Filter/MultipleSelectFilterField.vue
@@ -37,6 +37,11 @@ const handleSelectItem = (item: string) => {
   }
 };
 
+const handleRemoveFilter = e => {
+  e.stopPropagation();
+  emit('update:filter', null);
+};
+
 /* Functions */
 function addItem(item: string) {
   if (selectedItems.value.length === 0 || !props.filter) {
@@ -71,60 +76,73 @@ function removeItem(item: string) {
 }
 </script>
 <template>
-  <div class="flex-centered justify-content-between gap-4">
-    <p v-if="!inline">{{ label }}</p>
-    <div class="dropdown">
-      <AppButton
-        class="text-dark-emphasis w-100 border border-1 px-3 py-1 min-w-unset"
-        type="button"
-        data-bs-toggle="dropdown"
-        data-bs-auto-close="outside"
-      >
-        <slot name="dropdown-toggler-content">
-          <span class="flex-centered">
-            <template v-if="inline">
-              <i class="bi bi-filter text-subheader me-1"></i>{{ label }}
-              <span
-                v-if="selectedItems.length > 0"
-                class="bubble text-footnote ms-2"
-                :style="{ width: '16px', height: '16px' }"
-                >{{ selectedItems.length }}</span
-              >
-            </template>
-            <template v-else>
-              {{ selectedItems.length > 0 ? `${selectedItems.length} Selected` : 'All' }}
-            </template>
-          </span>
-        </slot>
-      </AppButton>
-      <ul class="dropdown-menu text-small mt-2">
-        <div class="overflow-auto" :style="{ maxHeight: '35vh' }">
-          <template v-for="item in items" :key="item">
-            <div class="d-flex align-items-center mt-2">
-              <div
-                class="visible-on-hover activate-on-sibling-hover"
-                :selected="selectedItems.includes(item.value) ? true : undefined"
-              >
-                <AppCheckBox
-                  :checked="selectedItems.includes(item.value)"
-                  @update:checked="handleSelectItem(item.value)"
-                  name="select-transaction-item"
-                  class="cursor-pointer"
-                />
-              </div>
-              <div
-                class="dropdown-item container-multiple-select activate-on-sibling-hover overflow-hidden w-100 p-3"
-                :class="{
-                  'is-selected': selectedItems.includes(item.value),
-                }"
-                @click="handleSelectItem(item.value)"
-              >
-                <p class="text-small text-semi-bold overflow-hidden">{{ item.label }}</p>
+  <div class="d-flex gap-3">
+    <div class="flex-centered justify-content-between gap-4">
+      <p v-if="!inline">{{ label }}</p>
+      <div class="rounded border border-1 flex-centered justify-content-between px-3 py-1">
+        <div class="dropdown">
+          <AppButton
+            class="text-dark-emphasis border-0 w-100 p-0 min-w-unset"
+            type="button"
+            data-bs-toggle="dropdown"
+            data-bs-auto-close="outside"
+            data-bs-offset="-8,5"
+          >
+            <slot name="dropdown-toggler-content">
+              <span class="flex-centered">
+                <template v-if="inline">
+                  <i class="bi bi-filter text-subheader me-1"></i>{{ label }}
+                </template>
+                <template v-else>
+                  {{ selectedItems.length > 0 ? `${selectedItems.length} Selected` : 'All' }}
+                </template>
+              </span>
+            </slot>
+          </AppButton>
+          <ul class="dropdown-menu text-small mt-2">
+            <div class="overflow-auto pe-3" :style="{ maxHeight: '35vh' }">
+              <div class="d-flex flex-column gap-2">
+                <template v-for="item in items" :key="item">
+                  <div class="d-flex align-items-center">
+                    <div
+                      v-if="selectedItems.length > 0"
+                      class="visible-on-hover activate-on-sibling-hover"
+                      :selected="selectedItems.includes(item.value) ? true : undefined"
+                    >
+                      <AppCheckBox
+                        :checked="selectedItems.includes(item.value)"
+                        @update:checked="handleSelectItem(item.value)"
+                        name="select-transaction-item"
+                        class="cursor-pointer"
+                      />
+                    </div>
+                    <div
+                      class="dropdown-item container-multiple-select activate-on-sibling-hover overflow-hidden w-100 p-3 mt-0"
+                      :class="{
+                        'is-selected': selectedItems.includes(item.value),
+                      }"
+                      @click="handleSelectItem(item.value)"
+                    >
+                      <p class="text-small text-semi-bold overflow-hidden">{{ item.label }}</p>
+                    </div>
+                  </div>
+                </template>
               </div>
             </div>
-          </template>
+          </ul>
         </div>
-      </ul>
+        <span
+          v-if="selectedItems.length > 0 && inline"
+          class="bubble text-footnote ms-2"
+          :style="{ width: '16px', height: '16px' }"
+          >{{ selectedItems.length }}</span
+        >
+        <span
+          v-if="selectedItems.length > 0"
+          class="bi bi-x-lg cursor-pointer ms-2"
+          @click="handleRemoveFilter"
+        ></span>
+      </div>
     </div>
   </div>
 </template>
