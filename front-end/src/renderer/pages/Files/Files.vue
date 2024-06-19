fbccab6c9d7e24a054272de4473234847d76c3d6
Svetoslav Borislavov <svetoslav.borislavov@limechain.tech>
2024-06-19 10:43:37 +0300

    feat: change sort labels in files
    Signed-off-by: Svetoslav Borislavov <svetoslav.borislavov@limechain.tech>

--- front-end/src/renderer/pages/Files/Files.vue
+++ front-end/src/renderer/pages/Files/Files.vue
@@ -412,14 +412,14 @@ watch(files, newFiles => {
                   :selected="sorting.file_id === 'asc' ? true : undefined"
                   @click="handleSortFiles('file_id', 'asc')"
                 >
-                  File ID A-Z
+                  File ID Asc
                 </li>
                 <li
                   class="dropdown-item"
                   :selected="sorting.account_id === 'desc' ? true : undefined"
                   @click="handleSortFiles('file_id', 'desc')"
                 >
-                  File ID Z-A
+                  File ID Dsc
                 </li>
                 <li
                   class="dropdown-item"
@@ -440,14 +440,14 @@ watch(files, newFiles => {
                   :selected="sorting.created_at === 'asc' ? true : undefined"
                   @click="handleSortFiles('created_at', 'asc')"
                 >
-                  Added At A-Z
+                  Date Added Asc
                 </li>
                 <li
                   class="dropdown-item"
                   :selected="sorting.created_at === 'desc' ? true : undefined"
                   @click="handleSortFiles('created_at', 'desc')"
                 >
-                  Added At Z-A
+                  Date Added Dsc
                 </li>
               </ul>
             </div>
