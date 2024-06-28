14159c0abb060a13aa6dc59a9e7354dfcbbd30f8
John Bair <john.bair@swirldslabs.com>
2024-06-28 11:33:30 -0700

    users.controller tests
    Signed-off-by: John Bair <john.bair@swirldslabs.com>

--- back-end/apps/api/src/users/users.controller.spec.ts
+++ back-end/apps/api/src/users/users.controller.spec.ts
@@ -1,5 +1,10 @@
 import { Test, TestingModule } from '@nestjs/testing';
 import { UsersController } from './users.controller';
+import { UsersService } from './users.service';
+import { UserStatus } from '@entities';
+import { GetUser } from '../decorators';
+
+const GetUserMock = jest.fn(() => ({}));
 
 describe('UsersController', () => {
   let controller: UsersController;
@@ -7,12 +12,131 @@ describe('UsersController', () => {
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
       controllers: [UsersController],
-    }).compile();
+      providers: [
+        {
+          provide: UsersService,
+          useValue: {
+            create: jest.fn(),
+            findAll: jest.fn(),
+            findOne: jest.fn(),
+            update: jest.fn(),
+            remove: jest.fn(),
+          }
+        },
+      ]
+    }).overrideProvider(GetUser).useValue(GetUserMock).compile();
 
     controller = module.get<UsersController>(UsersController);
   });
 
   it('should be defined', () => {
     expect(controller).toBeDefined();
+  });
+
+  it('should return an array of users', async () => {
+    const result = [
+      {
+        id: 1,
+        email: 'John@test.com',
+        password: 'Doe',
+        admin: true,
+        status: UserStatus.NONE,
+        createdAt: new Date(),
+        updatedAt: new Date(),
+        deletedAt: new Date(),
+        keys: [],
+        signerForTransactions: [],
+        observableTransactions: [],
+        approvableTransactions: [],
+        comments: []
+      }
+    ];
+    jest.spyOn(controller, 'getUsers').mockResolvedValue(result);
+
+    expect(await controller.getUsers()).toBe(result);
+  });
+
+  it('should return a user', async () => {
+    const result = {
+      id: 1,
+      email: 'John@test.com',
+      password: 'Doe',
+      admin: true,
+      status: UserStatus.NONE,
+      createdAt: new Date(),
+      updatedAt: new Date(),
+      deletedAt: new Date(),
+      keys: [],
+      signerForTransactions: [],
+      observableTransactions: [],
+      approvableTransactions: [],
+      comments: []
+    };
+    jest.spyOn(controller, 'getUser').mockResolvedValue(result);
+
+    expect(await controller.getUser(1)).toBe(result);
+  });
+
+  // it('should return the current user', async () => {
+  //   const result = {
+  //     id: 1,
+  //     email: 'John@test.com',
+  //     password: 'Doe',
+  //     admin: true,
+  //     status: UserStatus.NONE,
+  //     createdAt: new Date(),
+  //     updatedAt: new Date(),
+  //     deletedAt: new Date(),
+  //     keys: [],
+  //     signerForTransactions: [],
+  //     observableTransactions: [],
+  //     approvableTransactions: [],
+  //     comments: []
+  //   };
+  //   jest.spyOn(controller, 'getMe').mockResolvedValue(result);
+  //
+  //   expect(await controller.getMe(result)).toBe(result);
+  // });
+
+  it('should return the updated user', async () => {
+    const result = {
+      id: 1,
+      email: 'John@test.com',
+      password: 'Doe',
+      admin: true,
+      status: UserStatus.NONE,
+      createdAt: new Date(),
+      updatedAt: new Date(),
+      deletedAt: new Date(),
+      keys: [],
+      signerForTransactions: [],
+      observableTransactions: [],
+      approvableTransactions: [],
+      comments: []
+    };
+    jest.spyOn(controller, 'updateUser').mockResolvedValue(result);
+
+    expect(await controller.updateUser(1, result)).toBe(result);
+  });
+
+  it('should remove a user', async () => {
+    const result = {
+      id: 1,
+      email: 'John@test.com',
+      password: 'Doe',
+      admin: true,
+      status: UserStatus.NONE,
+      createdAt: new Date(),
+      updatedAt: new Date(),
+      deletedAt: new Date(),
+      keys: [],
+      signerForTransactions: [],
+      observableTransactions: [],
+      approvableTransactions: [],
+      comments: []
+    };
+    jest.spyOn(controller, 'removeUser').mockResolvedValue(result);
+
+    expect(await controller.removeUser(1)).toBe(result);
   });
 });
