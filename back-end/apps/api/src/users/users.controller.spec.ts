5517fbfec265c2231934589266135c20c1ce419e
John Bair <john.bair@swirldslabs.com>
2024-07-01 14:03:43 -0700

    Added remaining tests for the Users controller
    Signed-off-by: John Bair <john.bair@swirldslabs.com>

--- back-end/apps/api/src/users/users.controller.spec.ts
+++ back-end/apps/api/src/users/users.controller.spec.ts
@@ -1,10 +1,12 @@
 import { Test, TestingModule } from '@nestjs/testing';
 import { UsersController } from './users.controller';
 import { UsersService } from './users.service';
-import { UserStatus } from '@entities';
+import { User, UserStatus } from '@entities';
+import { describe } from 'node:test';
 
 describe('UsersController', () => {
   let controller: UsersController;
+  let user: User;
 
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
@@ -24,95 +26,112 @@ describe('UsersController', () => {
     }).compile();
 
     controller = module.get<UsersController>(UsersController);
+    user = {
+      id: 1,
+      email: 'John@test.com',
+      password: 'Doe',
+      admin: true,
+      status: UserStatus.NONE,
+      createdAt: new Date(),
+      updatedAt: new Date(),
+      deletedAt: new Date(),
+      keys: [],
+      signerForTransactions: [],
+      observableTransactions: [],
+      approvableTransactions: [],
+      comments: []
+    };
   });
 
   it('should be defined', () => {
     expect(controller).toBeDefined();
   });
 
-  it('should return an array of users', async () => {
-    const result = [
-      {
-        id: 1,
-        email: 'John@test.com',
-        password: 'Doe',
-        admin: true,
-        status: UserStatus.NONE,
-        createdAt: new Date(),
-        updatedAt: new Date(),
-        deletedAt: new Date(),
-        keys: [],
-        signerForTransactions: [],
-        observableTransactions: [],
-        approvableTransactions: [],
-        comments: []
+  describe('getUsers', () => {
+    it('should return an array of users', async () => {
+      const result = [user];
+
+      jest.spyOn(controller, 'getUsers').mockResolvedValue(result);
+
+      expect(await controller.getUsers()).toBe(result);
+    });
+
+    it('should return an empty array if no users exist', async () => {
+      jest.spyOn(controller, 'getUsers').mockResolvedValue([]);
+
+      expect(await controller.getUsers()).toEqual([]);
+    });
+  });
+
+  describe('getUser', () => {
+    it('should return a user', async () => {
+      jest.spyOn(controller, 'getUser').mockResolvedValue(user);
+
+      expect(await controller.getUser(1)).toBe(user);
+    });
+
+    it('should throw an error if the user does not exist', async () => {
+      jest.spyOn(controller, 'getUser').mockResolvedValue(null);
+
+      try {
+        await controller.getUser(1);
+      } catch (error) {
+        expect(error).toBeInstanceOf(Error);
       }
-    ];
-    jest.spyOn(controller, 'getUsers').mockResolvedValue(result);
-
-    expect(await controller.getUsers()).toBe(result);
+    });
   });
 
-  it('should return a user', async () => {
-    const result = {
-      id: 1,
-      email: 'John@test.com',
-      password: 'Doe',
-      admin: true,
-      status: UserStatus.NONE,
-      createdAt: new Date(),
-      updatedAt: new Date(),
-      deletedAt: new Date(),
-      keys: [],
-      signerForTransactions: [],
-      observableTransactions: [],
-      approvableTransactions: [],
-      comments: []
-    };
-    jest.spyOn(controller, 'getUser').mockResolvedValue(result);
+  describe('getMe', () => {
+    it('should return the current user', async () => {
+      jest.spyOn(controller, 'getMe').mockReturnValue(user);
 
-    expect(await controller.getUser(1)).toBe(result);
+      expect(await controller.getMe(user)).toBe(user);
+    });
   });
 
-  it('should return the updated user', async () => {
-    const result = {
-      id: 1,
-      email: 'John@test.com',
-      password: 'Doe',
-      admin: true,
-      status: UserStatus.NONE,
-      createdAt: new Date(),
-      updatedAt: new Date(),
-      deletedAt: new Date(),
-      keys: [],
-      signerForTransactions: [],
-      observableTransactions: [],
-      approvableTransactions: [],
-      comments: []
-    };
-    jest.spyOn(controller, 'updateUser').mockResolvedValue(result);
+  describe('updateUser', () => {
+    it('should return the updated user', async () => {
+      jest.spyOn(controller, 'updateUser').mockResolvedValue(user);
 
-    expect(await controller.updateUser(1, result)).toBe(result);
+      expect(await controller.updateUser(1, user)).toBe(user);
+    });
+
+    it('should throw an error if admin value is not supplied', async () => {
+      const invalidUser = { ...user, admin: null };
+
+      try {
+        await controller.updateUser(1, invalidUser);
+      } catch (error) {
+        expect(error).toBeInstanceOf(Error);
+      }
+    });
+
+    it('should throw an error if the user does not exist', async () => {
+      jest.spyOn(controller, 'updateUser').mockResolvedValue(null);
+
+      try {
+        await controller.updateUser(1, user);
+      } catch (error) {
+        expect(error).toBeInstanceOf(Error);
+      }
+    });
   });
 
-  it('should remove a user', async () => {
-    const result = {
-      id: 1,
-      email: 'John@test.com',
-      password: 'Doe',
-      admin: true,
-      status: UserStatus.NONE,
-      createdAt: new Date(),
-      updatedAt: new Date(),
-      deletedAt: new Date(),
-      keys: [],
-      signerForTransactions: [],
-      observableTransactions: [],
-      approvableTransactions: [],
-      comments: []
-    };
-    jest.spyOn(controller, 'removeUser').mockResolvedValue(result);
+  describe('removeUser', () => {
+    it('should remove a user', async () => {
+      jest.spyOn(controller, 'removeUser').mockResolvedValue(user);
 
-    expect(await controller.removeUser(1)).toBe(result);
+      expect(await controller.removeUser(1)).toBe(user);
+    });
+
+    it('should throw an error if the user does not exist', async () => {
+      jest.spyOn(controller, 'removeUser').mockResolvedValue(null);
+
+      try {
+        await controller.removeUser(1);
+      } catch (error) {
+        expect(error).toBeInstanceOf(Error);
+      }
+    });
   });
 });
