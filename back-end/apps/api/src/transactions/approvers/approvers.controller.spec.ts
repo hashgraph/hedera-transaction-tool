470078d6a946287420c936452eb16ff0d26163ed
John Bair <john.bair@swirldslabs.com>
2024-07-07 00:53:40 -0700

    Added tests for approvers, comments, and observers controllers
    Signed-off-by: John Bair <john.bair@swirldslabs.com>

--- back-end/apps/api/src/transactions/approvers/approvers.controller.spec.ts
+++ back-end/apps/api/src/transactions/approvers/approvers.controller.spec.ts
@@ -1,18 +1,117 @@
 import { Test, TestingModule } from '@nestjs/testing';
 import { ApproversController } from './approvers.controller';
+import { ApproversService } from './approvers.service';
+import { TransactionApprover, User, UserStatus } from '@entities';
 
 describe('ApproversController', () => {
   let controller: ApproversController;
+  let user: User;
+  let transactionApprover: TransactionApprover;
 
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
       controllers: [ApproversController],
+      providers: [
+        {
+          provide: ApproversService,
+          useValue: {
+            getTransactionApproverById: jest.fn(),
+            getApproversByTransactionId: jest.fn(),
+            getVerifiedApproversByTransactionId: jest.fn(),
+            getTransactionApproversById: jest.fn(),
+            getRootNodeFromNode: jest.fn(),
+            removeNode: jest.fn(),
+            createTransactionApprovers: jest.fn(),
+            updateTransactionApprover: jest.fn(),
+            removeTransactionApprover: jest.fn(),
+            approveTransaction: jest.fn(),
+            getCreatorsTransaction: jest.fn(),
+            getTreeStructure: jest.fn(),
+          }
+        },
+      ],
     }).compile();
 
     controller = module.get<ApproversController>(ApproversController);
+    user = {
+      id: 1,
+      email: 'John@test.com',
+      password: 'Doe',
+      admin: true,
+      status: UserStatus.NONE,
+      createdAt: new Date(),
+      updatedAt: new Date(),
+      deletedAt: null,
+      keys: [],
+      signerForTransactions: [],
+      observableTransactions: [],
+      approvableTransactions: [],
+      comments: []
+    };
+    transactionApprover = {
+      id: 1,
+      transaction: null,
+      createdAt: new Date(),
+      updatedAt: new Date(),
+      deletedAt: null,
+      user: user,
+      approved: false,
+      approvers: [],
+    };
   });
 
-  it('should be defined', () => {
-    expect(controller).toBeDefined();
+  describe('createTransactionApprovers', () => {
+    it('should return an approver', async () => {
+      const result = [transactionApprover];
+
+      jest.spyOn(controller, 'createTransactionApprovers').mockResolvedValue(result);
+
+      expect(await controller.createTransactionApprovers(user, 1, { approversArray: [{ userId: user.id }] })).toBe(result);
+    });
+  });
+
+  describe('approveTransaction', () => {
+    it('should return a boolean indicating if the approval was successful', async () => {
+      const result = true;
+      const approval = {
+        userKeyId: 1,
+        signature: Buffer.from('asdlfinfeianef203finasdfljn23f9inasdf'),
+        approved: true,
+      };
+
+      jest.spyOn(controller, 'approveTransaction').mockResolvedValue(result);
+
+      expect(await controller.approveTransaction(user, 1, approval)).toBe(result);
+    });
+  });
+
+  describe('getTransactionApproversByTransactionId', () => {
+    it('should return an array of approvers', async () => {
+      const result = [transactionApprover];
+
+      jest.spyOn(controller, 'getTransactionApproversByTransactionId').mockResolvedValue(result);
+
+      expect(await controller.getTransactionApproversByTransactionId(user, 1)).toBe(result);
+    });
+  });
+
+  describe('removeTransactionApprover', () => {
+    it('should return a boolean indicating if the removal was successful', async () => {
+      const result = true;
+
+      jest.spyOn(controller, 'removeTransactionApprover').mockResolvedValue(result);
+
+      expect(await controller.removeTransactionApprover(user, 1, 1)).toBe(result);
+    });
+  });
+
+  describe('updateTransactionApprover', () => {
+    it('should return an approver', async () => {
+      const result = transactionApprover;
+
+      jest.spyOn(controller, 'updateTransactionApprover').mockResolvedValue(result);
+
+      expect(await controller.updateTransactionApprover(user, 1, 1, { userId: 2 })).toBe(result);
+    });
   });
 });
--- back-end/apps/api/src/transactions/comments/comments.controller.spec.ts
+++ back-end/apps/api/src/transactions/comments/comments.controller.spec.ts
@@ -1,18 +1,81 @@
 import { Test, TestingModule } from '@nestjs/testing';
 import { CommentsController } from './comments.controller';
+import { CommentsService } from './comments.service';
+import { TransactionComment, User, UserStatus } from '@entities';
 
 describe('CommentsController', () => {
   let controller: CommentsController;
+  let user: User;
+  let comment: TransactionComment;
 
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
       controllers: [CommentsController],
+      providers: [
+        {
+          provide: CommentsService,
+          useValue: {
+            createComment: jest.fn(),
+            getTransactionComments: jest.fn(),
+            getTransactionCommentById: jest.fn(),
+          },
+        },
+      ]
     }).compile();
 
     controller = module.get<CommentsController>(CommentsController);
+    user = {
+      id: 1,
+      email: 'John@test.com',
+      password: 'Doe',
+      admin: true,
+      status: UserStatus.NONE,
+      createdAt: new Date(),
+      updatedAt: new Date(),
+      deletedAt: null,
+      keys: [],
+      signerForTransactions: [],
+      observableTransactions: [],
+      approvableTransactions: [],
+      comments: []
+    };
+    comment = {
+      id: 1,
+      transaction: null,
+      user: user,
+      message: 'test',
+      createdAt: new Date(),
+    }
   });
 
-  it('should be defined', () => {
-    expect(controller).toBeDefined();
+  describe('createComment', () => {
+    it('should return a comment', async () => {
+      const result = comment;
+
+      jest.spyOn(controller, 'createComment').mockResolvedValue(result);
+
+      expect(await controller.createComment(user, 1, { message: 'test' })).toBe(result);
+    });
   });
+
+  describe('getComments', () => {
+    it('should return an array of comments', async () => {
+      const result = [comment];
+
+      jest.spyOn(controller, 'getComments').mockResolvedValue(result);
+
+      expect(await controller.getComments(1)).toBe(result);
+    });
+  });
+
+  describe('getCommentById', () => {
+    it('should return a comment', async () => {
+      const result = comment;
+
+      jest.spyOn(controller, 'getCommentById').mockResolvedValue(result);
+
+      expect(await controller.getCommentById(1)).toBe(result);
+    });
+  });
+
 });
