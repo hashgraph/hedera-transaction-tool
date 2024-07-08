312fc234c254be8c928da99420d3fe21a18f0b74
John Bair <john.bair@swirldslabs.com>
2024-07-08 07:45:25 -0700

    Added tests for transaction groups controller
    Signed-off-by: John Bair <john.bair@swirldslabs.com>

--- back-end/apps/api/src/transactions/groups/transaction-groups.controller.spec.ts
+++ back-end/apps/api/src/transactions/groups/transaction-groups.controller.spec.ts
@@ -1,18 +1,83 @@
 import { Test, TestingModule } from '@nestjs/testing';
 import { TransactionGroupsController } from './transaction-groups.controller';
+import { TransactionGroup, User, UserStatus } from '@entities';
+import { TransactionGroupsService } from './transaction-groups.service';
 
 describe('TransactionGroupsController', () => {
   let controller: TransactionGroupsController;
+  let user: User;
+  let transactionGroup: TransactionGroup
 
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
       controllers: [TransactionGroupsController],
+      providers: [
+        {
+          provide: TransactionGroupsService,
+          useValue: {
+            createTransactionGroup: jest.fn(),
+            getTransactionGroups: jest.fn(),
+            removeTransactionGroup: jest.fn(),
+          }
+        },
+      ]
     }).compile();
 
     controller = module.get<TransactionGroupsController>(TransactionGroupsController);
+    user = {
+      id: 1,
+      email: 'John@test.com',
+      password: 'Doe',
+      admin: true,
+      status: UserStatus.NONE,
+      createdAt: new Date(),
+      updatedAt: new Date(),
+      deletedAt: null,
+      keys: [],
+      signerForTransactions: [],
+      observableTransactions: [],
+      approvableTransactions: [],
+      comments: []
+    };
+    transactionGroup = {
+      id: 1,
+      description: 'Test Transaction Group Description',
+      atomic: false,
+      groupItems: [],
+      createdAt: new Date(),
+    };
   });
 
-  it('should be defined', () => {
-    expect(controller).toBeDefined();
+  describe('createTransactionGroup', () => {
+    it('should return a transaction group', async () => {
+      const result = transactionGroup;
+      const body = {
+        description: 'Test Transaction Group Description',
+        atomic: false,
+        groupItems: []
+      };
+
+      jest.spyOn(controller, 'createTransactionGroup').mockResolvedValue(result);
+
+      expect(await controller.createTransactionGroup(user, body)).toEqual(result);
+    });
+  });
+
+  describe('getTransactionGroups', () => {
+    it('should return an array of transaction groups', async () => {
+      const result = [transactionGroup];
+
+      jest.spyOn(controller, 'getTransactionGroups').mockResolvedValue(result);
+
+      expect(await controller.getTransactionGroups()).toEqual(result);
+    });
+  });
+
+  describe('removeTransactionGroup', () => {
+    it('should return void', async () => {
+      jest.spyOn(controller, 'removeTransactionGroup').mockReturnValue(undefined);
+
+      expect(await controller.removeTransactionGroup(user, 1)).toBeUndefined();
+    });
   });
 });
