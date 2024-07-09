3512f9e82f5c2dfdf5a81c413f08788c642153a9
Svetoslav Borislavov <svetoslav.borislavov@limechain.tech>
2024-07-09 11:13:13 +0300

    feat: add test config for API service
    Signed-off-by: Svetoslav Borislavov <svetoslav.borislavov@limechain.tech>

---
+++ back-end/apps/api/jest.config.ts
@@ -0,0 +1,20 @@
+import type { Config } from 'jest';
+
+const config: Config = {
+  moduleFileExtensions: ['js', 'json', 'ts'],
+  rootDir: '../../',
+  testRegex: '.*\\.spec\\.ts$',
+  transform: {
+    '^.+\\.(t|j)s$': 'ts-jest',
+  },
+  collectCoverageFrom: ['<rootDir>/apps/api/**/*.(t|j)s'],
+  coverageDirectory: './coverage',
+  testEnvironment: 'node',
+  roots: ['<rootDir>/libs/', '<rootDir>/apps/api'],
+  moduleNameMapper: {
+    '^@app/common(|/.*)$': '<rootDir>/libs/common/src/$1',
+    '^@entities(|/.*)$': '<rootDir>/libs/common/src/database/entities/$1',
+  },
+};
+
+export default config;
