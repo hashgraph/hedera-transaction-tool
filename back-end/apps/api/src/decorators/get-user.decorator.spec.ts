95fa4bcd5ce594dac7111f3941af28f621cd7b4a
John Bair <john.bair@swirldslabs.com>
2024-07-09 16:07:23 -0700

    test: created test for the GetUser decorator.
    Signed-off-by: John Bair <john.bair@swirldslabs.com>

---
+++ back-end/apps/api/src/decorators/get-user.decorator.spec.ts
@@ -0,0 +1,32 @@
+import { ExecutionContext } from '@nestjs/common';
+import { GetUserFactory } from './get-user.decorator';
+import { UserStatus } from '@entities';
+
+describe('GetUser Decorator', () => {
+  it('should return the user object from the request', () => {
+    const user = {
+      id: 1,
+      email: 'john@test.com',
+      password: 'Doe',
+      admin: false,
+      status: UserStatus.NEW,
+      createdAt: new Date(),
+      updatedAt: new Date(),
+      deletedAt: null,
+      keys: [],
+      signerForTransactions: [],
+      observableTransactions: [],
+      approvableTransactions: [],
+      comments: [],
+    };
+
+    const mockExecutionContext: ExecutionContext = {
+      switchToHttp: jest.fn().mockReturnThis(),
+      getRequest: jest.fn().mockReturnValue( { user }),
+    } as unknown as ExecutionContext;
+
+    const userFactory = GetUserFactory(null, mockExecutionContext);
+
+    expect(userFactory).toBe(user);
+  });
+});
