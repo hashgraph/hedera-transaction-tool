4b2b865e7f73316ffc22c48625953619d9592601
John Bair <john.bair@swirldslabs.com>
2024-07-09 16:06:48 -0700

    feat: pulled the factory out of the createParamDecorator function to allow for unit testing of the function.
    Signed-off-by: John Bair <john.bair@swirldslabs.com>

--- back-end/apps/api/src/decorators/get-user.decorator.ts
+++ back-end/apps/api/src/decorators/get-user.decorator.ts
@@ -1,9 +1,9 @@
 import { createParamDecorator, ExecutionContext } from '@nestjs/common';
 import { User } from '@entities';
 
-export const GetUser = createParamDecorator(
-  (data: never, context: ExecutionContext): User => {
-    const request = context.switchToHttp().getRequest();
-    return request.user;
-  },
-);
+export const GetUserFactory = (data: unknown, context: ExecutionContext): User => {
+  const request = context.switchToHttp().getRequest();
+  return request.user;
+};
+
+export const GetUser = createParamDecorator(GetUserFactory);
