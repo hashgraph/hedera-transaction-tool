344cef0025d927f432b3efe75093fc74c27b9da3
Svetoslav Borislavov <svetoslav.borislavov@limechain.tech>
2024-07-09 11:13:13 +0300

    feat: email controller unit tests added
    Signed-off-by: Svetoslav Borislavov <svetoslav.borislavov@limechain.tech>

--- back-end/apps/notifications/src/email/email.controller.spec.ts
+++ back-end/apps/notifications/src/email/email.controller.spec.ts
@@ -1,22 +1,50 @@
 import { Test, TestingModule } from '@nestjs/testing';
-import { EmailController } from './email.controller';
-import { EmailService } from './email.service';
+import { mockDeep } from 'jest-mock-extended';
 
-describe('NotificationsController', () => {
-  let notificationsController: EmailController;
+import { EmailController } from './email.controller';
+
+import { EmailService } from './email.service';
+import { NotifyEmailDto, NotifyTransactionMembersDto } from './dtos';
+
+describe('EmailController', () => {
+  let controller: EmailController;
+  const emailService = mockDeep<EmailService>();
 
   beforeEach(async () => {
     const app: TestingModule = await Test.createTestingModule({
       controllers: [EmailController],
-      providers: [EmailService],
+      providers: [
+        {
+          provide: EmailService,
+          useValue: emailService,
+        },
+      ],
     }).compile();
 
-    notificationsController = app.get<EmailController>(EmailController);
+    controller = app.get<EmailController>(EmailController);
   });
 
-  describe('root', () => {
-    it('should return "Hello World!"', () => {
-      expect(notificationsController.getHello()).toBe('Hello World!');
-    });
+  it('should be defined', () => {
+    expect(controller).toBeDefined();
+  });
+
+  it('should invoke notify email with correct params', async () => {
+    const dto: NotifyEmailDto = {
+      email: 'receiver@email.com',
+      subject: 'Test subject',
+      text: 'Test text',
+    };
+
+    await controller.notifyEmail(dto);
+
+    expect(emailService.notifyEmail).toHaveBeenCalledWith(dto);
+  });
+
+  it('should invoke notify transaction members with correct params', async () => {
+    const dto: NotifyTransactionMembersDto = {};
+
+    await controller.notifyTransactionMembers(dto);
+
+    expect(emailService.notifyTransactionMembers).toHaveBeenCalledWith(dto);
   });
 });
