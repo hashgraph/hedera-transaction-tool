036240788560191a857749d047ffe80e0d8dd8c2
John Bair <john.bair@swirldslabs.com>
2024-07-23 23:10:41 -0700

    fix: user.keys was undefined for tests
    While in the actual app, user.keys should never be undefined or null, it was not being checked. Now it will be checked, and the keys will be retrieved if required.
    
    Signed-off-by: John Bair <john.bair@swirldslabs.com>

--- back-end/apps/api/src/user-keys/user-keys.service.ts
+++ back-end/apps/api/src/user-keys/user-keys.service.ts
@@ -24,6 +24,12 @@ export class UserKeysService {
 
   // Upload the provided user key for the provided user.
   async uploadKey(user: User, dto: UploadUserKeyDto): Promise<UserKey> {
+    // If the user doesn't have keys, get the keys
+    if (!user.keys) {
+      user.keys = await this.getUserKeys(user.id);
+    }
+
+    // Check if the user already has the maximum number of keys
     if (user.keys.length >= MAX_USER_KEYS) {
       throw new BadRequestException(`A user can only have up to ${MAX_USER_KEYS} keys.`);
     }
