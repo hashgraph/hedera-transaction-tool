c05506652409bd8294a56a1f1a004e65748c40a2
Svetoslav Borislavov <svetoslav.borislavov@limechain.tech>
2024-07-25 16:20:09 +0300

    Revert "Update back-end postgres to use snake_case naming strategy" (#677)

--- back-end/libs/common/src/database/database.module.ts
+++ back-end/libs/common/src/database/database.module.ts
@@ -2,7 +2,6 @@ import { Module } from '@nestjs/common';
 import { TypeOrmModule, TypeOrmModuleAsyncOptions } from '@nestjs/typeorm';
 import { EntityClassOrSchema } from '@nestjs/typeorm/dist/interfaces/entity-class-or-schema.type';
 import { ConfigService } from '@nestjs/config';
-import { SnakeNamingStrategy } from 'typeorm-naming-strategies';
 
 @Module({
   imports: [
@@ -18,7 +17,6 @@ import { SnakeNamingStrategy } from 'typeorm-naming-strategies';
           password: configService.getOrThrow('POSTGRES_PASSWORD'),
           synchronize: configService.getOrThrow('POSTGRES_SYNCHRONIZE'),
           autoLoadEntities: true,
-          namingStrategy: new SnakeNamingStrategy(),
         }) as TypeOrmModuleAsyncOptions,
       inject: [ConfigService],
     }),
