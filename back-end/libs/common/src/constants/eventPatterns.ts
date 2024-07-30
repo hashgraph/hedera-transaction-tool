0ec5582abb2b7f054f094e3fcdb2a3d9a2a55b95
Svetoslav Borislavov <svetoslav.borislavov@limechain.tech>
2024-07-30 09:35:50 +0300

    refactor: comment added
    Signed-off-by: Svetoslav Borislavov <svetoslav.borislavov@limechain.tech>

--- back-end/libs/common/src/constants/eventPatterns.ts
+++ back-end/libs/common/src/constants/eventPatterns.ts
@@ -4,4 +4,6 @@ export const GET_PORT = 'get_port';
 
 /* Email patterns */
 export const NOTIFY_EMAIL = 'notify_email';
+
+/* Transaction patterns */
 export const NOTIFY_TRANSACTION_SIGNERS = 'notify_transaction_signers';
