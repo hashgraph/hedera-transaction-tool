b4955b700b5202832a53c16ae1dafdbc1fcf9cba
Svetoslav Borislavov <svetoslav.borislavov@limechain.tech>
2024-07-30 09:35:50 +0300

    fix: typeorm import
    Signed-off-by: Svetoslav Borislavov <svetoslav.borislavov@limechain.tech>

--- back-end/apps/notifications/src/notifications/transactionNotifications.module.ts
+++ back-end/apps/notifications/src/notifications/transactionNotifications.module.ts
@@ -1,12 +1,38 @@
 import { Module } from '@nestjs/common';
+import { TypeOrmModule } from '@nestjs/typeorm';
 
 import { LoggerModule, MirrorNodeModule } from '@app/common';
+import {
+  Transaction,
+  TransactionApprover,
+  TransactionComment,
+  TransactionGroup,
+  TransactionGroupItem,
+  TransactionObserver,
+  TransactionSigner,
+  User,
+  UserKey,
+} from '@entities';
 
 import { TransactionNotificationsController } from './transactionNotifications.controller';
 import { TransactionNotificationsService } from './tranasctionNotifications.service';
 
 @Module({
-  imports: [LoggerModule, MirrorNodeModule],
+  imports: [
+    LoggerModule,
+    TypeOrmModule.forFeature([
+      User,
+      UserKey,
+      Transaction,
+      TransactionSigner,
+      TransactionApprover,
+      TransactionObserver,
+      TransactionComment,
+      TransactionGroup,
+      TransactionGroupItem,
+    ]),
+    MirrorNodeModule,
+  ],
   controllers: [TransactionNotificationsController],
   providers: [TransactionNotificationsService],
 })
