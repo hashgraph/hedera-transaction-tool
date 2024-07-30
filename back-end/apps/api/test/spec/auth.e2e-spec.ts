f38debe58a3ca3918b24482e820d2b16e840c62c
Svetoslav Borislavov <svetoslav.borislavov@limechain.tech>
2024-07-30 19:15:37 +0300

    feat: auth e2e signup tests added
    Signed-off-by: Svetoslav Borislavov <svetoslav.borislavov@limechain.tech>

--- back-end/apps/api/test/spec/auth.e2e-spec.ts
+++ back-end/apps/api/test/spec/auth.e2e-spec.ts
@@ -4,12 +4,13 @@ import * as request from 'supertest';
 
 import { closeApp, createNestApp } from '../utils';
 import { getCookieRaw } from '../utils/httpUtils';
-import { admin, validEmail } from '../utils/constants';
+import { admin, dummy, invalidEmail, validEmail } from '../utils/constants';
 
 describe('Auth (e2e)', () => {
   let app: NestExpressApplication;
   let server: ReturnType<typeof app.getHttpServer>;
-  let authCookie: string;
+  let adminAuthCookie: string;
+  let userAuthCookie: string;
 
   beforeEach(async () => {
     app = await createNestApp();
@@ -20,34 +21,86 @@ describe('Auth (e2e)', () => {
     await closeApp(app);
   });
 
-  it('/auth/login (POST) should login as admin', async () => {
-    const res = await request(server)
-      .post('/auth/login')
-      .send({
-        email: admin.email,
-        password: admin.password,
-      })
-      .expect(200);
+  describe('/auth/login', () => {
+    it('/auth/login (POST) should login as admin', async () => {
+      const res = await request(server)
+        .post('/auth/login')
+        .send({
+          email: admin.email,
+          password: admin.password,
+        })
+        .expect(200);
 
-    authCookie = getCookieRaw(res, 'Authentication');
+      adminAuthCookie = getCookieRaw(res, 'Authentication');
+    });
+
+    it('/auth/login (POST) should login as regular user', async () => {
+      const res = await request(server)
+        .post('/auth/login')
+        .send({
+          email: dummy.email,
+          password: dummy.password,
+        })
+        .expect(200);
+
+      userAuthCookie = getCookieRaw(res, 'Authentication');
+    });
   });
 
-  it('/auth/signup (POST) should register new user if sender is admin', () => {
-    return request(server)
-      .post('/auth/signup')
-      .set('Cookie', authCookie)
-      .send({
-        email: validEmail,
-      })
-      .expect(201);
-  });
+  describe('/auth/signup', () => {
+    it('/auth/signup (POST) should register new user if sender is admin', () => {
+      return request(server)
+        .post('/auth/signup')
+        .set('Cookie', adminAuthCookie)
+        .send({
+          email: validEmail,
+        })
+        .expect(201)
+        .then(res => {
+          expect(res.body).toEqual({
+            id: expect.any(Number),
+            email: validEmail,
+            createdAt: expect.any(String),
+          });
+        });
+    });
 
-  it('/auth/signup (POST) should not register new user if sender is NOT admin', () => {
-    return request(server)
-      .post('/auth/signup')
-      .send({
-        email: validEmail,
-      })
-      .expect(401);
+    it('/auth/signup (POST) should not register new user if sender is not logged', () => {
+      return request(server)
+        .post('/auth/signup')
+        .send({
+          email: validEmail,
+        })
+        .expect(401);
+    });
+
+    it('/auth/signup (POST) should not register new user if sender is NOT admin', () => {
+      return request(server)
+        .post('/auth/signup')
+        .set('Cookie', userAuthCookie)
+        .send({
+          email: validEmail,
+        })
+        .expect(403);
+    });
+
+    it('/auth/signup (POST) should throw on invalid email', async () => {
+      await request(server)
+        .post('/auth/signup')
+        .set('Cookie', adminAuthCookie)
+        .send({
+          email: invalidEmail,
+        })
+        .expect(400);
+    });
+
+    it('/auth/signup (POST) should throw on missing email', async () => {
+      await request(server)
+        .post('/auth/signup')
+        .set('Cookie', adminAuthCookie)
+        .send({})
+        .expect(400)
+        .expect({ statusCode: 400, message: 'No email specified.' });
+    });
   });
 });
