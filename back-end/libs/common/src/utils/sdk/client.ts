ac9d2291b3dca4eab310c55cccdcf3286d2d7bbd
Svetoslav Borislavov <svetoslav.borislavov@limechain.tech>
2024-07-30 19:15:38 +0300

    fix: client local node uri
    Signed-off-by: Svetoslav Borislavov <svetoslav.borislavov@limechain.tech>

--- back-end/libs/common/src/utils/sdk/client.ts
+++ back-end/libs/common/src/utils/sdk/client.ts
@@ -3,11 +3,15 @@ import { Client } from '@hashgraph/sdk';
 import { Network } from '@entities';
 import { MirrorNetwork } from '../mirrorNode';
 
-export const getClientFromName = (network: Network) =>
-  network !== Network.LOCAL_NODE
+export const getClientFromName = (network: Network) => {
+  const networkURI =
+    process.env.NODE_ENV !== 'test'
+      ? { 'host.docker.internal:50211': '0.0.3' }
+      : { '127.0.0.1:50211': '0.0.3' };
+
+  return network !== Network.LOCAL_NODE
     ? Client.forName(network)
     : /* Using host.docker.internal to access the host machine from the container, will work only in dev mode */
       /* Local node will be used only in development mode */
-      Client.forNetwork({ 'host.docker.internal:50211': '0.0.3' }).setMirrorNetwork(
-        MirrorNetwork.LOCAL_NODE,
-      );
+      Client.forNetwork(networkURI).setMirrorNetwork(MirrorNetwork.LOCAL_NODE);
+};
