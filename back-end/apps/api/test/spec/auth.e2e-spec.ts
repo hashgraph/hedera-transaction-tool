3be2f29e24e0a8577a5a6e037c8dd413cae3495c
Svetoslav Borislavov <svetoslav.borislavov@limechain.tech>
2024-07-30 19:15:37 +0300

    refactor: supertest utils added
    Signed-off-by: Svetoslav Borislavov <svetoslav.borislavov@limechain.tech>

--- back-end/apps/api/test/spec/auth.e2e-spec.ts
+++ back-end/apps/api/test/spec/auth.e2e-spec.ts
@@ -1,14 +1,13 @@
 import { NestExpressApplication } from '@nestjs/platform-express';
 
-import * as request from 'supertest';
-
 import { closeApp, createNestApp } from '../utils';
-import { getCookieRaw } from '../utils/httpUtils';
 import { admin, dummy, invalidEmail, validEmail } from '../utils/constants';
+import { getCookieRaw, Endpoint } from '../utils/httpUtils';
 
 describe('Auth (e2e)', () => {
   let app: NestExpressApplication;
   let server: ReturnType<typeof app.getHttpServer>;
+
   let adminAuthCookie: string;
   let userAuthCookie: string;
 
@@ -22,10 +21,15 @@ describe('Auth (e2e)', () => {
   });
 
   describe('/auth/login', () => {
-    it('/auth/login (POST) should login as admin', async () => {
-      const res = await request(server)
-        .post('/auth/login')
-        .send({
+    let endpoint: Endpoint;
+
+    beforeEach(() => {
+      endpoint = new Endpoint(server, '/auth/login');
+    });
+
+    it('(POST) should login as admin', async () => {
+      const res = await endpoint
+        .post({
           email: admin.email,
           password: admin.password,
         })
@@ -34,10 +38,9 @@ describe('Auth (e2e)', () => {
       adminAuthCookie = getCookieRaw(res, 'Authentication');
     });
 
-    it('/auth/login (POST) should login as regular user', async () => {
-      const res = await request(server)
-        .post('/auth/login')
-        .send({
+    it('(POST) should login as regular user', async () => {
+      const res = await endpoint
+        .post({
           email: dummy.email,
           password: dummy.password,
         })
@@ -48,13 +51,20 @@ describe('Auth (e2e)', () => {
   });
 
   describe('/auth/signup', () => {
-    it('/auth/signup (POST) should register new user if sender is admin', () => {
-      return request(server)
-        .post('/auth/signup')
-        .set('Cookie', adminAuthCookie)
-        .send({
-          email: validEmail,
-        })
+    let endpoint: Endpoint;
+
+    beforeEach(() => {
+      endpoint = new Endpoint(server, '/auth/signup');
+    });
+
+    it('(POST) should register new user if sender is admin', () => {
+      return endpoint
+        .post(
+          {
+            email: validEmail,
+          },
+          adminAuthCookie,
+        )
         .expect(201)
         .then(res => {
           expect(res.body).toEqual({
@@ -65,40 +75,39 @@ describe('Auth (e2e)', () => {
         });
     });
 
-    it('/auth/signup (POST) should not register new user if sender is not logged', () => {
-      return request(server)
-        .post('/auth/signup')
-        .send({
+    it('(POST) should not register new user if sender is not logged', () => {
+      return endpoint
+        .post({
           email: validEmail,
         })
         .expect(401);
     });
 
-    it('/auth/signup (POST) should not register new user if sender is NOT admin', () => {
-      return request(server)
-        .post('/auth/signup')
-        .set('Cookie', userAuthCookie)
-        .send({
-          email: validEmail,
-        })
+    it('(POST) should not register new user if sender is NOT admin', () => {
+      return endpoint
+        .post(
+          {
+            email: validEmail,
+          },
+          userAuthCookie,
+        )
         .expect(403);
     });
 
-    it('/auth/signup (POST) should throw on invalid email', async () => {
-      await request(server)
-        .post('/auth/signup')
-        .set('Cookie', adminAuthCookie)
-        .send({
-          email: invalidEmail,
-        })
+    it('(POST) should throw on invalid email', async () => {
+      await endpoint
+        .post(
+          {
+            email: invalidEmail,
+          },
+          adminAuthCookie,
+        )
         .expect(400);
     });
 
-    it('/auth/signup (POST) should throw on missing email', async () => {
-      await request(server)
-        .post('/auth/signup')
-        .set('Cookie', adminAuthCookie)
-        .send({})
+    it('(POST) should throw on missing email', async () => {
+      await endpoint
+        .post({}, adminAuthCookie)
         .expect(400)
         .expect({ statusCode: 400, message: 'No email specified.' });
     });
--- back-end/apps/api/test/utils/httpUtils.ts
+++ back-end/apps/api/test/utils/httpUtils.ts
@@ -1,6 +1,69 @@
-import supertest from 'supertest';
+import * as request from 'supertest';
 
-export const getCookieRaw = (res: supertest.Response, name: string) => {
+import { NestExpressApplication } from '@nestjs/platform-express';
+
+type ServerType = ReturnType<NestExpressApplication['getHttpServer']>;
+
+export const req = (
+  server: ServerType,
+  type: 'get' | 'post' | 'put' | 'delete',
+  endpoint: string,
+  cookie?: string,
+) => {
+  const req = request(server)[type](endpoint);
+
+  if (cookie) {
+    req.set('Cookie', cookie);
+  }
+
+  return req;
+};
+
+export const get = (server: ServerType, endpoint: string, cookie?: string) =>
+  req(server, 'get', endpoint, cookie);
+
+export const post = (server: ServerType, endpoint: string, cookie?: string) =>
+  req(server, 'post', endpoint, cookie);
+
+export const put = (server: ServerType, endpoint: string, cookie?: string) =>
+  req(server, 'put', endpoint, cookie);
+
+export const del = (server: ServerType, endpoint: string, cookie?: string) =>
+  req(server, 'delete', endpoint, cookie);
+
+export class Endpoint {
+  public endpoint: string;
+  public server: ServerType;
+
+  constructor(server: ServerType, endpoint: string) {
+    this.endpoint = endpoint;
+    this.server = server;
+  }
+
+  public get(cookie?: string) {
+    return get(this.server, this.endpoint, cookie);
+  }
+
+  public post(data?, cookie?: string) {
+    if (data) {
+      return post(this.server, this.endpoint, cookie).send(data);
+    }
+    return post(this.server, this.endpoint, cookie);
+  }
+
+  public put(data?, cookie?: string) {
+    if (data) {
+      return put(this.server, this.endpoint, cookie).send(data);
+    }
+    return put(this.server, this.endpoint, cookie);
+  }
+
+  public delete(cookie?: string) {
+    return del(this.server, this.endpoint, cookie);
+  }
+}
+
+export const getCookieRaw = (res: request.Response, name: string) => {
   if (Array.isArray(res.headers['set-cookie'])) {
     const cookies = res.headers['set-cookie'];
     return cookies.find(cookie => cookie.startsWith(name));
