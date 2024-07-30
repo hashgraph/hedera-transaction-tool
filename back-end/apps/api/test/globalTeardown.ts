56bb21ee69a9ca57e465fb228798d51699a65b7b
Svetoslav Borislavov <svetoslav.borislavov@limechain.tech>
2024-07-30 19:15:37 +0300

    fix: comment stopping Notification & Chain
    Signed-off-by: Svetoslav Borislavov <svetoslav.borislavov@limechain.tech>

--- back-end/apps/api/test/globalTeardown.ts
+++ back-end/apps/api/test/globalTeardown.ts
@@ -1,11 +1,11 @@
 import { StartedTestContainer } from 'testcontainers';
 
 export default async function globalTeardown() {
-  /* Stops the Notifications Service */
-  const result = await Promise.allSettled([
-    stopContainer(global.NOTIFICATIONS_SERVICE_CONTAINER),
-    stopContainer(global.CHAIN_SERVICE_CONTAINER),
-  ]);
+  /* Stops the Notifications, Chain Services */
+  // const result = await Promise.allSettled([
+  //   stopContainer(global.NOTIFICATIONS_SERVICE_CONTAINER),
+  //   stopContainer(global.CHAIN_SERVICE_CONTAINER),
+  // ]);
 
   /* Stops the Redis, Postgres, RabbitMQ containers */
   const result1 = await Promise.allSettled([
@@ -14,11 +14,11 @@ export default async function globalTeardown() {
     stopContainer(global.RABBITMQ_CONTAINER),
   ]);
 
-  result.forEach((res, i) => {
-    if (res.status === 'rejected') {
-      console.error(`Error stopping container ${i}: ${res.reason}`);
-    }
-  });
+  // result.forEach((res, i) => {
+  //   if (res.status === 'rejected') {
+  //     console.error(`Error stopping container ${i}: ${res.reason}`);
+  //   }
+  // });
 
   result1.forEach((res, i) => {
     if (res.status === 'rejected') {
