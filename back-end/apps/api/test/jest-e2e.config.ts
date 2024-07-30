ff138c1c2683c6baccc87ba69692de591ee2f2f3
Svetoslav Borislavov <svetoslav.borislavov@limechain.tech>
2024-07-30 19:14:42 +0300

    feat: jest e2e config added
    Signed-off-by: Svetoslav Borislavov <svetoslav.borislavov@limechain.tech>

---
+++ back-end/apps/api/test/jest-e2e.config.ts
@@ -0,0 +1,22 @@
+import type { Config } from 'jest';
+
+const config: Config = {
+  moduleFileExtensions: ['js', 'json', 'ts'],
+  rootDir: '../',
+  testRegex: '.e2e-spec.ts$',
+  maxWorkers: 1,
+  transform: {
+    '^.+\\.(t|j)s$': 'ts-jest',
+  },
+  collectCoverageFrom: ['<rootDir>/src/**/*.{!(module),}.(t|j)s'],
+  testEnvironment: 'node',
+  globalSetup: '<rootDir>/test/globalSetup.ts',
+  globalTeardown: '<rootDir>/test/globalTeardown.ts',
+  roots: ['<rootDir>../../libs/', '<rootDir>'],
+  moduleNameMapper: {
+    '^@app/common(|/.*)$': '<rootDir>../../libs/common/src/$1',
+    '^@entities(|/.*)$': '<rootDir>../../libs/common/src/database/entities/$1',
+  },
+};
+
+export default config;
