6f4dd45d17280a8fd93dd8cc284f0a0291079f7e
Svetoslav Borislavov <svetoslav.borislavov@limechain.tech>
2024-07-31 07:43:14 +0300

    feat: notification preferences entity added
    Signed-off-by: Svetoslav Borislavov <svetoslav.borislavov@limechain.tech>

---
+++ back-end/libs/common/src/database/entities/notification-preferences.entity.ts
@@ -0,0 +1,21 @@
+import { Column, Entity, JoinColumn, OneToOne, PrimaryGeneratedColumn } from 'typeorm';
+import { User } from './user.entity';
+
+@Entity()
+export class NotificationPreferences {
+  @PrimaryGeneratedColumn()
+  id: number;
+
+  @OneToOne(() => User)
+  @JoinColumn({ name: 'userId' })
+  user: User;
+
+  @Column()
+  userId: number;
+
+  @Column({ default: true })
+  transactionRequiredSignature: boolean;
+
+  @Column({ default: true })
+  transactionReadyForExecution: boolean;
+}
