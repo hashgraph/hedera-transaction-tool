2cc4fd860cda9aa7700d2bb49b58e11ac4cebe2d
Svetoslav Borislavov <svetoslav.borislavov@limechain.tech>
2024-07-31 07:43:54 +0300

    feat: notifications store created
    Signed-off-by: Svetoslav Borislavov <svetoslav.borislavov@limechain.tech>

--- front-end/src/renderer/stores/storeNotifications.ts
+++ front-end/src/renderer/stores/storeNotifications.ts
@@ -1,18 +1,60 @@
-import { ref } from 'vue';
+import { reactive } from 'vue';
 import { defineStore } from 'pinia';
 
-const userNotificationsStore = defineStore('notifications', () => {
+import {
+  getUserNotificationPreferences,
+  updateUserNotificationPreferences,
+} from '@renderer/services/organization';
+
+import useUserStore from './storeUser';
+import { isLoggedInOrganization } from '@renderer/utils/userStoreHelpers';
+import {
+  INotificationPreferencesCore,
+  IUpdateNotificationPreferencesDto,
+} from '@main/shared/interfaces';
+
+const useNotificationsStore = defineStore('notifications', () => {
+  const user = useUserStore();
+
   /* State */
-  const notifications = ref({
-    email: {
-      'threshhold-reached': true,
-      'required-signatures': true,
-    },
+  const notifications = reactive({
+    'threshold-reached': true,
+    'required-signatures': true,
   });
+
+  /* Actions */
+  async function fetchPreferences() {
+    if (!isLoggedInOrganization(user.selectedOrganization)) {
+      throw new Error('No organization selected');
+    }
+
+    const preferences = await getUserNotificationPreferences(user.selectedOrganization?.serverUrl);
+    updatePreference(preferences);
+  }
+
+  async function updatePreferences(data: IUpdateNotificationPreferencesDto) {
+    if (!isLoggedInOrganization(user.selectedOrganization)) {
+      throw new Error('No organization selected');
+    }
+
+    const newPreferences = await updateUserNotificationPreferences(
+      user.selectedOrganization.serverUrl,
+      data,
+    );
+
+    updatePreference(newPreferences);
+  }
+
+  function updatePreference(preferences: INotificationPreferencesCore) {
+    notifications['threshold-reached'] = preferences.transactionReadyForExecution;
+    notifications['required-signatures'] = preferences.transactionRequiredSignature;
+  }
 
   return {
     notifications,
+    fetchPreferences,
+    updatePreferences,
   };
 });
 
-export default userNotificationsStore;
+export default useNotificationsStore;
