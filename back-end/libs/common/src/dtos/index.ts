0d5578effc7a8b326f7a98d582ac7a2dea736482
Svetoslav Borislavov <svetoslav.borislavov@limechain.tech>
2024-08-20 13:49:05 +0300

    feat: created general notify dto
    Signed-off-by: Svetoslav Borislavov <svetoslav.borislavov@limechain.tech>

--- back-end/libs/common/src/dtos/index.ts
+++ back-end/libs/common/src/dtos/index.ts
@@ -3,3 +3,4 @@ export * from './transaction-executed.dto';
 export * from './notifications-notify-client.dto';
 export * from './notifications-notify-email.dto';
 export * from './notifications-notify-for-transaction.dto';
+export * from './notifications-notify-general.dto';
---
+++ back-end/libs/common/src/dtos/notifications-notify-general.dto.ts
@@ -0,0 +1,22 @@
+import { ArrayMinSize, IsArray, IsEnum, IsNotEmpty, IsNumber, IsOptional } from 'class-validator';
+import { NotificationType } from '../database/entities';
+
+export class NotifyGeneralDto {
+  @IsNotEmpty()
+  @IsEnum(NotificationType)
+  type: NotificationType;
+
+  @IsArray()
+  @IsNumber({}, { each: true })
+  @ArrayMinSize(1)
+  userIds: number[];
+
+  @IsNotEmpty()
+  content: string;
+
+  @IsOptional()
+  entityId?: number;
+
+  @IsOptional()
+  actorId?: number;
+}
