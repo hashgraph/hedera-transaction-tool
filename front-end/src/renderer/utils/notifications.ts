cfe27dd3b8a5ff53a4fe15748d8ff08f6f4e2ae4
Svetoslav Borislavov <svetoslav.borislavov@limechain.tech>
2024-08-20 13:50:11 +0300

    feat: add util for getting highlighted transactions
    Signed-off-by: Svetoslav Borislavov <svetoslav.borislavov@limechain.tech>

---
+++ front-end/src/renderer/utils/notifications.ts
@@ -0,0 +1,21 @@
+import { INotificationReceiver, ITransaction, NotificationType } from '@main/shared/interfaces';
+
+export const getNotifiedTransactions = (
+  notifications: INotificationReceiver[],
+  transactions: ITransaction[],
+  indicatorTypes: NotificationType[],
+): number[] => {
+  const indicatorNotifications = notifications.filter(nr =>
+    indicatorTypes.includes(nr.notification.type),
+  );
+
+  if (indicatorNotifications.length > 0) {
+    return transactions
+      .filter(t => {
+        return indicatorNotifications.some(nr => nr.notification.entityId === t.id);
+      })
+      .map(t => t.id);
+  }
+
+  return [];
+};
