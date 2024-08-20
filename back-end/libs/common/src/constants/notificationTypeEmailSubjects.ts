f959dc612716d60793a14636bb7ad6f57cb9bad9
Svetoslav Borislavov <svetoslav.borislavov@limechain.tech>
2024-08-20 14:03:22 +0300

    feat: update email subject
    Signed-off-by: Svetoslav Borislavov <svetoslav.borislavov@limechain.tech>

--- back-end/libs/common/src/constants/notificationTypeEmailSubjects.ts
+++ back-end/libs/common/src/constants/notificationTypeEmailSubjects.ts
@@ -1,6 +1,6 @@
 export enum NotificationTypeEmailSubjects {
   TRANSACTION_CREATED = 'Hedera Transaction Tool | Transaction has been created',
-  TRANSACTION_WAITING_FOR_SIGNATURES = 'Action Required: Review and Sign Transaction',
+  TRANSACTION_WAITING_FOR_SIGNATURES = 'Hedera Transaction Tool | Action Required | Review and Sign Transaction',
   TRANSACTION_READY_FOR_EXECUTION = 'Hedera Transaction Tool | Transaction ready for execution',
   TRANSCATION_EXECUTED = 'Hedera Transaction Tool | Transaction has been executed',
 }
