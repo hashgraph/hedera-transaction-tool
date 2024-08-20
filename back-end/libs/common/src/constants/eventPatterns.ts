0f57bdbd3dbb828e3a218a2a4b2e195d3a9003e8
Svetoslav Borislavov <svetoslav.borislavov@limechain.tech>
2024-08-20 13:50:10 +0300

    feat: add event pattern for update transaction indicator
    Signed-off-by: Svetoslav Borislavov <svetoslav.borislavov@limechain.tech>

--- back-end/libs/common/src/constants/eventPatterns.ts
+++ back-end/libs/common/src/constants/eventPatterns.ts
@@ -11,3 +11,4 @@ export const NOTIFY_TRANSACTION_WAITING_FOR_SIGNATURES =
   'notify_transaction_waiting_for_signatures';
 export const NOTIFY_TRANSACTION_CREATOR_ON_READY_FOR_EXECUTION =
   'notify_transaction_creator_on_ready_for_execution';
+export const UPDATE_INDICATOR_NOTIFICATION = 'update_indicator_notification';
