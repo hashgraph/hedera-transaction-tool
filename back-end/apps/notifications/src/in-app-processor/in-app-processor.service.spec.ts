f161a5a47a5c5c7375e3527256cf66217a81f02b
Svetoslav Borislavov <svetoslav.borislavov@limechain.tech>
2024-08-20 13:50:10 +0300

    feat: in app notification processor created
    Signed-off-by: Svetoslav Borislavov <svetoslav.borislavov@limechain.tech>

---
+++ back-end/apps/notifications/src/in-app-processor/in-app-processor.service.spec.ts
@@ -0,0 +1,58 @@
+import { Test, TestingModule } from '@nestjs/testing';
+
+import { Notification, NotificationType } from '@entities';
+
+import { InAppProcessorService } from './in-app-processor.service';
+import { WebsocketGateway } from '../websocket/websocket.gateway';
+import { mockDeep } from 'jest-mock-extended';
+
+describe('In App Processor Service', () => {
+  let service: InAppProcessorService;
+  const websocketGateway = mockDeep<WebsocketGateway>();
+
+  beforeEach(async () => {
+    jest.resetAllMocks();
+
+    const module: TestingModule = await Test.createTestingModule({
+      providers: [
+        InAppProcessorService,
+        {
+          provide: WebsocketGateway,
+          useValue: websocketGateway,
+        },
+      ],
+    }).compile();
+
+    service = module.get<InAppProcessorService>(InAppProcessorService);
+  });
+
+  it('should be defined', () => {
+    expect(service).toBeDefined();
+  });
+
+  it('should process in-app notification', async () => {
+    const notification: Notification = {
+      id: 1,
+      type: NotificationType.TRANSACTION_WAITING_FOR_SIGNATURES,
+      content: `A new transaction requires your review and signature.`,
+      entityId: 2,
+      actorId: null,
+      createdAt: new Date(),
+      notificationReceivers: [],
+    };
+
+    service.processNotification(notification, [1, 2]);
+
+    expect(websocketGateway.notifyUser).toHaveBeenCalledTimes(2);
+    expect(websocketGateway.notifyUser).toHaveBeenCalledWith(
+      1,
+      'notifications:new',
+      expect.any(Object),
+    );
+    expect(websocketGateway.notifyUser).toHaveBeenCalledWith(
+      2,
+      'notifications:new',
+      expect.any(Object),
+    );
+  });
+});
---
+++ back-end/apps/notifications/src/in-app-processor/in-app-processor.service.ts
@@ -0,0 +1,25 @@
+import { Injectable } from '@nestjs/common';
+import { plainToInstance } from 'class-transformer';
+
+import { Notification } from '@entities';
+
+import { WebsocketGateway } from '../websocket/websocket.gateway';
+import { NotificationDto } from './dtos';
+
+@Injectable()
+export class InAppProcessorService {
+  constructor(private readonly websocket: WebsocketGateway) {}
+
+  processNotification(notification: Notification, userIds: number[]) {
+    /* Create DTO from entity */
+    const dto = plainToInstance(NotificationDto, notification, {
+      excludeExtraneousValues: true,
+      exposeUnsetFields: false,
+    });
+
+    /* Notify users */
+    for (const user of userIds) {
+      this.websocket.notifyUser(user, 'notifications:new', dto);
+    }
+  }
+}
