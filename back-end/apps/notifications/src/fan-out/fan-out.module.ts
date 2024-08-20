e02060bfad9fc06702cd5b7fac365e0f71198626
Svetoslav Borislavov <svetoslav.borislavov@limechain.tech>
2024-08-20 13:50:10 +0300

    fix: fan out module import
    Signed-off-by: Svetoslav Borislavov <svetoslav.borislavov@limechain.tech>

--- back-end/apps/notifications/src/fan-out/fan-out.module.ts
+++ back-end/apps/notifications/src/fan-out/fan-out.module.ts
@@ -3,12 +3,12 @@ import { Module } from '@nestjs/common';
 import { LoggerModule } from '@app/common';
 
 import { EmailModule } from '../email/email.module';
+import { InAppModule } from '../in-app-processor/in-app-processor.module';
 
 import { FanOutService } from './fan-out.service';
-import { InAppProcessorService } from '../in-app-processor/in-app-processor.service';
 
 @Module({
-  imports: [LoggerModule, EmailModule, InAppProcessorService],
+  imports: [LoggerModule, EmailModule, InAppModule],
   controllers: [],
   providers: [FanOutService],
   exports: [FanOutService],
