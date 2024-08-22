3bf531da676627c81a46df064753b8692db36603
Svetoslav Borislavov <svetoslav.borislavov@limechain.tech>
2024-08-22 16:39:00 +0300

    feat: chain interfaces added
    Signed-off-by: Svetoslav Borislavov <svetoslav.borislavov@limechain.tech>

--- front-end/src/renderer/components/Transaction/TransactionProcessor/index.ts
+++ front-end/src/renderer/components/Transaction/TransactionProcessor/index.ts
@@ -1,3 +1,19 @@
+import { Key } from '@hashgraph/sdk';
+
+import { TransactionApproverDto } from '@main/shared/interfaces/organization/approvers';
+
 import TransactionProcessor from './TransactionProcessor.vue';
+
+export interface TransactionRequest {
+  transactionKey: Key;
+  transactionBytes: Uint8Array;
+  observers: number[];
+  approvers: TransactionApproverDto[];
+}
+
+export interface Handler {
+  setNext: (handler: Handler) => void;
+  handle: (transactionRequest: TransactionRequest) => void;
+}
 
 export default TransactionProcessor;
