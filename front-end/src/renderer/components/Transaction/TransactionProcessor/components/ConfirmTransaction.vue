f1b2e24c316b90d3536247321a35f66c3acebe90
Svetoslav Borislavov <svetoslav.borislavov@limechain.tech>
2024-08-22 16:39:00 +0300

    feat: confirm transaction handler added
    Signed-off-by: Svetoslav Borislavov <svetoslav.borislavov@limechain.tech>

--- front-end/src/renderer/components/Transaction/TransactionProcessor/components/ConfirmTransaction.vue
+++ front-end/src/renderer/components/Transaction/TransactionProcessor/components/ConfirmTransaction.vue
@@ -1,56 +1,108 @@
 <script setup lang="ts">
+import { computed, inject, ref } from 'vue';
 import { Transaction } from '@hashgraph/sdk';
 
+import useUserStore from '@renderer/stores/storeUser';
 import useNetworkStore from '@renderer/stores/storeNetwork';
 
 import { getDollarAmount } from '@renderer/services/mirrorNodeDataService';
 
 import { getTransactionType, stringifyHbar } from '@renderer/utils';
 
+import { USER_PASSWORD_MODAL_KEY, USER_PASSWORD_MODAL_TYPE } from '@renderer/providers';
+
 import AppButton from '@renderer/components/ui/AppButton.vue';
 import AppModal from '@renderer/components/ui/AppModal.vue';
 
+import { Handler, TransactionRequest } from '..';
+
 /* Props */
-const props = defineProps<{
-  show: boolean;
-  transaction: Transaction;
+defineProps<{
   signing: boolean;
 }>();
 
-/* Emits */
-const emit = defineEmits(['update:show', 'transaction:confirm']);
-
 /* Stores */
 const network = useNetworkStore();
+const user = useUserStore();
+
+/* Injected */
+const userPasswordModalRef = inject<USER_PASSWORD_MODAL_TYPE>(USER_PASSWORD_MODAL_KEY);
+
+/* State */
+const request = ref<TransactionRequest | null>(null);
+const nextHandler = ref<Handler | null>(null);
+const show = ref(false);
+
+/* Computed */
+const transaction = computed(() =>
+  request.value ? Transaction.fromBytes(request.value.transactionBytes) : null,
+);
 
 /* Handlers */
 const handleConfirmTransaction = (e: Event) => {
   e.preventDefault();
 
-  emit('transaction:confirm');
+  assertPassword();
 };
+
+function assertPassword() {
+  /* Verifies the user has entered his password */
+  const personalPassword = user.getPassword();
+  if (!personalPassword) {
+    if (!userPasswordModalRef) throw new Error('User password modal ref is not provided');
+    show.value = false;
+    userPasswordModalRef.value?.open(
+      'Enter your application password',
+      'Enter your application password to sign the transaction',
+      next,
+    );
+    return;
+  }
+}
+
+/* Actions */
+function next() {
+  if (nextHandler.value && request.value) {
+    nextHandler.value.handle(request.value);
+  }
+}
+
+function setNext(next: Handler) {
+  nextHandler.value = next;
+}
+
+function handle(req: TransactionRequest) {
+  request.value = req;
+  show.value = true;
+}
+
+function setShow(value: boolean) {
+  show.value = value;
+}
+
+/* Expose */
+defineExpose({
+  handle,
+  setNext,
+  setShow,
+});
 </script>
 <template>
   <!-- Confirm modal -->
-  <AppModal
-    :show="show"
-    @update:show="emit('update:show', $event)"
-    :close-on-click-outside="false"
-    :close-on-escape="false"
-  >
+  <AppModal v-model:show="show" :close-on-click-outside="false" :close-on-escape="false">
     <div class="p-5">
       <div>
-        <i class="bi bi-x-lg cursor-pointer" @click="emit('update:show', false)"></i>
+        <i class="bi bi-x-lg cursor-pointer" @click="show = false"></i>
       </div>
       <div class="text-center">
         <i class="bi bi-arrow-left-right large-icon"></i>
       </div>
-      <form @submit="handleConfirmTransaction">
+      <form v-if="transaction" @submit="handleConfirmTransaction">
         <h3 class="text-center text-title text-bold mt-5">Confirm Transaction</h3>
         <div class="container-main-bg text-small p-4 mt-5">
           <div class="d-flex justify-content-between p-3">
             <p>Type of Transaction</p>
-            <p data-testid="p-type-transaction">{{ getTransactionType(props.transaction) }}</p>
+            <p data-testid="p-type-transaction">{{ getTransactionType(transaction) }}</p>
           </div>
           <div class="d-flex justify-content-between p-3 mt-3">
             <p>Transaction ID</p>
@@ -84,7 +136,7 @@ const handleConfirmTransaction = (e: Event) => {
             type="button"
             color="borderless"
             data-testid="button-cancel-transaction"
-            @click="emit('update:show', false)"
+            @click="show = false"
             >Cancel</AppButton
           >
           <AppButton
