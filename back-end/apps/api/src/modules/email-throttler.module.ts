6e8ff90d8aea0ac4625d5e73577ff5e62f60f5a5
Svetoslav Borislavov <svetoslav.borislavov@limechain.tech>
2024-09-03 13:29:20 +0300

    feat: update @nestjs/throttler
    Signed-off-by: Svetoslav Borislavov <svetoslav.borislavov@limechain.tech>

--- back-end/apps/api/src/modules/email-throttler.module.ts
+++ back-end/apps/api/src/modules/email-throttler.module.ts
@@ -1,30 +1,28 @@
 import { Module } from '@nestjs/common';
-import { seconds, ThrottlerModule } from '@nestjs/throttler';
-import { ThrottlerStorageRedisService } from 'nestjs-throttler-storage-redis';
 import { ConfigService } from '@nestjs/config';
+import { seconds, ThrottlerModule } from '@nestjs/throttler';
+import { ThrottlerStorageRedisService } from '@nest-lab/throttler-storage-redis';
 
 @Module({
   imports: [
     ThrottlerModule.forRootAsync({
       imports: [],
       inject: [ConfigService],
-      useFactory: (configService: ConfigService) =>
-        ({
-          storage:
-            new ThrottlerStorageRedisService(configService.getOrThrow('REDIS_URL')),
-          throttlers: [
-            {
-              name: 'anonymous-minute',
-              ttl: seconds(60),
-              limit: configService.getOrThrow<number>('ANONYMOUS_MINUTE_LIMIT'),
-            },
-            {
-              name: 'anonymous-five-second',
-              ttl: seconds(5),
-              limit: configService.getOrThrow<number>('ANONYMOUS_FIVE_SECOND_LIMIT'),
-            },
-          ],
-        }),
+      useFactory: (configService: ConfigService) => ({
+        throttlers: [
+          {
+            name: 'anonymous-minute',
+            ttl: seconds(60),
+            limit: configService.getOrThrow<number>('ANONYMOUS_MINUTE_LIMIT'),
+          },
+          {
+            name: 'anonymous-five-second',
+            ttl: seconds(5),
+            limit: configService.getOrThrow<number>('ANONYMOUS_FIVE_SECOND_LIMIT'),
+          },
+        ],
+        storage: new ThrottlerStorageRedisService(configService.getOrThrow('REDIS_URL')),
+      }),
     }),
   ],
 })
--- back-end/apps/api/src/modules/user-throttler.module.ts
+++ back-end/apps/api/src/modules/user-throttler.module.ts
@@ -1,30 +1,28 @@
 import { Module } from '@nestjs/common';
-import { seconds, ThrottlerModule } from '@nestjs/throttler';
-import { ThrottlerStorageRedisService } from 'nestjs-throttler-storage-redis';
 import { ConfigService } from '@nestjs/config';
+import { seconds, ThrottlerModule } from '@nestjs/throttler';
+import { ThrottlerStorageRedisService } from '@nest-lab/throttler-storage-redis';
 
 @Module({
   imports: [
     ThrottlerModule.forRootAsync({
       imports: [],
       inject: [ConfigService],
-      useFactory: (configService: ConfigService) =>
-        ({
-          storage:
-            new ThrottlerStorageRedisService(configService.getOrThrow('REDIS_URL')),
-          throttlers: [
-            {
-              name: 'user-minute',
-              ttl: seconds(60),
-              limit: 100,
-            },
-            {
-              name: 'user-second',
-              ttl: seconds(1),
-              limit: 10,
-            },
-          ]
-        }),
+      useFactory: (configService: ConfigService) => ({
+        storage: new ThrottlerStorageRedisService(configService.getOrThrow('REDIS_URL')),
+        throttlers: [
+          {
+            name: 'user-minute',
+            ttl: seconds(60),
+            limit: 100,
+          },
+          {
+            name: 'user-second',
+            ttl: seconds(1),
+            limit: 10,
+          },
+        ],
+      }),
     }),
   ],
 })
