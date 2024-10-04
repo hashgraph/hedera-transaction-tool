ef5cf6f633e3744a0879e1f6ef195ce059be807b
Svetoslav Borislavov <svetoslav.borislavov@limechain.tech>
2024-10-04 08:32:47 +0300

    feat: steps connected in Migrate
    Signed-off-by: Svetoslav Borislavov <svetoslav.borislavov@limechain.tech>

--- front-end/src/renderer/pages/Migrate/Migrate.vue
+++ front-end/src/renderer/pages/Migrate/Migrate.vue
@@ -1,8 +1,18 @@
 <script setup lang="ts">
-import type { ModelValue } from './components/MigrateForm.vue';
+import type { RecoveryPhrase } from '@renderer/types';
+
+import { onMounted, ref } from 'vue';
 
 import useSetDynamicLayout from '@renderer/composables/useSetDynamicLayout';
-import MigrateForm from './components/MigrateForm.vue';
+
+import { resetDataLocal } from '@renderer/services/userService';
+
+import DecryptRecoveryPhrase from './components/DecryptRecoveryPhrase.vue';
+import SetupPersonal from './components/SetupPersonal.vue';
+import SetupOrganization from './components/SetupOrganization.vue';
+
+/* Types */
+type StepName = 'recoveryPhrase' | 'personal' | 'organization';
 
 /* Composables */
 useSetDynamicLayout({
@@ -11,27 +21,75 @@ useSetDynamicLayout({
   showMenu: false,
 });
 
+/* State */
+const step = ref<StepName>('recoveryPhrase');
+
+const recoveryPhrase = ref<RecoveryPhrase | null>(null);
+const email = ref<string | null>(null);
+const password = ref<string | null>(null);
+const personalId = ref<string | null>(null);
+const organizationId = ref<string | null>(null);
+
 /* Handlers */
-const handleFormSubmit = (value: ModelValue) => {
-  console.log(value);
+const handleSetRecoveryPhrase = async (value: RecoveryPhrase) => {
+  recoveryPhrase.value = value;
+  step.value = 'personal';
+};
+
+const handleSetPersonalUser = async (
+  _email: string,
+  _password: string | null,
+  _personalId: string,
+) => {
+  email.value = _email;
+  password.value = _password;
+  personalId.value = _personalId;
+  step.value = 'organization';
+};
+
+const handleSetOrganizationId = async (value: string) => {
+  organizationId.value = value;
 };
 
 /* Functions */
+const stepIs = (name: StepName) => step.value === name;
+
+/* Hooks */
+onMounted(async () => {
+  await resetDataLocal();
+});
 </script>
 <template>
   <div class="flex-column flex-centered flex-1 overflow-hidden p-6">
     <div
-      class="container-dark-border flex-column-100 col-12 col-xl-8 col-xxl-7 bg-modal-surface glow-dark-bg p-5"
+      class="container-dark-border col-12 col-sm-10 col-md-8 col-lg-6 col-xl-5 col-xxl-4 bg-modal-surface glow-dark-bg p-5"
     >
       <h4 class="text-title text-semi-bold text-center">Migrate</h4>
 
-      <p class="text-secondary text-small lh-base text-center mt-3">
-        Fill the fields with the corresponding data to automatically migrate the data from the old
-        tool to the new one
-      </p>
-
       <div class="fill-remaining mt-4">
-        <MigrateForm @submit="handleFormSubmit" />
+        <!-- Decrypt Recovery Phrase Step -->
+        <template v-if="stepIs('recoveryPhrase')">
+          <DecryptRecoveryPhrase @set-recovery-phrase="handleSetRecoveryPhrase" />
+        </template>
+
+        <!-- Setup Personal User Step -->
+        <template v-if="stepIs('personal') && recoveryPhrase">
+          <SetupPersonal
+            :recovery-phrase="recoveryPhrase"
+            @set-personal-user="handleSetPersonalUser"
+          />
+        </template>
+
+        <!-- Setup Organization Step -->
+        <template v-if="stepIs('organization') && recoveryPhrase && email && personalId">
+          <SetupOrganization
+            :recovery-phrase="recoveryPhrase"
+            :email="email"
+            :password="password"
+            :personal-id="personalId"
+            @set-organization-id="handleSetOrganizationId"
+          />
+        </template>
       </div>
     </div>
   </div>
