33737d77f8daa093195891d37b41435b9fbedef9
Svetoslav Borislavov <svetoslav.borislavov@limechain.tech>
2024-10-04 08:32:47 +0300

    feat: Summary step created
    Signed-off-by: Svetoslav Borislavov <svetoslav.borislavov@limechain.tech>

---
+++ front-end/src/renderer/pages/Migrate/components/SummaryItem.vue
@@ -0,0 +1,21 @@
+<script setup lang="ts">
+/* Props */
+defineProps<{
+  label: string;
+  value: string;
+  'data-testid'?: string;
+}>();
+</script>
+<template>
+  <div class="row">
+    <div class="col-5">
+      <p class="text-small text-semi-bold">{{ label }}</p>
+    </div>
+    <div class="col-7">
+      <p class="text-small text-secondary overflow-hidden" :data-testid="['data-testid']">
+        {{ value }}
+        <slot></slot>
+      </p>
+    </div>
+  </div>
+</template>
