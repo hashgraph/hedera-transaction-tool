fd29df1521858d2442f72b040616a203ff4b54f4
Svetoslav Borislavov <svetoslav.borislavov@limechain.tech>
2024-10-04 08:32:47 +0300

    feat: Begin keys import created
    Signed-off-by: Svetoslav Borislavov <svetoslav.borislavov@limechain.tech>

---
+++ front-end/src/renderer/pages/Migrate/components/BeginKeysImport.vue
@@ -0,0 +1,37 @@
+<script setup lang="ts">
+import type { RecoveryPhrase } from '@renderer/types';
+
+import { ref, watch } from 'vue';
+
+import { getDataMigrationKeysPath } from '@renderer/services/migrateDataService';
+import { searchEncryptedKeys } from '@renderer/services/encryptedKeys';
+
+import DecryptKeys from '@renderer/components/KeyPair/ImportEncrypted/components/DecryptKeys.vue';
+
+/* Props */
+const props = defineProps<{
+  recoveryPhrase: RecoveryPhrase;
+}>();
+
+/* Emits */
+defineEmits<{
+  (event: 'keysImported', importedCount: number): void;
+}>();
+
+/* State */
+const decryptKeysRef = ref<InstanceType<typeof DecryptKeys> | null>(null);
+
+/* Watchers */
+watch(decryptKeysRef, async () => {
+  if (!decryptKeysRef.value) return;
+
+  const keysPath = await getDataMigrationKeysPath();
+  const encryptedKeyPaths = await searchEncryptedKeys([keysPath]);
+  await decryptKeysRef.value?.process(encryptedKeyPaths, props.recoveryPhrase.words);
+});
+</script>
+<template>
+  <div>
+    <DecryptKeys ref="decryptKeysRef" @end="$emit('keysImported', $event)" />
+  </div>
+</template>
