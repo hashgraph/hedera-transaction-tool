688fc7fb1e2a9ee4aa96b36f392889d0e5d26910
Svetoslav Borislavov <svetoslav.borislavov@limechain.tech>
2024-10-04 08:32:47 +0300

    feat: start Migrate page
    Signed-off-by: Svetoslav Borislavov <svetoslav.borislavov@limechain.tech>

---
+++ front-end/src/renderer/pages/Migrate/Migrate.vue
@@ -0,0 +1,10 @@
+<script setup lang="ts"></script>
+<template>
+  <div class="flex-column flex-centered flex-1 overflow-hidden p-10">
+    <div
+      class="container-dark-border flex-column-100 col-8 col-xl-6 col-xxl-5 glow-dark-bg p-5 mx-auto"
+    >
+      <h2>Migrate</h2>
+    </div>
+  </div>
+</template>
