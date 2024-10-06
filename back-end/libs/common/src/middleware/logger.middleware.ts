6b6d65ad001c503f3f41787811cb651332a2b9a9
John Bair <john.bair@swirldslabs.com>
2024-10-06 00:19:32 -0700

    feat: removed pino, added winston, created winston logger middleware
    Signed-off-by: John Bair <john.bair@swirldslabs.com>

---
+++ back-end/libs/common/src/middleware/logger.middleware.ts
@@ -0,0 +1,30 @@
+import { Injectable, NestMiddleware, Inject } from '@nestjs/common';
+import { WINSTON_MODULE_PROVIDER } from 'nest-winston';
+import { Logger } from 'winston';
+import { maskSensitiveData } from '@app/common';
+
+@Injectable()
+export class LoggerMiddleware implements NestMiddleware {
+  constructor(@Inject(WINSTON_MODULE_PROVIDER) private readonly logger: Logger) {}
+
+  use(req: any, res: any, next: () => void) {
+    const { method, originalUrl, body = {}, query = {} } = req;
+    const start = Date.now();
+
+    const maskedBody = maskSensitiveData(body, ['password', 'email']);
+    const maskedQuery = maskSensitiveData(query, ['token']);
+
+    res.on('finish', () => {
+      const { statusCode } = res;
+      const duration = Date.now() - start;
+      const payload = Object.keys(maskedBody).length || Object.keys(maskedQuery).length
+        ? ` - Payload: ${JSON.stringify({ body: maskedBody, query: maskedQuery })}`
+        : '';
+      this.logger.info(
+        `${method} ${originalUrl} ${statusCode} - ${duration}ms${payload}`
+      );
+    });
+
+    next();
+  }
+}
