487a3fdd2cef22faf98d41682c18b158d8505e2a
Svetoslav Borislavov <svetoslav.borislavov@limechain.tech>
2024-10-08 17:33:34 +0300

    fix: transaction processor props
    Signed-off-by: Svetoslav Borislavov <svetoslav.borislavov@limechain.tech>

--- front-end/src/renderer/pages/CreateTransaction/components/Account/DeleteAccount.vue
+++ front-end/src/renderer/pages/CreateTransaction/components/Account/DeleteAccount.vue
@@ -459,7 +459,6 @@ const columnClass = 'col-4 col-xxxl-3';
 
     <TransactionProcessor
       ref="transactionProcessor"
-      :transaction-bytes="transaction?.toBytes() || null"
       :observers="observers"
       :approvers="approvers"
       :on-executed="handleExecuted"
