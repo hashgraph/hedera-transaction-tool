b8f355b059db08a0ce7457bdec2bdb66e3140efa
Svetoslav Borislavov <svetoslav.borislavov@limechain.tech>
2024-10-11 14:10:05 +0300

    fix: logger
    Signed-off-by: Svetoslav Borislavov <svetoslav.borislavov@limechain.tech>

--- back-end/apps/api/src/auth/auth.module.ts
+++ back-end/apps/api/src/auth/auth.module.ts
@@ -1,20 +1,20 @@
+import { Reflector } from '@nestjs/core';
 import { Module } from '@nestjs/common';
+import { ConfigService } from '@nestjs/config';
 import { PassportModule } from '@nestjs/passport';
 import { JwtModule } from '@nestjs/jwt';
-import { AuthController } from './auth.controller';
-import { AuthService } from './auth.service';
 import { TypeOrmModule } from '@nestjs/typeorm';
-import { UsersModule } from '../users/users.module';
-import { ConfigService } from '@nestjs/config';
-import { LoggerModule } from '@app/common';
-import { JwtStrategy, LocalStrategy, OtpJwtStrategy, OtpVerifiedStrategy } from './strategies';
 
 import { User } from '@entities';
-import { Reflector } from '@nestjs/core';
+
+import { UsersModule } from '../users/users.module';
+
+import { JwtStrategy, LocalStrategy, OtpJwtStrategy, OtpVerifiedStrategy } from './strategies';
+import { AuthController } from './auth.controller';
+import { AuthService } from './auth.service';
 
 @Module({
   imports: [
-    LoggerModule,
     PassportModule.register({}),
     JwtModule.registerAsync({
       imports: [],
