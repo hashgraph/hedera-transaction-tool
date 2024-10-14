b4face610b3f57f02a3b3d8645bedc4d400c679d
Svetoslav Borislavov <svetoslav.borislavov@limechain.tech>
2024-10-14 11:09:24 +0300

    fix: interfaces
    Signed-off-by: Svetoslav Borislavov <svetoslav.borislavov@limechain.tech>

--- front-end/src/main/shared/interfaces/organization/index.ts
+++ front-end/src/main/shared/interfaces/organization/index.ts
@@ -2,6 +2,7 @@ export * from './userKeys';
 export * from './user';
 export * from './transactions';
 export * from './signers';
+export * from './approvers';
 export * from './observers';
 export * from './notification-receiver';
 export * from './notification-preferences';
