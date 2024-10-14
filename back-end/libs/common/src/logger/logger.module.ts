78f7f2f923168773532f28e6136faf3054a7d343
Svetoslav Borislavov <svetoslav.borislavov@limechain.tech>
2024-10-14 08:43:41 +0300

    fix: logger
    Signed-off-by: Svetoslav Borislavov <svetoslav.borislavov@limechain.tech>

--- back-end/libs/common/src/logger/logger.module.ts
+++ back-end/libs/common/src/logger/logger.module.ts
@@ -4,21 +4,25 @@ import { transports, format } from 'winston';
 
 @Module({
   imports: [
-    WinstonModule.forRoot({
-      level: 'info',
-      format: format.combine(
-        format.timestamp({ format: 'YYYY-MM-DD HH:mm:ss' }),
-        format.printf(({ timestamp, level, message, stack }) => {
-          return stack
-            ? `${timestamp} [${level.toUpperCase()}]: ${message} - ${stack}`
-            : `${timestamp} [${level.toUpperCase()}]: ${message}`;
-        })
-      ),
-      transports: [
-        new transports.Console(),
-        new transports.File({ filename: 'combined.log' })
-      ]
-    })
-  ]
+    WinstonModule.forRootAsync({
+      useFactory() {
+        const consoleInstance = new transports.Console();
+        const fileInstance = new transports.File({ filename: 'combined.log' });
+
+        return {
+          level: 'info',
+          format: format.combine(
+            format.timestamp({ format: 'YYYY-MM-DD HH:mm:ss' }),
+            format.printf(({ timestamp, level, message, stack }) => {
+              return stack
+                ? `${timestamp} [${level.toUpperCase()}]: ${message} - ${stack}`
+                : `${timestamp} [${level.toUpperCase()}]: ${message}`;
+            }),
+          ),
+          transports: [consoleInstance, fileInstance],
+        };
+      },
+    }),
+  ],
 })
 export class LoggerModule {}
