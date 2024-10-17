1d29e6d145272b9c8c8e0f6f1347590c0cd3a55b
Svetoslav Borislavov <svetoslav.borislavov@limechain.tech>
2024-10-17 14:10:03 +0300

    feat: add error code infrastructure
    Signed-off-by: Svetoslav Borislavov <svetoslav.borislavov@limechain.tech>

--- back-end/apps/api/src/filters/bad-request-exception.filter.ts
+++ back-end/apps/api/src/filters/bad-request-exception.filter.ts
@@ -8,11 +8,10 @@ export class BadRequestExceptionFilter implements ExceptionFilter {
     const response = ctx.getResponse<Response>();
     const status = exception.getStatus();
 
-    response
-      .status(status)
-      .json({
-        statusCode: status,
-        message: 'The request is invalid. Please check the request body and try again.',
-      });
+    response.status(status).json({
+      statusCode: status,
+      message: 'The request is invalid. Please check the request body and try again.',
+      code: exception.message,
+    });
   }
 }
