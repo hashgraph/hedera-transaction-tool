8e04e8386d3bd9bad90388e472f902151a801911
Svetoslav Borislavov <svetoslav.borislavov@limechain.tech>
2024-10-24 10:13:15 +0300

    feat: appearance settings moved in a component
    Signed-off-by: Svetoslav Borislavov <svetoslav.borislavov@limechain.tech>

--- front-end/src/renderer/pages/Settings/components/GeneralTab/GeneralTab.vue
+++ front-end/src/renderer/pages/Settings/components/GeneralTab/GeneralTab.vue
@@ -1,36 +1,7 @@
 <script setup lang="ts">
-import type { Theme } from '@main/shared/interfaces';
-
-import { onBeforeMount, onBeforeUnmount, ref } from 'vue';
-
-import AppButton from '@renderer/components/ui/AppButton.vue';
 import NetworkSettings from './components/NetworkSettings.vue';
 import DefaultSettings from './components/DefaultSettings.vue';
-
-/* State */
-const theme = ref<Theme>('light');
-const onUpdateUnsubscribe = ref<() => void>();
-
-const handleThemeChange = (newTheme: Theme) => {
-  window.electronAPI.local.theme.toggle(newTheme);
-};
-
-/* Hooks */
-onBeforeMount(async () => {
-  onUpdateUnsubscribe.value = window.electronAPI.local.theme.onThemeUpdate(
-    (newTheme: { themeSource: Theme; shouldUseDarkColors: boolean }) => {
-      document.body.setAttribute('data-bs-theme', newTheme.shouldUseDarkColors ? 'dark' : 'light');
-      theme.value = newTheme.themeSource;
-    },
-  );
-  theme.value = await window.electronAPI.local.theme.mode();
-});
-
-onBeforeUnmount(() => {
-  if (onUpdateUnsubscribe.value) {
-    onUpdateUnsubscribe.value();
-  }
-});
+import AppearanceSettings from './components/AppearanceSettings.vue';
 </script>
 <template>
   <div>
@@ -38,32 +9,7 @@ onBeforeUnmount(() => {
     <NetworkSettings />
 
     <!-- Appearance -->
-    <div class="p-4 border border-2 rounded-3 mt-5">
-      <p>Appearance</p>
-      <div class="mt-4 btn-group">
-        <AppButton
-          :color="'secondary'"
-          data-testid="tab-appearance-dark"
-          :class="{ active: theme === 'dark' }"
-          @click="handleThemeChange('dark')"
-          >Dark</AppButton
-        >
-        <AppButton
-          :color="'secondary'"
-          data-testid="tab-appearance-light"
-          :class="{ active: theme === 'light' }"
-          @click="handleThemeChange('light')"
-          >Light</AppButton
-        >
-        <AppButton
-          :color="'secondary'"
-          data-testid="tab-appearance-system"
-          :class="{ active: theme === 'system' }"
-          @click="handleThemeChange('system')"
-          >System</AppButton
-        >
-      </div>
-    </div>
+    <AppearanceSettings />
 
     <!-- Default Settings -->
     <DefaultSettings />
