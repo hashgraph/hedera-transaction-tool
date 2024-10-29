2e8ac63dacc2b5c2d8b80df6a894a0cd15143ac0
Svetoslav Borislavov <svetoslav.borislavov@limechain.tech>
2024-10-29 11:22:46 +0200

    feat: add unit tests for the IPC handler
    Signed-off-by: Svetoslav Borislavov <svetoslav.borislavov@limechain.tech>

--- front-end/src/tests/main/modules/ipcHandlers/index.spec.ts
+++ front-end/src/tests/main/modules/ipcHandlers/index.spec.ts
@@ -1,11 +1,13 @@
 import listenForThemeEvents from '@main/modules/ipcHandlers/theme';
 import listenForUtilsEvents from '@main/modules/ipcHandlers/utils';
 import listenForLocalUserEvents from '@main/modules/ipcHandlers/localUser';
+import listenForUpdateEvents from '@main/modules/ipcHandlers/update';
 import registerListeners from '@main/modules/ipcHandlers/index';
 
 vi.mock('@main/modules/ipcHandlers/theme', () => ({ default: vi.fn() }));
 vi.mock('@main/modules/ipcHandlers/utils', () => ({ default: vi.fn() }));
 vi.mock('@main/modules/ipcHandlers/localUser', () => ({ default: vi.fn() }));
+vi.mock('@main/modules/ipcHandlers/update', () => ({ default: vi.fn() }));
 
 describe('index', () => {
   test('should call all event listeners', () => {
@@ -14,5 +16,6 @@ describe('index', () => {
     expect(listenForThemeEvents).toHaveBeenCalled();
     expect(listenForUtilsEvents).toHaveBeenCalled();
     expect(listenForLocalUserEvents).toHaveBeenCalled();
+    expect(listenForUpdateEvents).toHaveBeenCalled();
   });
 });
