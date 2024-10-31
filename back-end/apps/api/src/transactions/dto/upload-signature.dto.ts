b37437fcf1cb44e6ed92e3d7ea5bb3063a6f1f1c
Svetoslav Borislavov <svetoslav.borislavov@limechain.tech>
2024-10-31 10:33:38 +0200

    feat: update upload signature dto
    Signed-off-by: Svetoslav Borislavov <svetoslav.borislavov@limechain.tech>

--- back-end/apps/api/src/transactions/dto/upload-signature.dto.ts
+++ back-end/apps/api/src/transactions/dto/upload-signature.dto.ts
@@ -1,32 +1,34 @@
 import { ApiProperty } from '@nestjs/swagger';
-
-import { IsNumber, IsArray, ValidateNested } from 'class-validator';
-import { Type } from 'class-transformer';
+import { SignatureMap } from '@hashgraph/sdk';
 
 import { IsSignatureMap } from '@app/common';
+import { IsNotEmptyObject } from 'class-validator';
 
-export class UploadSignatureDto {
-  @IsNumber()
-  publicKeyId: number;
-
+export class UploadSignatureMapDto {
   @ApiProperty({
     type: 'object',
     example: {
-      '0.0.3':
-        '0xac244c7240650eaa32b60fd4d7d2ef9f49d3bcd1e3ae1df273ede1b4da32f32b25e389d5a8195b6efbc39ac62810348688976c5304fbef33e51cd7505592cd0f',
-      '0.0.5':
-        '0x053bc5e784dc767095fbdafaaefed3553dd384b86877276951c7eb634d1f0191288a2cc72e6477a1661a483a38935ab51297ec84555c1d0bcb68daf77fb49a0b',
-      '0.0.7':
-        '0xccad395302df6b0ea31d15d9ab9c58bc5a6dc6ec9a334dbfb09c321e6fba802bf8873ba03e3e81d80e499d56a318f663d897aff78cedeb1b7a3d43bdf4609a08',
+      '0.0.3': {
+        '0.0.2159149@1730378704.000000000': {
+          '302a300506032b657003210061f37fc1bbf3ff4453712ee6a305c5c7255955f7889ec3bf30426f1863158ef4':
+            '0xac244c7240650eaa32b60fd4d7d2ef9f49d3bcd1e3ae1df273ede1b4da32f32b25e389d5a8195b6efbc39ac62810348688976c5304fbef33e51cd7505592cd0f',
+        },
+      },
+      '0.0.5': {
+        '0.0.2159149@1730378704.000000000': {
+          '302a300506032b657003210061f37fc1bbf3ff4453712ee6a305c5c7255955f7889ec3bf30426f1863158ef4':
+            '0x053bc5e784dc767095fbdafaaefed3553dd384b86877276951c7eb634d1f0191288a2cc72e6477a1661a483a38935ab51297ec84555c1d0bcb68daf77fb49a0b',
+        },
+      },
+      '0.0.7': {
+        '0.0.2159149@1730378704.000000000': {
+          '302a300506032b657003210061f37fc1bbf3ff4453712ee6a305c5c7255955f7889ec3bf30426f1863158ef4':
+            '0xccad395302df6b0ea31d15d9ab9c58bc5a6dc6ec9a334dbfb09c321e6fba802bf8873ba03e3e81d80e499d56a318f663d897aff78cedeb1b7a3d43bdf4609a08',
+        },
+      },
     },
   })
+  @IsNotEmptyObject()
   @IsSignatureMap()
-  signatures: { [key: string]: Buffer };
-}
-
-export class UploadSignatureArrayDto {
-  @IsArray()
-  @ValidateNested({ each: true })
-  @Type(() => UploadSignatureDto)
-  signatures: UploadSignatureDto[];
+  signatureMap: SignatureMap;
 }
