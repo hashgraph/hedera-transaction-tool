87a1f6d02e2d1d82f8d0846a8b8d5af25d8a50c9
Svetoslav Borislavov <svetoslav.borislavov@limechain.tech>
2024-10-31 09:01:09 +0200

    feat: hide quit button
    Signed-off-by: Svetoslav Borislavov <svetoslav.borislavov@limechain.tech>

--- front-end/src/renderer/components/Header.vue
+++ front-end/src/renderer/components/Header.vue
@@ -15,7 +15,6 @@ import { useToast } from 'vue-toast-notification';
 import useCreateTooltips from '@renderer/composables/useCreateTooltips';
 
 import { logout } from '@renderer/services/organization';
-import { quit } from '@renderer/services/electronUtilsService';
 import { updateOrganizationCredentials } from '@renderer/services/organizationCredentials';
 
 import { GLOBAL_MODAL_LOADER_KEY } from '@renderer/providers';
@@ -78,10 +77,6 @@ const handleLogout = async () => {
   }
 };
 
-const handleQuit = async () => {
-  await quit();
-};
-
 /* Hooks */
 onUpdated(() => {
   createTooltips();
@@ -138,18 +133,6 @@ onUpdated(() => {
         data-bs-title="Log out"
       >
         <i class="text-icon-main bi bi-box-arrow-up-right"></i>
-      </span>
-      <span
-        class="container-icon"
-        data-testid="button-quit"
-        @click="handleQuit"
-        data-bs-toggle="tooltip"
-        data-bs-trigger="hover"
-        data-bs-placement="bottom"
-        data-bs-custom-class="wide-tooltip"
-        data-bs-title="Quit"
-      >
-        <i class="text-icon-main bi bi-escape"></i>
       </span>
     </div>
   </div>
