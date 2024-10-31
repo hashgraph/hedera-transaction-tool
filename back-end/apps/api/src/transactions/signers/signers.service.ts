6e0bddc68665157e49cf0d8fb910f2d1af7a9ed8
Svetoslav Borislavov <svetoslav.borislavov@limechain.tech>
2024-10-31 10:33:39 +0200

    feat: update signers service
    Signed-off-by: Svetoslav Borislavov <svetoslav.borislavov@limechain.tech>

--- back-end/apps/api/src/transactions/signers/signers.service.ts
+++ back-end/apps/api/src/transactions/signers/signers.service.ts
@@ -3,13 +3,11 @@ import { ClientProxy } from '@nestjs/microservices';
 import { InjectDataSource, InjectRepository } from '@nestjs/typeorm';
 
 import { DataSource, Repository } from 'typeorm';
-import { Transaction as SDKTransaction } from '@hashgraph/sdk';
+import { PublicKey, Transaction as SDKTransaction } from '@hashgraph/sdk';
 
 import {
-  addTransactionSignatures,
   CHAIN_SERVICE,
   emitUpdateTransactionStatus,
-  isAlreadySigned,
   isExpired,
   MirrorNodeService,
   notifySyncIndicators,
@@ -17,13 +15,13 @@ import {
   NOTIFICATIONS_SERVICE,
   PaginatedResourceDto,
   Pagination,
-  userKeysRequiredToSign,
   validateSignature,
   ErrorCodes,
+  safe,
 } from '@app/common';
 import { Transaction, TransactionSigner, TransactionStatus, User, UserKey } from '@entities';
 
-import { UploadSignatureArrayDto, UploadSignatureDto } from '../dto';
+import { UploadSignatureMapDto } from '../dto';
 
 @Injectable()
 export class SignersService {
@@ -102,168 +100,48 @@ export class SignersService {
   }
 
   /* Upload a signature for the given transaction id */
-  async uploadSignature(
+  async uploadSignatureMap(
     transactionId: number,
-    { publicKeyId, signatures }: UploadSignatureDto,
-    user: User,
-  ): Promise<TransactionSigner> {
-    /* Verify that the user has the key */
-    const userKey = user.keys.find(key => key.id === publicKeyId);
-
-    /* Verify that the transaction exists */
-    const transaction = await this.dataSource.manager.findOneBy(Transaction, { id: transactionId });
-    if (!transaction) throw new BadRequestException(ErrorCodes.TNF);
-
-    const sdkTransaction = SDKTransaction.fromBytes(transaction.transactionBytes);
-    if (isExpired(sdkTransaction)) throw new BadRequestException(ErrorCodes.TE);
-
-    /* Checks if the transaction is canceled */
-    if (transaction.status === TransactionStatus.CANCELED)
-      throw new BadRequestException(ErrorCodes.TC);
-
-    /* Verify that each signature corresponds the correct transaction for the given node and to the public key  */
-    for (const key in signatures) {
-      if (!validateSignature(sdkTransaction, key, signatures[key], userKey.publicKey)) {
-        throw new BadRequestException(ErrorCodes.ISNMPN);
-      }
-    }
-
-    /* Add the signatures to the transaction */
-    if (isAlreadySigned(sdkTransaction, userKey.publicKey))
-      throw new BadRequestException(ErrorCodes.SAD);
-
-    const keysIds = await userKeysRequiredToSign(
-      transaction,
-      user,
-      this.mirrorNodeService,
-      this.dataSource.manager,
-    );
-
-    if (!keysIds.includes(userKey.id)) {
-      throw new BadRequestException(ErrorCodes.KNRS);
-    }
-
-    try {
-      addTransactionSignatures(sdkTransaction, signatures, userKey.publicKey);
-    } catch (error) {
-      throw new BadRequestException(error.message);
-    }
-
-    /* Start a database transaction */
-    const queryRunner = this.dataSource.createQueryRunner();
-
-    await queryRunner.connect();
-    await queryRunner.startTransaction();
-
-    /* Update the transaction */
-    try {
-      Object.assign(transaction, {
-        transactionBytes: sdkTransaction.toBytes(),
-      });
-      await this.dataSource.manager.update(Transaction, { id: transactionId }, transaction);
-    } catch (error) {
-      await queryRunner.rollbackTransaction();
-      await queryRunner.release();
-      throw new BadRequestException(ErrorCodes.FST);
-    }
-
-    /* Create transaction signer record */
-    try {
-      const signer = this.repo.create({
-        user,
-        transaction,
-        userKey,
-      });
-      await this.repo.save(signer);
-
-      /* Commit the database transaction */
-      await queryRunner.commitTransaction();
-
-      emitUpdateTransactionStatus(this.chainService, transactionId);
-      notifyTransactionAction(this.notificationService);
-      notifySyncIndicators(this.notificationService, transactionId, transaction.status);
-
-      return signer;
-    } catch (error) {
-      await queryRunner.rollbackTransaction();
-      await queryRunner.release();
-      throw new BadRequestException(ErrorCodes.FST);
-    }
-  }
-
-  /* Upload a signature for the given transaction id */
-  async uploadSignatures(
-    transactionId: number,
-    { signatures: signaturesArray }: UploadSignatureArrayDto,
+    { signatureMap }: UploadSignatureMapDto,
     user: User,
   ): Promise<TransactionSigner[]> {
     /* Verify that the transaction exists */
     const transaction = await this.dataSource.manager.findOneBy(Transaction, { id: transactionId });
     if (!transaction) throw new BadRequestException(ErrorCodes.TNF);
 
-    const sdkTransaction = SDKTransaction.fromBytes(transaction.transactionBytes);
+    let sdkTransaction = SDKTransaction.fromBytes(transaction.transactionBytes);
     if (isExpired(sdkTransaction)) throw new BadRequestException(ErrorCodes.TE);
 
     /* Checks if the transaction is canceled */
     if (transaction.status === TransactionStatus.CANCELED)
-      throw new BadRequestException(ErrorCodes.TC);
+      throw new BadRequestException(ErrorCodes.TC); //TO DO Move in guard
+
+    /* Validates the signatures */
+    const { data: publicKeys, error } = await safe<PublicKey[]>(
+      validateSignature.bind(this, sdkTransaction, signatureMap),
+    );
+    if (error) throw new BadRequestException(ErrorCodes.ISNMPN);
 
     const userKeys: UserKey[] = [];
+    for (const publicKey of publicKeys) {
+      sdkTransaction = sdkTransaction.addSignature(publicKey, signatureMap);
 
-    for (const { publicKeyId, signatures } of signaturesArray) {
-      /* Get the user key */
-      const userKey = user.keys.find(key => key.id === publicKeyId);
-
-      /* Verify that the signature is not already added */
-      if (isAlreadySigned(sdkTransaction, userKey.publicKey))
-        throw new BadRequestException(ErrorCodes.SAD);
-
-      /* Verify that each signature corresponds the correct transaction for the given node and to the public key  */
-      for (const nodeAccountId in signatures) {
-        if (
-          !validateSignature(
-            sdkTransaction,
-            nodeAccountId,
-            signatures[nodeAccountId],
-            userKey.publicKey,
-          )
-        ) {
-          throw new BadRequestException(
-            `Invalid Signature for Node with Account ID: ${nodeAccountId}`,
-          );
-        }
-      }
-
-      /* Add the signatures to the transaction */
-      try {
-        addTransactionSignatures(sdkTransaction, signatures, userKey.publicKey);
-        userKeys.push(userKey);
-      } catch (error) {
-        throw new BadRequestException(error.message);
-      }
+      const userKey = user.keys.find(key => key.publicKey === publicKey.toStringRaw());
+      if (!userKey) throw new BadRequestException(ErrorCodes.PNY);
+      userKeys.push(userKey);
     }
 
     /* Start a database transaction */
     const queryRunner = this.dataSource.createQueryRunner();
-
     await queryRunner.connect();
     await queryRunner.startTransaction();
 
-    /* Update the transaction */
+    const signers: TransactionSigner[] = [];
     try {
       Object.assign(transaction, {
         transactionBytes: sdkTransaction.toBytes(),
       });
       await this.dataSource.manager.save(Transaction, transaction);
-    } catch (error) {
-      await queryRunner.rollbackTransaction();
-      await queryRunner.release();
-      throw new BadRequestException(ErrorCodes.FST);
-    }
-
-    /* Create transaction signer record */
-    try {
-      const signers: TransactionSigner[] = [];
 
       for (const userKey of userKeys) {
         const signer = this.repo.create({
@@ -277,17 +155,17 @@ export class SignersService {
       /* Commit the database transaction */
       await queryRunner.commitTransaction();
       await queryRunner.release();
-
-      emitUpdateTransactionStatus(this.chainService, transactionId);
-      notifyTransactionAction(this.notificationService);
-      notifySyncIndicators(this.notificationService, transactionId, transaction.status);
-
-      return signers;
     } catch (error) {
       await queryRunner.rollbackTransaction();
       await queryRunner.release();
       throw new BadRequestException(ErrorCodes.FST);
     }
+
+    emitUpdateTransactionStatus(this.chainService, transactionId);
+    notifyTransactionAction(this.notificationService);
+    notifySyncIndicators(this.notificationService, transactionId, transaction.status);
+
+    return signers;
   }
 
   /* Remove the signature for the given id */
