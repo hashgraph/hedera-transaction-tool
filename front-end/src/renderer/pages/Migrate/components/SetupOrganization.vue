cf6230e6513dee85e80396853cd16760f22fadb0
Svetoslav Borislavov <svetoslav.borislavov@limechain.tech>
2024-11-15 09:48:03 +0200

    fix: changed migration organization data bugs
    Signed-off-by: Svetoslav Borislavov <svetoslav.borislavov@limechain.tech>

--- front-end/src/renderer/pages/Migrate/components/SetupOrganization.vue
+++ front-end/src/renderer/pages/Migrate/components/SetupOrganization.vue
@@ -5,7 +5,7 @@ import type { ModelValue, SubmitCallback } from './SetupOrganizationForm.vue';
 
 import { ref } from 'vue';
 
-import { addOrganization } from '@renderer/services/organizationsService';
+import { addOrganization, deleteOrganization } from '@renderer/services/organizationsService';
 import { changePassword, login } from '@renderer/services/organization';
 import { addOrganizationCredentials } from '@renderer/services/organizationCredentials';
 
@@ -29,7 +29,9 @@ const emit = defineEmits<{
 const loading = ref(false);
 const loadingText = ref<string>('');
 const organizationId = ref<string | null>(null);
+const organizationURL = ref<string | null>(null);
 const organizationUserId = ref<number | null>(null);
+const organizationEmail = ref<string | null>(null);
 const organizationJwtToken = ref<string | null>(null);
 const loggedIn = ref(false);
 
@@ -37,19 +39,26 @@ const loggedIn = ref(false);
 const handleFormSubmit: SubmitCallback = async (formData: ModelValue) => {
   loading.value = true;
 
+  const sameOrganization = organizationURL.value === formData.organizationURL;
+  const sameEmail = organizationEmail.value === formData.organizationEmail;
+
   /* Add Organization */
-  if (!organizationId.value) {
+  if (!organizationId.value || !sameOrganization) {
     loadingText.value = 'Adding Organization...';
+    if (organizationId.value) {
+      await safeAwait(deleteOrganization(organizationId.value));
+    }
     const setupOrganizationResult = await safeAwait(setupOrganization(formData));
     if ('error' in setupOrganizationResult) {
       loading.value = false;
       throw setupOrganizationResult.error;
     }
     organizationId.value = setupOrganizationResult.data;
+    organizationURL.value = formData.organizationURL;
   }
 
   /* Login in Organization */
-  if (!loggedIn.value) {
+  if (!loggedIn.value || !sameOrganization || !sameEmail) {
     loadingText.value = 'Logging in Organization...';
     const loginInOrganizationResult = await safeAwait(loginInOrganization(formData));
     if (loginInOrganizationResult.error instanceof Error) {
@@ -88,6 +97,7 @@ const handleFormSubmit: SubmitCallback = async (formData: ModelValue) => {
       props.personalUser.personalId,
       organizationJwtToken.value || '',
       props.personalUser.password,
+      true,
     ),
   );
   if ('error' in addOrganizationCredentialsResult) {
@@ -113,26 +123,27 @@ const setupOrganization = async ({ organizationURL, organizationNickname }: Mode
   return id;
 };
 
-const loginInOrganization = async ({
-  organizationURL,
-  organizationEmail,
-  temporaryOrganizationPassword,
-}: ModelValue) => {
+const loginInOrganization = async (data: ModelValue) => {
   let email;
   if (props.personalUser.useKeychain) {
-    if (!organizationEmail) {
+    if (!data.organizationEmail) {
       throw new Error('(BUG) Organization email is required');
     }
-    email = organizationEmail;
+    email = data.organizationEmail;
   } else {
     email = props.personalUser.email;
   }
 
-  const { id, jwtToken } = await login(organizationURL, email, temporaryOrganizationPassword);
-  toggleAuthTokenInSessionStorage(organizationURL, jwtToken, false);
+  const { id, jwtToken } = await login(
+    data.organizationURL,
+    email,
+    data.temporaryOrganizationPassword,
+  );
+  toggleAuthTokenInSessionStorage(data.organizationURL, jwtToken, false);
 
   organizationUserId.value = id;
   organizationJwtToken.value = jwtToken;
+  organizationEmail.value = email;
 };
 
 const setNewPassword = async ({
