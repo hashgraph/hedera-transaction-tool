3bc9f4abf7c2bdaf8200f87410750eb795b4052e
Hristiyan <hristiyan.valkov@limechain.tech>
2024-12-05 13:18:54 +0200

    remove: template in Settings
    Signed-off-by: Hristiyan <hristiyan.valkov@limechain.tech>

--- front-end/src/renderer/pages/Settings/Settings.vue
+++ front-end/src/renderer/pages/Settings/Settings.vue
@@ -116,12 +116,10 @@ watch(router.currentRoute, newRoute => {
         </div>
       </div>
       <div class="flex-column-100 overflow-hidden mt-7">
-        <AppTabs :items="visibleTabItems" v-model:active-index="activeTabIndex">
-          <template #[activeTabTitle]>
+        <AppTabs :items="visibleTabItems" v-model:active-index="activeTabIndex"/>
             <div class="fill-remaining pe-4 mt-6">
               <RouterView />
             </div>
-          </template>
         </AppTabs>
       </div>
     </div>
