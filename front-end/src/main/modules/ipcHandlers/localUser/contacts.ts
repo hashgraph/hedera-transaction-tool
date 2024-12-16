db00785ee2518300239121c0d186a13b3647d9d7
Svetoslav Borislavov <svetoslav.borislavov@limechain.tech>
2024-12-16 11:18:02 +0200

    refactor: update contacts IPC module
    Signed-off-by: Svetoslav Borislavov <svetoslav.borislavov@limechain.tech>

--- front-end/src/main/modules/ipcHandlers/localUser/contacts.ts
+++ front-end/src/main/modules/ipcHandlers/localUser/contacts.ts
@@ -1,40 +1,12 @@
-import { ipcMain } from 'electron';
-
 import {
   addContact,
   getOrganizationContacts,
   removeContact,
   updateContact,
 } from '@main/services/localUser/contacts';
-import { Prisma } from '@prisma/client';
-
-const createChannelName = (...props) => ['contacts', ...props].join(':');
+import { createIPCChannel } from '@main/utils/electronInfra';
 
 export default () => {
   /* Contacts */
-
-  // Get Organization contacts
-  ipcMain.handle(
-    createChannelName('getOrganizationContacts'),
-    (_e, userId: string, organizationId: string, organizationUserIdOwner: number) =>
-      getOrganizationContacts(userId, organizationId, organizationUserIdOwner),
-  );
-
-  // Add
-  ipcMain.handle(
-    createChannelName('addContact'),
-    (_e, contact: Prisma.ContactUncheckedCreateInput) => addContact(contact),
-  );
-
-  // Update
-  ipcMain.handle(
-    createChannelName('updateContact'),
-    (_e, contactId: string, userId: string, contact: Prisma.ContactUncheckedUpdateInput) =>
-      updateContact(contactId, userId, contact),
-  );
-
-  // Remove
-  ipcMain.handle(createChannelName('removeContact'), (_e, userId: string, contactId: string) =>
-    removeContact(userId, contactId),
-  );
+  createIPCChannel('contacts', [addContact, getOrganizationContacts, updateContact, removeContact]);
 };
