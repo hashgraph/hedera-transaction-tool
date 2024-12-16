722c72addb923c91e4de75dfc6dfd835cd0bc6c2
Svetoslav Borislavov <svetoslav.borislavov@limechain.tech>
2024-12-16 11:11:36 +0200

    refactor: update claims IPC module
    Signed-off-by: Svetoslav Borislavov <svetoslav.borislavov@limechain.tech>

--- front-end/src/main/modules/ipcHandlers/localUser/claim.ts
+++ front-end/src/main/modules/ipcHandlers/localUser/claim.ts
@@ -1,35 +1,12 @@
-import { ipcMain } from 'electron';
-
-import { Prisma } from '@prisma/client';
-
 import { addClaim, getClaims, removeClaims, updateClaim } from '@main/services/localUser/claim';
-
-const createChannelName = (...props) => ['claim', ...props].join(':');
+import { createIPCChannel, renameFunc } from '@main/utils/electronInfra';
 
 export default () => {
   /* Claim */
-
-  // Add claim
-  ipcMain.handle(
-    createChannelName('add'),
-    (_e, userId: string, claimKey: string, claimValue: string) =>
-      addClaim(userId, claimKey, claimValue),
-  );
-
-  // Get claims
-  ipcMain.handle(createChannelName('get'), (_e, findArgs: Prisma.ClaimFindManyArgs) =>
-    getClaims(findArgs),
-  );
-
-  // Update claim
-  ipcMain.handle(
-    createChannelName('update'),
-    (_e, userId: string, claimKey: string, claimValue: string) =>
-      updateClaim(userId, claimKey, claimValue),
-  );
-
-  // Remove claim
-  ipcMain.handle(createChannelName('remove'), (_e, userId: string, claimKeys: string[]) =>
-    removeClaims(userId, claimKeys),
-  );
+  createIPCChannel('claim', [
+    renameFunc(addClaim, 'add'),
+    renameFunc(getClaims, 'get'),
+    renameFunc(updateClaim, 'update'),
+    renameFunc(removeClaims, 'remove'),
+  ]);
 };
