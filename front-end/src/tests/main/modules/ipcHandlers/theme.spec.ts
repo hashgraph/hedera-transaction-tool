e7046f130b0f8e47bbc71265837b33f780c1ae39
Svetoslav Borislavov <svetoslav.borislavov@limechain.tech>
2024-12-16 13:11:41 +0200

    refactor: theme IPC module unit tests
    Signed-off-by: Svetoslav Borislavov <svetoslav.borislavov@limechain.tech>

--- front-end/src/tests/main/modules/ipcHandlers/theme.spec.ts
+++ front-end/src/tests/main/modules/ipcHandlers/theme.spec.ts
@@ -1,40 +1,11 @@
-import { MockedClass, MockedObject } from 'vitest';
+import { getIPCHandler, invokeIPCHandler } from '../../_utils_';
 
 import registerThemeListeners, {
   removeListeners,
   sendUpdateThemeEventTo,
 } from '@main/modules/ipcHandlers/theme';
 
-import { BrowserWindow, ipcMain, nativeTheme } from 'electron';
-import { mockDeep } from 'vitest-mock-extended';
-
-vi.mock('@electron-toolkit/utils', () => ({ is: { dev: true } }));
-vi.mock('electron', () => {
-  const bw = vi.fn() as unknown as MockedClass<typeof BrowserWindow>;
-  bw.getAllWindows = vi.fn();
-  Object.defineProperty(bw.prototype, 'webContents', {
-    value: {
-      send: vi.fn(),
-    },
-    writable: false,
-    enumerable: true,
-  });
-
-  return {
-    BrowserWindow: bw,
-    ipcMain: {
-      on: vi.fn(),
-      handle: vi.fn(),
-    },
-    nativeTheme: {
-      removeAllListeners: vi.fn(),
-      listenerCount: vi.fn(),
-      shouldUseDarkColors: true,
-      themeSource: 'system',
-      on: vi.fn(),
-    },
-  };
-});
+import { BrowserWindow, nativeTheme } from 'electron';
 
 describe('registerThemeListeners', () => {
   beforeEach(() => {
@@ -42,70 +13,42 @@ describe('registerThemeListeners', () => {
     registerThemeListeners();
   });
 
-  const ipcMainMO = ipcMain as unknown as MockedObject<Electron.IpcMain>;
-  const event: Electron.IpcMainEvent = mockDeep<Electron.IpcMainEvent>();
-
   test('Should register handlers for each theme event', () => {
     const eventNames = ['isDark', 'toggle', 'mode'];
-
-    expect(
-      eventNames.every(util =>
-        ipcMainMO.handle.mock.calls.some(([channel]) => channel === `theme:${util}`),
-      ),
-    ).toBe(true);
+    expect(eventNames.every(util => getIPCHandler(`theme:${util}`))).toBe(true);
   });
 
-  test("Should return the nativeTheme's value in theme:isDark", () => {
-    const isDarkHandler = ipcMainMO.handle.mock.calls.find(([event]) => {
-      return event === 'theme:isDark';
-    });
-
-    expect(isDarkHandler).toBeDefined();
-
-    if (isDarkHandler) {
-      nativeTheme.themeSource = 'dark';
-      expect(isDarkHandler[1](event)).toEqual(true);
-    }
+  test("Should return the nativeTheme's value in theme:isDark", async () => {
+    Object.defineProperty(nativeTheme, 'shouldUseDarkColors', { value: true });
+    expect(await invokeIPCHandler('theme:isDark')).toEqual(true);
   });
 
-  test('Should change the themeSource in theme:toggle', () => {
-    const toggleHandler = ipcMainMO.handle.mock.calls.find(([event]) => {
-      return event === 'theme:toggle';
-    });
-
-    expect(toggleHandler).toBeDefined();
-
-    if (toggleHandler) {
-      nativeTheme.themeSource = 'light';
-      toggleHandler[1](event, 'dark');
-      expect(nativeTheme.themeSource).toEqual('dark');
-    }
+  test('Should change the themeSource in theme:toggle', async () => {
+    Object.defineProperty(nativeTheme, 'themeSource', { value: 'light' });
+    await invokeIPCHandler('theme:toggle', 'dark');
+    expect(nativeTheme.themeSource).toEqual('dark');
   });
 
-  test('Should return the themeSource in theme:mode', () => {
-    const modeHandler = ipcMainMO.handle.mock.calls.find(([event]) => {
-      return event === 'theme:mode';
-    });
-
-    expect(modeHandler).toBeDefined();
-
-    if (modeHandler) {
-      nativeTheme.themeSource = 'light';
-      expect(modeHandler[1](event)).toEqual('light');
-    }
+  test('Should return the themeSource in theme:mode', async () => {
+    nativeTheme.themeSource = 'light';
+    expect(await invokeIPCHandler('theme:mode')).toEqual('light');
   });
 });
 
 describe('sendUpdateThemeEventTo', () => {
   test('Should send the theme:update event to the window', () => {
     vi.mocked(nativeTheme.listenerCount).mockReturnValueOnce(0);
-    sendUpdateThemeEventTo(new BrowserWindow());
+    const window = {
+      webContents: {
+        send: vi.fn(),
+      },
+    } as unknown as BrowserWindow;
 
-    const nativeThemeMO = nativeTheme as unknown as MockedObject<Electron.NativeTheme>;
+    sendUpdateThemeEventTo(window);
 
-    nativeThemeMO.on.mock.calls[0][1]();
+    vi.mocked(nativeTheme).on.mock.calls[0][1]();
 
-    expect(BrowserWindow.prototype.webContents.send).toHaveBeenCalledWith('theme:update', {
+    expect(window.webContents.send).toHaveBeenCalledWith('theme:update', {
       shouldUseDarkColors: nativeTheme.shouldUseDarkColors,
       themeSource: nativeTheme.themeSource,
     });
@@ -116,9 +59,7 @@ describe('removeListeners', () => {
   test('Should remove all nativeTheme listeners for updated event', () => {
     removeListeners();
 
-    const nativeThemeMO = nativeTheme as unknown as MockedObject<Electron.NativeTheme>;
-
-    nativeThemeMO.on.mock.calls[0][1]();
+    vi.mocked(nativeTheme).on.mock.calls[0][1]();
 
     expect(nativeTheme.removeAllListeners).toHaveBeenCalledWith('updated');
   });
