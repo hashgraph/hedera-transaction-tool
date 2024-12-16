32ad84c2f8472011e930b3300db9fdfc09518410
Svetoslav Borislavov <svetoslav.borislavov@limechain.tech>
2024-12-16 12:41:39 +0200

    refactor: organizationCredentials IPC modules
    Signed-off-by: Svetoslav Borislavov <svetoslav.borislavov@limechain.tech>

--- front-end/src/main/modules/ipcHandlers/localUser/organizationCredentials.ts
+++ front-end/src/main/modules/ipcHandlers/localUser/organizationCredentials.ts
@@ -7,17 +7,17 @@ import {
   deleteOrganizationCredentials,
   tryAutoSignIn,
 } from '@main/services/localUser';
-import { createIPCChannel } from '@main/utils/electronInfra';
+import { createIPCChannel, renameFunc } from '@main/utils/electronInfra';
 
 export default () => {
   /* Organization Credentials */
   createIPCChannel('organizationCredentials', [
-    getOrganizationTokens,
-    organizationsToSignIn,
-    shouldSignInOrganization,
-    addOrganizationCredentials,
-    updateOrganizationCredentials,
-    deleteOrganizationCredentials,
-    tryAutoSignIn,
+    renameFunc(getOrganizationTokens, 'getOrganizationTokens'),
+    renameFunc(organizationsToSignIn, 'organizationsToSignIn'),
+    renameFunc(shouldSignInOrganization, 'shouldSignInOrganization'),
+    renameFunc(addOrganizationCredentials, 'addOrganizationCredentials'),
+    renameFunc(updateOrganizationCredentials, 'updateOrganizationCredentials'),
+    renameFunc(deleteOrganizationCredentials, 'deleteOrganizationCredentials'),
+    renameFunc(tryAutoSignIn, 'tryAutoSignIn'),
   ]);
 };
