7962696bf1701dfa66fc5404919e2c951a780ac3
Svetoslav Borislavov <svetoslav.borislavov@limechain.tech>
2024-12-16 12:01:01 +0200

    refactor: claim IPC unit tests
    Signed-off-by: Svetoslav Borislavov <svetoslav.borislavov@limechain.tech>

--- front-end/src/tests/main/modules/ipcHandlers/localUser/claim.spec.ts
+++ front-end/src/tests/main/modules/ipcHandlers/localUser/claim.spec.ts
@@ -1,84 +1,52 @@
-import { MockedObject } from 'vitest';
 import { mockDeep } from 'vitest-mock-extended';
+
+import { getIPCHandler, invokeIPCHandler } from '../../../_utils_';
 
 import registerClaimHandlers from '@main/modules/ipcHandlers/localUser/claim';
 
 import { Prisma } from '@prisma/client';
 import { addClaim, getClaims, removeClaims, updateClaim } from '@main/services/localUser/claim';
-import { ipcMain } from 'electron';
 
-vi.mock('electron', () => ({
-  ipcMain: { handle: vi.fn() },
-}));
-
-vi.mock('@main/services/localUser/claim', () => ({
-  addClaim: vi.fn(),
-  getClaims: vi.fn(),
-  removeClaims: vi.fn(),
-  updateClaim: vi.fn(),
-}));
+vi.mock('@main/services/localUser/claim', () => mockDeep());
 
 describe('IPC handlers Claims', () => {
   beforeEach(() => {
-    vi.clearAllMocks();
+    vi.resetAllMocks();
     registerClaimHandlers();
   });
 
-  const ipcMainMO = ipcMain as unknown as MockedObject<Electron.IpcMain>;
-  const event: Electron.IpcMainEvent = mockDeep<Electron.IpcMainEvent>();
+  const userId = 'userId';
+  const claimKey = 'claimKey';
+  const claimKeys = ['claimKey1', 'claimKey2'];
+  const claimValue = 'claimValue';
 
   test('Should register handlers for each event', () => {
     const events = ['add', 'get', 'update', 'remove'];
-
-    expect(
-      events.every(e => ipcMainMO.handle.mock.calls.some(([channel]) => channel === `claim:${e}`)),
-    ).toBe(true);
+    expect(events.every(e => getIPCHandler(`claim:${e}`))).toBe(true);
   });
 
   test('Should set up addClaim handler', async () => {
-    const handler = ipcMainMO.handle.mock.calls.find(([e]) => e === 'claim:add');
-    expect(handler).toBeDefined();
-
-    const userId = 'userId';
-    const claimKey = 'claimKey';
-    const claimValue = 'claimValue';
-
-    handler && (await handler[1](event, userId, claimKey, claimValue));
+    await invokeIPCHandler('claim:add', userId, claimKey, claimValue);
     expect(addClaim).toHaveBeenCalledWith(userId, claimKey, claimValue);
   });
 
   test('Should set up getClaims handler', async () => {
-    const handler = ipcMainMO.handle.mock.calls.find(([e]) => e === 'claim:get');
-    expect(handler).toBeDefined();
-
     const findArgs: Prisma.ClaimFindManyArgs = {
       where: { user_id: 'userId' },
     };
 
-    handler && (await handler[1](event, findArgs));
+    await invokeIPCHandler('claim:get', findArgs);
+
     expect(getClaims).toHaveBeenCalledWith(findArgs);
   });
 
   test('Should set up updateClaim handler', async () => {
-    const handler = ipcMainMO.handle.mock.calls.find(([e]) => e === 'claim:update');
-    expect(handler).toBeDefined();
-
-    const userId = 'userId';
-    const claimKey = 'claimKey';
-    const claimValue = 'claimValue';
-
-    handler && (await handler[1](event, userId, claimKey, claimValue));
+    await invokeIPCHandler('claim:update', userId, claimKey, claimValue);
     expect(updateClaim).toHaveBeenCalledWith(userId, claimKey, claimValue);
   });
 
   test('Should set up removeClaims handler', async () => {
-    const handler = ipcMainMO.handle.mock.calls.find(([e]) => e === 'claim:remove');
-    expect(handler).toBeDefined();
-
-    const userId = 'userId';
-    const claimKeys = ['claimKey1', 'claimKey2'];
-
-    handler && (await handler[1](event, userId, claimKeys));
+    await invokeIPCHandler('claim:remove', userId, claimKeys);
     expect(removeClaims).toHaveBeenCalledWith(userId, claimKeys);
   });
 });
