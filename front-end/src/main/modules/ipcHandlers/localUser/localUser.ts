317a6e50db3af094c5b51b91ca34a908af39a681
Svetoslav Borislavov <svetoslav.borislavov@limechain.tech>
2024-12-16 12:38:32 +0200

    refactor: localUser IPC unit tests
    Signed-off-by: Svetoslav Borislavov <svetoslav.borislavov@limechain.tech>

--- front-end/src/main/modules/ipcHandlers/localUser/localUser.ts
+++ front-end/src/main/modules/ipcHandlers/localUser/localUser.ts
@@ -11,11 +11,11 @@ import { createIPCChannel, renameFunc } from '@main/utils/electronInfra';
 export default () => {
   /* Local User */
   createIPCChannel('localUser', [
-    login,
-    register,
-    resetData,
-    comparePasswords,
-    changePassword,
+    renameFunc(login, 'login'),
+    renameFunc(register, 'register'),
+    renameFunc(resetData, 'resetData'),
+    renameFunc(comparePasswords, 'comparePasswords'),
+    renameFunc(changePassword, 'changePassword'),
     renameFunc(getUsersCount, 'usersCount'),
   ]);
 };
--- front-end/src/tests/main/modules/ipcHandlers/localUser/localUser.spec.ts
+++ front-end/src/tests/main/modules/ipcHandlers/localUser/localUser.spec.ts
@@ -1,4 +1,6 @@
-import { ipcMain } from 'electron';
+import { mockDeep } from 'vitest-mock-extended';
+
+import { getIPCHandler, invokeIPCHandler } from '../../../_utils_';
 
 import registerLocalUserHandlers from '@main/modules/ipcHandlers/localUser/localUser';
 
@@ -10,107 +12,62 @@ import {
   register,
   resetData,
 } from '@main/services/localUser';
-import { MockedObject } from 'vitest';
-import { mockDeep } from 'vitest-mock-extended';
 
-vi.mock('electron', () => ({
-  ipcMain: { handle: vi.fn() },
-}));
-
-vi.mock('@main/services/localUser', () => ({
-  changePassword: vi.fn(),
-  comparePasswords: vi.fn(),
-  getUsersCount: vi.fn(),
-  login: vi.fn(),
-  register: vi.fn(),
-  resetData: vi.fn(),
-}));
+vi.mock('@main/services/localUser', () => mockDeep());
 
 describe('IPC handlers localUser', () => {
   beforeEach(() => {
-    vi.clearAllMocks();
+    vi.resetAllMocks();
     registerLocalUserHandlers();
   });
 
-  const ipcMainMO = ipcMain as unknown as MockedObject<Electron.IpcMain>;
-  const event: Electron.IpcMainEvent = mockDeep<Electron.IpcMainEvent>();
+  const userId = 'user1';
+  const email = 'user@example.com';
+  const password = 'password';
+  const oldPassword = 'oldPassword';
+  const newPassword = 'newPassword';
 
   test('Should register handlers for each event', () => {
-    const event = ['register', 'resetData', 'usersCount', 'comparePasswords', 'changePassword'];
-
-    expect(
-      event.every(util =>
-        ipcMainMO.handle.mock.calls.some(([channel]) => channel === `localUser:${util}`),
-      ),
-    ).toBe(true);
+    const event = [
+      'login',
+      'register',
+      'resetData',
+      'usersCount',
+      'comparePasswords',
+      'changePassword',
+    ];
+    expect(event.every(util => getIPCHandler(`localUser:${util}`))).toBe(true);
   });
 
   test('Should set up login handler', async () => {
-    const loginHandler = ipcMainMO.handle.mock.calls.find(([e]) => e === 'localUser:login');
-    expect(loginHandler).toBeDefined();
-
-    const email = 'user@example.com';
-    const password = 'password';
     const autoRegister = true;
 
-    loginHandler && (await loginHandler[1](event, email, password, autoRegister));
+    await invokeIPCHandler('localUser:login', email, password, autoRegister);
     expect(login).toHaveBeenCalledWith(email, password, autoRegister);
   });
 
   test('Should set up register handler', async () => {
-    const registerHandler = ipcMainMO.handle.mock.calls.find(([e]) => e === 'localUser:register');
-    expect(registerHandler).toBeDefined();
-
-    const email = 'user@example.com';
-    const password = 'password';
-
-    registerHandler && (await registerHandler[1](event, email, password));
+    await invokeIPCHandler('localUser:register', email, password);
     expect(register).toHaveBeenCalledWith(email, password);
   });
 
   test('Should set up resetData handler', async () => {
-    const resetDataHandler = ipcMainMO.handle.mock.calls.find(([e]) => e === 'localUser:resetData');
-    expect(resetDataHandler).toBeDefined();
-
-    resetDataHandler && (await resetDataHandler[1](event));
+    await invokeIPCHandler('localUser:resetData');
     expect(resetData).toHaveBeenCalled();
   });
 
   test('Should set up usersCount handler', async () => {
-    const usersCountHandler = ipcMainMO.handle.mock.calls.find(
-      ([e]) => e === 'localUser:usersCount',
-    );
-    expect(usersCountHandler).toBeDefined();
-
-    usersCountHandler && (await usersCountHandler[1](event));
+    await invokeIPCHandler('localUser:usersCount');
     expect(getUsersCount).toHaveBeenCalled();
   });
 
   test('Should set up comparePasswords handler', async () => {
-    const comparePasswordsHandler = ipcMainMO.handle.mock.calls.find(
-      ([e]) => e === 'localUser:comparePasswords',
-    );
-    expect(comparePasswordsHandler).toBeDefined();
-
-    const userId = 'user1';
-    const password = 'password';
-
-    comparePasswordsHandler && (await comparePasswordsHandler[1](event, userId, password));
+    await invokeIPCHandler('localUser:comparePasswords', userId, password);
     expect(comparePasswords).toHaveBeenCalledWith(userId, password);
   });
 
   test('Should set up changePassword handler', async () => {
-    const changePasswordHandler = ipcMainMO.handle.mock.calls.find(
-      ([e]) => e === 'localUser:changePassword',
-    );
-    expect(changePasswordHandler).toBeDefined();
-
-    const userId = 'user1';
-    const oldPassword = 'oldPassword';
-    const newPassword = 'newPassword';
-
-    changePasswordHandler &&
-      (await changePasswordHandler[1](event, userId, oldPassword, newPassword));
+    await invokeIPCHandler('localUser:changePassword', userId, oldPassword, newPassword);
     expect(changePassword).toHaveBeenCalledWith(userId, oldPassword, newPassword);
   });
 });
