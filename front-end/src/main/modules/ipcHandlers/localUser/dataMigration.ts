b4590094e380129fe8f1652438497bd6fe2e923f
Svetoslav Borislavov <svetoslav.borislavov@limechain.tech>
2024-12-16 11:19:12 +0200

    refacotr: update dataMigration IPC module
    Signed-off-by: Svetoslav Borislavov <svetoslav.borislavov@limechain.tech>

--- front-end/src/main/modules/ipcHandlers/localUser/dataMigration.ts
+++ front-end/src/main/modules/ipcHandlers/localUser/dataMigration.ts
@@ -1,40 +1,17 @@
-import { ipcMain } from 'electron';
-
-import { MigrateUserDataResult } from '@main/shared/interfaces/migration';
-
 import {
   locateDataMigrationFiles,
   decryptMigrationMnemonic,
   getDataMigrationKeysPath,
   migrateUserData,
 } from '@main/services/localUser';
-
-const createChannelName = (...props) => ['dataMigration', ...props].join(':');
+import { createIPCChannel } from '@main/utils/electronInfra';
 
 export default () => {
-  // Locates the data migration files
-  ipcMain.handle(createChannelName('locateDataMigrationFiles'), async (): Promise<boolean> => {
-    return locateDataMigrationFiles();
-  });
-
-  // Decrypts the encrypted key in the given file path
-  ipcMain.handle(
-    createChannelName('decryptMigrationMnemonic'),
-    async (_e, password: string): Promise<string[] | null> => {
-      return decryptMigrationMnemonic(password);
-    },
-  );
-
-  // Gets the path of keys to migrate
-  ipcMain.handle(createChannelName('getDataMigrationKeysPath'), async (): Promise<string> => {
-    return getDataMigrationKeysPath();
-  });
-
-  // Decrypts the encrypted key in the given file path
-  ipcMain.handle(
-    createChannelName('migrateUserData'),
-    async (_e, userId: string): Promise<MigrateUserDataResult> => {
-      return migrateUserData(userId);
-    },
-  );
+  /* Data Migration */
+  createIPCChannel('dataMigration', [
+    locateDataMigrationFiles,
+    decryptMigrationMnemonic,
+    getDataMigrationKeysPath,
+    migrateUserData,
+  ]);
 };
