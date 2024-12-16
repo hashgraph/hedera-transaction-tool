6e2fd587d25f7b4f1112a8354334b0ce47daced3
Svetoslav Borislavov <svetoslav.borislavov@limechain.tech>
2024-12-16 11:31:37 +0200

    refactor: update organization IPC module
    Signed-off-by: Svetoslav Borislavov <svetoslav.borislavov@limechain.tech>

--- front-end/src/main/modules/ipcHandlers/localUser/organizations.ts
+++ front-end/src/main/modules/ipcHandlers/localUser/organizations.ts
@@ -1,40 +1,17 @@
-import { ipcMain } from 'electron';
-
-import { Prisma } from '@prisma/client';
-
 import {
   getOrganizations,
   addOrganization,
   updateOrganization,
   removeOrganization,
 } from '@main/services/localUser';
-
-const createChannelName = (...props) => ['organizations', ...props].join(':');
+import { createIPCChannel, renameFunc } from '@main/utils/electronInfra';
 
 export default () => {
   /* Organizations */
-
-  // Get all organizations
-  ipcMain.handle(createChannelName('getOrganizations'), () => getOrganizations());
-
-  // Add a organization
-  ipcMain.handle(
-    createChannelName('addOrganization'),
-    (_e, organization: Prisma.OrganizationCreateInput) => addOrganization(organization),
-  );
-
-  // Update a organization
-  ipcMain.handle(
-    createChannelName('updateOrganization'),
-    (
-      _e,
-      id: string,
-      organization: Prisma.OrganizationUncheckedUpdateWithoutOrganizationCredentialsInput,
-    ) => updateOrganization(id, organization),
-  );
-
-  // Delete specific organization
-  ipcMain.handle(createChannelName('deleteOrganization'), (_e, id: string) =>
-    removeOrganization(id),
-  );
+  createIPCChannel('organizations', [
+    addOrganization,
+    getOrganizations,
+    updateOrganization,
+    renameFunc(removeOrganization, 'deleteOrganization'),
+  ]);
 };
