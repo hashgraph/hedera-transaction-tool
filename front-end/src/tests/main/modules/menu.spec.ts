d6bc9d510f644dd6bda77ca9f73e475d93d84328
Svetoslav Borislavov <svetoslav.borislavov@limechain.tech>
2024-12-16 13:37:09 +0200

    refactor: menu unit tests
    Signed-off-by: Svetoslav Borislavov <svetoslav.borislavov@limechain.tech>

--- front-end/src/tests/main/modules/menu.spec.ts
+++ front-end/src/tests/main/modules/menu.spec.ts
@@ -1,88 +1,51 @@
-import {
-  Menu,
-  // shell
-} from 'electron';
+import { mockDeep } from 'vitest-mock-extended';
+
+import { Menu, shell } from 'electron';
 import menuBuilder from '@main/modules/menu';
-// import updater from '@main/modules/updater';
-import { MockedObject } from 'vitest';
 
-vi.mock('electron', () => ({
-  BrowserWindow: vi.fn(),
-  Menu: {
-    buildFromTemplate: vi.fn(),
-    setApplicationMenu: vi.fn(),
-  },
-  shell: {
-    openExternal: vi.fn(),
-  },
-}));
-
-vi.mock('@main/modules/updater', () => ({ default: vi.fn() }));
+vi.mock('electron', () => mockDeep());
+vi.mock('@main/modules/updater', () => mockDeep());
 
 describe('menuBuilder', () => {
-  const MenuObject = Menu as unknown as MockedObject<typeof Menu>;
-
-  test('Should build menu from template', () => {
+  test('should build menu from template', () => {
     menuBuilder();
 
-    expect(MenuObject.buildFromTemplate).toHaveBeenCalled();
-    expect(MenuObject.setApplicationMenu).toHaveBeenCalled();
+    expect(vi.mocked(Menu).buildFromTemplate).toHaveBeenCalled();
+    expect(vi.mocked(Menu).setApplicationMenu).toHaveBeenCalled();
   });
 
-  // test('Should call updater when "Check for updates" is clicked', () => {
-  //   menuBuilder(mainWindow);
+  test('should build for non-mac', () => {
+    Object.defineProperty(process, 'platform', { value: 'win32' });
 
-  //   const template = MenuObject.buildFromTemplate.mock.calls[0][0];
-  //   const labelItem = template.find(item => item.label === 'hedera-transaction-tool');
+    menuBuilder();
 
-  //   expect(labelItem).toBeDefined();
-  //   expect(labelItem?.submenu).toBeDefined();
-  //   expect(Array.isArray(labelItem?.submenu)).toBe(true);
+    expect(vi.mocked(Menu).buildFromTemplate).toHaveBeenCalledWith([
+      { role: 'editMenu' },
+      {
+        role: 'help',
+        submenu: [
+          {
+            label: 'Learn More',
+            click: expect.any(Function),
+          },
+        ],
+      },
+    ]);
 
-  //   if (Array.isArray(labelItem?.submenu)) {
-  //     const checkForUpdatesItem = labelItem?.submenu?.find(
-  //       subItem => subItem.label === 'Check for updates',
-  //     );
+    Object.defineProperty(process, 'platform', { value: 'darwin' });
+  });
 
-  //     expect(checkForUpdatesItem).toBeDefined();
-  //     expect(checkForUpdatesItem?.click).toBeDefined();
+  test('should invoke shell.openExternal on Learn More click', async () => {
+    menuBuilder();
 
-  //     checkForUpdatesItem?.click && checkForUpdatesItem.click({} as any, mainWindow, {} as any);
-  //     expect(updater).toHaveBeenCalledWith(mainWindow);
-  //   }
-  // });
+    const helpSection = vi.mocked(Menu).buildFromTemplate.mock.calls[2][0][2];
 
-  // test('Should open external link when "Learn More" is clicked', async () => {
-  //   menuBuilder(mainWindow);
+    expect(helpSection).toBeDefined();
 
-  //   const template = MenuObject.buildFromTemplate.mock.calls[0][0];
-  //   const helpItem = template.find(item => item.role === 'help');
+    if (helpSection.submenu) {
+      helpSection.submenu[0].click();
 
-  //   expect(helpItem).toBeDefined();
-  //   expect(helpItem?.submenu).toBeDefined();
-  //   expect(Array.isArray(helpItem?.submenu)).toBe(true);
-
-  //   if (Array.isArray(helpItem?.submenu)) {
-  //     const learnMoreItem = helpItem.submenu.find(subItem => subItem.label === 'Learn More');
-
-  //     expect(learnMoreItem).toBeDefined();
-  //     expect(learnMoreItem?.click).toBeDefined();
-
-  //     learnMoreItem?.click && learnMoreItem.click({} as any, mainWindow, {} as any);
-  //     expect(updater).toHaveBeenCalledWith(mainWindow);
-  //     expect(shell.openExternal).toHaveBeenCalledWith('https://electronjs.org');
-  //   }
-  // });
-
-  // test('Should not include mac specific menu items when not on mac', () => {
-  //   Object.defineProperty(process, 'platform', { value: 'win32' });
-  //   menuBuilder(mainWindow);
-
-  //   const testNumber = 4;
-
-  //   const template = MenuObject.buildFromTemplate.mock.calls[testNumber - 1][0];
-  //   const macItem = template.find(item => item.label === 'hedera-transaction-tool');
-
-  //   expect(macItem).toBeUndefined();
-  // });
+      expect(vi.mocked(shell).openExternal).toHaveBeenCalledWith('https://electronjs.org');
+    }
+  });
 });
