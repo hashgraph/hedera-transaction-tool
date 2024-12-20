07dc36774568f30e94039e7de689634390ba9444
Svetoslav Borislavov <svetoslav.borislavov@limechain.tech>
2024-12-20 13:02:00 +0200

    feat: add default empty page
    Signed-off-by: Svetoslav Borislavov <svetoslav.borislavov@limechain.tech>

---
+++ front-end/src/renderer/pages/Empty/Empty.vue
@@ -0,0 +1,26 @@
+<script setup lang="ts">
+import { onBeforeMount } from 'vue';
+
+import useUserStore from '@renderer/stores/storeUser';
+
+import { useRouter } from 'vue-router';
+
+import { isUserLoggedIn } from '@renderer/utils';
+
+/* Stores */
+const user = useUserStore();
+
+/* Composables */
+const router = useRouter();
+
+/* Hooks */
+onBeforeMount(async () => {
+  if (isUserLoggedIn(user.personal)) {
+    await router.push({ name: 'transactions' });
+  }
+});
+</script>
+
+<template>
+  <div></div>
+</template>
