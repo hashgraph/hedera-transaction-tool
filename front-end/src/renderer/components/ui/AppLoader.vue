eef0139b374c42958a663bdb536cfbf9cba09857
Svetoslav Borislavov <svetoslav.borislavov@limechain.tech>
2025-01-02 10:24:59 +0200

    refactor: scss updates
    Signed-off-by: Svetoslav Borislavov <svetoslav.borislavov@limechain.tech>

--- front-end/src/renderer/components/ui/AppLoader.vue
+++ front-end/src/renderer/components/ui/AppLoader.vue
@@ -5,7 +5,7 @@ defineProps<{
 }>();
 </script>
 <template>
-  <div class="d-flex justify-content-center align-items-center">
+  <div class="flex-centered">
     <div class="container-loader" data-testid="div-loader"></div>
     <p v-if="loadingText" class="mt-3">{{ loadingText }}</p>
   </div>
