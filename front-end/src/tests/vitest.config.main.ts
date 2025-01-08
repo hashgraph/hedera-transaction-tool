90ce9503d3573ef8a32b486dae9d8fa9d9931d3c
Svetoslav Borislavov <svetoslav.borislavov@limechain.tech>
2025-01-08 12:16:58 +0200

    feat: disable vitest watch mode by default
    Signed-off-by: Svetoslav Borislavov <svetoslav.borislavov@limechain.tech>

--- front-end/src/tests/vitest.config.main.ts
+++ front-end/src/tests/vitest.config.main.ts
@@ -11,6 +11,7 @@ export default defineConfig({
     environment: 'node',
     globals: true,
     include: ['src/tests/main/**/*.{test,spec}.{ts,js}'],
+    watch: false,
     coverage: {
       exclude: ['src/main/shared', 'src/main/electron-env.d.ts'],
     },
