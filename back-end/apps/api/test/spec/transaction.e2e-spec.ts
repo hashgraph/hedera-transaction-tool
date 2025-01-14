e5ed570c93f699adc4dd90e1fee90eafe9ad372c
Svetoslav Borislavov <svetoslav.borislavov@limechain.tech>
2025-01-14 12:32:18 +0200

    feat: update e2e tests
    Signed-off-by: Svetoslav Borislavov <svetoslav.borislavov@limechain.tech>

--- back-end/apps/api/test/spec/transaction.e2e-spec.ts
+++ back-end/apps/api/test/spec/transaction.e2e-spec.ts
@@ -849,14 +849,6 @@ describe('Transactions (e2e)', () => {
       endpoint = new Endpoint(server, '/transactions/archive');
     });
 
-    afterAll(async () => {
-      await resetDatabase();
-      await addHederaLocalnetAccounts();
-      testsAddedTransactionsCountUser = 0;
-      testsAddedTransactionsCountAdmin = 0;
-      addedTransactions = await addTransactions();
-    });
-
     it('(PATCH) should archive a transaction if creator', async () => {
       const transaction = await createTransaction(user, localnet1003);
       const { body: newTransaction } = await new Endpoint(server, '/transactions')
@@ -886,33 +878,86 @@ describe('Transactions (e2e)', () => {
     });
 
     it('(PATCH) should not archive a transaction if not verified', async () => {
-      const transaction = await createTransaction(user, localnet1003);
-      const { body: newTransaction } = await new Endpoint(server, '/transactions')
-        .post(transaction, null, userAuthToken)
-        .expect(201);
-
-      const { status } = await endpoint.patch(null, newTransaction.id.toString(), userNewAuthToken);
-
-      const transactionFromDb = await repo.findOne({ where: { id: newTransaction.id } });
-
-      expect(status).toEqual(403);
-      expect(transactionFromDb?.status).not.toEqual(TransactionStatus.ARCHIVED);
+      await endpoint.patch(null, '2', userNewAuthToken).expect(403);
     });
 
     it("(PATCH) should not archive a transaction if it's already executed", async () => {
-      const transaction = await createTransaction(user, localnet1003);
-      const { body: newTransaction } = await new Endpoint(server, '/transactions')
-        .post(transaction, null, userAuthToken)
-        .expect(201);
+      const transaction = addedTransactions.userTransactions[0];
+      const oldStatus = transaction.status;
+      await repo.update({ id: transaction.id }, { status: TransactionStatus.EXECUTED });
 
-      await repo.update({ id: newTransaction.id }, { status: TransactionStatus.EXECUTED });
+      const { status } = await endpoint.patch(null, transaction.id.toString(), userAuthToken);
 
-      const { status } = await endpoint.patch(null, newTransaction.id.toString(), userAuthToken);
-
-      const transactionFromDb = await repo.findOne({ where: { id: newTransaction.id } });
+      const transactionFromDb = await repo.findOne({ where: { id: transaction.id } });
 
       expect(status).toEqual(400);
       expect(transactionFromDb?.status).not.toEqual(TransactionStatus.ARCHIVED);
+
+      await repo.update({ id: transaction.id }, { status: oldStatus });
+    });
+  });
+
+  describe('/transactions/execute/:transactionId', () => {
+    let endpoint: Endpoint;
+
+    beforeAll(() => {
+      endpoint = new Endpoint(server, '/transactions/execute');
+    });
+
+    it('(PATCH) should emit execute event to chain if creator', async () => {
+      const transactionsEndpoint = new Endpoint(server, '/transactions');
+      const transaction = await createTransaction(user, localnet1003);
+      const { body: newTransaction } = await transactionsEndpoint
+        .post({ ...transaction, isManual: true }, null, userAuthToken)
+        .expect(201);
+
+      const sdkTransaction = AccountCreateTransaction.fromBytes(
+        Buffer.from(newTransaction.transactionBytes, 'hex'),
+      );
+      await sdkTransaction.sign(localnet1003.privateKey);
+      const signatureMap = getSignatureMapForPublicKeys(
+        [localnet1003.publicKeyRaw],
+        sdkTransaction,
+      );
+
+      console.log();
+
+      await transactionsEndpoint
+        .post(
+          { signatureMap: formatSignatureMap(signatureMap) },
+          `/${newTransaction.id}/signers`,
+          userAuthToken,
+        )
+        .expect(201);
+
+      await endpoint.patch(null, newTransaction.id.toString(), userAuthToken).expect(200);
+    });
+
+    it('(PATCH) should fail if not creator', async () => {
+      const transactionsEndpoint = new Endpoint(server, '/transactions');
+      const transaction = await createTransaction(user, localnet1003);
+      const { body: newTransaction } = await transactionsEndpoint
+        .post({ ...transaction, isManual: true }, null, userAuthToken)
+        .expect(201);
+
+      await endpoint.patch(null, newTransaction.id.toString(), adminAuthToken).expect(401);
+    });
+
+    it('(PATCH) should fail if not a manual transaction', async () => {
+      const transactionsEndpoint = new Endpoint(server, '/transactions');
+      const transaction = await createTransaction(user, localnet1003);
+      const { body: newTransaction } = await transactionsEndpoint
+        .post(transaction, null, userAuthToken)
+        .expect(201);
+
+      const { body } = await endpoint
+        .patch(null, newTransaction.id.toString(), userAuthToken)
+        .expect(400);
+      expect(body).toMatchObject(
+        expect.objectContaining({
+          code: ErrorCodes.IO,
+        }),
+      );
     });
   });
 
