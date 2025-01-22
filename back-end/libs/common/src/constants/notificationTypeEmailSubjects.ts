fbf3987c9908cff530c250362a0880ba61235560
Svetoslav Borislavov <svetoslav.borislavov@limechain.tech>
2025-01-22 05:48:48 +0200

    feat: send email on transaction cancel (#1470)
    Signed-off-by: Svetoslav Borislavov <svetoslav.borislavov@limechain.tech>

--- back-end/libs/common/src/constants/notificationTypeEmailSubjects.ts
+++ back-end/libs/common/src/constants/notificationTypeEmailSubjects.ts
@@ -3,4 +3,5 @@ export enum NotificationTypeEmailSubjects {
   TRANSACTION_WAITING_FOR_SIGNATURES = 'Hedera Transaction Tool | Action Required | Review and Sign Transaction',
   TRANSACTION_READY_FOR_EXECUTION = 'Hedera Transaction Tool | Transaction ready for execution',
   TRANSACTION_EXECUTED = 'Hedera Transaction Tool | Transaction has been executed',
+  TRANSACTION_CANCELLED = 'Hedera Transaction Tool | Transaction has been cancelled',
 }
