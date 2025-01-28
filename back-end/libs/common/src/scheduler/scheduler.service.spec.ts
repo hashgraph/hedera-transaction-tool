3ffb6a54987fa65928e46a1b7e27209746d04be6
Svetoslav Borislavov <svetoslav.borislavov@limechain.tech>
2025-01-28 17:06:19 +0200

    feat: create a scheduler service
    Signed-off-by: Svetoslav Borislavov <svetoslav.borislavov@limechain.tech>

---
+++ back-end/libs/common/src/scheduler/scheduler.service.spec.ts
@@ -0,0 +1,60 @@
+import { Test, TestingModule } from '@nestjs/testing';
+import { ConfigService } from '@nestjs/config';
+import { Redis } from 'ioredis';
+import { mockDeep } from 'jest-mock-extended';
+
+import { SchedulerService } from './scheduler.service';
+
+jest.mock('ioredis', () => {
+  return {
+    Redis: jest.fn().mockImplementation(() => mockDeep<Redis>()),
+  };
+});
+
+describe('SchedulerService', () => {
+  let service: SchedulerService;
+  const configService = mockDeep<ConfigService>();
+  const pubClient = mockDeep<Redis>();
+  const subClient = mockDeep<Redis>();
+
+  beforeEach(async () => {
+    const module: TestingModule = await Test.createTestingModule({
+      providers: [
+        SchedulerService,
+        {
+          provide: ConfigService,
+          useValue: configService,
+        },
+      ],
+    }).compile();
+
+    service = module.get<SchedulerService>(SchedulerService);
+    service.pubClient = pubClient;
+    service.subClient = subClient;
+  });
+
+  afterEach(() => {
+    jest.clearAllMocks();
+  });
+
+  describe('addListener', () => {
+    it('should add a listener for expiration events', async () => {
+      const handler = jest.fn();
+
+      await service.addListener(handler);
+
+      expect(subClient.on).toHaveBeenCalledWith('message', expect.any(Function));
+    });
+  });
+
+  describe('addReminder', () => {
+    it('should add a key with correct date', async () => {
+      const key = 'test';
+      const date = new Date();
+
+      await service.addReminder(key, date);
+
+      expect(pubClient.set).toHaveBeenCalledWith(key, key, 'PXAT', date.getTime());
+    });
+  });
+});
---
+++ back-end/libs/common/src/scheduler/scheduler.service.ts
@@ -0,0 +1,36 @@
+import { Injectable } from '@nestjs/common';
+import { ConfigService } from '@nestjs/config';
+
+import { Redis } from 'ioredis';
+
+/**
+ * @description This service is responsible for creating a reminder system using Redis.
+ * @notice Redis keyspace notifications are disabled by default, make sure to enable it in the Redis configuration file.
+ * @notice Redis will not publish the event on the exact time because of its passive & active expiration policy.
+ *          - Passive: Redis will check the expiration time when the key is accessed.
+ *          - Active: Each second, 10 times, Redis will check 20 randim keys to see if they are expired.
+ *         This makes the reminder system delayed at scale
+ */
+@Injectable()
+export class SchedulerService {
+  pubClient: Redis;
+  subClient: Redis;
+
+  constructor(private readonly configService: ConfigService) {
+    const redisURL = this.configService.get('REDIS_URL');
+
+    this.pubClient = new Redis(redisURL);
+    this.subClient = new Redis(redisURL);
+    this.subClient.subscribe('__keyevent@0__:expired');
+  }
+
+  addListener(handler: (key: string) => void) {
+    this.subClient.on('message', async (_channel, message) => {
+      await handler(message);
+    });
+  }
+
+  async addReminder(key: string, date: Date) {
+    await this.pubClient.set(key, key, 'PXAT', date.getTime());
+  }
+}
