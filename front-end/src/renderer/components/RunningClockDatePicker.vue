b17b2ec611e28e8c423d474b3414bb28244a904d
John Bair <john.bair@swirldslabs.com>
2025-02-07 17:11:08 -0800

    fix: date picker UX updates
    text-input and flow options are now used. 'Now' button now works like the stock now button of the DatePicker component.
    
    Signed-off-by: John Bair <john.bair@swirldslabs.com>

--- front-end/src/renderer/components/RunningClockDatePicker.vue
+++ front-end/src/renderer/components/RunningClockDatePicker.vue
@@ -1,7 +1,7 @@
 <script setup lang="ts">
 import type { VueDatePickerProps } from '@vuepic/vue-datepicker';
 
-import { onMounted, onUnmounted, ref, watch } from 'vue';
+import { onMounted, onUnmounted, ref } from 'vue';
 
 import AppDatePicker from '@renderer/components/ui/AppDatePicker.vue';
 
@@ -35,27 +35,6 @@ function stopInterval() {
   intervalId.value && clearInterval(intervalId.value);
 }
 
-function resetTime(date: Date) {
-  const newDate = new Date(date);
-  newDate.setHours(0, 0, 0, 0);
-  return newDate;
-}
-
-function handleUpdateValidStart(date: Date) {
-  const today = new Date();
-  today.setHours(0, 0, 0, 0);
-  const selectedDate = new Date(date);
-  selectedDate.setHours(0, 0, 0, 0);
-
-  const isNewDate = selectedDate.getTime() !== today.getTime();
-
-  if (isNewDate) {
-    emit('update:modelValue', resetTime(date));
-  } else {
-    emit('update:modelValue', date);
-  }
-}
-
 /* Hooks */
 onMounted(async () => {
   startInterval();
@@ -64,21 +43,11 @@ onMounted(async () => {
 onUnmounted(() => {
   stopInterval();
 });
-
-/* Watchers */
-watch(
-  () => props.modelValue,
-  (newVal, oldVal) => {
-    if (newVal.toDateString() !== oldVal.toDateString()) {
-      emit('update:modelValue', resetTime(newVal));
-    }
-  },
-);
 </script>
 <template>
   <AppDatePicker
     :model-value="modelValue"
-    @update:model-value="handleUpdateValidStart"
+    @update:model-value="emit('update:modelValue', $event)"
     :minDate="minDate"
     :maxDate="maxDate"
     :clearable="false"
