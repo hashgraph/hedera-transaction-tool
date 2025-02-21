ba4918149c4cfae616c710be616558ead585faed
John Bair <john.bair@swirldslabs.com>
2025-02-21 14:16:37 -0800

    chore: move reusable code out of CreateTransactionGroup
    Signed-off-by: John Bair <john.bair@swirldslabs.com>

---
+++ front-end/src/renderer/utils/transferTransactions.ts
@@ -0,0 +1,33 @@
+import { stringifyHbarWithFont } from './index';
+
+export function formatHbarTransfers(transfers): string {
+  if (transfers.length === 0) {
+    return 'No transfers';
+  }
+
+  if (transfers.length === 1) {
+    const amount = transfers[0].amount;
+    if (amount.isNegative()) {
+      return 'Missing receiver';
+    } else {
+      return 'Missing sender';
+    }
+  }
+
+  if (transfers.length === 2) {
+    // the JS SDK sorts the order of the transfers by account ID. We don't want this. We want the sender to be on the
+    // left and the receiver to be on the right. So we need to check if the amount is negative or positive and then
+    // arrange the transfers accordingly
+    let sender = transfers[0];
+    let receiver = transfers[1];
+    if (receiver.amount.isNegative()) {
+      sender = transfers[1];
+      receiver = transfers[0];
+    }
+    return `${sender.accountId} --> ${stringifyHbarWithFont(
+      receiver.amount, "text-semi-bold text-pink"
+    )} --> ${receiver.accountId}`;
+  }
+
+  return 'Multiple transfers';
+}
