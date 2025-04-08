c456474a7c026391c0e2a2cfe14b3fc1d292c644
John Bair <john.bair@swirldslabs.com>
2025-04-08 15:36:08 -0700

    feat: allow batch submit of signatures (sign-all flow)
    Signed-off-by: John Bair <john.bair@swirldslabs.com>

--- front-end/src/renderer/types/index.ts
+++ front-end/src/renderer/types/index.ts
@@ -1,6 +1,14 @@
+import { Transaction } from '@hashgraph/sdk';
+
 export * from './userStore';
 
 export enum KeyType {
   ED25519 = 'ED25519',
   ECDSA = 'ECDSA'
+}
+
+export type SignatureItem = {
+  publicKeys: string[];
+  transaction: Transaction;
+  transactionId: number;
 }
