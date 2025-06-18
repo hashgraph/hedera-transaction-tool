a1b5e6d894d445fce0c90514f09676b47b82caa0
John Bair <john.bair@swirldslabs.com>
2025-06-18 12:11:14 -0700

    chore: moving the shared mirror node api usages and types to a shared location for back-end and front-end
    Signed-off-by: John Bair <john.bair@swirldslabs.com>

--- front-end/src/renderer/composables/useAccountId.ts
+++ front-end/src/renderer/composables/useAccountId.ts
@@ -1,4 +1,4 @@
-import type { IAccountInfoParsed, CryptoAllowance } from '@main/shared/interfaces';
+import type { CryptoAllowance, IAccountInfoParsed } from 'lib';
 
 import { computed, ref, watch } from 'vue';
 import { AccountId, Client, Hbar } from '@hashgraph/sdk';
