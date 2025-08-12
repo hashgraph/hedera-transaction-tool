bfe119069400ac26fc99ec47a96915d154f6b6ea
John Bair <john.bair@swirldslabs.com>
2025-08-12 08:28:20 -0700

    fix: account update require signature waivability
    Signed-off-by: John Bair <john.bair@swirldslabs.com>

--- back-end/libs/common/src/models/account-update-transaction.model.ts
+++ back-end/libs/common/src/models/account-update-transaction.model.ts
@@ -1,22 +1,48 @@
-import { AccountUpdateTransaction } from '@hashgraph/sdk';
+import { AccountId, AccountUpdateTransaction, Key } from '@hashgraph/sdk';
 
 import { TransactionBaseModel } from './transaction.model';
 
 export default class AccountUpdateTransactionModel extends TransactionBaseModel<AccountUpdateTransaction> {
+  private readonly TREASURY_ACCOUNT = AccountId.fromString('0.0.2');
+  private readonly ADMIN_ACCOUNT = AccountId.fromString('0.0.50');
+  private readonly MINIMUM_SYSTEM_ACCOUNT = AccountId.fromString('0.0.3');
+  private readonly MAXIMUM_SYSTEM_ACCOUNT = AccountId.fromString('0.0.1000');
+
   // New key is required:
   // https://docs.hedera.com/hedera/sdks-and-apis/sdks/accounts-and-hbar/update-an-account
-  getNewKeys() {
+  getNewKeys(): Key[] {
     if (this.transaction.key != null) {
       return [this.transaction.key];
     }
     return [];
   }
 
+  // According to documentation, if account is between 3 and 1000 inclusive,
+  // the key is not required IF the fee payer is 2 or 50. In all other cases, the key is required.
+  // https://github.com/hiero-ledger/hiero-consensus-node/blob/main/hedera-node/docs/privileged-transactions.md#waived-signing-requirements-for-crypto-updates
   getSigningAccounts(): Set<string> {
     const set = super.getSigningAccounts();
-    if (this.transaction.accountId) {
-      set.add(this.transaction.accountId.toString());
+    const feePayer = super.getFeePayerAccountId();
+    const accountId = this.transaction.accountId;
+    if (accountId) {
+      if (!this.isSystemAccount(accountId) || !this.isPrivilegedFeePayer(feePayer)) {
+        set.add(accountId.toString());
+      }
     }
     return set;
+  }
+
+  private isSystemAccount(accountId: AccountId): boolean {
+    return (
+      accountId.compare(this.MINIMUM_SYSTEM_ACCOUNT) >= 0 &&
+      accountId.compare(this.MAXIMUM_SYSTEM_ACCOUNT) <= 0
+    );
+  }
+
+  private isPrivilegedFeePayer(feePayer?: AccountId): boolean {
+    return (
+      feePayer != null &&
+      (feePayer.equals(this.TREASURY_ACCOUNT) || feePayer.equals(this.ADMIN_ACCOUNT))
+    );
   }
 }
--- front-end/src/renderer/utils/transactionSignatureModels/transaction.model.ts
+++ front-end/src/renderer/utils/transactionSignatureModels/transaction.model.ts
@@ -1,4 +1,4 @@
-import { Key, Transaction as SDKTransaction } from '@hashgraph/sdk';
+import { AccountId, Key, Transaction as SDKTransaction } from '@hashgraph/sdk';
 
 import { getAccountInfo, getNodeInfo } from '@renderer/services/mirrorNodeDataService';
 import { compareKeys } from '../sdk';
@@ -15,10 +15,10 @@ export abstract class TransactionBaseModel<T extends SDKTransaction> {
     return this.transaction.toBytesAsync();
   }
 
-  getFeePayerAccountId(): string | null {
+  getFeePayerAccountId(): AccountId | null {
     const payerId = this.transaction.transactionId?.accountId;
     if (payerId) {
-      return payerId.toString();
+      return payerId;
     }
     return null;
   }
@@ -64,7 +64,7 @@ export abstract class TransactionBaseModel<T extends SDKTransaction> {
 
     if (feePayerAccountId) {
       try {
-        const accountInfo = await getAccountInfo(feePayerAccountId, mirrorNodeLink);
+        const accountInfo = await getAccountInfo(feePayerAccountId.toString(), mirrorNodeLink);
         if (accountInfo.key) {
           signatureKeys.push(accountInfo.key);
           payerKey[feePayerAccountId] = accountInfo.key;
