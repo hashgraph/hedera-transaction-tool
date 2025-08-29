0d04c9ab35ffc96d4b17db766f99cb308ab423ed
Eric Le Ponner <eric.leponner@icloud.com>
2025-08-29 19:59:12 +0200

    feat: TransactionImportRow now displays transaction id and type.
    Signed-off-by: Eric Le Ponner <eric.leponner@icloud.com>

--- front-end/src/renderer/components/TransactionImportRow.vue
+++ front-end/src/renderer/components/TransactionImportRow.vue
@@ -1,23 +1,60 @@
 <script setup lang="ts">
-
 /* Props */
+import { computed } from 'vue';
 import type { TransactionMatch } from '@main/services/localUser';
+import { decode } from 'msgpackr';
+import { hexToUint8Array } from '@renderer/utils';
 
 const props = defineProps<{
-  transaction: TransactionMatch
+  transaction: TransactionMatch;
 }>();
+
+/* Computed */
+const nickName = computed(() => basename(props.transaction.filePath));
+
+const decodedTransaction = computed(() => {
+  let result: unknown | null;
+  const bytes = hexToUint8Array(props.transaction.transactionBytes);
+  try {
+    result = decode(bytes);
+  } catch  {
+    result = null;
+  }
+  return result;
+});
+
+const transactionId = computed(() => {
+  return fetchString(decodedTransaction.value, "transactionId") ?? "?"
+});
+
+const transactionType = computed(() => {
+  return fetchString(decodedTransaction.value, "type") ?? "?"
+});
 
 /* Function */
 function basename(filePath: string): string {
   return filePath.substring(filePath.lastIndexOf('/') + 1);
 }
 
+function fetchString(obj: unknown, propertyName: string): string|null {
+  let result: string|null
+  if (propertyName in obj && typeof obj[propertyName] === 'string') {
+    result = obj[propertyName]
+  } else {
+    result = null
+  }
+  return result
+}
+
 </script>
 
 <template>
   <div class="overflow-x-auto">
-    <strong>{{ basename(props.transaction.filePath) }}</strong>
-    <p class="text-muted small">{{ props.transaction.transactionBytes.length }} bytes</p>
+    <strong>{{ nickName }}</strong>
+    <p class="text-muted small">
+      <span class="text-numeric">{{ transactionId }}</span> -
+      <span>{{ transactionType}}</span>
+    </p>
   </div>
 </template>
 
