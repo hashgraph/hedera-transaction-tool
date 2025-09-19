7e043e580bfc8ba33d04d173f463149c5556d2b1
Eric Le Ponner <eric.leponner@icloud.com>
2025-09-19 14:48:56 +0200

    feat: added (empty) TransactionImportButton to Transactions vue.
    Signed-off-by: Eric Le Ponner <eric.leponner@icloud.com>

---
+++ front-end/src/renderer/components/TransactionImportButton.vue
@@ -0,0 +1,32 @@
+<script setup lang="ts">
+import AppButton from '@renderer/components/ui/AppButton.vue';
+import { showOpenDialog } from '@renderer/services/electronUtilsService.ts';
+
+/* Handlers */
+async function handleImport() {
+  const result = await showOpenDialog(
+    'Select Signature Files',
+    'Select',
+    [{ name: '.csv, .zip or a folder ', extensions: ['csv', 'zip'] }],
+    ['openFile', 'openDirectory', 'multiSelections'],
+    'Import Signature Files',
+  );
+
+  if (result.canceled) return;
+
+  console.log('result=' + JSON.stringify(result, null, 2));
+}
+</script>
+
+<template>
+  <AppButton
+    color="primary"
+    data-testid="button-create-new"
+    data-bs-toggle="dropdown"
+    @click="handleImport"
+  >
+    <span>Import</span>
+  </AppButton>
+</template>
+
+<style scoped></style>
