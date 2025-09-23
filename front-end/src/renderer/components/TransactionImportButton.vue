941e668a936da8cb1849736b5d3607d4ac33da7e
Eric Le Ponner <eric.leponner@icloud.com>
2025-09-23 15:45:18 +0200

    feat: TransactionImportButton now displays TransactionImportModal.
    Signed-off-by: Eric Le Ponner <eric.leponner@icloud.com>

--- front-end/src/renderer/components/TransactionImportButton.vue
+++ front-end/src/renderer/components/TransactionImportButton.vue
@@ -1,20 +1,30 @@
 <script setup lang="ts">
+import { ref } from 'vue';
 import AppButton from '@renderer/components/ui/AppButton.vue';
 import { showOpenDialog } from '@renderer/services/electronUtilsService.ts';
+import { filterForImportV1 } from '@renderer/services/importV1.ts';
+import type { V1ImportFilterResult } from '@shared/interfaces';
+import TransactionImportModal from '@renderer/components/TransactionImportModal.vue';
+
+/* State */
+const emptyFilterResult: V1ImportFilterResult = { candidates: [], ignoredPaths: [] };
+const filterResult = ref<V1ImportFilterResult>(emptyFilterResult);
+const isImportModalVisible = ref(false);
 
 /* Handlers */
 async function handleImport() {
   const result = await showOpenDialog(
-    'Select Signature Files',
+    'Import From Transaction Tool V1',
     'Select',
-    [{ name: '.csv, .zip or a folder ', extensions: ['csv', 'zip'] }],
+    [{ name: '.zip', extensions: ['zip'] }],
     ['openFile', 'openDirectory', 'multiSelections'],
-    'Import Signature Files',
+    'Select ZIP files created by TT V1',
   );
 
   if (result.canceled) return;
 
-  console.log('result=' + JSON.stringify(result, null, 2));
+  filterResult.value = await filterForImportV1(result.filePaths);
+  isImportModalVisible.value = true
 }
 </script>
 
@@ -27,6 +37,7 @@ async function handleImport() {
   >
     <span>Import</span>
   </AppButton>
+  <TransactionImportModal v-model:show="isImportModalVisible" :filter-result="filterResult" />
 </template>
 
 <style scoped></style>
