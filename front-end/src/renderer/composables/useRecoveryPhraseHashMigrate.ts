5211e1278a62e3396d9ac68422db4f6355fb60ed
Simon Viénot <simon.vienot@icloud.com>
2025-10-30 14:43:28 +0100

    Null the mnemonic hash of key pairs needing migration when the use chooses to skip this step.
    Signed-off-by: Simon Viénot <simon.vienot@icloud.com>

--- front-end/src/renderer/composables/useRecoveryPhraseHashMigrate.ts
+++ front-end/src/renderer/composables/useRecoveryPhraseHashMigrate.ts
@@ -164,6 +164,7 @@ export default function useRecoveryPhraseHashMigrate() {
 
   return {
     getRequiredKeysToMigrate,
+    tryMigrateOrganizationKeys,
     getKeysToUpdateForRecoveryPhrase,
     redirectIfRequiredKeysToMigrate,
     updateKeyPairsHash,
