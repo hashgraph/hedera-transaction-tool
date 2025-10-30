6b77769ca7722381c594ca8ec7d9899f5ed98ae7
John Bair <john.bair@swirldslabs.com>
2025-10-30 16:22:00 -0700

    feat: show freeze transaction requirements
    Signed-off-by: John Bair <john.bair@swirldslabs.com>

--- front-end/src/renderer/components/Transaction/Create/Freeze/Freeze.vue
+++ front-end/src/renderer/components/Transaction/Create/Freeze/Freeze.vue
@@ -9,6 +9,7 @@ import { createFreezeTransaction, getFreezeData } from '@renderer/utils/sdk';
 
 import BaseTransaction from '@renderer/components/Transaction/Create/BaseTransaction';
 import FreezeFormData from '@renderer/components/Transaction/Create/Freeze/FreezeFormData.vue';
+import { isAccountId } from '@renderer/utils';
 
 /* State */
 const baseTransactionRef = ref<InstanceType<typeof BaseTransaction> | null>(null);
@@ -29,6 +30,24 @@ const createTransaction = computed<CreateTransactionFunc>(() => {
     });
 });
 
+// Start time is required if freeze type is FreezeOnly, FreezeUpgrade, or TelemetryUpgrade
+// FileId and FileHash are required if freeze type is PrepareUpgrade, FreezeUpgrade, or TelemetryUpgrade
+const createDisabled = computed(() => {
+  const missingFileInfo =
+    !isAccountId(data.fileId) ||
+    typeof data.fileHash !== 'string' ||
+    data.fileHash.trim() === '';
+
+  const needsFileInfo = data.freezeType === 2 || data.freezeType === 3 || data.freezeType === 5;
+  const needsStartTimestamp = data.freezeType === 1 || data.freezeType === 3 || data.freezeType === 5;
+
+  return (
+    data.freezeType === -1 ||
+    (needsStartTimestamp && data.startTimestamp == null) ||
+    (needsFileInfo && missingFileInfo)
+  );
+});
+
 /* Handlers */
 const handleDraftLoaded = async (transaction: Transaction) => {
   handleUpdateData(await getFreezeData(transaction));
@@ -41,6 +60,7 @@ const handleUpdateData = (newData: FreezeData) => {
 <template>
   <BaseTransaction
     ref="baseTransactionRef"
+    :create-disabled="createDisabled"
     :create-transaction="createTransaction"
     @draft-loaded="handleDraftLoaded"
   >
