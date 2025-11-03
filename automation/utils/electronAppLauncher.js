8e04f12ffa8f34f0f9dcc25838af299de14bfbf8
Eric Le Ponner <eric.leponner@icloud.com>
2025-11-03 23:41:40 +0100

    Added traces in electronAppLauncher.
    Signed-off-by: Eric Le Ponner <eric.leponner@icloud.com>

--- automation/utils/electronAppLauncher.js
+++ automation/utils/electronAppLauncher.js
@@ -2,9 +2,17 @@ const { _electron: electron } = require('playwright');
 require('dotenv').config();
 
 async function launchHederaTransactionTool() {
-  return await electron.launch({
-    executablePath: process.env.EXECUTABLE_PATH,
-  });
+  console.log("process.env.EXECUTABLE_PATH=" + process.env.EXECUTABLE_PATH);
+  try {
+    const result = electron.launch({
+      executablePath: process.env.EXECUTABLE_PATH,
+    });
+    console.log("result=" + result);
+    return result;
+  } catch(reason) {
+    console.error(reason);
+    throw reason;
+  }
 }
 
 module.exports = { launchHederaTransactionTool };
