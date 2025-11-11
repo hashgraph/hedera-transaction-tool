44794daf4375a66346f0f97b80d75e839324892c
Eric Le Ponner <eric.leponner@icloud.com>
2025-11-11 11:21:46 +0100

    Commented warningToastOptions (because unused for now).
    Signed-off-by: Eric Le Ponner <eric.leponner@icloud.com>

--- front-end/src/renderer/utils/toastOptions.ts
+++ front-end/src/renderer/utils/toastOptions.ts
@@ -3,10 +3,10 @@ import type { ToastProps } from 'vue-toast-notification';
 export const errorToastOptions: ToastProps = {
   duration: 0,
 }
-
-export const warningToastOptions: ToastProps = {
-  duration: 0,
-}
+//
+// export const warningToastOptions: ToastProps = {
+//   duration: 0,
+// }
 
 export const successToastOptions: ToastProps = {
   duration: 4000,
