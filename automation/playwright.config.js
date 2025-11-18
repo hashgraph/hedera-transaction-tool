c47fdc666cb0ab8faca3139f9ec21ae57380d61e
Eric Le Ponner <91124272+ericleponner@users.noreply.github.com>
2025-11-18 16:47:58 +0100

    chore: setting playwright retries and timeout according CI or DEV environment (#2008)
    Signed-off-by: Eric Le Ponner <eric.leponner@icloud.com>

--- automation/playwright.config.js
+++ automation/playwright.config.js
@@ -3,7 +3,8 @@ import { defineConfig } from 'playwright/test';
 export default defineConfig({
   reporter: process.env.CI ? [['github'], ['list']] : 'list',
   reportSlowTests: null,
-  retries: 3,
+  retries: process.env.CI ? 3 : 0,
+  timeout: process.env.CI ? 30_000 : 3600_000,
   workers: 1,
 
   projects: [
