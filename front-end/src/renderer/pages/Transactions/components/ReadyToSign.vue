e9c07a9493afc31bc60e9165eb0a815b43c3d6c2
Simon Viénot <simon.vienot@icloud.com>
2025-11-21 19:16:42 +0100

    Hide 'Date Modified' column in ReadyToSign table.
    Signed-off-by: Simon Viénot <simon.vienot@icloud.com>

--- front-end/src/renderer/pages/Transactions/components/ReadyToSign.vue
+++ front-end/src/renderer/pages/Transactions/components/ReadyToSign.vue
@@ -455,6 +455,7 @@ const handleSignSingle = async (index: number) => {
                   ></i>
                 </div>
               </th>
+<!--
               <th @contextmenu.prevent="showContextMenu">
                 <div
                   class="table-sort-link"
@@ -473,6 +474,7 @@ const handleSignSingle = async (index: number) => {
                   ></i>
                 </div>
               </th>
+-->
               <th class="text-center">
                 <span>Actions</span>
               </th>
@@ -505,6 +507,7 @@ const handleSignSingle = async (index: number) => {
                     />
                     <span v-else>N/A</span>
                   </td>
+<!--
                   <td>
                     <DateTimeString
                       v-if="groups.get(group[0])"
@@ -514,6 +517,7 @@ const handleSignSingle = async (index: number) => {
                     />
                     <span v-else>N/A</span>
                   </td>
+-->
                   <td class="text-center">
                     <div class="d-flex justify-content-center gap-4">
                       <AppButton
@@ -569,6 +573,7 @@ const handleSignSingle = async (index: number) => {
                       />
                       <span v-else>N/A</span>
                     </td>
+<!--
                     <td :data-testid="`td-transaction-date-modified-for-sign-${index}`">
                       <DateTimeString
                         v-if="tx.transaction instanceof Transaction"
@@ -578,6 +583,7 @@ const handleSignSingle = async (index: number) => {
                       />
                       <span v-else>N/A</span>
                     </td>
+-->
                     <td class="text-center">
                       <div class="d-flex justify-content-center gap-4">
                         <AppButton
