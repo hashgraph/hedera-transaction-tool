2b76ab35f2756627caea64debd86d3a9e27854dd
Eric Le Ponner <eric.leponner@icloud.com>
2025-11-27 14:54:06 +0100

    Added TransactionNodesController/Service to Transactions module.
    Signed-off-by: Eric Le Ponner <eric.leponner@icloud.com>

--- back-end/apps/api/src/transactions/transactions.module.ts
+++ back-end/apps/api/src/transactions/transactions.module.ts
@@ -22,6 +22,8 @@ import { CommentsController, CommentsService } from './comments';
 import { SignersController, SignersService } from './signers';
 import { ObserversController, ObserversService } from './observers';
 import { ApproversController, ApproversService } from './approvers';
+import { TransactionNodesController } from './nodes/transaction-nodes.controller';
+import { TransactionNodesService } from './nodes/transaction-nodes.service';
 
 @Module({
   imports: [
@@ -47,6 +49,7 @@ import { ApproversController, ApproversService } from './approvers';
     ObserversController,
     ApproversController,
     TransactionGroupsController,
+    TransactionNodesController
   ],
   providers: [
     TransactionsService,
@@ -55,6 +58,7 @@ import { ApproversController, ApproversService } from './approvers';
     ObserversService,
     ApproversService,
     TransactionGroupsService,
+    TransactionNodesService
   ],
   exports: [TransactionsService],
 })
