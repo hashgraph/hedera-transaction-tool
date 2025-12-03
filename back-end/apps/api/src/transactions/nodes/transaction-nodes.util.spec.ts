c20131106bad5216d55cb05cb157cc21d915cce1
Eric Le Ponner <eric.leponner@icloud.com>
2025-12-03 14:20:44 +0100

    Improved unit testing for transaction nodes (step #2).
    Signed-off-by: Eric Le Ponner <eric.leponner@icloud.com>

--- back-end/apps/api/src/transactions/nodes/transaction-nodes.util.spec.ts
+++ back-end/apps/api/src/transactions/nodes/transaction-nodes.util.spec.ts
@@ -2,13 +2,15 @@ import { ITransactionNode } from '../dto/ITransactionNode';
 import { compareTransactionNodes } from './transaction-nodes.util';
 import { TransactionStatus } from '@entities';
 
+const singleNodeDate1 = 0;
+
 const singleNode1: ITransactionNode = {
   transactionId: 1,
   groupId: undefined,
   description: 'Single node 1',
-  createdAt: new Date().toISOString(),
-  validStart: new Date().toISOString(),
-  updatedAt: new Date().toISOString(),
+  createdAt: new Date(singleNodeDate1).toISOString(),
+  validStart: new Date(singleNodeDate1 + 2000).toISOString(),
+  updatedAt: new Date(singleNodeDate1 + 1000).toISOString(),
   executedAt: undefined,
   status: TransactionStatus.NEW,
   statusCode: undefined,
@@ -18,13 +20,15 @@ const singleNode1: ITransactionNode = {
   groupCollectedCount: undefined,
 };
 
+const singleNodeDate2 = 1000;
+
 const singleNode2: ITransactionNode = {
   transactionId: 2,
   groupId: undefined,
   description: 'Single node 2',
-  createdAt: new Date().toISOString(),
-  validStart: new Date().toISOString(),
-  updatedAt: new Date().toISOString(),
+  createdAt: new Date(singleNodeDate2).toISOString(),
+  validStart: new Date(singleNodeDate2 + 2000).toISOString(),
+  updatedAt: new Date(singleNodeDate2 + 1000).toISOString(),
   executedAt: undefined,
   status: TransactionStatus.NEW,
   statusCode: undefined,
@@ -34,13 +38,15 @@ const singleNode2: ITransactionNode = {
   groupCollectedCount: undefined,
 };
 
+const groupNodeDate1 = 2000;
+
 const groupNode1: ITransactionNode = {
   transactionId: undefined,
   groupId: 1,
   description: 'Group node 1',
-  createdAt: new Date().toISOString(),
-  validStart: new Date().toISOString(),
-  updatedAt: new Date().toISOString(),
+  createdAt: new Date(groupNodeDate1).toISOString(),
+  validStart: new Date(groupNodeDate1 + 2000).toISOString(),
+  updatedAt: new Date(groupNodeDate1 + 1000).toISOString(),
   executedAt: undefined,
   status: TransactionStatus.NEW,
   statusCode: undefined,
@@ -50,13 +56,15 @@ const groupNode1: ITransactionNode = {
   groupCollectedCount: 21,
 };
 
+const groupNodeDate2 = 3000;
+
 const groupNode2: ITransactionNode = {
   transactionId: undefined,
   groupId: 2,
   description: 'Group node 2',
-  createdAt: new Date().toISOString(),
-  validStart: new Date().toISOString(),
-  updatedAt: new Date().toISOString(),
+  createdAt: new Date(groupNodeDate2).toISOString(),
+  validStart: new Date(groupNodeDate2 + 2000).toISOString(),
+  updatedAt: new Date(groupNodeDate2 + 1000).toISOString(),
   executedAt: undefined,
   status: TransactionStatus.NEW,
   statusCode: undefined,
@@ -65,6 +73,26 @@ const groupNode2: ITransactionNode = {
   groupItemCount: 21,
   groupCollectedCount: 10,
 };
+
+const malformedNodeDate2 = 3000;
+
+const malformedNode: ITransactionNode = {
+  // This node is malformed because both transactionId and groupId are undefined
+  // => it's useful only to reach 100% coverage of compareTransactionNodes()
+  transactionId: undefined,
+  groupId: undefined,
+  description: 'Malformed node',
+  createdAt: new Date(malformedNodeDate2).toISOString(),
+  validStart: new Date(malformedNodeDate2 + 2000).toISOString(),
+  updatedAt: new Date(malformedNodeDate2 + 1000).toISOString(),
+  executedAt: undefined,
+  status: TransactionStatus.NEW,
+  statusCode: undefined,
+  sdkTransactionId: undefined,
+  transactionType: undefined,
+  groupItemCount: 21,
+  groupCollectedCount: 10,
+}
 
 describe('Transaction Node Utils', () => {
   it('compareTransactionNodes()', () => {
@@ -78,5 +106,8 @@ describe('Transaction Node Utils', () => {
 
     expect(compareTransactionNodes(groupNode1, singleNode1)).toBe(+1);
     expect(compareTransactionNodes(singleNode1, groupNode1)).toBe(-1);
+
+    expect(compareTransactionNodes(singleNode1, malformedNode)).toBe(+1);
+    expect(compareTransactionNodes(malformedNode, singleNode1)).toBe(-1);
   });
 });
--- back-end/apps/api/src/transactions/nodes/transaction-nodes.util.ts
+++ back-end/apps/api/src/transactions/nodes/transaction-nodes.util.ts
@@ -5,9 +5,6 @@ export function compareTransactionNodes(n1: ITransactionNode, n2: ITransactionNo
   if (result === 0) {
     result = compareNodesByTransactionId(n1, n2);
   }
-  if (result === 0) {
-    result = compareNodesByCreatedAtd(n1, n2);
-  }
   return result;
 }
 
@@ -41,16 +38,6 @@ function compareNodesByTransactionId(n1: ITransactionNode, n2: ITransactionNode)
     result = +1;
   } else if (n2.transactionId) {
     result = -1
-  } else {
-    result = 0;
-  }
-  return result;
-}
-
-function compareNodesByCreatedAtd(n1: ITransactionNode, n2: ITransactionNode): number {
-  let result: number;
-  if (n1.createdAt != n2.createdAt) {
-    result = n1.createdAt < n2.createdAt ? +1 : -1;
   } else {
     result = 0;
   }
