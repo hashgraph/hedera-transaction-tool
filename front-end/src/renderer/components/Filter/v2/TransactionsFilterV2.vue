988168199c9dbdc1eff562ce33fcb4dd60b35932
Eric Le Ponner <eric.leponner@icloud.com>
2025-12-04 19:50:19 +0100

    Added TransactionsFilterV2.
    Signed-off-by: Eric Le Ponner <eric.leponner@icloud.com>

---
+++ front-end/src/renderer/components/Filter/v2/TransactionsFilterV2.vue
@@ -0,0 +1,18 @@
+<script setup lang="ts">
+import StatusFieldV2 from '@renderer/components/Filter/v2/StatusFieldV2.vue';
+import TransactionTypeFilterV2 from '@renderer/components/Filter/v2/TransactionTypeFilterV2.vue';
+import { BackEndTransactionType, TransactionStatus } from '@shared/interfaces';
+
+/* Models */
+const statusFilter = defineModel<TransactionStatus[]>('statusFilter', { required: true });
+const transactionTypeFilter = defineModel<BackEndTransactionType[]>('transactionTypeFilter', {
+  required: true,
+});
+</script>
+
+<template>
+  <div class="d-flex flex-wrap gap-4 text-small">
+    <StatusFieldV2 v-model:status-filter="statusFilter" />
+    <TransactionTypeFilterV2 v-model:transaction-type-filter="transactionTypeFilter" />
+  </div>
+</template>
