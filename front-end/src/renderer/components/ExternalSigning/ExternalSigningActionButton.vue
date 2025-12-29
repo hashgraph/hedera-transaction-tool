13ac1ef5924794b2ec08553e21916948f21abdb8
Eric Le Ponner <eric.leponner@icloud.com>
2025-12-29 09:18:04 +0100

    Experimental external signing work.
    Signed-off-by: Eric Le Ponner <eric.leponner@icloud.com>

---
+++ front-end/src/renderer/components/ExternalSigning/ExternalSigningActionButton.vue
@@ -0,0 +1,20 @@
+<script setup lang="ts">
+import { ref } from 'vue';
+import AppButton from '@renderer/components/ui/AppButton.vue';
+import ExternalSigningActionModal from '@renderer/components/ExternalSigning/ExternalSigningActionModal.vue';
+
+/* State */
+const isModalShow = ref(false);
+</script>
+
+<template>
+  <AppButton
+    v-bind="$attrs"
+    color="primary"
+    type="button"
+    @click.prevent="isModalShow = true"
+  >
+    Request External Signing
+  </AppButton>
+  <ExternalSigningActionModal v-model:show="isModalShow" />
+</template>
---
+++ front-end/src/renderer/components/ExternalSigning/Request/ReviewExternalSigningRequestModal.vue
@@ -0,0 +1,12 @@
+<script setup lang="ts">
+import AppModal from '@renderer/components/ui/AppModal.vue';
+
+/* Models */
+const show = defineModel<boolean>('show', { required: true });
+</script>
+
+<template>
+  <AppModal v-model:show="show"> </AppModal>
+</template>
+
+<style scoped></style>
