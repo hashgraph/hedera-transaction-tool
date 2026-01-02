51d2345f04cd293c02927111c8c0ac92090d67b0
Eric Le Ponner <eric.leponner@icloud.com>
2026-01-02 17:15:22 +0100

    SignTransactionFileModal now displays transaction that user needs to sign.
    Signed-off-by: Eric Le Ponner <eric.leponner@icloud.com>

---
+++ front-end/src/shared/utils/transactionFile.ts
@@ -0,0 +1,68 @@
+import { Transaction as SDKTransaction } from '@hashgraph/sdk';
+import { computeSignatureKey, hexToUint8Array } from '@renderer/utils';
+import type { TransactionFileItem } from '@shared/interfaces';
+import type { AccountByIdCache } from '@renderer/caches/mirrorNode/AccountByIdCache.ts';
+import type { NodeByIdCache } from '@renderer/caches/mirrorNode/NodeByIdCache.ts';
+import { flattenKeyList } from '@renderer/services/keyPairService.ts';
+
+export async function filterTransactionFileItemsToBeSigned(
+  transactionFileItems: TransactionFileItem[],
+  userPublicKeys: string[],
+  mirrorNetwork: string,
+  accountInfoCache: AccountByIdCache,
+  nodeInfoCache: NodeByIdCache,
+): Promise<TransactionFileItem[]> {
+  const result: TransactionFileItem[] = [];
+  for (const item of transactionFileItems) {
+    try {
+      const transactionBytes = hexToUint8Array(item.transactionBytes);
+      const sdkTransaction = SDKTransaction.fromBytes(transactionBytes);
+      const missingSignerKeys = await collectMissingSignerKeys(
+        sdkTransaction,
+        userPublicKeys,
+        mirrorNetwork,
+        accountInfoCache,
+        nodeInfoCache,
+      );
+      if (missingSignerKeys.length > 0) {
+        result.push(item);
+      }
+    } catch {
+      // Silently ignored
+    }
+  }
+  return result;
+}
+
+async function collectMissingSignerKeys(
+  transaction: SDKTransaction,
+  userPublicKeys: string[],
+  mirrorNodeLink: string,
+  accountInfoCache: AccountByIdCache,
+  nodeInfoCache: NodeByIdCache,
+): Promise<string[]> {
+  const result: string[] = [];
+
+  const audit = await computeSignatureKey(
+    transaction,
+    mirrorNodeLink,
+    accountInfoCache,
+    nodeInfoCache,
+    null,
+  );
+  const signatureKeys = transaction._signerPublicKeys;
+  for (const key of audit.signatureKeys) {
+    for (const flatKey of flattenKeyList(key)) {
+      if (!signatureKeys.has(flatKey.toStringDer())) {
+        // flatKey must sign the transaction
+        // => checks if flatKey is part of user public keys
+        if (userPublicKeys.includes(flatKey.toStringRaw())) {
+          // User is able to sign transaction with flatKey
+          result.push(flatKey.toStringDer());
+        }
+      }
+    }
+  }
+
+  return result;
+}
