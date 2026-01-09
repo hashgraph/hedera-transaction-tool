7ea14b8653252417df307d60680178928c11829e
Eric Le Ponner <eric.leponner@icloud.com>
2026-01-09 17:10:19 +0100

    Added TransactionBrowserKeySection.
    Signed-off-by: Eric Le Ponner <eric.leponner@icloud.com>

---
+++ front-end/src/renderer/components/ExternalSigning/TransactionBrowser/TransactionBrowserKeySection.vue
@@ -0,0 +1,80 @@
+<script setup lang="ts">
+import type { ITransactionBrowserItem } from '@renderer/components/ExternalSigning/TransactionBrowser/ITransactionBrowserItem.ts';
+import SignatureStatus from '@renderer/components/SignatureStatus.vue';
+import { computed, ref, watch } from 'vue';
+import { Transaction } from '@hashgraph/sdk';
+import {
+  computeSignatureKey,
+  getUInt8ArrayFromBytesString,
+  hexToUint8Array,
+  type SignatureAudit,
+} from '@renderer/utils';
+import useUserStore from '@renderer/stores/storeUser';
+import useNetwork from '@renderer/stores/storeNetwork';
+import { AccountByIdCache } from '@renderer/caches/mirrorNode/AccountByIdCache.ts';
+import { NodeByIdCache } from '@renderer/caches/mirrorNode/NodeByIdCache.ts';
+
+/* Props */
+const props = defineProps<{
+  item: ITransactionBrowserItem;
+}>();
+
+/* Stores */
+const user = useUserStore();
+const network = useNetwork();
+
+/* Injected */
+const accountByIdCache = AccountByIdCache.inject();
+const nodeByIdCache = NodeByIdCache.inject();
+
+/* State */
+const signatureKeyObject = ref<SignatureAudit | null>(null);
+
+/* Computed */
+const transaction = computed<Transaction | null>(() => {
+  let result: Transaction | null;
+  try {
+    result = Transaction.fromBytes(hexToUint8Array(props.item.transactionBytes));
+  } catch (error) {
+    console.log('error=' + error);
+    result = null;
+  }
+  return result;
+});
+const signersPublicKeys = computed(() => {
+  return transaction.value ? [...transaction.value._signerPublicKeys] : [];
+});
+
+/* Handlers */
+const updateSignatureKeyObject = async () => {
+  if (transaction.value !== null) {
+    try {
+      signatureKeyObject.value = await computeSignatureKey(
+        transaction.value,
+        network.mirrorNodeBaseURL,
+        accountByIdCache,
+        nodeByIdCache,
+        user.selectedOrganization,
+      );
+    } catch (error) {
+      console.log('error=' + error);
+      signatureKeyObject.value = null;
+    }
+  } else {
+    signatureKeyObject.value = null;
+  }
+};
+
+/* Watchers */
+watch(transaction, updateSignatureKeyObject, { immediate: true });
+</script>
+
+<template>
+  <SignatureStatus
+    v-if="signatureKeyObject"
+    :signature-key-object="signatureKeyObject"
+    :public-keys-signed="signersPublicKeys"
+  />
+</template>
+
+<style scoped></style>
