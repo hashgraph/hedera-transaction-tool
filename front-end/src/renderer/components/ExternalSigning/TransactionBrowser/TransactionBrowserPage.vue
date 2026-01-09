2a21b786fd0f57ce2bbba75a6051406cdf5a7625
Eric Le Ponner <eric.leponner@icloud.com>
2026-01-09 08:49:45 +0100

    Added TransactionBrowser view.
    Signed-off-by: Eric Le Ponner <eric.leponner@icloud.com>

---
+++ front-end/src/renderer/components/ExternalSigning/TransactionBrowser/TransactionBrowserPage.vue
@@ -0,0 +1,100 @@
+<script setup lang="ts">
+import { computed } from 'vue';
+import {
+  AccountAllowanceApproveTransaction,
+  AccountCreateTransaction,
+  AccountDeleteTransaction,
+  AccountUpdateTransaction,
+  FileCreateTransaction,
+  FileUpdateTransaction,
+  FileAppendTransaction,
+  FreezeTransaction,
+  NodeCreateTransaction,
+  NodeDeleteTransaction,
+  NodeUpdateTransaction,
+  TransferTransaction,
+  Transaction as SDKTransaction,
+  SystemDeleteTransaction,
+  SystemUndeleteTransaction,
+} from '@hashgraph/sdk';
+import type { ITransactionBrowserItem } from './ITransactionBrowserItem';
+import { hexToUint8Array } from '@renderer/utils';
+import { getTransactionType, getTransactionValidStart } from '@renderer/utils/sdk/transactions.ts';
+
+/* Props */
+const props = defineProps<{
+  item: ITransactionBrowserItem;
+}>();
+
+/* Computed */
+const transaction = computed(() => {
+  let result: SDKTransaction | null;
+  try {
+    const transactionBytes = hexToUint8Array(props.item.transactionBytes);
+    result = SDKTransaction.fromBytes(transactionBytes);
+  } catch {
+    result = null;
+  }
+  return result;
+});
+const transactionId = computed(() => transaction.value?.transactionId);
+const transactionType = computed(() => {
+  return transaction.value !== null ? getTransactionType(transaction.value, false, true) : null;
+});
+const description = computed(() => props.item.description ?? '');
+const validStartDate = computed(() => {
+  return transaction.value !== null ? getTransactionValidStart(transaction.value) : null;
+});
+const creatorEmail = computed(() => props.item.creatorEmail ?? '');
+</script>
+
+<template>
+  <div>
+    <div>Transaction ID: {{ transactionId?.toString() }}</div>
+    <div>Transaction Type: {{ transactionType }}</div>
+    <div>Valid Start: {{ validStartDate }}</div>
+    <div>description: {{ description }}</div>
+    <div>Creator e-mail: {{ creatorEmail }}</div>
+
+    <div
+      v-if="
+        transaction instanceof FileCreateTransaction ||
+        transaction instanceof FileUpdateTransaction ||
+        transaction instanceof FileAppendTransaction
+      "
+    >
+      File transaction properties
+    </div>
+    <div
+      v-if="
+        transaction instanceof AccountCreateTransaction ||
+        transaction instanceof AccountUpdateTransaction
+      "
+    >
+      Account delete transaction properties
+    </div>
+    <div v-if="transaction instanceof AccountDeleteTransaction">Account transaction properties</div>
+    <div v-if="transaction instanceof TransferTransaction">Transfer transaction properties</div>
+    <div v-if="transaction instanceof AccountAllowanceApproveTransaction">
+      Allowance transaction properties
+    </div>
+    <div v-if="transaction instanceof FreezeTransaction">Freeze transaction properties</div>
+    <div
+      v-if="
+        transaction instanceof SystemDeleteTransaction ||
+        transaction instanceof SystemUndeleteTransaction
+      "
+    >
+      System delete/undelete transaction properties
+    </div>
+    <div
+      v-if="
+        transaction instanceof NodeCreateTransaction ||
+        transaction instanceof NodeUpdateTransaction ||
+        transaction instanceof NodeDeleteTransaction
+      "
+    >
+      System delete/undelete transaction properties
+    </div>
+  </div>
+</template>
---
+++ front-end/src/renderer/components/ExternalSigning/TransactionBrowser/TransactionBrowserRow.vue
@@ -0,0 +1,67 @@
+<script setup lang="ts">
+import { computed } from 'vue';
+import { Transaction } from '@hashgraph/sdk';
+import type { ITransactionBrowserItem } from './ITransactionBrowserItem';
+import TransactionId from '@renderer/components/ui/TransactionId.vue';
+import DateTimeString from '@renderer/components/ui/DateTimeString.vue';
+import AppButton from '@renderer/components/ui/AppButton.vue';
+import { hexToUint8Array } from '@renderer/utils';
+import { getTransactionType, getTransactionValidStart } from '@renderer/utils/sdk/transactions.ts';
+
+/* Props */
+const props = defineProps<{
+  item: ITransactionBrowserItem;
+  index: number;
+}>();
+
+/* Emits */
+const emit = defineEmits(['navigate']);
+
+/* Computed */
+const transaction = computed(() => {
+  return Transaction.fromBytes(hexToUint8Array(props.item.transactionBytes));
+});
+const transactionId = computed(() => transaction.value?.transactionId);
+const transactionType = computed(() => {
+  return getTransactionType(transaction.value, false, true);
+});
+const description = computed(() => props.item.description ?? '');
+const validStartDate = computed(() => {
+  return getTransactionValidStart(transaction.value);
+});
+const creatorEmail = computed(() => props.item.creatorEmail ?? '');
+
+function handleDetails() {
+  emit('navigate', props.index);
+}
+</script>
+
+<template>
+  <tr>
+    <td>
+      <TransactionId v-if="transactionId" :transaction-id="transactionId" wrap />
+    </td>
+    <td class="text-bold">
+      {{ transactionType }}
+    </td>
+    <td>
+      <span class="text-wrap-two-line-ellipsis">
+        {{ description }}
+      </span>
+    </td>
+    <td>
+      <DateTimeString :date="validStartDate" compact wrap />
+    </td>
+    <td>{{ creatorEmail }}</td>
+    <td>
+      <AppButton
+        :data-testid="`button-external-transaction-details-${props.index}`"
+        color="secondary"
+        type="button"
+        @click="handleDetails"
+      >
+        Details
+      </AppButton>
+    </td>
+  </tr>
+</template>
