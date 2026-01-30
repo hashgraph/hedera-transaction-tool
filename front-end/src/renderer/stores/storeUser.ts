2a35eb0453ad52e9f21e60f2c4aea2f7879a7f5c
John Bair <john.bair@swirldslabs.com>
2026-01-29 16:20:25 -0800

    fix: correct version retrieval method and enhance modal loading state handling
    Signed-off-by: John Bair <john.bair@swirldslabs.com>

--- front-end/src/renderer/stores/storeUser.ts
+++ front-end/src/renderer/stores/storeUser.ts
@@ -22,7 +22,7 @@ import useVersionCheck from '@renderer/composables/useVersionCheck';
 
 import { safeAwait } from '@renderer/utils';
 import * as ush from '@renderer/utils/userStoreHelpers';
-import { getVersionStatusForOrg, triggeringOrganizationServerUrl } from './versionState';
+import { getVersionStatusForOrg } from './versionState';
 
 import useNetworkStore from './storeNetwork';
 import useOrganizationConnection from './storeOrganizationConnection';
@@ -199,7 +199,6 @@ const useUserStore = defineStore('user', () => {
           const versionStatus = getVersionStatusForOrg(organization.serverUrl);
 
           if (versionStatus === 'belowMinimum') {
-            triggeringOrganizationServerUrl.value = organization.serverUrl;
             return;
           }
         }
