f944fde9e69d1b47540aad78cbbf7dda3c651983
John Bair <john.bair@swirldslabs.com>
2026-03-02 16:12:32 -0800

    feat: add new email subjects for transaction and user registration notifications
    Signed-off-by: John Bair <john.bair@swirldslabs.com>

--- back-end/libs/common/src/constants/notificationTypeEmailSubjects.ts
+++ back-end/libs/common/src/constants/notificationTypeEmailSubjects.ts
@@ -1,9 +1,11 @@
 export enum NotificationTypeEmailSubjects {
   TRANSACTION_CREATED = 'Transaction has been created',
+  TRANSACTION_EXPIRED = 'Transaction has expired',
   TRANSACTION_WAITING_FOR_SIGNATURES = 'Action Required | Review and Sign Transaction',
   TRANSACTION_READY_FOR_EXECUTION = 'Transaction ready for execution',
   TRANSACTION_EXECUTED = 'Transaction has been executed',
   TRANSACTION_CANCELLED = 'Transaction has been cancelled',
   TRANSACTION_WAITING_FOR_SIGNATURES_REMINDER = 'Approaching Transaction valid start',
   TRANSACTION_WAITING_FOR_SIGNATURES_REMINDER_MANUAL = 'Action Required | Approaching Transaction valid start',
+  USER_REGISTERED = 'A new user has finished registration',
 }
