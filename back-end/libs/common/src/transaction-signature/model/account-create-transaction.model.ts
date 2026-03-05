a80400cad92cf1ec5a97e2f518032d991d9e0206
Dian Deskov <dian.deskov@limechain.tech>
2026-03-05 12:01:38 +0200

    fix: add getNewKeys() override for AccountCreateTransaction to fix signing and notifications
    Signed-off-by: Dian Deskov <dian.deskov@limechain.tech>

--- back-end/libs/common/src/transaction-signature/model/account-create-transaction.model.ts
+++ back-end/libs/common/src/transaction-signature/model/account-create-transaction.model.ts
@@ -1,7 +1,14 @@
-import { AccountCreateTransaction } from '@hashgraph/sdk';
+import { AccountCreateTransaction, Key } from '@hashgraph/sdk';
 import { TransactionBaseModel } from './transaction-base.model';
 
 export class AccountCreateTransactionModel
   extends TransactionBaseModel<AccountCreateTransaction> {
   static readonly TRANSACTION_TYPE = 'AccountCreateTransaction';
+
+  getNewKeys(): Key[] {
+    if (this.transaction.key != null) {
+      return [this.transaction.key];
+    }
+    return [];
+  }
 }
