49563d6349e50bd4f28fd5869cfa9d962799f34c
Eric Le Ponner <91124272+ericleponner@users.noreply.github.com>
2026-03-16 21:17:40 +0100

    feat: make Sign All action modal (#2478)
    * Added AppConfirmModal.cancel() callback (to be used later)
    
    Signed-off-by: Eric Le Ponner <eric.leponner@icloud.com>
    
    * Sign All action now displays a modal dialog.
    
    Signed-off-by: Eric Le Ponner <eric.leponner@icloud.com>
    
    * Moved activate reset from performSignAll() to confirmSigning().
    
    Signed-off-by: Eric Le Ponner <eric.leponner@icloud.com>
    
    * SignGroupButton.didSignAll() now transmit signed value
    
    Signed-off-by: Eric Le Ponner <eric.leponner@icloud.com>
    
    * SignAllController.progressText is now correctly reset.
    
    Signed-off-by: Eric Le Ponner <eric.leponner@icloud.com>
    
    * Added missing await in TransactionGroupDetails.didSignAll()
    
    Signed-off-by: Eric Le Ponner <eric.leponner@icloud.com>
    
    * Fixed call to invokeCallback() in SignAllController.
    
    Signed-off-by: Eric Le Ponner <eric.leponner@icloud.com>
    
    * Make sure that props.cancel() is called when props.show is switched to false.
    
    Signed-off-by: Eric Le Ponner <eric.leponner@icloud.com>
    
    * Improved fonts in SignAllController
    
    Signed-off-by: Eric Le Ponner <eric.leponner@icloud.com>
    
    ---------
    
    Signed-off-by: Eric Le Ponner <eric.leponner@icloud.com>
    Co-authored-by: John Bair <117694970+jbair06@users.noreply.github.com>

---
+++ front-end/src/renderer/pages/TransactionGroupDetails/SignAllController.vue
@@ -0,0 +1,142 @@
+<script setup lang="ts">
+import { ref, watch } from 'vue';
+import useUserStore from '@renderer/stores/storeUser.ts';
+import usePersonalPassword from '@renderer/composables/usePersonalPassword.ts';
+import { assertIsLoggedInOrganization, getErrorMessage, signTransactions } from '@renderer/utils';
+import AppConfirmModal from '@renderer/components/ui/AppConfirmModal.vue';
+import { AccountByIdCache } from '@renderer/caches/mirrorNode/AccountByIdCache.ts';
+import { NodeByIdCache } from '@renderer/caches/mirrorNode/NodeByIdCache.ts';
+import { PublicKeyOwnerCache } from '@renderer/caches/backend/PublicKeyOwnerCache.ts';
+import { ToastManager } from '@renderer/utils/ToastManager.ts';
+import { TransactionStatus } from '@shared/interfaces';
+import { getTransactionGroupById, type IGroup } from '@renderer/services/organization';
+import AppModal from '@renderer/components/ui/AppModal.vue';
+import AppCustomIcon from '@renderer/components/ui/AppCustomIcon.vue';
+
+/* Props */
+const props = defineProps<{
+  groupOrId: IGroup | number | null;
+  callback: (groupId: number, signed: boolean) => Promise<void>;
+}>();
+const activate = defineModel<boolean>('activate', { required: true });
+
+/* Composables */
+const { getPasswordV2 } = usePersonalPassword();
+
+/* Injected */
+const accountByIdCache = AccountByIdCache.inject();
+const nodeByIdCache = NodeByIdCache.inject();
+const publicKeyOwnerCache = PublicKeyOwnerCache.inject();
+const toastManager = ToastManager.inject();
+
+/* Stores */
+const user = useUserStore();
+
+/* State */
+const isConfirmModalShown = ref(false);
+const isSigningOnGoing = ref(false);
+const progressText = ref<string>('Loading group items…');
+
+/* Handlers */
+const confirmSigning = () => {
+  isConfirmModalShown.value = false;
+  activate.value = false;
+
+  getPasswordV2(performSignAll, {
+    subHeading: 'Enter your application password to decrypt your private key',
+  });
+};
+
+const cancelSigning = () => {
+  isConfirmModalShown.value = false;
+  activate.value = false;
+};
+
+const performSignAll = async (personalPassword: string | null) => {
+  //  if (passwordModalOpened(personalPassword)) return;
+
+  if (props.groupOrId !== null) {
+    isSigningOnGoing.value = true;
+    const groupId = typeof props.groupOrId == 'number' ? props.groupOrId : props.groupOrId.id;
+    try {
+      let group: IGroup;
+      if (typeof props.groupOrId === 'number') {
+        assertIsLoggedInOrganization(user.selectedOrganization);
+        const serverUrl = user.selectedOrganization.serverUrl;
+        group = await getTransactionGroupById(serverUrl, props.groupOrId);
+      } else {
+        group = props.groupOrId;
+      }
+      let itemsToSign = group.groupItems.map(item => item.transaction) ?? [];
+      itemsToSign = itemsToSign.filter(
+        item => item.status === TransactionStatus.WAITING_FOR_SIGNATURES,
+      );
+      progressText.value = `Signing ${itemsToSign.length} transactions`;
+      const signed = await signTransactions(
+        itemsToSign,
+        personalPassword,
+        accountByIdCache,
+        nodeByIdCache,
+        publicKeyOwnerCache,
+        toastManager,
+      );
+
+      if (signed) {
+        toastManager.success('Transactions signed successfully');
+      } else {
+        toastManager.error('Transactions not signed');
+      }
+      invokeCallback(groupId, signed);
+    } catch {
+      toastManager.error('Transactions not signed');
+      invokeCallback(groupId, false);
+    } finally {
+      progressText.value = 'Loading group items…';
+      isSigningOnGoing.value = false;
+    }
+  } // else bug
+};
+
+const invokeCallback = async (groupId: number, signed: boolean) => {
+  try {
+    await props.callback(groupId, signed);
+  } catch (error) {
+    toastManager.error(getErrorMessage(error, 'Transactions not signed'));
+  }
+};
+
+/* Hooks */
+watch(activate, () => {
+  if (activate.value) {
+    isConfirmModalShown.value = true;
+  }
+});
+</script>
+
+<template>
+  <AppConfirmModal
+    v-model:show="isConfirmModalShown"
+    title="Sign all transactions?"
+    text="Are you sure you want to sign all transactions?"
+    :callback="confirmSigning"
+    :cancel="cancelSigning"
+  />
+
+  <AppModal
+    :show="isSigningOnGoing"
+    :close-on-click-outside="false"
+    :close-on-escape="false"
+    class="small-modal"
+  >
+    <div class="p-4">
+      <div class="text-center">
+        <AppCustomIcon :name="'group'" style="height: 80px" />
+      </div>
+      <h3 class="text-center text-title text-bold mt-4">Signing all transactions</h3>
+      <p class="text-center text-small text-secondary mt-4 mb-4">{{ progressText }}</p>
+      <p class="text-center text-small text-secondary mt-6 mb-4">
+        <span class="spinner-border spinner-border me-2" role="status" inert></span>{{ ' ' }}
+      </p>
+    </div>
+  </AppModal>
+</template>
