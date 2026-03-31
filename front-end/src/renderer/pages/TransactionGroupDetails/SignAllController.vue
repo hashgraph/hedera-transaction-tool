e04da98091d76159fe03fd290d7adcadafb7e108
Eric Le Ponner <eric.leponner@icloud.com>
2026-03-31 16:17:38 +0200

    SignAllController now delegates user password input to ActionController.
    Signed-off-by: Eric Le Ponner <eric.leponner@icloud.com>

--- front-end/src/renderer/pages/TransactionGroupDetails/SignAllController.vue
+++ front-end/src/renderer/pages/TransactionGroupDetails/SignAllController.vue
@@ -1,7 +1,6 @@
 <script setup lang="ts">
 import { ref } from 'vue';
 import useUserStore from '@renderer/stores/storeUser.ts';
-import usePersonalPassword from '@renderer/composables/usePersonalPassword.ts';
 import { assertIsLoggedInOrganization, getErrorMessage, signTransactions } from '@renderer/utils';
 import { AccountByIdCache } from '@renderer/caches/mirrorNode/AccountByIdCache.ts';
 import { NodeByIdCache } from '@renderer/caches/mirrorNode/NodeByIdCache.ts';
@@ -18,9 +17,6 @@ const props = defineProps<{
 }>();
 const activate = defineModel<boolean>('activate', { required: true });
 
-/* Composables */
-const { getPasswordV2 } = usePersonalPassword();
-
 /* Injected */
 const accountByIdCache = AccountByIdCache.inject();
 const nodeByIdCache = NodeByIdCache.inject();
@@ -33,13 +29,8 @@ const user = useUserStore();
 /* State */
 const progressText = ref<string>('Loading group items…');
 
-const handleSignAll = async () => {
-  getPasswordV2(performSignAll, {
-    subHeading: 'Enter your application password to decrypt your private key',
-  });
-};
-
-const performSignAll = async (personalPassword: string | null) => {
+/* Handlers */
+const handleSignAll = async (personalPassword: string | null) => {
   if (props.groupOrId !== null) {
     const groupId = typeof props.groupOrId == 'number' ? props.groupOrId : props.groupOrId.id;
     try {
@@ -95,6 +86,7 @@ const invokeCallback = async (groupId: number, signed: boolean) => {
     :actionCallback="handleSignAll"
     confirm-title="Sign all transactions?"
     confirm-text="Are you sure you want to sign all transactions?"
+    :personal-password-required="true"
     progress-icon-name="group"
     progress-title="Sign all transactions"
     :progress-text="progressText"
