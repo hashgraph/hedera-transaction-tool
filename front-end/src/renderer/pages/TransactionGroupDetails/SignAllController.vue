619c38ec14d6859971805082f42f01f053a2e1c1
Eric Le Ponner <eric.leponner@icloud.com>
2026-04-06 15:30:19 +0200

    Re-factored SignAllController to produce an ActionReport.
    Signed-off-by: Eric Le Ponner <eric.leponner@icloud.com>

--- front-end/src/renderer/pages/TransactionGroupDetails/SignAllController.vue
+++ front-end/src/renderer/pages/TransactionGroupDetails/SignAllController.vue
@@ -1,7 +1,12 @@
 <script lang="ts" setup>
 import { ref } from 'vue';
 import useUserStore from '@renderer/stores/storeUser.ts';
-import { assertIsLoggedInOrganization, getErrorMessage, signTransactions } from '@renderer/utils';
+import {
+  assertIsLoggedInOrganization,
+  collectMissingKeys,
+  collectRequiredKeys,
+  signItems,
+} from '@renderer/utils';
 import { AccountByIdCache } from '@renderer/caches/mirrorNode/AccountByIdCache.ts';
 import { NodeByIdCache } from '@renderer/caches/mirrorNode/NodeByIdCache.ts';
 import { PublicKeyOwnerCache } from '@renderer/caches/backend/PublicKeyOwnerCache.ts';
@@ -9,7 +14,11 @@ import { ToastManager } from '@renderer/utils/ToastManager.ts';
 import { TransactionStatus } from '@shared/interfaces';
 import { getTransactionGroupById, type IGroup } from '@renderer/services/organization';
 import ActionController from '@renderer/components/ActionController/ActionController.vue';
-import type { ActionReport } from '@renderer/components/ActionController/ActionReport.ts';
+import {
+  type ActionReport,
+  ActionStatus,
+  makeBugReport,
+} from '@renderer/components/ActionController/ActionReport.ts';
 
 /* Props */
 const props = defineProps<{
@@ -28,61 +37,105 @@ const toastManager = ToastManager.inject();
 const user = useUserStore();
 
 /* State */
-const progressText = ref<string>('Loading group items…');
+const progressText = ref<string>('');
 
 /* Handlers */
-const handleSignAll = async (personalPassword: string | null): Promise<ActionReport | null> => {
+const handleSignAll = async (userPersonalPassword: string | null): Promise<ActionReport | null> => {
+  let result: ActionReport | null;
   if (props.groupOrId !== null) {
     const groupId = typeof props.groupOrId == 'number' ? props.groupOrId : props.groupOrId.id;
     try {
-      let group: IGroup;
-      if (typeof props.groupOrId === 'number') {
-        assertIsLoggedInOrganization(user.selectedOrganization);
-        const serverUrl = user.selectedOrganization.serverUrl;
-        group = await getTransactionGroupById(serverUrl, props.groupOrId);
-      } else {
-        group = props.groupOrId;
-      }
-      let itemsToSign = group.groupItems.map(item => item.transaction) ?? [];
-      itemsToSign = itemsToSign.filter(
-        item => item.status === TransactionStatus.WAITING_FOR_SIGNATURES,
-      );
-      progressText.value = `Signing ${itemsToSign.length} transactions`;
-      const signed = await signTransactions(
-        itemsToSign,
-        personalPassword,
-        accountByIdCache,
-        nodeByIdCache,
-        publicKeyOwnerCache,
-        toastManager,
-      );
-
-      if (signed) {
-        toastManager.success('Transactions signed successfully');
-      } else {
-        toastManager.error('Transactions not signed');
-      }
-      await invokeCallback(groupId, signed);
-    } catch {
-      toastManager.error('Transactions not signed');
-      await invokeCallback(groupId, false);
+      result = await performSignAll(userPersonalPassword);
     } finally {
-      progressText.value = 'Loading group items…';
+      await props.callback(groupId, true);
     }
   } else {
-    // Bug
-    toastManager.error('Unable to sign transactions: group is not available');
+    result = makeBugReport('Sign All', 'Failed to sign transactions');
   }
-
-  return null;
+  return result;
 };
 
-const invokeCallback = async (groupId: number, signed: boolean) => {
-  try {
-    await props.callback(groupId, signed);
-  } catch (error) {
-    toastManager.error(getErrorMessage(error, 'Failed to reload group items'));
+const performSignAll = async (
+  userPersonalPassword: string | null,
+): Promise<ActionReport | null> => {
+  let result: ActionReport | null;
+
+  progressText.value = '';
+  if (props.groupOrId !== null) {
+    // 1) fetches group items (if needed)
+    let group: IGroup;
+    if (typeof props.groupOrId === 'number') {
+      assertIsLoggedInOrganization(user.selectedOrganization);
+      const serverUrl = user.selectedOrganization.serverUrl;
+      progressText.value = 'Loading group items…';
+      group = await getTransactionGroupById(serverUrl, props.groupOrId);
+    } else {
+      group = props.groupOrId;
+    }
+
+    // 2) filters group items waiting for signatures
+    let itemsToSign = group.groupItems.map(item => item.transaction) ?? [];
+    itemsToSign = itemsToSign.filter(
+      item => item.status === TransactionStatus.WAITING_FOR_SIGNATURES,
+    );
+
+    // 3) checks if user has all the required private keys
+    progressText.value = 'Collecting required keys…';
+    const signatureItems = await collectRequiredKeys(
+      itemsToSign,
+      accountByIdCache,
+      nodeByIdCache,
+      publicKeyOwnerCache,
+    );
+    const missingKeys = collectMissingKeys(signatureItems);
+    const missingKeyCount = missingKeys.length;
+    if (missingKeyCount > 0) {
+      // User needs to setup some private keys in Settings
+      result = {
+        status: ActionStatus.Error,
+        title: 'Sign All',
+        what: missingKeyCount == 1 ? 'Key is missing' : `${missingKeyCount} keys are missing`,
+        next:
+          missingKeyCount == 1
+            ? 'Go to Settings and add the missing key'
+            : 'Go to Settings and add the missing keys',
+      };
+    } else {
+      // 4) performs required signing
+      progressText.value = `Signing ${itemsToSign.length} transactions…`;
+      const rejectedItems = await signItems(signatureItems, userPersonalPassword);
+      const rejectedItemCount = rejectedItems.length;
+      if (rejectedItemCount > 0) {
+        // Some signatures have not been done
+        const signedItemCount = signatureItems.length - rejectedItemCount;
+        if (signedItemCount > 0) {
+          // Operation is partial
+          result = {
+            status: ActionStatus.Error,
+            title: 'Sign All',
+            what: `${signedItemCount} transactions signed, but ${rejectedItemCount} transactions were not signed`,
+            next: 'Check status for unsigned transactions',
+          };
+        } else {
+          // Nothing signed => sign all fully rejected
+          result = {
+            status: ActionStatus.Error,
+            title: 'Sign All',
+            what: 'Transactions were not signed',
+            next: 'Check status of transactions',
+          };
+        }
+      } else {
+        // All signatures have been done successfully
+        toastManager.success('Transactions signed successfully');
+        result = null;
+      }
+    }
+  } else {
+    result = makeBugReport('Sign All', 'Failed to sign transactions');
   }
+
+  return result;
 };
 </script>
 
