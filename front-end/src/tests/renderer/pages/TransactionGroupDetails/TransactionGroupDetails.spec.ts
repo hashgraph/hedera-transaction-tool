f777bd03f92f0b9a9c38dfdbebcc60647a3a3412
Eric Le Ponner <eric.leponner@icloud.com>
2026-04-14 10:20:44 +0200

    Fixed mocking in TransactionGroupDetails.spec.ts
    Signed-off-by: Eric Le Ponner <eric.leponner@icloud.com>

--- front-end/src/tests/renderer/pages/TransactionGroupDetails/TransactionGroupDetails.spec.ts
+++ front-end/src/tests/renderer/pages/TransactionGroupDetails/TransactionGroupDetails.spec.ts
@@ -202,20 +202,8 @@ vi.mock('@hiero-ledger/sdk', async importOriginal => {
   };
 });
 
-vi.mock('@renderer/caches/mirrorNode/AccountByIdCache.ts', () => ({
-  AccountByIdCache: {
-    inject: vi.fn(() => ({})),
-  },
-}));
-
-vi.mock('@renderer/caches/mirrorNode/NodeByIdCache.ts', () => ({
-  NodeByIdCache: {
-    inject: vi.fn(() => ({})),
-  },
-}));
-
-vi.mock('@renderer/caches/backend/PublicKeyOwnerCache.ts', () => ({
-  PublicKeyOwnerCache: {
+vi.mock('@renderer/caches/AppCache.ts', () => ({
+  AppCache: {
     inject: vi.fn(() => ({})),
   },
 }));
