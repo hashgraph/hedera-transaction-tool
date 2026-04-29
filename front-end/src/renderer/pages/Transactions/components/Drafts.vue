e98be4788641d5d31c008c5b467ea9ad5381d5d3
Yasen Dudin <37876854+yasenltd@users.noreply.github.com>
2026-04-29 10:09:19 +0300

    feat: extend fe unit component tests coverage (#2748)
    * feat: extend fe unit component tests coverage
    
    Signed-off-by: yasenltd <yasenltd@gmail.com>
    
    * update refetch transactions when sorting on page 1
    
    Signed-off-by: yasenltd <yasenltd@gmail.com>
    
    ---------
    
    Signed-off-by: yasenltd <yasenltd@gmail.com>
    Co-authored-by: John Bair <117694970+jbair06@users.noreply.github.com>

--- front-end/src/renderer/pages/Transactions/components/Drafts.vue
+++ front-end/src/renderer/pages/Transactions/components/Drafts.vue
@@ -81,7 +81,7 @@ const handleSort = async (field: string, direction: string) => {
         a: TransactionDraft | TransactionGroup,
         b: TransactionDraft | TransactionGroup,
       ) =>
-        direction == 'desc'
+        direction == 'asc'
           ? a.created_at.getTime() - b.created_at.getTime()
           : b.created_at.getTime() - a.created_at.getTime();
       list.value?.sort(cmp);
@@ -107,7 +107,47 @@ const handleSort = async (field: string, direction: string) => {
         if (typeA > typeB) {
           returnValue = 1;
         }
-        if (direction == 'asc') {
+        if (direction == 'desc') {
+          returnValue *= -1;
+        }
+        return returnValue;
+      };
+      list.value?.sort(cmp);
+      break;
+    }
+    case 'description': {
+      const cmp = (
+        a: TransactionDraft | TransactionGroup,
+        b: TransactionDraft | TransactionGroup,
+      ) => {
+        const descriptionA = getDraftDescription(a);
+        const descriptionB = getDraftDescription(b);
+        let returnValue = 0;
+
+        if (descriptionA < descriptionB) {
+          returnValue = -1;
+        }
+        if (descriptionA > descriptionB) {
+          returnValue = 1;
+        }
+        if (direction == 'desc') {
+          returnValue *= -1;
+        }
+        return returnValue;
+      };
+      list.value?.sort(cmp);
+      break;
+    }
+    case 'isTemplate': {
+      const cmp = (
+        a: TransactionDraft | TransactionGroup,
+        b: TransactionDraft | TransactionGroup,
+      ) => {
+        const isTemplateA = (a as TransactionDraft).isTemplate ? 1 : 0;
+        const isTemplateB = (b as TransactionDraft).isTemplate ? 1 : 0;
+        let returnValue = isTemplateA - isTemplateB;
+
+        if (direction == 'desc') {
           returnValue *= -1;
         }
         return returnValue;
