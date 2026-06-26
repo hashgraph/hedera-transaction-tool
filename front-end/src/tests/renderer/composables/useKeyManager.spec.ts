b5bd36e8d52e472ca892376e123820864f6d6174
Eric Le Ponner <eric.leponner@icloud.com>
2026-06-26 16:35:24 +0200

    Added unit test for useKeyManager.
    Signed-off-by: Eric Le Ponner <eric.leponner@icloud.com>

---
+++ front-end/src/tests/renderer/composables/useKeyManager.spec.ts
@@ -0,0 +1,57 @@
+import { ref } from 'vue';
+import type { KeyPair } from '@prisma/client';
+import type { IUserKey } from '@shared/interfaces';
+import useKeyManager from '@renderer/composables/useKeyManager.ts';
+
+describe('useKeyManager', () => {
+
+  test('abc', async () => {
+    const keyPairs = ref<KeyPair[]>([]);
+    const userKeys = ref<IUserKey[]>([]);
+    const manager = useKeyManager(keyPairs, userKeys);
+    expect(manager.keyInfos.value.length).toBe(0);
+
+    // Adds a key pair
+    const SAMPLE_KEY_PAIR: KeyPair = {
+      id: '36703f32-fb06-44ef-972a-3e18b62be01e',
+      user_id: '4f7e9ed9-f52e-4301-beff-0519a4c60c57',
+      index: -1,
+      public_key: 'a588d46597c0a3fb8c7c875387d2bdc96c44bee65fa1fef8c917a94de6396141',
+      private_key: 'xxxxyyyyyzzzz',
+      type: 'ECDSA',
+      organization_id: '732d44e5-c867-4377-8a57-aa4694fc97bc',
+      secret_hash: null,
+      nickname: 'Nice key pair',
+      organization_user_id: 1,
+    };
+    keyPairs.value.push(SAMPLE_KEY_PAIR);
+    expect(manager.keyInfos.value.length).toBe(1);
+    expect(manager.keyInfos.value[0]).toEqual({
+      publicKey: SAMPLE_KEY_PAIR.public_key,
+      keyPair: SAMPLE_KEY_PAIR,
+      userKey: null,
+    });
+    expect(manager.keyInfos.value[0].mnemonicHash()).toBe(null);
+    expect(manager.keyInfos.value[0].index()).toBe(-1);
+
+    // Adds matching user key
+    const SAMPLE_USER_KEY: IUserKey = {
+      id: 42,
+      userId: 7,
+      mnemonicHash: undefined,
+      index: undefined,
+      publicKey: SAMPLE_KEY_PAIR.public_key,
+      deletedAt: undefined,
+    };
+    userKeys.value.push(SAMPLE_USER_KEY);
+    expect(manager.keyInfos.value.length).toBe(1);
+    expect(manager.keyInfos.value[0]).toEqual({
+      publicKey: SAMPLE_KEY_PAIR.public_key,
+      keyPair: SAMPLE_KEY_PAIR,
+      userKey: SAMPLE_USER_KEY,
+    });
+    expect(manager.keyInfos.value[0].mnemonicHash()).toBe(null);
+    expect(manager.keyInfos.value[0].index()).toBe(-1);
+  });
+
+});
