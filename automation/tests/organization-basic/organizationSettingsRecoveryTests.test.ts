d717fb09557a91e28941a068f6019b8a3c33de10
Eric Le Ponner <eric.leponner@icloud.com>
2026-06-30 14:51:34 +0200

    Adjusted automation test to KeysTab evolutions.
    Signed-off-by: Eric Le Ponner <eric.leponner@icloud.com>

--- automation/tests/organization-basic/organizationSettingsRecoveryTests.test.ts
+++ automation/tests/organization-basic/organizationSettingsRecoveryTests.test.ts
@@ -135,8 +135,9 @@ test.describe('Organization Settings (Recovery) tests @organization-basic', () =
     await organizationPage.recoverPrivateKey(window);
     await settingsPage.clickOnSettingsButton();
     await settingsPage.clickOnKeysTab();
-    const missingKeyHidden = await organizationPage.isFirstMissingKeyHidden();
-    expect(missingKeyHidden).toBe(true);
+    const restoreButtonCount = await organizationPage.countKeyRestoreButtons();
+    const uploadButtonCount = await organizationPage.countKeyUploadButtons();
+    expect(restoreButtonCount + uploadButtonCount).toBe(0);
   });
 
   test('Verify organization user can change password', async () => {
