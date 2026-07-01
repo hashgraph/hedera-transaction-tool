d54ff40993b66278d0af42f3bbadc5528f480133
Simon Viénot <simon.vienot@icloud.com>
2026-07-01 13:33:59 +0200

    Leverage TransactionAccessGuard in CommentsController.
    Signed-off-by: Simon Viénot <simon.vienot@icloud.com>

--- back-end/apps/api/src/transactions/comments/comments.controller.ts
+++ back-end/apps/api/src/transactions/comments/comments.controller.ts
@@ -4,22 +4,21 @@ import { ApiTags } from '@nestjs/swagger';
 import { User } from '@entities';
 
 import { JwtAuthGuard, JwtBlackListAuthGuard, VerifiedUserGuard } from '../../guards';
+import { TransactionAccessGuard } from '../../guards/transaction-access.guard';
 
-import { GetUser } from '../../decorators/get-user.decorator';
+import { GetUser } from '../../decorators';
 
 import { CommentsService } from './comments.service';
 
-import { CreateCommentDto } from '../dto/create-comment.dto';
+import { CreateCommentDto } from '../dto';
 
 @ApiTags('Transaction Comments')
-@Controller('transactions{/:transactionId}/comments')
-@UseGuards(JwtBlackListAuthGuard, JwtAuthGuard, VerifiedUserGuard)
-//TODO add serializer
+@Controller('transactions/:transactionId/comments')
+@UseGuards(JwtBlackListAuthGuard, JwtAuthGuard, VerifiedUserGuard, TransactionAccessGuard)
 export class CommentsController {
   constructor(private commentsService: CommentsService) {}
 
   @Post()
-  //TODO need some sort of guard or check to ensure user can comment here
   createComment(
     @GetUser() user: User,
     @Param('transactionId', ParseIntPipe) transactionId: number,
@@ -34,8 +33,11 @@ export class CommentsController {
   }
 
   @Get('/:id')
-  getCommentById(@Param('id', ParseIntPipe) id: number) {
-    return this.commentsService.getTransactionCommentById(id);
+  getCommentById(
+    @Param('transactionId', ParseIntPipe) transactionId: number,
+    @Param('id', ParseIntPipe) id: number,
+  ) {
+    return this.commentsService.getTransactionCommentById(transactionId, id);
   }
 
   //TODO add update and remove routes
