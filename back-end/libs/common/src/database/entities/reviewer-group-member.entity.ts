ca0e2e7f4cc05ab9a7a53147b4836a0e47cb5056
John Bair <john.bair@swirldslabs.com>
2026-08-12 10:18:11 -0700

    feat: add ReviewerGroup and ReviewerGroupMember entities
    Signed-off-by: John Bair <john.bair@swirldslabs.com>

---
+++ back-end/libs/common/src/database/entities/reviewer-group-member.entity.ts
@@ -0,0 +1,47 @@
+import {
+  Column,
+  CreateDateColumn,
+  DeleteDateColumn,
+  Entity,
+  Index,
+  JoinColumn,
+  ManyToOne,
+  PrimaryGeneratedColumn,
+} from 'typeorm';
+import { ReviewerGroup } from './reviewer-group.entity';
+import { User } from './user.entity';
+import { UserKey } from './user-key.entity';
+
+@Entity()
+@Index(['groupId', 'userId'], { unique: true })
+export class ReviewerGroupMember {
+  @PrimaryGeneratedColumn()
+  id: number;
+
+  @ManyToOne(() => ReviewerGroup, group => group.members, { onDelete: 'CASCADE' })
+  @JoinColumn({ name: 'groupId' })
+  group: ReviewerGroup;
+
+  @Column()
+  groupId: number;
+
+  @ManyToOne(() => User)
+  @JoinColumn({ name: 'userId' })
+  user: User;
+
+  @Column()
+  userId: number;
+
+  @ManyToOne(() => UserKey)
+  @JoinColumn({ name: 'userKeyId' })
+  userKey: UserKey;
+
+  @Column()
+  userKeyId: number;
+
+  @CreateDateColumn()
+  createdAt: Date;
+
+  @DeleteDateColumn()
+  deletedAt: Date;
+}
