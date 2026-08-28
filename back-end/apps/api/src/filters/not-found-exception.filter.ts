bedc10e34c553cacd4ee68e42e6b1af4c55239b2
John Bair <john.bair@swirldslabs.com>
2026-08-28 14:23:47 -0700

    style: drop the no-logging comment in not-found-exception.filter.ts
    Signed-off-by: John Bair <john.bair@swirldslabs.com>

--- back-end/apps/api/src/filters/not-found-exception.filter.ts
+++ back-end/apps/api/src/filters/not-found-exception.filter.ts
@@ -1,8 +1,6 @@
 import { ExceptionFilter, Catch, ArgumentsHost, NotFoundException } from '@nestjs/common';
 import { Response } from 'express';
 
-// Deliberately no logging here -- LoggerMiddleware already logs every request, 404s
-// included, at WARN (see its comment). Logging here too would duplicate that.
 @Catch(NotFoundException)
 export class NotFoundExceptionFilter implements ExceptionFilter {
   catch(exception: NotFoundException, host: ArgumentsHost) {
