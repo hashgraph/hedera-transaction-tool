1de4ca53b45d84fea3c1ddf8644044744fbabff2
Simon Viénot <simon.vienot@icloud.com>
2026-09-04 19:51:40 +0200

    Adjust unit tests.
    Signed-off-by: Simon Viénot <simon.vienot@icloud.com>

--- back-end/apps/api/src/users/users.controller.spec.ts
+++ back-end/apps/api/src/users/users.controller.spec.ts
@@ -1,10 +1,12 @@
+import { ExecutionContext } from '@nestjs/common';
+import { Reflector } from '@nestjs/core';
 import { Test, TestingModule } from '@nestjs/testing';
 import { mockDeep } from 'jest-mock-extended';
 
 import { BlacklistService, guardMock, VersionCheckResult } from '@app/common';
 import { Client, User, UserStatus } from '@entities';
 
-import { VerifiedUserGuard } from '../guards';
+import { UserThrottlerGuard, VerifiedUserGuard } from '../guards';
 
 import { UsersController } from './users.controller';
 import { UsersService } from './users.service';
@@ -32,6 +34,8 @@ describe('UsersController', () => {
       ],
     })
       .overrideGuard(VerifiedUserGuard)
+      .useValue(guardMock())
+      .overrideGuard(UserThrottlerGuard)
       .useValue(guardMock())
       .compile();
 
@@ -275,6 +279,8 @@ describe('UsersController', () => {
       })
         .overrideGuard(VerifiedUserGuard)
         .useValue(guardMock())
+        .overrideGuard(UserThrottlerGuard)
+        .useValue(guardMock())
         .compile();
 
       controller = module.get<UsersController>(UsersController);
@@ -287,7 +293,7 @@ describe('UsersController', () => {
 
         userService.getOwnerOfPublicKey.mockResolvedValue(email);
 
-        expect(await controller.getUserByPublicKey(publicKey)).toBe(email);
+        expect(await controller.getUserByPublicKey(user, publicKey)).toBe(email);
       });
 
       it('should return null if no user is found for the given public key', async () => {
@@ -295,7 +301,7 @@ describe('UsersController', () => {
 
         userService.getOwnerOfPublicKey.mockResolvedValue(null);
 
-        expect(await controller.getUserByPublicKey(publicKey)).toBeNull();
+        expect(await controller.getUserByPublicKey(user, publicKey)).toBeNull();
       });
 
       it('should throw an error if the service fails', async () => {
@@ -303,7 +309,9 @@ describe('UsersController', () => {
 
         userService.getOwnerOfPublicKey.mockRejectedValue(new Error('Database error'));
 
-        await expect(controller.getUserByPublicKey(publicKey)).rejects.toThrow('Database error');
+        await expect(controller.getUserByPublicKey(user, publicKey)).rejects.toThrow(
+          'Database error',
+        );
       });
     });
   });
