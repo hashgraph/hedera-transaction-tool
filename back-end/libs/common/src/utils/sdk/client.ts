c6755f951f73498c43c1a065b09c049e2471f368
Simon Viénot <simon.vienot@icloud.com>
2026-09-04 18:06:14 +0200

    test: upgrade to solo 0.88.0 (#3371)
    * Upgrade solo version to 0.88.0.
    
    Signed-off-by: Simon Viénot <simon.vienot@icloud.com>
    
    * Change hard-coded solo port (for Local Network): 8081 -> 38081
    
    Signed-off-by: Simon Viénot <simon.vienot@icloud.com>
    
    * Change 127.0.0.1:50211 -> 127.0.0.1:35211
    
    Signed-off-by: Simon Viénot <simon.vienot@icloud.com>
    
    * Fix (automation's) fileNetworkSupport.ts client configuration.
    
    Signed-off-by: Simon Viénot <simon.vienot@icloud.com>
    
    * Augment maxTransactionFee to 10 for createComplexKeyAccountForUsers().
    
    Signed-off-by: Simon Viénot <simon.vienot@icloud.com>
    
    * Add unit test to make Codecov happy.
    
    Signed-off-by: Simon Viénot <simon.vienot@icloud.com>
    
    ---------
    
    Signed-off-by: Simon Viénot <simon.vienot@icloud.com>

--- back-end/libs/common/src/utils/sdk/client.ts
+++ back-end/libs/common/src/utils/sdk/client.ts
@@ -10,9 +10,9 @@ export const LOCAL_NODE = 'local-node';
 export const getLocalClientNetwork = (env: string|undefined): Record<string, string> => {
   switch (env) {
     case 'test':
-      return { '127.0.0.1:50211': '0.0.3' };
+      return { '127.0.0.1:35211': '0.0.3' };
     default:
-      return { 'host.docker.internal:50211': '0.0.3' };
+      return { 'host.docker.internal:35211': '0.0.3' };
   }
 };
 
