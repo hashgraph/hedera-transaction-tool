f10e05f82b4e0406ec36440b23eab341647a1690
Eric Le Ponner <eric.leponner@icloud.com>
2026-09-08 16:20:04 +0200

    Adjusted ToastManager unit tests.
    Signed-off-by: Eric Le Ponner <eric.leponner@icloud.com>

--- front-end/src/tests/renderer/utils/ToastManager.spec.ts
+++ front-end/src/tests/renderer/utils/ToastManager.spec.ts
@@ -6,36 +6,38 @@ describe('ToastManager', () => {
     const toastManager = new ToastManager();
     const message = 'Nice success message';
     toastManager.success(message);
-    const toastEntry = toastManager.findEntry(message);
-    expect(toastEntry).not.toBeNull();
-    expect(toastEntry!.type).toBe('success');
+    expect(toastManager.findEntry(message, 'success')).not.toBeNull();
   });
 
   test('check info', () => {
     const toastManager = new ToastManager();
     const message = 'Nice info message';
     toastManager.info(message);
-    const toastEntry = toastManager.findEntry(message);
-    expect(toastEntry).not.toBeNull();
-    expect(toastEntry!.type).toBe('info');
+    expect(toastManager.findEntry(message, 'info')).not.toBeNull();
   });
 
   test('check warning', () => {
     const toastManager = new ToastManager();
     const message = 'Nice warning message';
     toastManager.warning(message);
-    const toastEntry = toastManager.findEntry(message);
-    expect(toastEntry).not.toBeNull();
-    expect(toastEntry!.type).toBe('warning');
+    expect(toastManager.findEntry(message, 'warning')).not.toBeNull();
   });
 
   test('check error', () => {
     const toastManager = new ToastManager();
     const message = 'Nice error message';
     toastManager.error(message);
-    const toastEntry = toastManager.findEntry(message);
-    expect(toastEntry).not.toBeNull();
-    expect(toastEntry!.type).toBe('error');
+    expect(toastManager.findEntry(message, 'error')).not.toBeNull();
+  });
+
+  test('check dismiss after timeout', () => {
+    vi.useFakeTimers();
+    const toastManager = new ToastManager();
+    const message = 'Nice success message';
+    toastManager.success(message);
+    expect(toastManager.findEntry(message, 'success')).not.toBeNull();
+    vi.advanceTimersByTime(4000);
+    expect(toastManager.findEntry(message, 'success')).toBeNull();
   });
 
   test('check duplicated error messages', async () => {
